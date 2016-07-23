6346f130db5f0c63127928e1ce0a6e20fafabb8a
billba <billba@microsoft.com>
2016-07-23 11:44:46 -0700

    this space is just wrong

--- src/Outgoing.tsx
+++ src/Outgoing.tsx
@@ -8,6 +8,6 @@ export const Outgoing = (props: {
     enableSend: boolean
 }) =>
     <div id="outgoingFrame">
-        <textarea id="outgoing" value={ props.outgoingMessage } onChange={ e => props.updateMessage(e.target.value) } disabled={ !props.enableSend } / >
+        <textarea id="outgoing" value={ props.outgoingMessage } onChange={ e => props.updateMessage(e.target.value) } disabled={ !props.enableSend } />
         <button id="send" onClick={ props.sendMessage } disabled={ !props.outgoingMessage || props.outgoingMessage.length == 0 || !props.enableSend } >send</button>
     </div>;
