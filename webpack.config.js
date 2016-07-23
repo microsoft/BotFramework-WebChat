38e544e0e59263130a8456973cc6e1a4deb77c5c
billba <billba@microsoft.com>
2016-07-23 11:44:19 -0700

    load React from CDN

--- webpack.config.js
+++ webpack.config.js
@@ -31,5 +31,7 @@ module.exports = {
     // This is important because it allows us to avoid bundling all of our
     // dependencies, which allows browsers to cache those libraries between builds.
     externals: {
+        "react": "React",
+        "react-dom": "ReactDOM",
     },
 };
