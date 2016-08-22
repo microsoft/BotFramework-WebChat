a7195c3c429da47568b21da83deba0e22d928b56
Bill Barnes <billba@microsoft.com>
2016-08-22 14:46:20 -0700

    get images from channelData, simplify componentry

--- src/ImageMessage.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-
-export const ImageMessage = (props: {
-    images: string[]
-}) =>
-    <div class="imageMessage">
-        { props.images.map(path => <img src= { path }/>) }
-    </div>;
