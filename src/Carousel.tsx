7c0d50a33532a7c73a59d6b179c2301afc2cda55
Dan Marshall <danmar@microsoft.com>
2016-09-19 21:19:09 -0700

    animated scrolling v1

--- src/Carousel.tsx
+++ src/Carousel.tsx
@@ -12,6 +12,7 @@ export interface State {
     scrollIndex: number;
     itemWidth: number;
     ul?: HTMLUListElement
+    scrollDiv?: HTMLDivElement;
 }
 
 export class Carousel extends React.Component<Props, State> {
@@ -32,9 +33,34 @@ export class Carousel extends React.Component<Props, State> {
     }
 
     private scrollBy(increment: number) {
-        this.state.scrollIndex -= increment;
+        //this.state.scrollIndex -= increment;
+        //this.state.scrollDiv.scrollLeft += increment * this.state.itemWidth;
+        //this.setState(this.state);
 
-        this.setState(this.state);
+        //TODO: cancel existing animation when clicking fast
+        //TODO: animate to boundaries
+
+        var unit = increment * this.state.itemWidth;
+
+        var div = document.createElement('div');
+        div.className = 'wc-animate-scroll';
+        div.style.left = this.state.scrollDiv.scrollLeft + 'px';
+        document.body.appendChild(div);
+
+        //capture ComputedStyle every millisecond
+        var captureTimer = setInterval(() => {
+            var num = parseFloat(getComputedStyle(div).left);
+            this.state.scrollDiv.scrollLeft = num;
+        }, 1);
+
+        div.style.left = (this.state.scrollDiv.scrollLeft + unit) + 'px';
+
+        //stop capturing
+        var finishTimer = setTimeout(() => {
+            clearInterval(captureTimer);
+            document.body.removeChild(div);
+        }, 1000 * parseFloat(getComputedStyle(div).transitionDuration) || 5000);
+
     }
 
     render() {
@@ -43,13 +69,11 @@ export class Carousel extends React.Component<Props, State> {
             return <li><Attachment attachment={attachment} actions={this.props.actions}/></li>;
         }) as React.ReactElement<any>[];
 
-        let style = {
-            marginLeft: this.state.itemWidth ? this.state.scrollIndex * this.state.itemWidth : 0
-        };
-
         return <div className="wc-carousel">
             <button className="scroll previous" onClick={() => { this.scrollBy(-1); } }>left</button>
-            <ul style={style} ref={(ul: HTMLUListElement) => { this.state.ul = ul; } }>{items}</ul>
+            <div className="wc-carousel-scroll" ref={(div: HTMLDivElement) => { this.state.scrollDiv = div; } }>
+                <ul ref={(ul: HTMLUListElement) => { this.state.ul = ul; } }>{items}</ul>
+            </div>
             <button className="scroll next" onClick={() => { this.scrollBy(1); } }>right</button>
         </div>;
     }
