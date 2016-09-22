e4834ff3537136e5c41d0aeade493aa4e49c215a
Dan Marshall <danmar@microsoft.com>
2016-09-21 19:27:34 -0700

    added raw markup for card types receipt styling thumbnail styling

--- src/Attachment.tsx
+++ src/Attachment.tsx
@@ -20,28 +20,93 @@ export const Attachment = (props: {
     // REVIEW we need to make sure each button.type is one of these
 
     const content = props.attachment.content;
+    const cardPrefix = 'application/vnd.microsoft.card.';
 
-    switch (props.attachment.contentType) {
-        case "image/png":
-            return <img src={ props.attachment.contentUrl }/>;
+    if (props.attachment.contentType && props.attachment.contentType.indexOf(cardPrefix) == 0) {
 
-        case "application/vnd.microsoft.card.hero":
-
+        const buttonList = () => {
             return (
-                <div className="wc-card">
-                    <img src={ content.images[0].url } />
-                    <p><b>{ content.title }</b></p>
-                    <p><i>{ content.subtitle }</i></p>
-                    <p>{ content.text }</p>
-                    <ul className="wc-card-buttons">
-                        { content.buttons.map(button => <li><button onClick={ () => buttonActions[button.type](button.value) }>{ button.title }</button></li>) }
-                    </ul>
-                </div>
+                <ul className="wc-card-buttons">
+                    { content.buttons.map(button => <li><button onClick={ () => buttonActions[button.type](button.value) }>{ button.title }</button></li>) }
+                </ul>
             );
+        };
 
-        case "application/vnd.microsoft.card.thumbnail":
-        case "application/vnd.microsoft.card.signin":
-        case "application/vnd.microsoft.card.receipt":
-            //TODO
+        const cardType = props.attachment.contentType.substring(cardPrefix.length);
+        const className = 'wc-card ' + cardType;
+
+        switch (cardType) {
+
+            case 'hero':
+                return (
+                    <div className={className}>
+                        <img src={ content.images[0].url } />
+                        <h2>{ content.title }</h2>
+                        <h3>{ content.subtitle }</h3>
+                        <p>{ content.text }</p>
+                        {buttonList() }
+                    </div>
+                );
+
+            case 'thumbnail':
+                return (
+                    <div className={className}>
+                        <h2>{ content.title }</h2>
+                        <p>
+                            <img src={ content.images[0].url } />
+                            <h3>{ content.subtitle }</h3>
+                            { content.text }
+                        </p>
+                        {buttonList() }
+                    </div>
+                );
+
+            case 'signin':
+                return (
+                    <div className={className}>
+                        <h3>{ content.text }</h3>
+                        {buttonList() }
+                    </div>
+                );
+
+            case 'receipt':
+
+                const itemImage = (item) => {
+                    if (item.image) {
+                        return <img src={ item.image.url }/>
+                    }
+                };
+
+                return (
+                    <div className={className}>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th colSpan="2">{ content.title }</th>
+                                </tr>
+                                { content.facts.map(fact => <tr><th>{ fact.key }</th><th>{ fact.value }</th></tr>) }
+                            </thead>
+                            <tbody>
+                                { content.items.map(item => <tr><td>{ itemImage(item) }<span>{ item.title }</span></td><td>{ item.price }</td></tr>) }
+                            </tbody>
+                            <tfoot>
+                                <tr>
+                                    <td>Tax</td>
+                                    <td>{ content.tax }</td>
+                                </tr>
+                                <tr>
+                                    <td>Total</td>
+                                    <td>{ content.total }</td>
+                                </tr>
+                            </tfoot>
+                        </table>
+                    </div>
+                );
+        }
+    } else {
+        switch (props.attachment.contentType) {
+            case "image/png":
+                return <img src={ props.attachment.contentUrl }/>;
+        }
     }
 }
