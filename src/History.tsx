4e1b0d7543c4d3f85b9b9548d1f977125448381a
Eric Anderson <eanders@microsoft.com>
2016-10-03 13:32:34 -0700

    whitespace

--- src/History.tsx
+++ src/History.tsx
@@ -28,28 +28,28 @@ export class History extends React.Component<Props, {}> {
         return (
             <div className="wc-message-groups" ref={ ref => this.scrollMe = ref } onScroll={ e => this.props.actions.setAutoscroll((e.target as any).scrollTop + (e.target as any).offsetHeight >= (e.target as any).scrollHeight) }>
                 <div className="wc-message-group">
-                    { this.props.activities
-                        .filter(activity => activity.type === "message" && (activity.from.id != this.props.userId || !activity.id))
-                        .map((activity:Message) => {
-                            let clickable = this.props.debuggerVisible;
-                            let selected = activity === this.props.selectedActivity;
-                            let contentClassName = 'wc-message-content' + (clickable ? ' clickable' : '') + (selected ? ' selected' : '');
-                            return (
-                                <div className={ 'wc-message wc-message-from-' + (activity.from.id === 'user' ? 'me' : 'bot') }>
-                                    <div className={ contentClassName } onClick={ clickable ? (e) => { this.props.actions.onMessageClicked(activity, e); } : () => {} }>
-                                        <svg className="wc-message-callout">
-                                            <path className="point-left" d="m0,0 h12 v10 z" />
-                                            <path className="point-right" d="m0,10 v-10 h12 z" />
-                                        </svg>
-                                        <HistoryMessage
-                                            activity={ activity }
-                                            actions={ this.props.actions }/>
-                                    </div>
-                                    <div className="wc-message-from">{ activity.from.id === 'user' ? 'you' : activity.from.id }</div>
+                { this.props.activities
+                    .filter(activity => activity.type === "message" && (activity.from.id != this.props.userId || !activity.id))
+                    .map((activity:Message) => {
+                        let clickable = this.props.debuggerVisible;
+                        let selected = activity === this.props.selectedActivity;
+                        let contentClassName = 'wc-message-content' + (clickable ? ' clickable' : '') + (selected ? ' selected' : '');
+                        return (
+                            <div className={ 'wc-message wc-message-from-' + (activity.from.id === 'user' ? 'me' : 'bot') }>
+                                <div className={ contentClassName } onClick={ clickable ? (e) => { this.props.actions.onMessageClicked(activity, e); } : () => {} }>
+                                    <svg className="wc-message-callout">
+                                        <path className="point-left" d="m0,0 h12 v10 z" />
+                                        <path className="point-right" d="m0,10 v-10 h12 z" />
+                                    </svg>
+                                    <HistoryMessage
+                                        activity={ activity }
+                                        actions={ this.props.actions }/>
                                 </div>
-                            );
-                        })
-                    }
+                                <div className="wc-message-from">{ activity.from.id === 'user' ? 'you' : activity.from.id }</div>
+                            </div>
+                        );
+                    })
+                }
                 </div>
             </div>
         );
