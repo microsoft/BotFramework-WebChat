22d03764d0058c67e8ef3136716ce3c3e1ec116d
billba <billba@microsoft.com>
2016-10-20 17:11:42 -0700

    update type declarations

--- public/botchat.d.ts
+++ public/botchat.d.ts
@@ -1,16 +1,16 @@
-import * as React from 'react';
-
-export interface Props {
-    user: {
-        id: string;
-        name: string;
-    };
-    secret?: string;
-    token?: string;
-    debug?: string;
-    title?: string;
-    allowMessagesFrom?: string[];
-    directLineDomain?: string;
+export interface FormatOptions {
+    showHeader?: boolean
 }
 
-export declare class UI extends React.Component<Props, {}> { }
+export interface ChatProps {
+    user: { id: string, name: string },
+    secret?: string,
+    token?: string,
+    title?: string,
+    allowMessagesFrom?: string[],
+    directLineDomain?: string,
+    allowMessageSelection?: boolean,
+    formatOptions?: FormatOptions
+}
+
+export declare const App: (props:ChatProps) => {};
