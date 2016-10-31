503c556fc396c5c029d743c8fa2e04ddd74bc760
Dan Marshall <danmar@microsoft.com>
2016-10-31 16:22:24 -0700

    dock chat stream to bottom

--- src/History.tsx
+++ src/History.tsx
@@ -56,18 +56,20 @@ export class History extends React.Component<Props, {}> {
         return (
             <div className="wc-message-groups" ref={ ref => this.scrollMe = ref }>
                 <div className="wc-message-group">
-                { state.history.activities.map((activity:Activity, index) =>
-                    <div key={index} className={ 'wc-message wc-message-from-' + (activity.from.id === state.connection.user.id ? 'me' : 'bot') }>
-                        <div className={ 'wc-message-content' + (this.props.onActivitySelected ? ' clickable' : '') + (activity === state.history.selectedActivity ? ' selected' : '') } onClick={ e => this.onActivitySelected(e, activity) }>
-                            <svg className="wc-message-callout">
-                                <path className="point-left" d="m0,6 l6 6 v-12 z" />
-                                <path className="point-right" d="m6,6 l-6 6 v-12 z" />
-                            </svg>
-                            <HistoryMessage store={ this.props.store } activity={ activity } onImageLoad={ this.onImageLoad }/>
+                    <div className="wc-message-group-content">
+                    { state.history.activities.map((activity:Activity, index) =>
+                        <div key={index} className={ 'wc-message wc-message-from-' + (activity.from.id === state.connection.user.id ? 'me' : 'bot') }>
+                            <div className={ 'wc-message-content' + (this.props.onActivitySelected ? ' clickable' : '') + (activity === state.history.selectedActivity ? ' selected' : '') } onClick={ e => this.onActivitySelected(e, activity) }>
+                                <svg className="wc-message-callout">
+                                    <path className="point-left" d="m0,6 l6 6 v-12 z" />
+                                    <path className="point-right" d="m6,6 l-6 6 v-12 z" />
+                                </svg>
+                                <HistoryMessage store={ this.props.store } activity={ activity } onImageLoad={ this.onImageLoad }/>
+                            </div>
+                            <div className="wc-message-from">{ activity.from.id === state.connection.user.id ? 'you' : activity.from.id }</div>
                         </div>
-                        <div className="wc-message-from">{ activity.from.id === state.connection.user.id ? 'you' : activity.from.id }</div>
+                    ) }
                     </div>
-                ) }
                 </div>
             </div>
         );
