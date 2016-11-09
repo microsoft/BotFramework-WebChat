125f3d16c50af34e95f1928dee1e6eb3fa31c96c
billba <billba@microsoft.com>
2016-11-09 05:31:22 -0800

    comment out backchannel support in preparation for public release

--- built/App.js
+++ built/App.js
@@ -7,57 +7,73 @@ var __assign = (this && this.__assign) || Object.assign || function(t) {
     }
     return t;
 };
-var _this = this;
 var React = require('react');
 var ReactDOM = require('react-dom');
 var Chat_1 = require('./Chat');
 require('core-js/shim');
-exports.App = function (props, container) {
-    ReactDOM.render(React.createElement(AppContainer, props), container);
-};
-var receiveBackchannelMessageFromHostingPage = function (props) { return function (event) {
+/* & {
+//  experimental backchannel support
+    allowMessagesFrom?: string[],
+    onBackchannelMessage?: (backchannel: any) => void
+}
+*/
+/*
+// experimental backchannel support
+function isBackchannel(activity: Activity, user: User):activity is Message {
+    return activity.type === "message" && activity.from.id !== user.id && activity.text === "backchannel" && activity.channelData && activity.channelData.backchannel;
+}
+
+const receiveBackchannelMessageFromHostingPage = (props: AppProps) => (event: MessageEvent) => {
     if (props.allowMessagesFrom.indexOf(event.origin) === -1) {
         console.log("Rejecting backchannel message from unknown source", event.source);
         return;
     }
+
     if (!event.data) {
         console.log("Empty backchannel message from source", event.source);
         return;
     }
+
     console.log("Received backchannel message", event.data, "from", event.source);
+
     props.botConnection.postMessage("backchannel", props.user, { backchannel: event.data })
-        .retry(2)
-        .subscribe(function (success) {
+    .retry(2)
+    .subscribe(success => {
         console.log("backchannel message sent to bot");
-    }, function (error) {
+    }, error => {
         console.log("failed to send backchannel message to bot");
     });
-}; };
-function isBackchannel(activity) {
-    return activity.type === "message" && activity.text === "backchannel" && activity.channelData && activity.channelData.backchannel;
 }
-var AppContainer = function (props) {
+*/
+exports.App = function (props, container) {
     console.log("BotChat.App props", props);
-    if (props.allowMessagesFrom) {
-        console.log("adding event listener for messages from hosting web page");
-        window.addEventListener("message", receiveBackchannelMessageFromHostingPage(props), false);
-    }
-    if (props.onBackchannelMessage) {
-        console.log("adding event listener for messages to hosting web page");
-        props = Object.assign({}, props, {
-            botConnection: Object.assign({}, props.botConnection, {
-                activity$: props.botConnection.activity$.do(function (activity) {
-                    if (isBackchannel(activity)) {
-                        _this.props.onBackchannelMessage(activity.channelData.backchannel);
-                    }
-                }).filter(function (activity) { return !isBackchannel(activity); })
-            })
-        });
-    }
-    return (React.createElement("div", {className: "wc-app"}, 
+    /*
+        // experimental backchannel support
+        if (props.allowMessagesFrom) {
+            console.log("adding event listener for messages from hosting web page");
+            window.addEventListener("message", receiveBackchannelMessageFromHostingPage(props), false);
+        }
+    
+        if (props.onBackchannelMessage) {
+            console.log("adding event listener for messages to hosting web page");
+            props = Object.assign({}, props, {
+                botConnection: Object.assign({}, props.botConnection, {
+                    activity$: props.botConnection.activity$.do(activity => {
+                            if (isBackchannel(activity, props.user)) {
+                                this.props.onBackchannelMessage(activity.channelData.backchannel);
+                            }
+                        }).filter(activity => !isBackchannel(activity, props.user))
+                })
+            });
+        }
+    */
+    ReactDOM.render(React.createElement(AppContainer, props), container);
+};
+var AppContainer = function (props) {
+    return React.createElement("div", {className: "wc-app"}, 
         React.createElement("div", {className: "wc-app-left-container"}, 
             React.createElement(Chat_1.Chat, __assign({}, props))
         )
-    ));
+    );
 };
 //# sourceMappingURL=App.js.map
--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Chat, ChatProps } from './Chat';
-import { Activity, Message, IBotConnection } from './BotConnection';
+import { Activity, Message, User, IBotConnection } from './BotConnection';
 import { DirectLine } from './directLine';
 import 'core-js/shim';
 
-export type AppProps = ChatProps & {
+export type AppProps = ChatProps
+/* & {
+//  experimental backchannel support 
     allowMessagesFrom?: string[],
     onBackchannelMessage?: (backchannel: any) => void
 }
+*/
 
-export const App = (props: AppProps, container: HTMLElement) => {
-    ReactDOM.render(React.createElement(AppContainer, props), container);
-} 
+/*
+// experimental backchannel support 
+function isBackchannel(activity: Activity, user: User):activity is Message {
+    return activity.type === "message" && activity.from.id !== user.id && activity.text === "backchannel" && activity.channelData && activity.channelData.backchannel;
+}
 
 const receiveBackchannelMessageFromHostingPage = (props: AppProps) => (event: MessageEvent) => {
     if (props.allowMessagesFrom.indexOf(event.origin) === -1) {
@@ -35,13 +40,13 @@ const receiveBackchannelMessageFromHostingPage = (props: AppProps) => (event: Me
         console.log("failed to send backchannel message to bot");
     });
 }
+*/
 
-function isBackchannel(activity: Activity):activity is Message {
-    return activity.type === "message" && activity.text === "backchannel" && activity.channelData && activity.channelData.backchannel;
-}
 
-const AppContainer = (props: AppProps) => {
+export const App = (props: AppProps, container: HTMLElement) => {
     console.log("BotChat.App props", props);
+/*
+    // experimental backchannel support
     if (props.allowMessagesFrom) {
         console.log("adding event listener for messages from hosting web page");
         window.addEventListener("message", receiveBackchannelMessageFromHostingPage(props), false);
@@ -52,19 +57,20 @@ const AppContainer = (props: AppProps) => {
         props = Object.assign({}, props, {
             botConnection: Object.assign({}, props.botConnection, {
                 activity$: props.botConnection.activity$.do(activity => {
-                        if (isBackchannel(activity)) {
+                        if (isBackchannel(activity, props.user)) {
                             this.props.onBackchannelMessage(activity.channelData.backchannel);
                         }
-                    }).filter(activity => !isBackchannel(activity))
+                    }).filter(activity => !isBackchannel(activity, props.user))
             })
         });
     }
+*/
+    ReactDOM.render(React.createElement(AppContainer, props), container);
+} 
 
-    return (
-        <div className="wc-app">
-            <div className="wc-app-left-container">
-                <Chat { ...(props as ChatProps) } />
-            </div>
+const AppContainer = (props: AppProps) =>
+    <div className="wc-app">
+        <div className="wc-app-left-container">
+            <Chat { ...(props as ChatProps) } />
         </div>
-    );
-}
+    </div>;
