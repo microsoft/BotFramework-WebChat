5ab6953be0e2b2747ccd484561429833869015f5
billba <billba@microsoft.com>
2016-11-20 02:05:12 -0800

    initial

---
+++ tests/history.ts
@@ -0,0 +1,16 @@
+import { expect, use } from 'chai';
+import chaiSubset = require('chai-subset');
+import { history, HistoryState, HistoryAction } from '../src/Store';
+
+var window;
+
+use(chaiSubset);
+
+describe('history', () => {
+    it('should start with an empty history', () => {
+        expect(history(undefined, { type: undefined })).to.containSubset({
+            activities: [],
+            selectedActivity: null
+        })
+    });
+})
