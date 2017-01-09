2b7fe7d714d95735575218e9ef4312aa7894dc19
Bill Barnes <bill@billba-mbp.local>
2017-01-09 07:51:42 -0800

    move nested functions outside for performance

--- src/Attachment.tsx
+++ src/Attachment.tsx
@@ -5,6 +5,33 @@ import { FormatState } from './Store';
 
 const regExpCard = /\^application\/vnd\.microsoft\.card\./i;
 
+const buttons = (
+    buttons: Button[],
+    onClickButton: (type: string, value: string) => void
+) => buttons &&
+    <ul className="wc-card-buttons">
+        { buttons.map((button, index) => <li key={ index }><button onClick={ () => onClickButton(button.type, button.value) }>{ button.title }</button></li>) }
+    </ul>;
+
+const imageWithOnLoad = (url: string, onImageLoad: () => void, thumbnailUrl?: string, autoPlay?:boolean, loop?: boolean) =>
+    <img src={ url } autoPlay = { autoPlay } loop = { loop } poster = { thumbnailUrl } onLoad={ onImageLoad } />;
+
+const audio = (audioUrl: string, autoPlay?:boolean, loop?: boolean) =>
+    <audio src={ audioUrl } autoPlay={ autoPlay } controls loop={ loop } />;
+
+const videoWithOnLoad = (videoUrl: string, onImageLoad: () => void, thumbnailUrl?: string, autoPlay?:boolean, loop?: boolean) =>
+    <video src={ videoUrl } poster={ thumbnailUrl } autoPlay={ autoPlay } controls loop={ loop } onLoadedMetadata={ onImageLoad } />;
+
+const attachedImage = (images: { url: string }[], onImageLoad: () => void, ) =>
+    images && images.length > 0 && imageWithOnLoad(images[0].url, onImageLoad);
+
+const isGifMedia = (url: string) =>
+    url.slice((url.lastIndexOf(".") - 1 >>> 0) + 2).toLowerCase() == 'gif';
+
+const title = (title: string) => renderIfNonempty(title, title => <h1>{ title }</h1>);
+const subtitle = (subtitle: string) => renderIfNonempty(subtitle, subtitle => <h2>{ subtitle }</h2>);
+const text = (text: string) => renderIfNonempty(text, text => <p>{ text }</p>);
+
 export const AttachmentView = (props: {
     format: FormatState;
     attachment: Attachment,
@@ -15,41 +42,17 @@ export const AttachmentView = (props: {
 
     const attachment = props.attachment;
 
-    const buttons = (buttons?: Button[]) => buttons &&
-        <ul className="wc-card-buttons">
-            { buttons.map((button, index) => <li key={ index }><button onClick={ () => props.onClickButton(button.type, button.value) }>{ button.title }</button></li>) }
-        </ul>;
-
-    const imageWithOnLoad = (url: string, thumbnailUrl?: string, autoPlay?:boolean, loop?: boolean) =>
-        <img src={ url } autoPlay = { autoPlay } loop = { loop } poster = { thumbnailUrl } onLoad={ props.onImageLoad } />;
-
-    const audio = (audioUrl: string, autoPlay?:boolean, loop?: boolean) =>
-        <audio src={ audioUrl } autoPlay={ autoPlay } controls loop={ loop } />;
-
-    const videoWithOnLoad = (videoUrl: string, thumbnailUrl?: string, autoPlay?:boolean, loop?: boolean) =>
-        <video src={ videoUrl } poster={ thumbnailUrl } autoPlay={ autoPlay } controls loop={ loop } onLoadedMetadata={ props.onImageLoad } />;
-
-    const attachedImage = (images?: { url: string }[]) =>
-        images && images.length > 0 && imageWithOnLoad(images[0].url);
-
-    const isGifMedia = (url: string) =>
-        url.slice((url.lastIndexOf(".") - 1 >>> 0) + 2).toLowerCase() == 'gif';
-
-    const title = (title: string) => renderIfNonempty(title, title => <h1>{ title }</h1>);
-    const subtitle = (subtitle: string) => renderIfNonempty(subtitle, subtitle => <h2>{ subtitle }</h2>);
-    const text = (text: string) => renderIfNonempty(text, text => <p>{ text }</p>);
-
     switch (attachment.contentType) {
         case "application/vnd.microsoft.card.hero":
             if (!attachment.content)
                 return null;
             return (
                 <div className='wc-card hero'>
-                    { attachedImage(attachment.content.images) }
+                    { attachedImage(attachment.content.images, props.onImageLoad) }
                     { title(attachment.content.title) }
                     { subtitle(attachment.content.subtitle) }
                     { text(attachment.content.text) }
-                    { buttons(attachment.content.buttons) }
+                    { buttons(attachment.content.buttons, props.onClickButton) }
                 </div>
             );
 
@@ -59,10 +62,10 @@ export const AttachmentView = (props: {
             return (
                 <div className='wc-card thumbnail'>
                     { title(attachment.content.title) }
-                    { attachedImage(attachment.content.images) }
+                    { attachedImage(attachment.content.images, props.onImageLoad) }
                     { subtitle(attachment.content.subtitle) }
                     { text(attachment.content.text) }
-                    { buttons(attachment.content.buttons) }
+                    { buttons(attachment.content.buttons, props.onClickButton) }
                 </div>
             );
 
@@ -71,11 +74,11 @@ export const AttachmentView = (props: {
                 return null;
             return (
                 <div className='wc-card video'>
-                    { videoWithOnLoad(attachment.content.media[0].url, attachment.content.image ? attachment.content.image.url : null, attachment.content.autostart, attachment.content.autoloop) }
+                    { videoWithOnLoad(attachment.content.media[0].url, props.onImageLoad, attachment.content.image ? attachment.content.image.url : null, attachment.content.autostart, attachment.content.autoloop) }
                     { title(attachment.content.title) }
                     { subtitle(attachment.content.subtitle) }
                     { text(attachment.content.text) }
-                    { buttons(attachment.content.buttons) }
+                    { buttons(attachment.content.buttons, props.onClickButton) }
                 </div>
             );
 
@@ -83,16 +86,14 @@ export const AttachmentView = (props: {
         case "application/vnd.microsoft.card.animation":
             if (!attachment.content || !attachment.content.media || attachment.content.media.length === 0)
                 return null;            
-
-            const contentFunction = isGifMedia(attachment.content.media[0].url) ? imageWithOnLoad : videoWithOnLoad; 
-
+            const media = isGifMedia(attachment.content.media[0].url) ? imageWithOnLoad : videoWithOnLoad; 
             return (
                 <div className='wc-card animation'>
-                    { contentFunction(attachment.content.media[0].url, attachment.content.image ? attachment.content.image.url : null, attachment.content.autostart, attachment.content.autoloop) }
+                    { media(attachment.content.media[0].url, props.onImageLoad, attachment.content.image ? attachment.content.image.url : null, attachment.content.autostart, attachment.content.autoloop) }
                     { title(attachment.content.title) }
                     { subtitle(attachment.content.subtitle) }
                     { text(attachment.content.text) }
-                    { buttons(attachment.content.buttons) }
+                    { buttons(attachment.content.buttons, props.onClickButton) }
                 </div>
             );
 
@@ -105,7 +106,7 @@ export const AttachmentView = (props: {
                     { title(attachment.content.title) }
                     { subtitle(attachment.content.subtitle) }
                     { text(attachment.content.text) }
-                    { buttons(attachment.content.buttons) }
+                    { buttons(attachment.content.buttons, props.onClickButton) }
                 </div>
             );
 
@@ -115,7 +116,7 @@ export const AttachmentView = (props: {
             return (
                 <div className='wc-card signin'>
                     { text(attachment.content.text) }
-                    { buttons(attachment.content.buttons) }
+                    { buttons(attachment.content.buttons, props.onClickButton) }
                 </div>
             );
 
@@ -133,7 +134,7 @@ export const AttachmentView = (props: {
                         </thead>
                         <tbody>{ attachment.content.items && attachment.content.items.map((item, i) =>
                             <tr key={'item' + i}>
-                                <td>{ item.image && imageWithOnLoad(item.image.url) }<span>{ item.title }</span></td>
+                                <td>{ imageWithOnLoad(item.image.url, props.onImageLoad) }<span>{ item.title }</span></td>
                                 <td>{ item.price }</td>
                             </tr>) }
                         </tbody>
@@ -161,24 +162,17 @@ export const AttachmentView = (props: {
         case "image/jpg":
         case "image/jpeg":
         case "image/gif":
-            return imageWithOnLoad(attachment.contentUrl);
+            return imageWithOnLoad(attachment.contentUrl, props.onImageLoad);
 
         case "audio/mpeg":
         case "audio/mp4":
             return audio(attachment.contentUrl);
 
         case "video/mp4":
-            return videoWithOnLoad(attachment.contentUrl);
+            return videoWithOnLoad(attachment.contentUrl, props.onImageLoad);
 
         default:
-            return (
-                <span>{
-                    (regExpCard.test((attachment as any).contentType)
-                        ? props.format.strings.unknownCard
-                        : props.format.strings.unknownFile
-                    ).replace('%1', (attachment as any).contentType)
-                }
-                </span>
-            );
+            const unknown = regExpCard.test((attachment as any).contentType) ? props.format.strings.unknownCard : props.format.strings.unknownFile;
+            return <span>{ unknown..replace('%1', (attachment as any).contentType) }</span>;
     }        
 }
