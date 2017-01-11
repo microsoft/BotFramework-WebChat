3a7ecdf9eaf940f6706d91c5bb10b1f6849dae4d
Bill Barnes <bill@billba-mbp.local>
2017-01-11 13:23:42 -0800

    npm stuff

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -10,8 +10,18 @@ module.exports = {
         filename: "./webpacked/botchat.js",
     },
 
-    // Enable sourcemaps for debugging webpack's output.
-    devtool: "source-map",
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compressor: {
+                warnings: false
+            }
+        })
+    ],
 
     resolve: {
         extensions: ["", ".webpack.js", ".web.js", ".js"]
