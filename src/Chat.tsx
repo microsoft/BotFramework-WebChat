81cdfdca65ec167b3a9cc97b062ba37f01fff528
Angel Malavar <angel.malavar@gmail.com>
2017-01-17 16:38:43 -0600

    Change add image an some style issues

--- src/Chat.tsx
+++ src/Chat.tsx
@@ -103,7 +103,7 @@ export class Chat extends React.Component<ChatProps, {}> {
             </div>;
 
         let add_url = "http://www.chron.com/";
-        let img_src = "http://www.hearstmediahouston.com/img/logo.png";
+        let img_src = "https://upload.wikimedia.org/wikipedia/commons/7/70/Houston_Chronicle_Logo_2016.svg";
         let add = <div className="wc-add"><a href={add_url} target="_blank" title="Click here for more info"><img src={img_src} /></a></div>
 
         return (
