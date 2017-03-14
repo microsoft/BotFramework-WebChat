eba17443489eec2c23f80c4ba2e36e54c3402e3a
Dan Marshall <danmar@microsoft.com>
2017-03-13 18:54:36 -0700

    show suggestedActions animate down

--- src/SuggestedActions.tsx
+++ src/SuggestedActions.tsx
@@ -10,23 +10,33 @@ export interface State {
 }
 
 export class SuggestedActions extends React.Component<Props, State> {
+
     constructor(props: Props) {
         super(props);
     }
 
+    actionClick(e: React.MouseEvent<HTMLButtonElement>, cardAction: CardAction) {
+        
+        //click is only valid if there are props.actions
+        if (this.props.actions) {
+            this.props.onCardAction(cardAction.type, cardAction.value);
+        }
+    
+        e.stopPropagation();
+    }
+
+    shouldComponentUpdate(nextProps: Props) {
+
+        //update only when there are actions. We want the old actions to remain displayed as it animates down.
+        return !!nextProps.actions;
+    }
+
     render() {
-
         if (!this.props.actions) return null;
-
-        const onCardAction = (cardAction: CardAction) => cardAction &&
-            (e => {
-                this.props.onCardAction(cardAction.type, cardAction.value);
-                e.stopPropagation();
-            });
 
         return (
             <ul className="wc-suggested-actions">
-                { this.props.actions.map((action, index) => <li key={ index }><button onClick={ onCardAction(action) }>{ action.title }</button></li>) }
+                { this.props.actions.map((action, index) => <li key={ index }><button onClick={ e => this.actionClick(e, action) }>{ action.title }</button></li>) }
             </ul>
         );
     }
