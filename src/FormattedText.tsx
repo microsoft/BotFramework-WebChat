1e9ec22b2652f29824f7ada72965bbd0b15901c8
Carlos Reyes-Rico (Collabera) <v-careye@microsoft.com>
2017-04-24 18:07:11 -0700

    Merging with MS master branch

--- src/FormattedText.tsx
+++ src/FormattedText.tsx
@@ -1,36 +1,10 @@
 import * as MarkdownIt from 'markdown-it';
 import * as React from 'react';
 
-export interface MarkdownItOptions{
-  html?:         boolean,       // Enable HTML tags in source
-  xhtmlOut?:     boolean,       // Use '/' to close single tags (<br />).
-                                // This is only for full CommonMark compatibility.
-  breaks?:       boolean,       // Convert '\n' in paragraphs into <br>
-  langPrefix?:   string,        // CSS language prefix for fenced blocks. Can be
-                                // useful for external highlighters.
-  linkify?:      boolean,       // Autoconvert URL-like text to links
-
-  // Enable some language-neutral replacement + quotes beautification
-  typographer?:  boolean,
-
-  // Double + single quotes replacement pairs, when typographer enabled,
-  // and smartquotes on. Could be either a String or an Array.
-  //
-  // For example, you can use '«»„“' for Russian, '„“‚‘' for German,
-  // and ['«\xA0', '\xA0»', '‹\xA0', '\xA0›'] for French (including nbsp).
-  quotes?: Object,
-
-  // Highlighter function. Should return escaped HTML,
-  // or '' if the source string is not changed and should be escaped externaly.
-  // If result starts with <pre... internal wrapper is skipped.
-  highlight?: Object    
-}
-
 export interface IFormattedTextProps {
     text: string,
     format: string,
-    onImageLoad: () => void,
-    markdownOptions?: MarkdownItOptions
+    onImageLoad: () => void
 }
 
 export const FormattedText = (props: IFormattedTextProps) => {
