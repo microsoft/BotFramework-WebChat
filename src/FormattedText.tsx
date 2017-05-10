d222174d99da8e19295a367e6595990ecd3591e5
Dan Marshall <danmar@microsoft.com>
2017-05-09 18:55:08 -0700

    configure MarkdownIt to open links in new tab

--- src/FormattedText.tsx
+++ src/FormattedText.tsx
@@ -27,6 +27,29 @@ const renderPlainText = (text: string) => {
 
 const markdownIt = new MarkdownIt({ html: true, linkify: true, typographer: true });
 
+//configure MarkdownIt to open links in new tab
+//from https://github.com/markdown-it/markdown-it/blob/master/docs/architecture.md#renderer
+
+markdownIt
+// Remember old renderer, if overriden, or proxy to default renderer
+var defaultRender = markdownIt.renderer.rules.link_open || function(tokens, idx, options, env, self) {
+  return self.renderToken(tokens, idx, options);
+};
+
+markdownIt.renderer.rules.link_open = function (tokens, idx, options, env, self) {
+  // If you are sure other plugins can't add `target` - drop check below
+  var aIndex = tokens[idx].attrIndex('target');
+
+  if (aIndex < 0) {
+    tokens[idx].attrPush(['target', '_blank']); // add new attribute
+  } else {
+    tokens[idx].attrs[aIndex][1] = '_blank';    // replace value of existing attr
+  }
+
+  // pass token to default renderer.
+  return defaultRender(tokens, idx, options, env, self);
+};
+
 const renderMarkdown = (
     text: string,
     onImageLoad: () => void
