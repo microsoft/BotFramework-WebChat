bd618ddc565fdb9d89eef1febef8ae3f06ac4273
Dan Marshall <danmar@microsoft.com>
2017-06-15 18:58:44 -0700

    eliminate webpack warnings

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -35,7 +35,12 @@ module.exports = {
             // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
             { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
             // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                loader: "source-map-loader",
+                exclude: [/node_modules/]
+            },
             {
                 test: require.resolve('microsoft-adaptivecards'),
                 use: [{ loader: 'expose-loader', options: 'AdaptiveCards' }]
