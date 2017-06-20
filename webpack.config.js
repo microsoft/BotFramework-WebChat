501f8dafccac66a3dac40355ded1a58d90301e25
Dan Marshall <danmar@microsoft.com>
2017-06-20 14:14:36 -0700

    whitespace cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -21,9 +21,9 @@ module.exports = {
             // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
             { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
             // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { 
-                enforce: "pre", 
-                test: /\.js$/, 
+            {
+                enforce: "pre",
+                test: /\.js$/,
                 loader: "source-map-loader",
                 exclude: [/node_modules/]
             },
