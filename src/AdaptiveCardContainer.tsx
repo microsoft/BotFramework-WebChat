f05a01dc339d6c3fcfaf64b54aa47e7b3aaf89db
Dan Marshall <danmar@microsoft.com>
2017-06-19 21:24:11 -0700

    Adaptivecards error handling (#518)
    * Adaptive receipt (#2)
    
    * receipt card test
    
    Adding support for receipt card
    
    * Recept Card in version 0.5
    
    Recept Card in version 0.5
    
    * load adaptivecards hostconfig from json file, webpacked into bundle (#495)
    
    * remove Action.Http from cards
    
    * corrected alpha channel colors
    
    * add AdaptiveCards.md
    
    * rename to Bot Builder SDK
    
    * change SDK to Bot Builder SDK
    
    * fix typos
    
    * elsewhere
    
    * lowercase titles
    
    * added 'it'
    
    * separates to separate
    
    * handle undefined
    
    * imBack for string
    
    * Small language tweaks (#503)
    
    * Adaptivecards (#505)
    
    * edits from feedback
    
    * moved Action.Http
    
    * readme.md (#491)
    
    updated version of readme.md
    
    * edited test/README.md (#510)
    
    * edited test/README.md
    
    * Update README.md
    
    * error handling
    
    Adding SVG to represent the error occures in the JSON parsing or
    adaptive card missing properties.
    
    * Revert "error handling"
    
    This reverts commit 92a4d27d6ae87eaf976f9de1289949effbdc5bf5.
    
    * error handling for ac
    
    1. using this.forceUPdate()
    2. svg error message
    
    * minior fixes
    
    * use state for error rendering
    
    * change forceUpdate() to setState()
    
    change forceUpdate() to setState()
    
    * merge
    
    * fix minor bug in ac tests
    
    * add style to error
    
    * latest AdaptiveCards lib
    
    * remove 'lighter' text
    
    * reformatted
    
    * explicit if statement
    
    * catch hyperlink clicks

--- src/AdaptiveCardContainer.tsx
+++ src/AdaptiveCardContainer.tsx
@@ -14,6 +14,10 @@ export interface Props {
     className?: string
 }
 
+export interface State {
+    errors?: string[]
+}
+
 class LinkedAdaptiveCard extends AdaptiveCards.AdaptiveCard {
     constructor(public adaptiveCardContainer: AdaptiveCardContainer) {
         super();
@@ -71,7 +75,7 @@ AdaptiveCards.AdaptiveCard.onExecuteAction = (action: AdaptiveCards.ExternalActi
     }
 };
 
-export class AdaptiveCardContainer extends React.Component<Props, {}> {
+export class AdaptiveCardContainer extends React.Component<Props, State> {
     private div: HTMLDivElement;
 
     constructor(props: Props) {
@@ -87,6 +91,7 @@ export class AdaptiveCardContainer extends React.Component<Props, {}> {
 
         //do not allow form elements to trigger a parent click event
         switch ((e.target as HTMLElement).tagName) {
+            case 'A':
             case 'AUDIO':
             case 'VIDEO':
             case 'BUTTON':
@@ -101,13 +106,11 @@ export class AdaptiveCardContainer extends React.Component<Props, {}> {
     }
 
     componentDidMount() {
-
         const adaptiveCard = new LinkedAdaptiveCard(this);
         adaptiveCard.parse(cardWithoutHttpActions(this.props.card));
         const errors = adaptiveCard.validate();
 
         if (errors.length === 0) {
-
             let renderedCard: HTMLElement;
             try {
                 renderedCard = adaptiveCard.render();
@@ -120,7 +123,7 @@ export class AdaptiveCardContainer extends React.Component<Props, {}> {
                 errors.push(ve);
 
                 if (e.stack) {
-                    konsole.log(e.stack);
+                    ve.message += '\n' + e.stack;
                 }
             }
 
@@ -139,21 +142,45 @@ export class AdaptiveCardContainer extends React.Component<Props, {}> {
             }
         }
 
-        //render errors
-        errors.forEach(e => {
-            var div = document.createElement('div');
-            div.innerText = e.message;
-            this.div.appendChild(div);
-        });
+        if (errors.length > 0) {
+            console.log('Error(s) rendering AdaptiveCard:');
+            errors.forEach(e => console.log(e.message));
+            this.setState({ errors: errors.map(e => e.message) });
+        }
     }
 
     render() {
-        const wrappedChildren = this.props.children ? <div className="non-adaptive-content">{this.props.children}</div> : null;
+        let wrappedChildren: JSX.Element;
+        const hasErrors = this.state && this.state.errors && this.state.errors.length > 0;
+
+        if (hasErrors) {
+            wrappedChildren = (
+                <div>
+                    <svg className="error-icon" viewBox="0 0 15 12.01">
+                        <path d="M7.62 8.63v-.38H.94a.18.18 0 0 1-.19-.19V.94A.18.18 0 0 1 .94.75h10.12a.18.18 0 0 1 .19.19v3.73H12V.94a.91.91 0 0 0-.07-.36 1 1 0 0 0-.5-.5.91.91 0 0 0-.37-.08H.94a.91.91 0 0 0-.37.07 1 1 0 0 0-.5.5.91.91 0 0 0-.07.37v7.12a.91.91 0 0 0 .07.36 1 1 0 0 0 .5.5.91.91 0 0 0 .37.08h6.72c-.01-.12-.04-.24-.04-.37z M11.62 5.26a3.27 3.27 0 0 1 1.31.27 3.39 3.39 0 0 1 1.8 1.8 3.36 3.36 0 0 1 0 2.63 3.39 3.39 0 0 1-1.8 1.8 3.36 3.36 0 0 1-2.62 0 3.39 3.39 0 0 1-1.8-1.8 3.36 3.36 0 0 1 0-2.63 3.39 3.39 0 0 1 1.8-1.8 3.27 3.27 0 0 1 1.31-.27zm0 6a2.53 2.53 0 0 0 1-.21A2.65 2.65 0 0 0 14 9.65a2.62 2.62 0 0 0 0-2 2.65 2.65 0 0 0-1.39-1.39 2.62 2.62 0 0 0-2 0A2.65 2.65 0 0 0 9.2 7.61a2.62 2.62 0 0 0 0 2A2.65 2.65 0 0 0 10.6 11a2.53 2.53 0 0 0 1.02.26zM13 7.77l-.86.86.86.86-.53.53-.86-.86-.86.86-.53-.53.86-.86-.86-.86.53-.53.86.86.86-.86zM1.88 7.13h2.25V4.88H1.88zm.75-1.5h.75v.75h-.75zM5.63 2.63h4.5v.75h-4.5zM1.88 4.13h2.25V1.88H1.88zm.75-1.5h.75v.75h-.75zM9 5.63H5.63v.75h2.64A4 4 0 0 1 9 5.63z" />
+                    </svg>
+                    <div className="error-text">Can't render card</div>
+                </div>
+            );
+        } else if (this.props.children) {
+            wrappedChildren = (
+                <div className="non-adaptive-content">
+                    {this.props.children}
+                </div>
+            );
+        } else {
+            wrappedChildren = null;
+        }
+
         return (
-            <div className={classList('wc-card', 'wc-adaptive-card', this.props.className)} ref={div => this.div = div} onClick={e => this.onClick(e)}>
+            <div className={classList('wc-card', 'wc-adaptive-card', this.props.className, hasErrors && 'error')} ref={div => this.div = div} onClick={e => this.onClick(e)}>
                 {wrappedChildren}
             </div>
         )
+    }
+
+    componentDidUpdate() {
+        if (this.props.onImageLoad) this.props.onImageLoad();
     }
 }
 
