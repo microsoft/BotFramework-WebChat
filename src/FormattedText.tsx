bfec00717f0b2d6d1090625e3b8723b7b4117e3c
Carlos Reyes-Rico (Collabera) <v-careye@microsoft.com>
2017-11-17 12:57:20 -0800

    Adding space character.

--- src/FormattedText.tsx
+++ src/FormattedText.tsx
@@ -59,7 +59,7 @@ const renderMarkdown = (
     if (text.trim()) {
         const src = text
                 // convert <br> tags to blank lines for markdown
-                .replace(/<br\s*\/?>/ig, '\r\n\r\n')
+                 .replace(/<br\s*\/?>/ig, '\r\n\r\n')
                 // URL encode all links
                  .replace(/\[(.*?)\]\((.*?)\)/ig, (match, text, url) => `[${text}](${markdownIt.normalizeLink(url)})`);
         __html = markdownIt.render(src);        
