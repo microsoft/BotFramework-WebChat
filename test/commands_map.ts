fe040ea1bfe2a921c75139b7015a2b8c6d4652a2
William Wong <compulim@hotmail.com>
2017-12-11 11:04:01 -0800

    Add typing test

--- test/commands_map.ts
+++ test/commands_map.ts
@@ -7,7 +7,7 @@ const path = require('path');
 declare let module: any;
 
 interface ISendActivity {
-    (conversationId: string, activity: dl.Message): void;
+    (conversationId: string, activity: dl.Message | dl.Typing): void;
 }
 
 interface CommandValues {
@@ -556,6 +556,16 @@ var commands_map: CommandValuesMap = {
         },
         server: function (conversationId, sendActivity) {
             sendActivity(conversationId, server_content.thumbnail_card);
+        }
+    },
+    "typing": {
+        do: function (nightmare) {
+        },
+        client: function () {
+            return document.querySelectorAll('.wc-message-wrapper:last-child .wc-typing').length >= 0;
+        },
+        server: function (conversationId, sendActivity) {
+            sendActivity(conversationId, server_content.typing);
         }
     },
     "upload": {
