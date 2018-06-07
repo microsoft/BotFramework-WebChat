a61368369563d0435d091348c40432c1cc1f981a
William Wong <compulim@hotmail.com>
2018-06-06 17:49:16 -0700

    Copy more files

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,4 +10,16 @@ chmod +x ~/blobxfer
 PACKAGE_VERSION=$(node -p require\(\'$TRAVIS_BUILD_DIR/package.json\'\).version)
 echo $PACKAGE_VERSION
 
-~/blobxfer upload --local-path $TRAVIS_BUILD_DIR/package.json --remote-path botframework-webchat/$PACKAGE_VERSION --storage-account webchattest --storage-account-key $AZURE_BLOB_KEY
+mkdir $TRAVIS_BUILD_DIR/dist
+
+mv $TRAVIS_BUILD_DIR/adaptivecards-hostconfig.json $TRAVIS_BUILD_DIR/dist
+mv $TRAVIS_BUILD_DIR/botchat-fullwindow.css $TRAVIS_BUILD_DIR/dist
+mv $TRAVIS_BUILD_DIR/botchat.css $TRAVIS_BUILD_DIR/dist
+mv $TRAVIS_BUILD_DIR/botchat.js $TRAVIS_BUILD_DIR/dist
+mv $TRAVIS_BUILD_DIR/botchat.js.map $TRAVIS_BUILD_DIR/dist
+mv $TRAVIS_BUILD_DIR/botchat-es5.js $TRAVIS_BUILD_DIR/dist
+mv $TRAVIS_BUILD_DIR/botchat-es5.js.map $TRAVIS_BUILD_DIR/dist
+mv $TRAVIS_BUILD_DIR/CognitiveServices.js $TRAVIS_BUILD_DIR/dist
+mv $TRAVIS_BUILD_DIR/CognitiveServices.js.map $TRAVIS_BUILD_DIR/dist
+
+~/blobxfer upload --local-path $TRAVIS_BUILD_DIR/dist/* --remote-path botframework-webchat/$PACKAGE_VERSION --storage-account webchattest --storage-account-key $AZURE_BLOB_KEY
