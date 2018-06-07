3ec7fbe60bef4ae08ed8135a65b48991a977daf0
William Wong <compulim@hotmail.com>
2018-06-06 19:54:11 -0700

    Update keys

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -20,11 +20,11 @@ cp $TRAVIS_BUILD_DIR/CognitiveServices.js $TRAVIS_BUILD_DIR/dist
 cp $TRAVIS_BUILD_DIR/CognitiveServices.js.map $TRAVIS_BUILD_DIR/dist
 
 # Upload to based on version from package.json
-~/blobxfer upload --local-path $TRAVIS_BUILD_DIR/dist --remote-path $PACKAGE_NAME/$PACKAGE_VERSION --storage-account webchattest --storage-account-key $AZURE_BLOB_KEY
+~/blobxfer upload --local-path $TRAVIS_BUILD_DIR/dist --remote-path $PACKAGE_NAME/$PACKAGE_VERSION --storage-account $CDN_BLOB_ACCOUNT --storage-account-key $CDN_BLOB_KEY
 
 # If TRAVIS_TAG is present, it means this is going PRODUCTION
 if [-n $TRAVIS_TAG]
 then
 # Upload to /latest/
-~/blobxfer upload --local-path $TRAVIS_BUILD_DIR/dist --remote-path $PACKAGE_NAME/latest --storage-account webchattest --storage-account-key $AZURE_BLOB_KEY
+~/blobxfer upload --local-path $TRAVIS_BUILD_DIR/dist --remote-path $PACKAGE_NAME/latest --storage-account $CDN_BLOB_ACCOUNT --storage-account-key $CDN_BLOB_KEY
 fi
