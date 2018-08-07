66b3fa9e3b0b4108077d5f5c3dd0b105a1ecf621
Jan Dvorak <dvorakj@gmail.com>
2018-08-07 21:58:58 +0200

    preset some default props

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,13 @@ export type AppProps = ChatProps;
 
 export const App = (props: AppProps, container: HTMLElement) => {
     konsole.log("BotChat.App props", props);
+
+    // FEEDYOU generate user ID if not present in props
+    props.user = {...{id: makeid(), name: 'User'}, ...props.user};
+    props.showUploadButton = props.hasOwnProperty('showUploadButton') ? props.showUploadButton : false;
+    props.resize = props.hasOwnProperty('resize') ? props.resize : 'detect';
+    props.locale = props.hasOwnProperty('locale') ? props.locale : 'cs-cz';
+
     ReactDOM.render(React.createElement(AppContainer, props), container);
 }
 
@@ -14,3 +21,13 @@ const AppContainer = (props: AppProps) =>
     <div className="wc-app">
         <Chat { ...props } />
     </div>;
+
+function makeid() {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (var i = 0; i < 11; i++)
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
