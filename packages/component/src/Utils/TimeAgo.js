cc5b4495a4f067531be02b547c0a66a8608bd30c
William Wong <compulim@hotmail.com>
2018-08-23 00:50:09 -0700

    Add more time duration

--- packages/component/src/Utils/TimeAgo.js
+++ packages/component/src/Utils/TimeAgo.js
@@ -2,19 +2,31 @@ import React from 'react';
 
 import Timer from './Timer';
 
-function ago(date) {
-  // We need to use HTML.Intl to create this
+function ago(date, locale) {
+  // TODO: HTML Intl does not support internationalization of time duration
+  //       We need to allow user to pass their internationalization logic
 
   const now = Date.now();
   const delta = now - new Date(date).getTime();
   const deltaInMinutes = Math.floor(delta / 60000);
+  const deltaInHours = Math.floor(delta / 3600000);
 
   if (deltaInMinutes < 1) {
     return 'Just now';
   } else if (deltaInMinutes === 1) {
     return 'A minute ago';
-  } else {
+  } else if (deltaInHours < 1) {
     return `${ deltaInMinutes } minutes ago`;
+  } else if (deltaInHours === 1) {
+    return `An hour ago`;
+  } else if (deltaInHours < 5) {
+    return `${ deltaInHours } hours ago`;
+  } else if (deltaInHours <= 24) {
+    return `Today`;
+  } else if (deltaInHours <= 48) {
+    return `Yesterday`;
+  } else {
+    return new Intl.DateTimeFormat(locale).format(date);
   }
 }
 
@@ -36,7 +48,8 @@ export default class TimeAgo extends React.Component {
     this.handleInterval = this.handleInterval.bind(this);
 
     this.state = {
-      text: ago(props.value),
+      // TODO: We need to pass locale
+      text: ago(props.value, 'en-US'),
       timer: nextTimer(props.value)
     }
   }
@@ -51,7 +64,8 @@ export default class TimeAgo extends React.Component {
 
   updateText(props) {
     this.setState(() => ({
-      text: ago(props.value),
+      // TODO: We need to pass locale
+      text: ago(props.value, 'en-US'),
       timer: nextTimer(props.value)
     }));
   }
