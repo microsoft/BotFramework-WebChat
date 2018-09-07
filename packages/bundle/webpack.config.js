72a2f4e7b26f5b8ea1c2d8075bdd5465f867263e
William Wong <compulim@hotmail.com>
2018-09-06 23:49:33 -0700

    Add Webpack visualizer

--- packages/bundle/webpack.config.js
+++ packages/bundle/webpack.config.js
@@ -1,9 +1,11 @@
 const { resolve } = require('path');
+const Visualizer = require('webpack-visualizer-plugin');
 
 module.exports = {
   entry: './lib/index.js',
   output: {
     path: resolve(__dirname, 'dist'),
     filename: 'BotChat-es5.js'
-  }
+  },
+  plugins: [new Visualizer()]
 };
