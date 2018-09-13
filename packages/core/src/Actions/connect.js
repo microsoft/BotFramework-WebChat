2932bed03f6b7a4dc77770203e282caa21640c1f
William Wong <compulim@hotmail.com>
2018-09-13 09:44:13 -0700

    Add comment

--- packages/core/src/Actions/connect.js
+++ packages/core/src/Actions/connect.js
@@ -1,3 +1,7 @@
+// TODO: Mark actions with correct family
+//       E.g. POST_ACTIVITY belongs to DIRECT_LINE
+//       E.g. SEND_MESSAGE belongs to WEB_CHAT
+//       We are trying to curb down the number of DIRECT_LINE/* to make backend easier to rebuild
 const CONNECT = 'DIRECT_LINE/CONNECT';
 const CONNECT_PENDING = `${ CONNECT }_PENDING`;
 const CONNECT_REJECTED = `${ CONNECT }_REJECTED`;
