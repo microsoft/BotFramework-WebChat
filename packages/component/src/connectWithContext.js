a161a81aa00bbf044d864d45f174e6454fa40fe0
William Wong <compulim@hotmail.com>
2018-09-16 04:12:21 -0700

    Add TODO

--- packages/component/src/connectWithContext.js
+++ packages/component/src/connectWithContext.js
@@ -4,6 +4,7 @@ import React from 'react';
 
 import Context from './Context';
 
+// TODO: Consider using "store" props, instead of "storeKey"
 export default function (stateSelector, contextSelector) {
   const createConnected = memoize(storeKey => connect(state => stateSelector(state), null, null, { storeKey }));
 
