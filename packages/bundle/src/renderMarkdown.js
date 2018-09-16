aadc36ee7d593f62129db04510dd165d546daebf
William Wong <compulim@hotmail.com>
2018-09-16 14:56:42 -0700

    Move sanitize to bundle-full

--- packages/bundle/src/renderMarkdown.js
+++ packages/bundle/src/renderMarkdown.js
@@ -1,5 +1,42 @@
 import iterator from 'markdown-it-for-inline';
 import MarkdownIt from 'markdown-it';
+import sanitizeHTML from 'sanitize-html';
+
+const ALLOWED_TAGS = [
+  'a',
+  'b',
+  'blockquote',
+  'br',
+  'caption',
+  'code',
+  'div',
+  'em',
+  'h1',
+  'h2',
+  'h3',
+  'h4',
+  'h5',
+  'h6',
+  'hr',
+  'i',
+  'img',
+  'li',
+  'nl',
+  'ol',
+  'p',
+  'pre',
+  'span',
+  'strike',
+  'strong',
+  'table',
+  'tbody',
+  'td',
+  'tfoot',
+  'th',
+  'thead',
+  'tr',
+  'ul'
+];
 
 const customMarkdownIt = new MarkdownIt({
   breaks: true,
@@ -27,4 +64,8 @@ const customMarkdownIt = new MarkdownIt({
   }
 });
 
-export default customMarkdownIt.render.bind(customMarkdownIt)
+export default function render(markdown) {
+  const html = customMarkdownIt.render(markdown);
+
+  return sanitizeHTML(html, { allowedTags: ALLOWED_TAGS });
+}
--- packages/component/src/Attachment/TextContent.js
+++ packages/component/src/Attachment/TextContent.js
@@ -1,44 +1,7 @@
 import classNames from 'classnames';
 import React from 'react';
-import SanitizedHTML from 'react-sanitized-html';
 
 import Context from '../Context';
-
-const ALLOWED_TAGS = [
-  'a',
-  'b',
-  'blockquote',
-  'br',
-  'caption',
-  'code',
-  'div',
-  'em',
-  'h1',
-  'h2',
-  'h3',
-  'h4',
-  'h5',
-  'h6',
-  'hr',
-  'i',
-  'img',
-  'li',
-  'nl',
-  'ol',
-  'p',
-  'pre',
-  'span',
-  'strike',
-  'strong',
-  'table',
-  'tbody',
-  'td',
-  'tfoot',
-  'th',
-  'thead',
-  'tr',
-  'ul'
-];
 
 export default ({
   contentType = 'text/markdown',
@@ -47,10 +10,9 @@ export default ({
   <Context.Consumer>
     { ({ renderMarkdown, styleSet }) =>
       (contentType === 'text/markdown' && renderMarkdown) ?
-        <SanitizedHTML
-          allowedTags={ ALLOWED_TAGS }
+        <div
           className={ classNames('markdown', styleSet.textContent + '') }
-          html={ renderMarkdown(text || '') }
+          dangerouslySetInnerHTML={{ __html: renderMarkdown(text || '') }}
         />
       :
         (text || '').split('\n').map((line, index) =>
