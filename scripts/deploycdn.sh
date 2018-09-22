d7704f7f5c74c98844761a2dbc1e6117b5eda293
William Wong <compulim@hotmail.com>
2018-09-21 19:00:30 -0700

    Uncomment tag section

--- scripts/deploycdn.sh
+++ scripts/deploycdn.sh
@@ -9,12 +9,12 @@ echo Will publish to CDN at $PACKAGE_NAME/$PACKAGE_VERSION/*
 # Upload to based on version from package.json
 ~/blobxfer upload --local-path $TRAVIS_BUILD_DIR/packages/bundle/dist --remote-path $PACKAGE_NAME/$PACKAGE_VERSION --storage-account $CDN_BLOB_ACCOUNT --storage-account-key $CDN_BLOB_KEY
 
-# # If TRAVIS_TAG is present, it means this is going PRODUCTION
-# if [ -n "$TRAVIS_TAG" ]
-# then
-# # Upload to /latest/
-# ~/blobxfer upload --local-path $TRAVIS_BUILD_DIR/dist --remote-path $PACKAGE_NAME/latest --storage-account $CDN_BLOB_ACCOUNT --storage-account-key $CDN_BLOB_KEY
-# fi
+# If TRAVIS_TAG is present, it means this is going PRODUCTION
+if [ -n "$TRAVIS_TAG" ]
+then
+# Upload to /latest/
+~/blobxfer upload --local-path $TRAVIS_BUILD_DIR/dist --remote-path $PACKAGE_NAME/latest --storage-account $CDN_BLOB_ACCOUNT --storage-account-key $CDN_BLOB_KEY
+fi
 
 # If on "v4" branch, deploy to "v4" tag too
 if [ "$TRAVIS_BRANCH" = "master" ]
