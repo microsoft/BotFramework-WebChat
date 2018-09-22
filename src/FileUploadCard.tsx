daa7d16ac96e2d35783830419e0cb91df20273fe
sebi <gore.sebyx@yahoo.com>
2018-09-23 01:56:36 +0300

    improve package

---
+++ src/FileUploadCard.tsx
@@ -0,0 +1,11 @@
+// import * as React from "react";
+// import Dropzone from 'react-dropzone'
+//
+//
+// export class FileUploadCard extends React.Component<any, any>{
+//     render(){
+//         return(
+//             <Dropzone />
+//         )
+//     }
+// }
