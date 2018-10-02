7067f4f6be4c4fa040d228b75a4329176f850612
William Wong <compulim@hotmail.com>
2018-10-02 14:43:34 -0700

    Add warnings for non-supported browser

--- packages/bundle/src/createBrowserWebSpeechPonyfillFactory.js
+++ packages/bundle/src/createBrowserWebSpeechPonyfillFactory.js
@@ -1,4 +1,12 @@
 export default function () {
+  if (!window.SpeechRecognition && !window.webkitSpeechRecognition) {
+    console.warn('Web Chat: This browser does not support Web Speech API Speech Recognition.');
+  }
+
+  if (!window.speechSynthesis) {
+    console.warn('Web Chat: This browser does not support Web Speech API Speech Synthesis.');
+  }
+
   return () => ({
     SpeechGrammarList: window.SpeechGrammarList || window.webkitSpeechGrammarList,
     SpeechRecognition: window.SpeechRecognition || window.webkitSpeechRecognition,
