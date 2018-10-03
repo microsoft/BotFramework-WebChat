2971482a79bf10781d184fae8af840b128502884
William Wong <compulim@hotmail.com>
2018-10-02 17:43:06 -0700

    Update polyfill

--- packages/bundle/src/index-es5.ts
+++ packages/bundle/src/index-es5.ts
@@ -1,11 +1,18 @@
-export * from './index';
 
 // below are shims for compatibility with old browsers (IE 10 being the main culprit)
-import 'core-js/modules/es6.string.starts-with';
-import 'core-js/modules/es6.array.find';
 import 'core-js/modules/es6.array.find-index';
+import 'core-js/modules/es6.array.find';
+import 'core-js/modules/es6.array.iterator';
+import 'core-js/modules/es6.object.assign';
+import 'core-js/modules/es6.math.sign';
+import 'core-js/modules/es6.promise';
+import 'core-js/modules/es6.string.starts-with';
+import 'core-js/modules/es6.symbol';
+import 'core-js/modules/es7.array.includes';
+import 'whatwg-fetch';
 
-// Polyfill Promise if needed
-if (typeof (window as any).Promise === 'undefined') {
-  (window as any).Promise = require('bluebird');
-}
+// URLSearchParams
+
+console.log('Bundled with ES5 polyfills');
+
+export * from './index';
