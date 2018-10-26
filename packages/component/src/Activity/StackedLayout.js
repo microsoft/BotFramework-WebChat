69a03590d9ea586e33f130ae63aea1572ef83f3c
William Wong <compulim@hotmail.com>
2018-10-26 15:51:17 -0700

    Clean up

--- packages/component/src/Activity/StackedLayout.js
+++ packages/component/src/Activity/StackedLayout.js
@@ -6,7 +6,6 @@ import { Constants } from 'botframework-webchat-core';
 
 import Avatar from './Avatar';
 import Bubble from './Bubble';
-import Context from '../Context';
 import SendStatus from './SendStatus';
 import Timestamp from './Timestamp';
 
