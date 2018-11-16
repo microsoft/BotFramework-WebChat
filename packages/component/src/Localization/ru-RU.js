a08a03a7e3aa3082d27ab13dd236f0d299576d2f
odysseus1973 <aslebedev@gmail.com>
2018-11-16 09:53:32 +0300

    Some additions in Russian localization

--- packages/component/src/Localization/ru-RU.js
+++ packages/component/src/Localization/ru-RU.js
@@ -1,16 +1,45 @@
+function xMinutesAgo(date) {
+  const now = Date.now();
+  const deltaInMs = now - new Date(date).getTime();
+  const deltaInMinutes = Math.floor(deltaInMs / 60000);
+  const deltaInHours = Math.floor(deltaInMs / 3600000);
+
+  if (deltaInMinutes < 1) {
+    return 'Сейчас';
+  } else if (deltaInMinutes === 1) {
+    return 'Минуту назад';
+  } else if (deltaInHours < 1 && deltaInMinutes < 5) {
+    return `${ deltaInMinutes } минуты назад`;
+  } else if (deltaInHours < 1 && deltaInMinutes >= 5) {
+    return `${ deltaInMinutes } минут назад`;
+  } else if (deltaInHours === 1) {
+    return `Час назад`;
+  } else if (deltaInHours < 5) {
+    return `${ deltaInHours } часа назад`;
+  } else if (deltaInHours <= 24) {
+    return `Сегодня`;
+  } else if (deltaInHours <= 48) {
+    return `Вчера`;
+  } else if (deltaInHours <= 72) {
+    return `Позавчера`;
+  } else {
+    return new Intl.DateTimeFormat('ru-RU').format(date);
+  }
+}
+
 export default {
   'Chat': 'Чат',
   'Listening…': 'прослушивание…',
   'retry': 'повторить',
   'Send failed, {retry}': 'не удалось отправить, {retry}',
   'Send': 'Отправить',
-  'Sending': 'отправка',
+  'Sending': 'Отправка',
   // 'Speak': '',
-  // 'Starting…': '',
+  'Starting…': 'Запуск...',
   'Tax': 'Налог',
   'Total': 'Итого',
   'Type your message': 'Введите ваше сообщение',
-  // 'Upload file': '',
-  'VAT': 'VAT'
-  // 'X minutes ago':
+  'Upload file': 'Загрузить файл',
+  'VAT': 'НДС',
+  'X minutes ago': xMinutesAgo
 }
