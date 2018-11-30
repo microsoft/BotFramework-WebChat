5c3316cc5c662ba345e2f6e787fcc3d1eccd6b1f
Corina <corina.aj@gmail.com>
2018-11-29 21:37:26 -0800

    Update SuggestedAction.js
    My original fix didn't go through; sorry I'm new to updating PR's =_=

--- packages/component/src/Styles/StyleSet/SuggestedAction.js
+++ packages/component/src/Styles/StyleSet/SuggestedAction.js
@@ -27,7 +27,6 @@ export default function createSuggestedActionStyle({
       paddingRight: paddingWide,
       borderRadius: suggestedActionBorderRadius,
 
-
       '&:disabled': {
         background: suggestedActionDisabledBackground,
         border: suggestedActionDisabledBorder,
