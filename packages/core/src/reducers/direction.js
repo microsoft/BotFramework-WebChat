41008903daf4cdf7d726da5f280f55cc88f78ee7
Guy Ambar <guy@meetleo.co>
2018-12-04 08:18:18 +0200

    removed directions and default state constants

--- packages/core/src/reducers/direction.js
+++ packages/core/src/reducers/direction.js
@@ -1,9 +1,5 @@
 import { SET_DIRECTION } from '../actions/setDirection';
 
-const DEFAULT_STATE = 'ltr';
-
-const DIRECTIONS = ['ltr', 'rtl'];
-
 export default function (state = DEFAULT_STATE, { payload, type }) {
   switch (type) {
     case SET_DIRECTION:
