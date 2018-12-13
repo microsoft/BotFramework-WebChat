29fa70e5baf02a0089292bf06201def7aa7b5621
Corina <v-corgum@microsoft.com>
2018-12-13 15:32:05 -0800

    Fix Upload button accessibility

--- packages/component/src/SendBox/UploadButton.js
+++ packages/component/src/SendBox/UploadButton.js
@@ -78,7 +78,7 @@ class UploadButton extends React.Component {
     return (
       <div className={ classNames(ROOT_CSS + '', styleSet.uploadButton + '') }>
         <input
-          aria-label={ uploadFileString }
+          aria-hidden="true"
           disabled={ disabled }
           multiple={ true }
           onChange={ this.handleFileChange }
@@ -86,9 +86,10 @@ class UploadButton extends React.Component {
           role="button"
           tabIndex={ -1 }
           type="file"
-        />
+          />
         <IconButton
-          alt={ localize('Upload file', language) }
+          aria-label={ uploadFileString }
+          alt={ uploadFileString }
           disabled={ disabled }
           onClick={ this.handleClick }
         >
