ee92ada39449c141562c79951dd86dfecfaa7774
William Wong <compulim@hotmail.com>
2018-12-14 12:21:58 -0800

    Clean up coding style

--- packages/core/src/sagas/markActivityForSpeakSaga.js
+++ packages/core/src/sagas/markActivityForSpeakSaga.js
@@ -12,17 +12,19 @@ import markActivity from '../actions/markActivity';
 
 export default function* () {
   yield whileConnected(function* (_, userID) {
-    yield whileSpeaking(function* () {
-      yield takeEvery(
-        ({ payload, type }) =>
-          type === INCOMING_ACTIVITY
-          && payload
-          && payload.activity
-          && speakableActivity(payload.activity, userID),
-        function* ({ payload: { activity } }) {
-          yield put(markActivity(activity, 'speak', true));
-        }
-      );
-    });
+    yield whileSpeaking(markActivityForSpeakSaga.bind(null, userID));
   });
+}
+
+function* markActivityForSpeakSaga(userID) {
+  yield takeEvery(
+    ({ payload, type }) =>
+      type === INCOMING_ACTIVITY
+      && payload
+      && payload.activity
+      && speakableActivity(payload.activity, userID),
+    function* () {
+      yield put(markActivity(activity, 'speak', true));
+    }
+  );
 }
