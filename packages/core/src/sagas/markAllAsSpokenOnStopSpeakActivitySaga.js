b2826b0777cd782ea7aadd88b5647d0d3e4d13f7
William Wong <compulim@hotmail.com>
2018-12-18 20:38:54 -0800

    Clean up

--- packages/core/src/sagas/markAllAsSpokenOnStopSpeakActivitySaga.js
+++ packages/core/src/sagas/markAllAsSpokenOnStopSpeakActivitySaga.js
@@ -4,12 +4,12 @@ import {
   takeEvery
 } from 'redux-saga/effects';
 
-import markActivity from '../actions/markActivity';
 import { STOP_SPEAKING_ACTIVITY } from '../actions/stopSpeakingActivity';
+import markActivity from '../actions/markActivity';
 
 export default function* () {
   yield takeEvery(STOP_SPEAKING_ACTIVITY, function* () {
-    const activities = yield select(({ activities }) => activities);
+    const { activities } = yield select();
 
     for (let activity of activities) {
       if (activity.channelData && activity.channelData.speak) {
