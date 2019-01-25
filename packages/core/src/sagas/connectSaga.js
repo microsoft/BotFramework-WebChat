ea8f88f5a0911d974bbbd7c3a9b67493fd290211
William Wong <compulim@hotmail.com>
2019-01-24 20:28:38 -0800

    Rework connect saga

--- packages/core/src/sagas/connectSaga.js
+++ packages/core/src/sagas/connectSaga.js
@@ -10,8 +10,7 @@ import {
 import { decode } from 'jsonwebtoken';
 import random from 'math-random';
 
-import callUntil from './effects/callUntil';
-import forever from './effects/forever';
+import updateConnectionStatus, { UPDATE_CONNECTION_STATUS } from '../actions/updateConnectionStatus';
 
 import createPromiseQueue from '../createPromiseQueue';
 
@@ -26,108 +25,162 @@ import {
 import {
   DISCONNECT,
   DISCONNECT_PENDING,
-  DISCONNECT_REJECTED,
   DISCONNECT_FULFILLED
 } from '../actions/disconnect';
 
 // const UNINITIALIZED = 0;
-// const CONNECTING = 1;
+const CONNECTING = 1;
 const ONLINE = 2;
-// const EXPIRED_TOKEN = 3;
-// const FAILED_TO_CONNECT = 4;
+const EXPIRED_TOKEN = 3;
+const FAILED_TO_CONNECT = 4;
 const ENDED = 5;
 
 function randomUserID() {
   return `r_${ random().toString(36).substr(2, 10) }`;
 }
 
-export default function* () {
-  for (;;) {
-    const { payload: { directLine, userID } } = yield take(CONNECT);
-    const { token } = directLine;
-    const { user: userIDFromToken } = decode(token) || {};
-
-    if (userIDFromToken) {
-      if (userID && userID !== userIDFromToken) {
-        console.warn('Web Chat: user ID is both specified in the Direct Line token and passed in, will use the user ID from the token.');
-      }
-
-      userID = userIDFromToken;
-    } else if (userID) {
-      if (typeof userID !== 'string') {
-        console.warn('Web Chat: user ID must be a string.');
-        userID = randomUserID();
-      } else if (/^dl_/.test(userID)) {
-        console.warn('Web Chat: user ID prefixed with "dl_" is reserved and must be embedded into the Direct Line token to prevent forgery.');
-        userID = randomUserID();
-      }
-    } else {
-      // Only specify "default-user" if not found from token and not passed in
-      userID = randomUserID();
-    }
-
-    const connectTask = yield fork(connectSaga, directLine, userID);
-
-    yield take(DISCONNECT);
-    yield call(disconnectSaga, connectTask, directLine);
-  }
-}
-
-function* connectSaga(directLine, userID) {
-  const meta = { userID };
-
-  yield put({ type: CONNECT_PENDING, meta });
-
+function* observeAndPutConnectionStatusUpdate(directLine) {
   const connectionStatusQueue = createPromiseQueue();
-  const connectionStatusSubscription = directLine.connectionStatus$.subscribe({ next: connectionStatusQueue.push });
-
-  // DirectLineJS start the connection only after the first subscriber for activity$, but not connectionStatus$
-  const activitySubscription = directLine.activity$.subscribe({ next: () => 0 });
+  const connectionStatusSubscription = directLine.connectionStatus$.subscribe({
+    next: connectionStatusQueue.push
+  });
 
   try {
-    try {
-      yield callUntil(connectionStatusQueue.shift, [], connectionStatus => connectionStatus === ONLINE);
-      yield put({ type: CONNECT_FULFILLING, meta, payload: { directLine } });
-      yield put({ type: CONNECT_FULFILLED, meta, payload: { directLine } });
-    } catch (err) {
-      yield put({ type: CONNECT_REJECTED, error: true, meta, payload: err });
-    } finally {
-      if (yield cancelled()) {
-        yield put({ type: CONNECT_REJECTED, error: true, meta, payload: new Error('cancelled') });
-      }
-    }
+    for (;;) {
+      const connectionStatus = yield call(connectionStatusQueue.shift);
 
-    yield forever();
+      console.log(`DirectLineJS connectionStatus updated to: ${ connectionStatus }`);
+
+      yield put(updateConnectionStatus(connectionStatus));
+    }
   } finally {
-    // TODO: [P2] DirectLineJS should kill the connection when we unsubscribe
-    //       But currently in v3, DirectLineJS does not have this functionality
-    //       Thus, we need to call "end()" explicitly
-    directLine.end();
-    activitySubscription.unsubscribe();
+    console.log(`Stop subscribing connectionStatus from DirectLineJS.`);
+
     connectionStatusSubscription.unsubscribe();
   }
 }
 
-function* disconnectSaga(connectTask, directLine) {
-  yield put({ type: DISCONNECT_PENDING });
+function negativeUpdateConnectionStatusAction({ payload, type }) {
+  if (type === UPDATE_CONNECTION_STATUS) {
+    const { connectionStatus } = payload;
 
-  const connectionStatusQueue = createPromiseQueue();
-  const unsubscribe = directLine.connectionStatus$.subscribe({ next: connectionStatusQueue.push });
+    return connectionStatus !== CONNECTING && connectionStatus !== ONLINE;
+  }
+}
 
-  // DirectLineJS should cancel underlying REST/WS when we cancel
-  // the connect task, which subsequently unsubscribe connectionStatus$
-  yield cancel(connectTask);
+function rectifyUserID(directLine, userIDFromAction) {
+  const { token } = directLine;
+  const { user: userIDFromToken } = decode(token) || {};
 
-  try {
-    yield callUntil(connectionStatusQueue.shift, [], connectionStatus => connectionStatus === ENDED);
-    yield put({ type: DISCONNECT_FULFILLED });
-  } catch (err) {
-    yield put({ type: DISCONNECT_REJECTED, error: true, payload: err });
-  } finally {
-    if (yield cancelled()) {
-      yield put({ type: DISCONNECT_REJECTED, error: true, payload: new Error('cancelled') });
+  if (userIDFromToken) {
+    if (userIDFromAction && userIDFromAction !== userIDFromToken) {
+      console.warn('Web Chat: user ID is both specified in the Direct Line token and passed in, will use the user ID from the token.');
     }
 
-    unsubscribe();
+    return userIDFromToken;
+  } else if (userIDFromAction) {
+    if (typeof userIDFromAction !== 'string') {
+      console.warn('Web Chat: user ID must be a string.');
+
+      return randomUserID();
+    } else if (/^dl_/.test(userIDFromAction)) {
+      console.warn('Web Chat: user ID prefixed with "dl_" is reserved and must be embedded into the Direct Line token to prevent forgery.');
+
+      return randomUserID();
+    }
+  } else {
+    return randomUserID();
+  }
+
+  return userIDFromAction;
+}
+
+function* connectSaga(directLine) {
+  // DirectLineJS start the connection only after the first subscriber for activity$, but not connectionStatus$
+  const activitySubscription = directLine.activity$.subscribe(() => 0);
+
+  try {
+    for (;;) {
+      const { payload: { connectionStatus } } = yield take(UPDATE_CONNECTION_STATUS);
+
+      // We will ignore DISCONNECT actions until we connect
+
+      if (connectionStatus === ONLINE) {
+        // TODO: [P2] DirectLineJS should kill the connection when we unsubscribe
+        //       But currently in v3, DirectLineJS does not have this functionality
+        //       Thus, we need to call "end()" explicitly
+
+        return () => {
+          activitySubscription.unsubscribe();
+
+          try {
+            // TODO: [P3] DirectLineJS will throw an error saying "conversation ended", it seems redundant
+            directLine.end();
+          } catch (err) {}
+        };
+      } else if (connectionStatus === ENDED || connectionStatus === EXPIRED_TOKEN || connectionStatus === FAILED_TO_CONNECT) {
+        // If we receive anything negative, we will assume the connection is errored out
+        throw new Error('Failed to connect');
+      }
+    }
+  } finally {
+    if (yield cancelled()) {
+      activitySubscription.unsubscribe();
+
+      throw new Error('Cancelled');
+    }
+  }
+}
+
+export default function* () {
+  for (;;) {
+    console.log('Waiting for CONNECT');
+
+    const { payload: { directLine, userID: userIDFromAction } } = yield take(CONNECT);
+    const userID = rectifyUserID(directLine, userIDFromAction);
+    const updateConnectionStatusTask = yield fork(observeAndPutConnectionStatusUpdate, directLine);
+
+    try {
+      const meta = { userID };
+      let endDirectLine;
+
+      yield put({ type: CONNECT_PENDING, meta });
+
+      try {
+        endDirectLine = yield call(connectSaga, directLine);
+      } catch (err) {
+        yield put({ type: CONNECT_REJECTED, error: true, meta, payload: err });
+
+        continue;
+      }
+
+      // At this point, we established connection to Direct Line.
+      // Any errors from this point, we need to make sure we call endDirectLine() to release resources.
+      try {
+        yield put({ type: CONNECT_FULFILLING, meta, payload: { directLine } });
+        yield put({ type: CONNECT_FULFILLED, meta, payload: { directLine } });
+
+        const terminateAction = yield take([DISCONNECT, negativeUpdateConnectionStatusAction]);
+
+        // Even if the connection is interrupted, we will still emitting DISCONNECT_PENDING.
+        // This will makes handling logic easier. If CONNECT_FULFILLED, we guarantee DISCONNECT_PENDING.
+        yield put({ type: DISCONNECT_PENDING });
+
+        endDirectLine();
+
+        if (terminateAction.type === DISCONNECT) {
+          // For graceful disconnect, we wait until Direct Line say it is ended
+          yield take(negativeUpdateConnectionStatusAction);
+        }
+      } finally {
+        // It is meaningless to continue to use the Direct Line object even disconnect fail.
+        // And we will still unsubscribe to incoming activities.
+        yield put({ type: DISCONNECT_FULFILLED });
+
+        endDirectLine();
+      }
+    } finally {
+      yield cancel(updateConnectionStatusTask);
+    }
   }
 }
