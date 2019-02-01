e3c092d3b5277d79a4f28fd1928eac2ec823fe23
William Wong <compulim@hotmail.com>
2019-01-31 17:03:37 -0800

    Add test

---
+++ __tests__/username.js
@@ -0,0 +1,28 @@
+import { By, Key } from 'selenium-webdriver';
+
+import { imageSnapshotOptions, timeouts } from './constants.json';
+
+import allImagesLoaded from './setup/conditions/allImagesLoaded';
+import botConnected from './setup/conditions/botConnected';
+import minNumActivitiesShown from './setup/conditions/minNumActivitiesShown';
+
+// selenium-webdriver API doc:
+// https://seleniumhq.github.io/selenium/docs/api/javascript/module/selenium-webdriver/index_exports_WebDriver.html
+
+test('send username in activity', async () => {
+  const { driver, pageObjects } = await setupWebDriver();
+
+  await driver.wait(botConnected(), timeouts.directLine);
+
+  const input = await driver.findElement(By.css('input[type="text"]'));
+
+  await input.sendKeys('user name', Key.RETURN);
+  await driver.wait(minNumActivitiesShown(3), 2000);
+
+  // Hide cursor before taking screenshot
+  await pageObjects.hideCursor();
+
+  const base64PNG = await driver.takeScreenshot();
+
+  expect(base64PNG).toMatchImageSnapshot(imageSnapshotOptions);
+}, 60000);
