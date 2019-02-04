6ce338e28159e56b0e1185a6b8118eede9c73c4f
William Wong <compulim@users.noreply.github.com>
2019-02-04 11:31:41 -0800

    Update zh-YUE.js

--- packages/component/src/Localization/zh-YUE.js
+++ packages/component/src/Localization/zh-YUE.js
@@ -32,12 +32,12 @@ function xMinutesAgo(dateStr) {
 }
 
 export default {
-  // FAILED_CONNECTION_NOTIFICATION: '',
+  FAILED_CONNECTION_NOTIFICATION: 'Web Chat 接駁唔倒。',
   // Do not localize {Retry}; it is a placeholder for "Retry". English translation should be, "Send failed. Retry."
   SEND_FAILED_KEY: '傳送唔倒。{Retry}。',
-  // SLOW_CONNECTION_NOTIFICATION: '',
+  SLOW_CONNECTION_NOTIFICATION: 'Web Chat 接駁嘅時間比平時長。',
   'Chat': '傾偈',
-  // 'Download file': '',
+  'Download file': '下載檔案',
   'Microphone off': '閂咪',
   'Microphone on': '開咪',
   'Left': '左',
