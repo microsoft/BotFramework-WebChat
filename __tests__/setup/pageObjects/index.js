e69a490d9e8eea27cf0b80eb79c261a4db691887
William Wong <compulim@hotmail.com>
2019-02-08 19:14:57 -0800

    Add more page objects

--- __tests__/setup/pageObjects/index.js
+++ __tests__/setup/pageObjects/index.js
@@ -1,7 +1,21 @@
+import dispatchAction from './dispatchAction';
 import hideCursor from './hideCursor';
+import pingBot from './pingBot';
+import sendMessageViaSendBox from './sendMessageViaSendBox';
+
+function mapMap(map, mapper) {
+  return Object.keys(map).reduce((final, key) => {
+    final[key] = mapper.call(map, map[key], key);
+
+    return final;
+  }, {});
+}
 
 export default function (driver) {
-  return {
-    hideCursor: hideCursor.bind(null, driver)
-  };
+  return mapMap({
+    dispatchAction,
+    hideCursor,
+    pingBot,
+    sendMessageViaSendBox
+  }, fn => fn.bind(null, driver));
 }
