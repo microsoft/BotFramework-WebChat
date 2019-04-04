8980bab9b6ae71315840a6fff8286d9821bdba3c
Corina <v-corgum@microsoft.com>
2019-04-04 11:48:37 -0700

    Fix order of style options

--- packages/component/src/Styles/defaultStyleSetOptions.js
+++ packages/component/src/Styles/defaultStyleSetOptions.js
@@ -58,10 +58,10 @@ const DEFAULT_OPTIONS = {
   sendBoxButtonColorOnHover: '#333',
   sendBoxHeight: 40,
   sendBoxTextColor: 'Black',
-  sendBoxBorderTop: 'solid 1px #E6E6E6',
+  sendBoxBorderBottom: '',
   sendBoxBorderLeft: '',
   sendBoxBorderRight: '',
-  sendBoxBorderBottom: '',
+  sendBoxBorderTop: 'solid 1px #E6E6E6',
 
   // Visually show spoken text
   showSpokenText: false,
