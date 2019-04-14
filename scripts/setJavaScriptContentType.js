64117d0c06b10bf48f4d2a6c2ca161c12f81116e
William Wong <compulim@hotmail.com>
2019-04-13 17:18:42 -0700

    Add set content type script

---
+++ scripts/setJavaScriptContentType.js
@@ -0,0 +1,57 @@
+const { extname } = require('path');
+
+const {
+  Aborter,
+  BlobURL,
+  ContainerURL,
+  ServiceURL,
+  SharedKeyCredential,
+  StorageURL
+} = require('@azure/storage-blob');
+
+const BLOB_DELIMITER = '/';
+const BLOB_OPERATION_TIMEOUT = 15000;
+const TARGET_EXTNAME = '.js';
+const TARGET_CONTENT_TYPE = 'text/javascript; charset=utf-8';
+
+async function main(accountName, accountKey, container, prefix) {
+  const containerURL = ContainerURL.fromServiceURL(
+    new ServiceURL(
+      `https://${ accountName }.blob.core.windows.net`,
+      StorageURL.newPipeline(new SharedKeyCredential(accountName, accountKey))
+    ),
+    container
+  );
+
+  const { segment } = await containerURL.listBlobHierarchySegment(
+    Aborter.timeout(BLOB_OPERATION_TIMEOUT),
+    BLOB_DELIMITER,
+    null,
+    { prefix }
+  );
+
+  console.log([
+    `Found ${ segment.blobItems.length } blob in container "${ container }" with prefix "${ prefix || '' }"`,
+    ...segment.blobItems.map(({ name, properties: { contentLength } }) => `  ${ name } (${ contentLength } bytes)`),
+    ''
+  ].join('\n'));
+
+  await Promise.all(
+    segment.blobItems
+      .filter(({ name }) => extname(name) === TARGET_EXTNAME)
+      .map(async ({ name }) => {
+        console.log(`Setting content type for blob "${ name }"`);
+
+        const blobURL = BlobURL.fromContainerURL(containerURL, name);
+
+        await blobURL.setHTTPHeaders(
+          Aborter.timeout(BLOB_OPERATION_TIMEOUT),
+          {
+            blobContentType: TARGET_CONTENT_TYPE
+          }
+        )
+      })
+  );
+}
+
+main(...process.argv.slice(2)).catch(err => console.error(err));
