22c95b3cd0a3d18d1af12b240fa26f301f7d6122
William Wong <compulim@hotmail.com>
2019-04-22 17:12:21 -0700

    Add test

---
+++ packages/embed/src/setups/createElement.spec.js
@@ -0,0 +1,27 @@
+import createElement from './createElement';
+
+test('Create an element with attribute', () => {
+  const script = createElement('script', { src: 'https://example.com/test.js' });
+
+  expect(script).toHaveProperty('outerHTML', '<script src="https://example.com/test.js"></script>');
+});
+
+test('Create an element with two children', () => {
+  const ul = createElement('ul', {}, createElement('li', {}, 'Buy eggs'), createElement('li', {}, 'Buy milk'))
+
+  expect(ul).toHaveProperty('outerHTML', '<ul><li>Buy eggs</li><li>Buy milk</li></ul>');
+});
+
+test('Create an element with click event', () => {
+  let clickCalled = jest.fn();
+  const button = createElement('button', { onClick: clickCalled });
+
+  button.click();
+  expect(clickCalled).toHaveBeenCalledTimes(1);
+});
+
+test('Create an element with stylesheet', () => {
+  const image = createElement('img', { style: { backgroundColor: 'Black' } });
+
+  expect(image).toHaveProperty('outerHTML', '<img style="background-color: Black;">');
+});
