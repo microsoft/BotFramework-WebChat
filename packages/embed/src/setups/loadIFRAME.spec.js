eca6b84f5701603a3f2e6c034e2d03aeba202deb
William Wong <compulim@hotmail.com>
2019-04-22 17:26:23 -0700

    Add test

---
+++ packages/embed/src/setups/loadIFRAME.spec.js
@@ -0,0 +1,12 @@
+import loadIFRAME from './loadIFRAME';
+
+test('Load a page', async () => {
+  const loadTask = loadIFRAME('index.html');
+  const target = document.body.querySelector('iframe');
+
+  target.dispatchEvent(new Event('load'));
+
+  await loadTask;
+
+  expect(document.body.firstChild).toHaveProperty('outerHTML', '<div style="height: 100%; overflow: hidden;"><iframe src="index.html" style="border: 0px; height: 100%; width: 100%;"></iframe></div>');
+});
