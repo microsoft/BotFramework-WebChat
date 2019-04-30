c5a672bfeae11885581eda543f38248f7f04731b
Bruce Haley <v-bruhal@microsoft.com>
2019-04-30 10:49:40 -0700

    More file path tweaks

--- scripts/PushBitsToGithub.ps1
+++ scripts/PushBitsToGithub.ps1
@@ -62,7 +62,7 @@ Write-Host "git push origin $branchName"
 git push origin $branchName
 
 if ($LASTEXITCODE -eq 0) {
+    Add-Content -Path "./PushLocation.md" -Value "Bits pushed to GitHub here: [https://github.com/Microsoft/botbuilder-webchat/tree/$branchName/$repoRootPath](https://github.com/Microsoft/botbuilder-webchat/tree/$branchName/$repoRootPath)"
     Write-Host 'Writing Push Location section to the build summary page'
-    Add-Content -Path "$newFilesPath\PushLocation.md" -Value "Bits pushed to GitHub here: [https://github.com/Microsoft/botbuilder-webchat/tree/$branchName/$repoRootPath](https://github.com/Microsoft/botbuilder-webchat/tree/$branchName/$repoRootPath)"
-    Write-Host "##vso[task.addattachment type=Distributedtask.Core.Summary;name=Push_Location;] $newFilesPath\PushLocation.md"
+    Write-Host "##vso[task.addattachment type=Distributedtask.Core.Summary;name=Push_Location;] ./PushLocation.md"
 }
