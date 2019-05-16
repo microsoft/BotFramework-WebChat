8d95907ef224828d980ae122c3cedc1e2cc132f0
TJ Durnford <tjdford@gmail.com>
2019-05-16 15:13:28 -0700

    Updated markdown test

--- packages/bundle/src/__tests__/renderMarkdown.spec.js
+++ packages/bundle/src/__tests__/renderMarkdown.spec.js
@@ -6,6 +6,6 @@ describe('renderMarkdown', () => {
   });
 
   it('should properly render newline characters to markdown', () => {
-    expect(renderMarkdown('Same line.\nSame line.  \n2nd line.\n\r3rd line.')).toBe("<p>Same line.\nSame line.<br />\n2nd line.</p>\n<p>3rd line.</p>\n");
+    expect(renderMarkdown('Same line.\nSame line.  \n2nd line.\n\r3rd line.')).toBe('<p>Same line.\nSame line.<br />\n2nd line.</p>\n<p>3rd line.</p>\n');
   });
 });
