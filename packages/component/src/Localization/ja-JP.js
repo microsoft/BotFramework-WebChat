070d8132788dee89957890f98b92c1d224f4ee90
William Wong <compulim@users.noreply.github.com>
2019-05-23 13:20:10 -0700

    Fix eslint for recent `pt-PT` commit (#2020)
    * Sort and remove trailing commas
    
    * Disable magic-number and fix then-else rule
    
    * Update entry

--- packages/component/src/Localization/ja-JP.js
+++ packages/component/src/Localization/ja-JP.js
@@ -33,7 +33,7 @@ function xMinutesAgo(dateStr) {
     hour12: false,
     minute: '2-digit',
     month: '2-digit',
-    year: 'numeric',
+    year: 'numeric'
   });
 }
 
--- packages/component/src/Localization/ru-RU.js
+++ packages/component/src/Localization/ru-RU.js
@@ -38,11 +38,11 @@ function xMinutesAgo(dateStr) {
   return date.toLocaleString(
     'ru-RU',
     {
-      year: 'numeric',
-      month: '2-digit',
       day: '2-digit',
       hour: '2-digit',
-      minute: '2-digit'
+      minute: '2-digit',
+      month: '2-digit',
+      year: 'numeric'
     }
   );
 }
