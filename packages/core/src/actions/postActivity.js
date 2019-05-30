467410c4dae0d38d27ee3b0b27d033ad88e925b0
Corina <14900841+corinagum@users.noreply.github.com>
2019-05-30 14:54:28 -0700

    Update packages/core/src/actions/postActivity.js
    Co-Authored-By: William Wong <compulim@users.noreply.github.com>

--- packages/core/src/actions/postActivity.js
+++ packages/core/src/actions/postActivity.js
@@ -5,7 +5,7 @@ export const POST_ACTIVITY_FULFILLED = `${POST_ACTIVITY}_${FULFILLED}`;
 export const POST_ACTIVITY_PENDING = `${POST_ACTIVITY}_${PENDING}`;
 export const POST_ACTIVITY_REJECTED = `${POST_ACTIVITY}_${REJECTED}`;
 
-export default function(activity, method = 'keyboard') {
+export default function postActivity(activity, method = 'keyboard') {
   return {
     type: POST_ACTIVITY,
     meta: { method },
