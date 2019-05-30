3830db7f8227835969c0d2304e81df856aeecb20
Corina <14900841+corinagum@users.noreply.github.com>
2019-05-30 14:55:25 -0700

    Update packages/core/src/actions/setDictateState.js
    Co-Authored-By: William Wong <compulim@users.noreply.github.com>

--- packages/core/src/actions/setDictateState.js
+++ packages/core/src/actions/setDictateState.js
@@ -1,6 +1,6 @@
 const SET_DICTATE_STATE = 'WEB_CHAT/SET_DICTATE_STATE';
 
-export default function(dictateState) {
+export default function setDictateState(dictateState) {
   return {
     type: SET_DICTATE_STATE,
     payload: { dictateState }
