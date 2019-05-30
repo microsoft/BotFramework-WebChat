8db7bfa1469d541447bf81364a671ee657436c4a
Corina <14900841+corinagum@users.noreply.github.com>
2019-05-30 14:55:44 -0700

    Update packages/core/src/actions/setSendBox.js
    Co-Authored-By: William Wong <compulim@users.noreply.github.com>

--- packages/core/src/actions/setSendBox.js
+++ packages/core/src/actions/setSendBox.js
@@ -1,6 +1,6 @@
 const SET_SEND_BOX = 'WEB_CHAT/SET_SEND_BOX';
 
-export default function(text) {
+export default function setSendBox(text) {
   return {
     type: SET_SEND_BOX,
     payload: { text }
