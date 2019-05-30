0d7c1b3504be75106fe25e40a2bfbc4562cb6c6c
Corina <14900841+corinagum@users.noreply.github.com>
2019-05-30 14:48:27 -0700

    Update TypingActivity.js
    Co-Authored-By: William Wong <compulim@users.noreply.github.com>

--- packages/component/src/Styles/StyleSet/TypingActivity.js
+++ packages/component/src/Styles/StyleSet/TypingActivity.js
@@ -1,4 +1,4 @@
-export default function({ avatarSize }) {
+export default function createTypingActivityStyle({ avatarSize }) {
   return {
     alignItems: 'center',
     // TODO: [P2] We should not set "display" in styleSet, this will allow the user to break the layout for no good reasons.
