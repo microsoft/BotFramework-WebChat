be47b32da6d5feed25c38ddc776db1baa3d6cef2
William Wong <compulim@hotmail.com>
2019-06-17 14:06:07 -0700

    Update SSH again

--- samples/19.a.single-sign-on-for-enterprise-apps/init.sh
+++ samples/19.a.single-sign-on-for-enterprise-apps/init.sh
@@ -2,7 +2,8 @@
 
 # Start sshd for debugging thru Azure Web App
 sed -i "s/SSH_PORT/$SSH_PORT/g" /etc/ssh/sshd_config
-/usr/sbin/sshd
+service ssh start
+# /usr/sbin/sshd
 
 # Start both bot server and web server
 ./node_modules/.bin/concurrently --kill-others --names "bot,web" "node bot" "node web"
