7dfe34f79fc5a6a55decb76466be2460c197efe8
TJ Durnford <tjdford@gmail.com>
2019-06-18 09:24:07 -0700

    Fixed markdown renderer in Playground (#2073)
    * Fixed markdown renderer in Playground
    
    * Updated CHANGELOG.md
    
    * Fixed markdown renderer in Playground
    
    * Updated CHANGELOG.md
    
    * Modified renderMarkdown function

--- packages/bundle/src/__tests__/renderMarkdown.spec.js
+++ packages/bundle/src/__tests__/renderMarkdown.spec.js
@@ -4,60 +4,48 @@ import renderMarkdown from '../renderMarkdown';
 
 describe('renderMarkdown', () => {
   it('should render markdown', () => {
-    const styleSet = {
-      options: {
-        markdownRespectCRLF: true
-      }
-    };
-    expect(renderMarkdown('**Hello!**', styleSet)).not.toBeFalsy();
+    const options = { markdownRespectCRLF: true };
+    expect(renderMarkdown('**Hello!**', options)).not.toBeFalsy();
   });
 
   it('should properly render newline characters to markdown', () => {
-    const styleSet = {
-      options: {
-        markdownRespectCRLF: true
-      }
-    };
-    expect(renderMarkdown('Same line.\nSame line.  \n2nd line.', styleSet)).toBe(
+    const options = { markdownRespectCRLF: true };
+    expect(renderMarkdown('Same line.\nSame line.  \n2nd line.', options)).toBe(
       '<p>Same line.\nSame line.<br />\n2nd line.</p>\n'
     );
   });
 
   it('should respect CRFL', () => {
-    const styleSet = {
-      options: {
-        markdownRespectCRLF: true
-      }
-    };
-    expect(renderMarkdown('Same Line.\n\rSame Line.\r\n2nd line.', styleSet)).toBe(
+    const options = { markdownRespectCRLF: true };
+    expect(renderMarkdown('Same Line.\n\rSame Line.\r\n2nd line.', options)).toBe(
       '<p>Same Line.\nSame Line.</p>\n<p>2nd line.</p>\n'
     );
   });
 
   it('should respect LFCR', () => {
-    const styleSet = { options: { markdownRespectCRLF: false } };
-    expect(renderMarkdown('Same Line.\r\nSame Line.\n\r2nd line.', styleSet)).toBe(
+    const options = { markdownRespectCRLF: false };
+    expect(renderMarkdown('Same Line.\r\nSame Line.\n\r2nd line.', options)).toBe(
       '<p>Same Line.\nSame Line.</p>\n<p>2nd line.</p>\n'
     );
   });
 
   it('should render bold text', () => {
-    const styleSet = { options: { markdownRespectCRLF: true } };
-    expect(renderMarkdown('**Message with Markdown**\r\nShould see bold text.', styleSet)).toBe(
+    const options = { markdownRespectCRLF: true };
+    expect(renderMarkdown('**Message with Markdown**\r\nShould see bold text.', options)).toBe(
       '<p><strong>Message with Markdown</strong></p>\n<p>Should see bold text.</p>\n'
     );
   });
 
   it('should render code correctly', () => {
-    const styleSet = { options: { markdownRespectCRLF: true } };
-    expect(renderMarkdown(`\`\`\`\n${JSON.stringify({ hello: 'World!' }, null, 2)}\n\`\`\``, styleSet)).toBe(
+    const options = { markdownRespectCRLF: true };
+    expect(renderMarkdown(`\`\`\`\n${JSON.stringify({ hello: 'World!' }, null, 2)}\n\`\`\``, options)).toBe(
       '<pre><code>{\n  "hello": "World!"\n}\n</code></pre>\n'
     );
   });
 
   it('should render sip protocol links correctly', () => {
-    const styleSet = { options: { markdownRespectCRLF: true } };
-    expect(renderMarkdown(`[example@test.com](sip:example@test.com)`, styleSet)).toBe(
+    const options = { markdownRespectCRLF: true };
+    expect(renderMarkdown(`[example@test.com](sip:example@test.com)`, options)).toBe(
       '<p><a href="sip:example@test.com" target="_blank">example@test.com</a></p>\n'
     );
   });
