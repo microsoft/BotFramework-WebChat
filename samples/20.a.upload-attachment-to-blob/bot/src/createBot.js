303222beba048ebb1a77b431de86b5165c25a7d0
William Wong <compulim@hotmail.com>
2019-06-25 14:59:11 -0700

    Initial commit

---
+++ samples/20.a.upload-attachment-to-blob/bot/src/createBot.js
@@ -0,0 +1,90 @@
+const { ActivityHandler, TurnContext } = require("botbuilder");
+const bytes = require("bytes");
+
+const createBotAdapter = require("./createBotAdapter");
+
+const {
+  Aborter,
+  BlockBlobURL,
+  SharedKeyCredential,
+  StorageURL
+} = require("@azure/storage-blob");
+
+const { AZURE_STORAGE_ACCOUNT_KEY, AZURE_STORAGE_ACCOUNT_NAME } = process.env;
+
+module.exports = () => {
+  const bot = new ActivityHandler();
+
+  // Handler for "event" activity
+  bot.onEvent(async (context, next) => {
+    const {
+      activity: { name, value }
+    } = context;
+
+    if (name === "upload") {
+      // For async operations that are outside of BotBuilder, we should use proactive messaging.
+      const reference = TurnContext.getConversationReference(context.activity);
+
+      await context.sendActivity({ type: "typing" });
+
+      const { files } = value;
+      const pipeline = StorageURL.newPipeline(
+        new SharedKeyCredential(
+          AZURE_STORAGE_ACCOUNT_NAME,
+          AZURE_STORAGE_ACCOUNT_KEY
+        )
+      );
+
+      const properties = await Promise.all(
+        files.map(file => {
+          const blockBlobURL = new BlockBlobURL(file, pipeline);
+
+          return blockBlobURL.getProperties(Aborter.timeout(5000));
+        })
+      );
+
+      const adapter = createBotAdapter();
+
+      await adapter.continueConversation(reference, async context => {
+        await context.sendActivity({
+          attachmentLayout: "carousel",
+          attachments: properties.map(
+            ({ contentLength, metadata: { name } }, index) => ({
+              contentType: "application/vnd.microsoft.card.thumbnail",
+              content: {
+                buttons: [
+                  {
+                    title: "Get blob URL",
+                    type: "openurl",
+                    value: files[index]
+                  }
+                ],
+                images: [
+                  {
+                    alt: "",
+                    url: "/document.png"
+                  }
+                ],
+                subtitle: bytes(contentLength, { unitSeparator: " " }),
+                title: name
+              }
+            })
+          ),
+          text:
+            "Here is your upload summary.\n\nWe will not redistribute your uploads and will remove it tomorrow.",
+          type: "message"
+        });
+      });
+    } else {
+      await next();
+    }
+  });
+
+  // Handler for "message" activity
+  bot.onMessage(async (context, next) => {
+    await context.sendActivity("Please upload one or more files.");
+    await next();
+  });
+
+  return bot;
+};
---
+++ samples/20.a.upload-attachment-to-blob/rest-api/src/routes/azureStorage/uploadSASToken.js
@@ -0,0 +1,56 @@
+const uuidV4 = require("uuid/v4");
+const {
+  BlobSASPermissions,
+  generateBlobSASQueryParameters,
+  SharedKeyCredential
+} = require("@azure/storage-blob");
+
+const {
+  AZURE_STORAGE_ACCOUNT_KEY,
+  AZURE_STORAGE_ACCOUNT_NAME,
+  AZURE_STORAGE_CONTAINER_NAME
+} = process.env;
+
+const FIFTEEN_MINUTES = 15 * 60 * 1000;
+
+function pad(value, count = 2, delimiter = "0") {
+  value += "";
+  count -= value.length;
+
+  return new Array(Math.max(0, count)).fill(delimiter).join("") + value;
+}
+
+module.exports = (req, res) => {
+  // TODO: Verify if the HTTP request is sent from a valid client
+  // TODO: Set up storage lifecycle management, https://docs.microsoft.com/en-us/azure/storage/blobs/storage-lifecycle-management-concepts
+
+  const now = new Date();
+  const blobName = [
+    now.getUTCFullYear(),
+    pad(now.getUTCMonth() + 1),
+    pad(now.getUTCDate()),
+    pad(now.getUTCHours()),
+    uuidV4()
+  ].join("/");
+  const permissions = new BlobSASPermissions();
+
+  permissions.create = true;
+
+  const sasQuery = generateBlobSASQueryParameters(
+    {
+      blobName,
+      containerName: AZURE_STORAGE_CONTAINER_NAME,
+      expiryTime: new Date(Date.now() + FIFTEEN_MINUTES),
+      permissions: permissions.toString()
+    },
+    new SharedKeyCredential(
+      AZURE_STORAGE_ACCOUNT_NAME,
+      AZURE_STORAGE_ACCOUNT_KEY
+    )
+  );
+
+  res.send({
+    sasQuery: sasQuery.toString(),
+    url: `https://${AZURE_STORAGE_ACCOUNT_NAME}.blob.core.windows.net/${AZURE_STORAGE_CONTAINER_NAME}/${blobName}`
+  });
+};
