73184217e03aa705ba0389d9a34ac8c4683e24c7
William Wong <compulim@hotmail.com>
2019-06-25 17:07:34 -0700

    Rename

--- samples/20.a.upload-attachment-to-blob/init.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Start sshd for debugging thru Azure Web App
-sed -i "s/SSH_PORT/$SSH_PORT/g" /etc/ssh/sshd_config
-service ssh start
-# /usr/sbin/sshd
-
-# Start both bot server and web server
-./node_modules/.bin/concurrently --kill-others --names "bot,web" "node bot" "node web"
