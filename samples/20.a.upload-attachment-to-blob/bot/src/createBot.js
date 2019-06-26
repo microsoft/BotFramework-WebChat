332c733a6e0557ec3a92a946fc08f4ec19217138
William Wong <compulim@hotmail.com>
2019-06-25 17:00:20 -0700

    Clean up

--- samples/20.a.upload-attachment-to-blob/bot/src/createBot.js
+++ samples/20.a.upload-attachment-to-blob/bot/src/createBot.js
@@ -15,16 +15,18 @@ const { AZURE_STORAGE_ACCOUNT_KEY, AZURE_STORAGE_ACCOUNT_NAME } = process.env;
 module.exports = () => {
   const bot = new ActivityHandler();
 
-  // Handler for "event" activity
+  // Handler for "event" activity.
   bot.onEvent(async (context, next) => {
     const {
       activity: { name, value }
     } = context;
 
+    // Intercepts all "upload" event activities.
     if (name === "upload") {
       // For async operations that are outside of BotBuilder, we should use proactive messaging.
       const reference = TurnContext.getConversationReference(context.activity);
 
+      // The file upload validation could take more than a second, we should send a typing indicator.
       await context.sendActivity({ type: "typing" });
 
       const { files } = value;
@@ -37,7 +39,12 @@ module.exports = () => {
 
       const properties = await Promise.all(
         files.map(file => {
+          // You should verify the URL if it is from the blob account and container that this service owned.
+
           const blockBlobURL = new BlockBlobURL(file, pipeline);
+
+          // After the bot receive the URL, it should validate its content and associate it with user ID.
+          // If the content should be kept for longer than a day, it should copy it to another blob container because we set up a rule to clean the storage daily.
 
           return blockBlobURL.getProperties(Aborter.timeout(5000));
         })
@@ -45,6 +52,7 @@ module.exports = () => {
 
       const adapter = createBotAdapter();
 
+      // Resumes the conversation when everything is ready.
       await adapter.continueConversation(reference, async context => {
         await context.sendActivity({
           attachmentLayout: "carousel",
@@ -54,6 +62,8 @@ module.exports = () => {
               content: {
                 buttons: [
                   {
+                    // Although we send the blob URL back to the user, since it does not contains SAS, it is not actually downloadable.
+                    // We put the blob URL here for completeness only. Because nevertheless, the user already obtained that URL in order to upload.
                     title: "Get blob URL",
                     type: "openurl",
                     value: files[index]
--- samples/20.a.upload-attachment-to-blob/bot/src/getConversationState.js
+++
@@ -1,6 +0,0 @@
-const { ConversationState, MemoryStorage } = require("botbuilder");
-
-const storage = new MemoryStorage();
-const conversationState = new ConversationState(storage);
-
-module.exports = () => conversationState;
