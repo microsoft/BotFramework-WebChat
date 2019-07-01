3b0609028877fb23015153b0fdaa1a0a04b16ccd
TJ Durnford <tjdford@gmail.com>
2019-07-01 10:55:28 -0700

    Updated test description

--- packages/bundle/src/__tests__/createDirectLine.spec.js
+++ packages/bundle/src/__tests__/createDirectLine.spec.js
@@ -13,17 +13,17 @@ import * as WebChatMinimal from '../index-minimal';
 import * as WebChatES5 from '../index-es5';
 
 describe('Web Chat createDirectLine', () => {
-  it('Full', () => {
+  it('for full bundle', () => {
     const directLine = WebChatFull.createDirectLine({});
     expect(directLine.botAgent).toBe(`WebChat/${WebChatFull.version} (Full)`);
   });
 
-  it('Minimal', () => {
+  it('for minimal bundle', () => {
     const directLine = WebChatMinimal.createDirectLine({});
     expect(directLine.botAgent).toBe(`WebChat/${WebChatMinimal.version} (Minimal)`);
   });
 
-  it('ES5', () => {
+  it('for ES5 bundle', () => {
     const directLine = WebChatES5.createDirectLine({});
     expect(directLine.botAgent).toBe(`WebChat/${WebChatES5.version} (ES5)`);
   });
