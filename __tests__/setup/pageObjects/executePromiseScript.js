d5b72e25ba22e6896cc86c6051b8ff783e3356d1
William Wong <compulim@hotmail.com>
2019-07-08 12:42:31 -0700

    Add comments

--- __tests__/setup/pageObjects/executePromiseScript.js
+++ __tests__/setup/pageObjects/executePromiseScript.js
@@ -1,3 +1,7 @@
+// executeAsyncScript is not running a Promise function and is not able to deal with errors.
+// https://seleniumhq.github.io/selenium/docs/api/javascript/module/selenium-webdriver/index_exports_WebDriver.html#executeAsyncScript
+// This function will use executeAsyncScript to run a Promise function in an async fashion.
+
 export default async function executePromiseScript(driver, fn, ...args) {
   const { error, result } = await driver.executeAsyncScript(
     (fn, args, callback) => {
