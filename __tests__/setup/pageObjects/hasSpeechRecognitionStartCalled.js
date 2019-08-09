255ee10646916ca82a5aa077b832f7af5e2e141c
William Wong <compulim@hotmail.com>
2019-08-09 10:26:08 -0700

    Add comment

--- __tests__/setup/pageObjects/hasSpeechRecognitionStartCalled.js
+++ __tests__/setup/pageObjects/hasSpeechRecognitionStartCalled.js
@@ -1,3 +1,5 @@
+// Checks if Web Chat has called "speechRecognition.start()" and pending for a series of speech events.
+
 export default async function hasSpeechRecognitionStartCalled(driver) {
   return await driver.executeScript(() => window.WebSpeechMock.hasSpeechRecognitionStartCalled());
 }
