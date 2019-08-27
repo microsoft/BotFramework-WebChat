cc894499e6ba04162a35c19e10904913fe43fe8c
William Wong <compulim@hotmail.com>
2019-08-27 03:17:25 -0700

    Improve performance

--- packages/component/src/SendBox/TextBox.js
+++ packages/component/src/SendBox/TextBox.js
@@ -1,7 +1,7 @@
 import { css } from 'glamor';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
-import React, { useCallback } from 'react';
+import React, { useCallback, useMemo } from 'react';
 
 import { Context as TypeFocusSinkContext } from '../Utils/TypeFocusSink';
 import { useLocalize } from '../Localization/Localize';
@@ -64,34 +64,43 @@ const connectSendTextBox = (...selectors) => {
 const useSendTextBox = () =>
   useWebChat(({ disabled, focusSendBox, scrollToEnd, sendBoxValue, setSendBox, stopDictate, submitSendBox }) => ({
     disabled,
-    onChange: ({ target: { value } }) => {
-      setSendBox(value);
-      stopDictate();
-    },
-    onKeyPress: event => {
-      const { key, shiftKey } = event;
+    onChange: useCallback(
+      ({ target: { value } }) => {
+        setSendBox(value);
+        stopDictate();
+      },
+      [setSendBox, stopDictate]
+    ),
+    onKeyPress: useCallback(
+      event => {
+        const { key, shiftKey } = event;
 
-      if (key === 'Enter' && !shiftKey) {
+        if (key === 'Enter' && !shiftKey) {
+          event.preventDefault();
+
+          if (sendBoxValue) {
+            scrollToEnd();
+            submitSendBox();
+            focusSendBox();
+          }
+        }
+      },
+      [focusSendBox, scrollToEnd, submitSendBox]
+    ),
+    onSubmit: useCallback(
+      event => {
         event.preventDefault();
+
+        // Consider clearing the send box only after we received POST_ACTIVITY_PENDING
+        // E.g. if the connection is bad, sending the message essentially do nothing but just clearing the send box
 
         if (sendBoxValue) {
           scrollToEnd();
           submitSendBox();
-          focusSendBox();
         }
-      }
-    },
-    onSubmit: event => {
-      event.preventDefault();
-
-      // Consider clearing the send box only after we received POST_ACTIVITY_PENDING
-      // E.g. if the connection is bad, sending the message essentially do nothing but just clearing the send box
-
-      if (sendBoxValue) {
-        scrollToEnd();
-        submitSendBox();
-      }
-    },
+      },
+      [scrollToEnd, sendBoxValue, submitSendBox]
+    ),
     value: sendBoxValue
   }));
 
@@ -103,45 +112,53 @@ const TextBox = ({ className }) => {
     options: { sendBoxTextWrap }
   } = styleSet;
 
-  return (
-    <form
-      className={classNames(ROOT_CSS + '', styleSet.sendBoxTextArea + '', styleSet.sendBoxTextBox + '', className + '')}
-      onSubmit={onSubmit}
-    >
-      {
-        <TypeFocusSinkContext.Consumer>
-          {({ sendFocusRef }) =>
-            !sendBoxTextWrap ? (
-              <input
-                aria-label={typeYourMessageString}
-                data-id="webchat-sendbox-input"
-                disabled={disabled}
-                onChange={onChange}
-                placeholder={typeYourMessageString}
-                ref={sendFocusRef}
-                type="text"
-                value={value}
-              />
-            ) : (
-              <div>
-                <textarea
+  return useMemo(
+    () => (
+      <form
+        className={classNames(
+          ROOT_CSS + '',
+          styleSet.sendBoxTextArea + '',
+          styleSet.sendBoxTextBox + '',
+          className + ''
+        )}
+        onSubmit={onSubmit}
+      >
+        {
+          <TypeFocusSinkContext.Consumer>
+            {({ sendFocusRef }) =>
+              !sendBoxTextWrap ? (
+                <input
                   aria-label={typeYourMessageString}
                   data-id="webchat-sendbox-input"
                   disabled={disabled}
                   onChange={onChange}
-                  onKeyPress={onKeyPress}
                   placeholder={typeYourMessageString}
                   ref={sendFocusRef}
-                  rows="1"
+                  type="text"
                   value={value}
                 />
-                <div>{value + '\n'}</div>
-              </div>
-            )
-          }
-        </TypeFocusSinkContext.Consumer>
-      }
-    </form>
+              ) : (
+                <div>
+                  <textarea
+                    aria-label={typeYourMessageString}
+                    data-id="webchat-sendbox-input"
+                    disabled={disabled}
+                    onChange={onChange}
+                    onKeyPress={onKeyPress}
+                    placeholder={typeYourMessageString}
+                    ref={sendFocusRef}
+                    rows="1"
+                    value={value}
+                  />
+                  <div>{value + '\n'}</div>
+                </div>
+              )
+            }
+          </TypeFocusSinkContext.Consumer>
+        }
+      </form>
+    ),
+    [disabled, onChange, onKeyPress, onSubmit, styleSet, typeYourMessageString, value]
   );
 };
 
