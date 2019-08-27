6cfc4e5b579e6d16790ffe14f0240bd47811ce0d
Kevin William Jasker <kevinjasker@gmail.com>
2019-08-27 15:20:03 -0700

    Only show first message in FloatinIcon preview

--- src/FloatingIcon.tsx
+++ src/FloatingIcon.tsx
@@ -13,8 +13,7 @@ export interface Node {
 
 interface FloatingIconProps {
     visible?: boolean;
-    format?: FormatState;
-    activities?: Activity[];
+    activity?: Activity & { text: string };
     clicked?: () => void;
 }
 
@@ -27,23 +26,16 @@ class FloatingIconView extends React.Component<FloatingIconProps> {
     }
 
     render() {
-        const { visible, activities, format } = this.props;
-
-        const filtered = filteredActivities(
-            activities,
-            format.strings.pingMessage
-        );
-
-        const lastActivity: any = filtered.length > 0 ? filtered[filtered.length - 1] : null;
+        const { visible, activity } = this.props;
 
         return (
             <div
                 className="wc-floating-wrap"
                 onClick={() => this.props.clicked() }
             >
-                {lastActivity !== null && lastActivity.text !== '' && (
-                    <span className={`wc-floating-message ${visible && lastActivity != null ? 'visible' : '' }`}>
-                        {lastActivity.text}
+                {activity && activity.text !== '' && (
+                    <span className={`wc-floating-message ${visible && activity != null ? 'visible' : '' }`}>
+                        {activity.text}
                     </span>
                 )}
 
@@ -58,15 +50,14 @@ class FloatingIconView extends React.Component<FloatingIconProps> {
 export const FloatingIcon = connect(
     (state: ChatState) => {
         return {
-            // passed down to MessagePaneView
-            format: state.format,
-            activities: state.history ? state.history.activities : []
+            activities: state.history ?
+              state.history.activities.filter((activity: Activity & { text: string }) => activity.text !== state.format.strings.pingMessage) :
+              null
         };
     }, {
 
     }, (stateProps: any, dispatchProps: any, ownProps: any): FloatingIconProps => ({
-        format: stateProps.format,
-        activities: stateProps.activities,
+        activity: stateProps.activities ? stateProps.activities[0] : null,
         visible: ownProps.visible,
         clicked: ownProps.clicked
     })
