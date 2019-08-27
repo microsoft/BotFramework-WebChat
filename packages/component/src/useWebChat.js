48f536b8e194fadbf9ed4c2ad483efbe660b3802
William Wong <compulim@hotmail.com>
2019-08-27 03:24:05 -0700

    Clean up

--- packages/component/src/useWebChat.js
+++ packages/component/src/useWebChat.js
@@ -1,18 +1,14 @@
 import { createSelectorHook } from 'react-redux';
-// import { createDispatchHook, createSelectorHook } from 'react-redux';
 import { useContext } from 'react';
 
 import WebChatReduxContext from './WebChatReduxContext';
 import WebChatContext from './Context';
 
-// const useDispatch = createDispatchHook(WebChatReduxContext);
 const useSelector = createSelectorHook(WebChatReduxContext);
 
 export default function useWebChat(selector) {
   const context = useContext(WebChatContext);
-  // const dispatch = useDispatch();
   const state = useSelector(state => state);
 
   return selector({ ...state, ...context });
-  // return selector({ ...state, ...context, dispatch });
 }
