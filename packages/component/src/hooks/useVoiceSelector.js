dd136cee74529d5839371997e7f49474357b1e81
William Wong <compulim@hotmail.com>
2019-09-17 02:54:07 -0700

    Add tests

--- packages/component/src/hooks/useVoiceSelector.js
+++ packages/component/src/hooks/useVoiceSelector.js
@@ -5,5 +5,5 @@ import WebChatUIContext from '../WebChatUIContext';
 export default function useVoiceSelector(activity) {
   const context = useContext(WebChatUIContext);
 
-  return useCallback(voices => context.selectVoice(voices, activity));
+  return useCallback(voices => context.selectVoice(voices, activity), [activity, context]);
 }
