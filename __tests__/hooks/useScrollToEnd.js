f8ff80481e2916f6745d708480559f696785c50d
William Wong <compulim@hotmail.com>
2019-09-16 23:46:19 -0700

    Add tests

---
+++ __tests__/hooks/useScrollToEnd.js
@@ -0,0 +1,8 @@
+import { timeouts } from '../constants.json';
+
+// selenium-webdriver API doc:
+// https://seleniumhq.github.io/selenium/docs/api/javascript/module/selenium-webdriver/index_exports_WebDriver.html
+
+jest.setTimeout(timeouts.test);
+
+test.todo('calling scrollToEnd should scroll to end');
