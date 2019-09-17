01836ed51a927fbee7b8b0e72fe779cf301d219c
William Wong <compulim@hotmail.com>
2019-09-17 01:46:33 -0700

    Add tests

--- __tests__/hooks/useScrollToEnd.js
+++ __tests__/hooks/useScrollToEnd.js
@@ -1,8 +1,31 @@
-import { timeouts } from '../constants.json';
+import { imageSnapshotOptions, timeouts } from '../constants.json';
+
+import minNumActivitiesShown from '../setup/conditions/minNumActivitiesShown';
+import scrollToBottomCompleted from '../setup/conditions/scrollToBottomCompleted';
+import uiConnected from '../setup/conditions/uiConnected';
 
 // selenium-webdriver API doc:
 // https://seleniumhq.github.io/selenium/docs/api/javascript/module/selenium-webdriver/index_exports_WebDriver.html
 
 jest.setTimeout(timeouts.test);
 
-test.todo('calling scrollToEnd should scroll to end');
+test('calling scrollToEnd should scroll to end', async () => {
+  const { driver, pageObjects } = await setupWebDriver();
+
+  await driver.wait(uiConnected(), timeouts.directLine);
+
+  await pageObjects.sendMessageViaSendBox('help');
+  await driver.wait(minNumActivitiesShown(2), timeouts.directLine);
+  await driver.wait(scrollToBottomCompleted(), timeouts.scrollToBottom);
+
+  await driver.executeScript(() => {
+    document.querySelector('[role="log"] > *').scrollTop = 0;
+  });
+
+  expect(await driver.takeScreenshot()).toMatchImageSnapshot(imageSnapshotOptions);
+
+  await pageObjects.runHook('useScrollToEnd', [], scrollToEnd => scrollToEnd());
+  await driver.wait(scrollToBottomCompleted(), timeouts.scrollToBottom);
+
+  expect(await driver.takeScreenshot()).toMatchImageSnapshot(imageSnapshotOptions);
+});
