f7a4458607e441790544f82f220ef525c3d1d231
William Wong <compulim@hotmail.com>
2019-09-17 02:54:41 -0700

    Typo

--- __tests__/hooks/useActivity.js
+++ __tests__/hooks/useActivity.js
@@ -47,7 +47,7 @@ test('should return list of activities', async () => {
   `);
 });
 
-test('setter should throw an exception', async () => {
+test('setter should throw exception', async () => {
   const { pageObjects } = await setupWebDriver();
 
   await expect(pageObjects.runHook('useActivities', [], result => result[1]())).rejects.toThrow();
