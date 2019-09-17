0e2960881ef51d5a2977f3d8cbc186539312c3ae
William Wong <compulim@hotmail.com>
2019-09-17 01:08:01 -0700

    Add tests

--- __tests__/hooks/useDictateState.js
+++ __tests__/hooks/useDictateState.js
@@ -1,10 +1,38 @@
 import { timeouts } from '../constants.json';
 
+import uiConnected from '../setup/conditions/uiConnected';
+
 // selenium-webdriver API doc:
 // https://seleniumhq.github.io/selenium/docs/api/javascript/module/selenium-webdriver/index_exports_WebDriver.html
 
 jest.setTimeout(timeouts.test);
 
-test.todo('getter should return dictate state');
+test('getter should return dictate state', async () => {
+  const { driver, pageObjects } = await setupWebDriver({
+    props: {
+      webSpeechPonyfillFactory: () => window.WebSpeechMock
+    }
+  });
 
-test.todo('setter should throw exception');
+  await driver.wait(uiConnected(), timeouts.directLine);
+
+  expect((await pageObjects.runHook('useDictateState'))[0]).toMatchInlineSnapshot(`0`);
+
+  await pageObjects.clickMicrophoneButton();
+
+  expect((await pageObjects.runHook('useDictateState'))[0]).toMatchInlineSnapshot(`1`);
+
+  await pageObjects.putSpeechRecognitionResult('recognizing', 'Hello');
+
+  expect((await pageObjects.runHook('useDictateState'))[0]).toMatchInlineSnapshot(`2`);
+
+  await pageObjects.clickMicrophoneButton();
+
+  expect((await pageObjects.runHook('useDictateState'))[0]).toMatchInlineSnapshot(`3`);
+});
+
+test('setter should throw exception', async () => {
+  const { pageObjects } = await setupWebDriver();
+
+  await expect(pageObjects.runHook('useDictateState', [], dictateState => dictateState[1]())).rejects.toThrow();
+});
