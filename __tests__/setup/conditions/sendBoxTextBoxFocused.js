dcf31e613c5f81b262a67dd89a7af303f473a446
William Wong <compulim@hotmail.com>
2019-09-18 09:33:05 -0700

    Clean up

--- __tests__/setup/conditions/sendBoxTextBoxFocused.js
+++ __tests__/setup/conditions/sendBoxTextBoxFocused.js
@@ -1,13 +1,22 @@
 import { By, Condition } from 'selenium-webdriver';
 
 import getSendBoxTextBox, { CSS_SELECTOR } from '../elements/getSendBoxTextBox';
-import hasElement from './hasElement';
 
 export default function sendBoxTextBoxFocused() {
   return new Condition('Send box text box to be focused', async driver => {
     // Make sure the send box text box is visible
     await getSendBoxTextBox(driver);
 
-    return await hasElement(By.css(CSS_SELECTOR + ':focus')).fn(driver);
+    try {
+      await driver.findElement(By.css(CSS_SELECTOR + ':focus'));
+
+      return true;
+    } catch (err) {
+      if (err.name === 'NoSuchElementError') {
+        return false;
+      }
+
+      throw err;
+    }
   });
 }
