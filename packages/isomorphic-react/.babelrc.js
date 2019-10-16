c56500e79944d8a066c211134ae9b401e61d4523
Corina <14900841+corinagum@users.noreply.github.com>
2019-10-16 10:01:32 -0700

    Add package 'isomorphic react'

---
+++ packages/isomorphic-react/.babelrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  plugins: [],
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          browsers: ['last 2 versions']
+        }
+      }
+    ]
+  ]
+};
