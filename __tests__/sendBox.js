f2eef80465d76b4c823bcb5a91004cd6e6a0eb65
William Wong <compulim@hotmail.com>
2019-10-25 19:37:39 -0700

    Add test for #2331

--- __tests__/sendBox.js
+++ __tests__/sendBox.js
@@ -1,5 +1,6 @@
 import { imageSnapshotOptions, timeouts } from './constants.json';
 
+import actionDispatched from './setup/conditions/actionDispatched';
 import minNumActivitiesShown from './setup/conditions/minNumActivitiesShown';
 import uiConnected from './setup/conditions/uiConnected';
 
@@ -30,4 +31,24 @@ test('should focus send box when message is being sent', async () => {
   const base64PNG = await driver.takeScreenshot();
 
   expect(base64PNG).toMatchImageSnapshot(imageSnapshotOptions);
+});
+
+test('should trim outgoing message when being sent', async () => {
+  const { driver, pageObjects } = await setupWebDriver();
+
+  await driver.wait(uiConnected(), timeouts.directLine);
+  await pageObjects.sendMessageViaSendBox(
+    '\u00A0\u00A0There should be no space before and after this message.\u00A0\u00A0',
+    { waitForSend: false }
+  );
+  await driver.wait(
+    actionDispatched(
+      ({ payload: { activity } = {}, type }) =>
+        type === 'DIRECT_LINE/INCOMING_ACTIVITY' &&
+        activity.from.role === 'user' &&
+        activity.text === 'There should be no space before and after this message.'
+    ),
+    timeouts.directLine
+  );
+  await driver.wait(minNumActivitiesShown(2), timeouts.directLine);
 });
--- __tests__/setup/conditions/actionDispatched.js
+++ __tests__/setup/conditions/actionDispatched.js
@@ -1,12 +1,15 @@
 import { Condition } from 'selenium-webdriver';
 
-export default function actionDispatched(type) {
-  return new Condition(
-    'Action to dispatch',
-    async driver =>
-      await driver.executeScript(
-        type => ~window.WebChatTest.actions.findIndex(({ type: target }) => target === type),
-        type
-      )
-  );
+export default function actionDispatched(predicateOrType) {
+  if (typeof predicateOrType === 'string') {
+    const expectedType = predicateOrType;
+
+    predicateOrType = ({ type }) => type === expectedType;
+  }
+
+  return new Condition('Action to dispatch', async driver => {
+    const actions = await driver.executeScript(() => JSON.parse(JSON.stringify(window.WebChatTest.actions)));
+
+    return ~actions.findIndex(action => predicateOrType(action));
+  });
 }
