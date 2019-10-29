0bf094ebd444ccff5c1e5e3d4d7a60c4d03d87ce
William Wong <compulim@hotmail.com>
2019-10-28 22:22:42 -0700

    Filter out payload.directLine

--- __tests__/setup/conditions/actionDispatched.js
+++ __tests__/setup/conditions/actionDispatched.js
@@ -14,7 +14,7 @@ export default function actionDispatched(predicateOrType) {
           window.WebChatTest.actions.map(action => {
             // Filter out payload on DIRECT_LINE/CONNECT* because the content is not stringifiable
 
-            if (/^DIRECT_LINE\/CONNECT/.test(action.type)) {
+            if (/^DIRECT_LINE\/CONNECT/.test(action.type) && action.payload && action.payload.directLine) {
               action.payload.directLine = {};
             }
 
