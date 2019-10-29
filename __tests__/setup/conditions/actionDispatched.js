e8ebe07d8a54e51a5beea02aa82587ffac4b642f
William Wong <compulim@hotmail.com>
2019-10-29 09:20:20 -0700

    Better filtering

--- __tests__/setup/conditions/actionDispatched.js
+++ __tests__/setup/conditions/actionDispatched.js
@@ -1,21 +1,24 @@
 import { Condition } from 'selenium-webdriver';
 
 export default function actionDispatched(predicateOrType) {
+  const message =
+    typeof predicateOrType === 'string' ? `action "${predicateOrType}" to dispatch` : 'action to dispatch';
+
   if (typeof predicateOrType === 'string') {
     const expectedType = predicateOrType;
 
     predicateOrType = ({ type }) => type === expectedType;
   }
 
-  return new Condition('Action to dispatch', async driver => {
+  return new Condition(message, async driver => {
     const actions = await driver.executeScript(() =>
       JSON.parse(
         JSON.stringify(
           window.WebChatTest.actions.map(action => {
-            // Filter out payload on DIRECT_LINE/CONNECT* because the content is not stringifiable
+            // Filter out payload on DIRECT_LINE/* because some content is not stringifiable
 
-            if (/^DIRECT_LINE\/CONNECT/.test(action.type) && action.payload && action.payload.directLine) {
-              action.payload.directLine = {};
+            if (/^DIRECT_LINE\//.test(action.type) && action.payload && action.payload.directLine) {
+              return simpleUpdateIn(action, ['payload', 'directLine'], () => ({}));
             }
 
             return action;
