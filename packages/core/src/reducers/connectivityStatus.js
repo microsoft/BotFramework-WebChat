028e36aaa5b3012037ba3fce5cc752048c3fa4e4
William Wong <compulim@users.noreply.github.com>
2019-10-29 10:45:27 +0800

    Fix saga error (#2421)

--- packages/core/src/reducers/connectivityStatus.js
+++ packages/core/src/reducers/connectivityStatus.js
@@ -8,41 +8,43 @@ import { SAGA_ERROR } from '../actions/sagaError';
 const DEFAULT_STATE = 'uninitialized';
 
 export default function connectivityStatus(state = DEFAULT_STATE, { type, meta }) {
-  switch (type) {
-    case CONNECT_PENDING:
-    case RECONNECT_PENDING:
-      if (state !== 'uninitialized') {
-        state = 'reconnecting';
-      }
+  if (state !== 'sagaerror') {
+    switch (type) {
+      case CONNECT_PENDING:
+      case RECONNECT_PENDING:
+        if (state !== 'uninitialized') {
+          state = 'reconnecting';
+        }
 
-      break;
+        break;
 
-    case CONNECT_FULFILLED:
-      state = 'connected';
-      break;
+      case CONNECT_FULFILLED:
+        state = 'connected';
+        break;
 
-    case RECONNECT_FULFILLED:
-      state = 'reconnected';
-      break;
+      case RECONNECT_FULFILLED:
+        state = 'reconnected';
+        break;
 
-    case CONNECT_REJECTED:
-      state = 'error';
-      break;
+      case CONNECT_REJECTED:
+        state = 'error';
+        break;
 
-    case CONNECT_STILL_PENDING:
-      state = 'connectingslow';
-      break;
+      case CONNECT_STILL_PENDING:
+        state = 'connectingslow';
+        break;
 
-    case DISCONNECT_FULFILLED:
-      state = meta.error ? 'error' : 'notconnected';
-      break;
+      case DISCONNECT_FULFILLED:
+        state = meta && meta.error ? 'error' : 'notconnected';
+        break;
 
-    case SAGA_ERROR:
-      state = 'sagaerror';
-      break;
+      case SAGA_ERROR:
+        state = 'sagaerror';
+        break;
 
-    default:
-      break;
+      default:
+        break;
+    }
   }
 
   return state;
