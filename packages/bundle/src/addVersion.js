1af265f91a0088725d7f34e2a8c53d946b8046a9
William Wong <compulim@hotmail.com>
2019-10-30 21:06:27 -0700

    Use lowercase for npm_package_version

--- packages/bundle/src/addVersion.js
+++ packages/bundle/src/addVersion.js
@@ -21,7 +21,7 @@ function setMetaTag(name, content) {
 
 export default function addVersion(variant) {
   setMetaTag('botframework-webchat:bundle:variant', variant);
-  setMetaTag('botframework-webchat:bundle:version', process.env.NPM_PACKAGE_VERSION);
+  setMetaTag('botframework-webchat:bundle:version', process.env.npm_package_version);
   setMetaTag('botframework-webchat:core:version', coreVersion);
   setMetaTag('botframework-webchat:ui:version', componentVersion);
 }
