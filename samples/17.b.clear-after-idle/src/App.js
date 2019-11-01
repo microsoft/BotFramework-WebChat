acb3da04009ae563f76f1078e8a1ce2c16ecfe27
TJ Durnford <tjdford@gmail.com>
2019-11-01 14:45:03 -0700

    Bumped to Web Chat v4.6.0

--- samples/17.b.clear-after-idle/src/App.js
+++ samples/17.b.clear-after-idle/src/App.js
@@ -1,11 +1,8 @@
-import React, { useCallback, useEffect, useState } from "react";
-import ReactWebChat, {
-  createDirectLine,
-  createStore
-} from "botframework-webchat";
+import React, { useCallback, useEffect, useState } from 'react';
+import ReactWebChat, { createDirectLine, createStore } from 'botframework-webchat';
 
-import "./App.css";
-import Timer from "./Timer";
+import './App.css';
+import Timer from './Timer';
 
 const TIME_INTERVAL = 30000;
 
@@ -14,43 +11,46 @@ function App() {
   const [store, setStore] = useState();
   const [timeRemaining, setTimeRemaining] = useState();
 
+  const storeMiddleware = useCallback(
+    ({ dispatch }) => next => action => {
+      if (action.type === 'DIRECT_LINE/CONNECT_FULFILLED') {
+        dispatch({
+          type: 'WEB_CHAT/SEND_EVENT',
+          payload: {
+            name: 'webchat/join',
+            value: { language: window.navigator.language }
+          }
+        });
+      }
+
+      if (action.type === 'WEB_CHAT/SUBMIT_SEND_BOX') {
+        // Reset the timer when the user sends an activity
+        setTimeRemaining(TIME_INTERVAL);
+      }
+
+      return next(action);
+    },
+    [setTimeRemaining]
+  );
+
   const initConversation = useCallback(() => {
-    setStore(
-      createStore({}, ({ dispatch }) => next => action => {
-        if (action.type === "DIRECT_LINE/CONNECT_FULFILLED") {
-          dispatch({
-            type: "WEB_CHAT/SEND_EVENT",
-            payload: {
-              name: "webchat/join",
-              value: { language: window.navigator.language }
-            }
-          });
-        }
-        if (action.type === "WEB_CHAT/SUBMIT_SEND_BOX") {
-          // Reset the timer when the user sends an activity
-          setTimeRemaining(TIME_INTERVAL);
-        }
-        return next(action);
-      })
-    );
+    setStore(createStore({}, storeMiddleware));
 
     (async function() {
       // In this demo, we are using Direct Line token from MockBot.
       // Your client code must provide either a secret or a token to talk to your bot.
       // Tokens are more secure. To learn about the differences between secrets and tokens
-      // and to understand the risks associated with using secrets, visit https://docs.microsoft.com/en-us/azure/bot-service/rest-api/bot-framework-rest-direct-line-3-0-authentication?view=azure-bot-service-4.0
-      const res = await fetch(
-        "https://webchat-mockbot.azurewebsites.net/directline/token",
-        { method: "POST" }
-      );
+      // and to understand the risks associated with using secrets, visit
+      // https://docs.microsoft.com/en-us/azure/bot-service/rest-api/bot-framework-rest-direct-line-3-0-authentication?view=azure-bot-service-4.0
+      const res = await fetch('https://webchat-mockbot.azurewebsites.net/directline/token', { method: 'POST' });
       const { token } = await res.json();
 
       setDirectLine(createDirectLine({ token }));
     })().catch(error => console.log(error));
-  }, [setStore, setDirectLine, setTimeRemaining]);
+  }, [setStore, setDirectLine, setTimeRemaining, storeMiddleware]);
 
   const restartConversation = useCallback(() => {
-    alert("Restarting Conversation");
+    alert('Restarting Conversation');
     initConversation();
   }, [initConversation]);
 
@@ -60,11 +60,7 @@ function App() {
 
   return (
     <div className="App">
-      <Timer
-        onComplete={restartConversation}
-        setTimeRemaining={setTimeRemaining}
-        timeRemaining={timeRemaining}
-      />
+      <Timer onComplete={restartConversation} setTimeRemaining={setTimeRemaining} timeRemaining={timeRemaining} />
       <ReactWebChat className="chat" directLine={directLine} store={store} />
     </div>
   );
