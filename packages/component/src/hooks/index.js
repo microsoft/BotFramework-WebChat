52abd1f0303c31bf2ed5fe3f0e1ad8ea0d5fb19e
William Wong <compulim@hotmail.com>
2019-11-03 14:13:25 -0800

    Add useLocalizeDate

--- packages/component/src/hooks/index.js
+++ packages/component/src/hooks/index.js
@@ -1,6 +1,7 @@
 import useActivities from './useActivities';
 import useLanguage from './useLanguage';
 import useLocalize from './useLocalize';
+import useLocalizeDate from './useLocalizeDate';
 import useReferenceGrammarID from './useReferenceGrammarID';
 import useStyleOptions from './useStyleOptions';
 import useStyleSet from './useStyleSet';
@@ -11,6 +12,7 @@ export {
   useActivities,
   useLanguage,
   useLocalize,
+  useLocalizeDate,
   useReferenceGrammarID,
   useSendBoxDictationStarted,
   useStyleOptions,
