f5fa6091da3089a36e0d689d60292e56b88c84f6
William Wong <compulim@users.noreply.github.com>
2019-11-23 04:46:33 +0800

    Add React Hooks for customization (part 9) (#2550)
    * Add multiple simple property hooks
    
    * Fix test
    
    * Fix ESLint

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -47,6 +47,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
    -  PR [#2547](https://github.com/microsoft/BotFramework-WebChat/pull/2547): `useEmitTypingIndicator`, `usePeformCardAction`, `usePostActivity`, `useSendEvent`, `useSendFiles`, `useSendMessage`, `useSendMessageBack`, `useSendPostBack`
    -  PR [#2548](https://github.com/microsoft/BotFramework-WebChat/pull/2548): `useDisabled`
    -  PR [#2549](https://github.com/microsoft/BotFramework-WebChat/pull/2549): `useSuggestedActions`
+   -  PR [#2550](https://github.com/microsoft/BotFramework-WebChat/pull/2550): `useConnectivityStatus`, `useGroupTimestamp`, `useTimeoutForSend`, `useUserID`, `useUsername`
    -  PR [#2551](https://github.com/microsoft/BotFramework-WebChat/pull/2551): `useLastTypingAt`, `useSendTypingIndicator`, `useTypingIndicator`
    -  PR [#2552](https://github.com/microsoft/BotFramework-WebChat/pull/2552): `useFocusSendBox`, `useScrollToEnd`, `useSendBoxValue`, `useSubmitSendBox`, `useTextBoxSubmit`, `useTextBoxValue`
 -  Bring your own Adaptive Cards package by specifying `adaptiveCardsPackage` prop, by [@compulim](https://github.com/compulim) in PR [#2543](https://github.com/microsoft/BotFramework-WebChat/pull/2543)
--- packages/component/src/hooks/index.js
+++ packages/component/src/hooks/index.js
@@ -9,6 +9,8 @@ import useLastTypingAt from './useLastTypingAt';
 import useLocalize from './useLocalize';
 import useLocalizeDate from './useLocalizeDate';
 import usePerformCardAction from './usePerformCardAction';
+import useConnectivityStatus from './useConnectivityStatus';
+import useGroupTimestamp from './useGroupTimestamp';
 import usePostActivity from './usePostActivity';
 import useReferenceGrammarID from './useReferenceGrammarID';
 import useRenderMarkdownAsHTML from './useRenderMarkdownAsHTML';
@@ -24,6 +26,9 @@ import useStyleOptions from './useStyleOptions';
 import useStyleSet from './useStyleSet';
 import useSubmitSendBox from './useSubmitSendBox';
 import useSuggestedActions from './useSuggestedActions';
+import useTimeoutForSend from './useTimeoutForSend';
+import useUserID from './useUserID';
+import useUsername from './useUsername';
 
 import { useSendBoxDictationStarted } from '../BasicSendBox';
 import { useTextBoxSubmit } from '../SendBox/TextBox';
@@ -36,6 +41,8 @@ export {
   useDisabled,
   useEmitTypingIndicator,
   useFocusSendBox,
+  useConnectivityStatus,
+  useGroupTimestamp,
   useLanguage,
   useLastTypingAt,
   useLocalize,
@@ -58,5 +65,8 @@ export {
   useSubmitSendBox,
   useSuggestedActions,
   useTextBoxSubmit,
-  useTypingIndicatorVisible
+  useTimeoutForSend,
+  useTypingIndicatorVisible,
+  useUserID,
+  useUsername
 };
