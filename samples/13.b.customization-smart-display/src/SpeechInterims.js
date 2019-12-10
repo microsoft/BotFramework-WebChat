dc5b9758162869ee7816242ae10b313013184727
William Wong <compulim@hotmail.com>
2019-12-09 16:20:07 -0800

    Remove unnecessary code

--- samples/13.b.customization-smart-display/src/SpeechInterims.js
+++ samples/13.b.customization-smart-display/src/SpeechInterims.js
@@ -1,30 +1,9 @@
 import './SpeechInterims.css';
 
-import { hooks, Constants } from 'botframework-webchat';
+import { hooks } from 'botframework-webchat';
 import React from 'react';
 
-const { useActivities, useDictateState, useDictateInterims } = hooks;
-
-// TODO: [P1] Some of our hooks are not in master yet, we are copying the code here.
-const {
-  DictateState: { DICTATING, STARTING }
-} = Constants;
-
-// TODO: [P1] Some of our hooks are not in master yet, we are copying the code here.
-function activityIsSpeakingOrQueuedToSpeak({ channelData: { speak } = {} }) {
-  return !!speak;
-}
-
-// TODO: [P1] Some of our hooks are not in master yet, we are copying the code here.
-function useSendBoxSpeechInterimsVisible() {
-  const [activities] = useActivities();
-  const [dictateState] = useDictateState();
-
-  return [
-    (dictateState === STARTING || dictateState === DICTATING) &&
-      !activities.filter(activityIsSpeakingOrQueuedToSpeak).length
-  ];
-}
+const { useDictateInterims, useSendBoxSpeechInterimsVisible } = hooks;
 
 const CustomDictationInterims = () => {
   const [dictateInterims] = useDictateInterims();
