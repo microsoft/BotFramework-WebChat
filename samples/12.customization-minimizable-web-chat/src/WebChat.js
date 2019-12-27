84c25f2c4f00264f8b6fade7406d3cd4b33b7258
William Wong <compulim@users.noreply.github.com>
2019-12-28 03:55:22 +0800

    Sample 12 prevent reconnection and move to functional component (#2758)
    * Move to functional components
    
    * Add entry
    
    * Remove timeout

--- samples/12.customization-minimizable-web-chat/src/WebChat.js
+++ samples/12.customization-minimizable-web-chat/src/WebChat.js
@@ -1,48 +1,35 @@
-import memoize from 'memoize-one';
-import React from 'react';
+import React, { useEffect, useMemo } from 'react';
 import ReactWebChat, { createDirectLine, createStyleSet } from 'botframework-webchat';
 
 import './WebChat.css';
 
-export default class extends React.Component {
-  constructor(props) {
-    super(props);
+const WebChat = ({ className, onFetchToken, store, token }) => {
+  const directLine = useMemo(() => createDirectLine({ token }), [token]);
 
-    this.createDirectLine = memoize(token => createDirectLine({ token }));
-
-    this.state = {
-      styleSet: createStyleSet({
+  const styleSet = useMemo(
+    () =>
+      createStyleSet({
         backgroundColor: 'Transparent'
-      })
-    };
-  }
+      }),
+    []
+  );
 
-  componentDidMount() {
-    !this.props.token && this.props.onFetchToken();
-  }
+  useEffect(() => {
+    onFetchToken();
+  }, [onFetchToken]);
 
-  render() {
-    const {
-      props: { className, store, token },
-      state: { styleSet }
-    } = this;
-
-    return token ? (
-      <ReactWebChat
-        className={`${className || ''} web-chat`}
-        directLine={this.createDirectLine(token)}
-        store={store}
-        styleSet={styleSet}
-      />
-    ) : (
-      <div className={`${className || ''} connect-spinner`}>
-        <div className="content">
-          <div className="icon">
-            <span className="ms-Icon ms-Icon--Robot" />
-          </div>
-          <p>Please wait while we are connecting.</p>
+  return token ? (
+    <ReactWebChat className={`${className || ''} web-chat`} directLine={directLine} store={store} styleSet={styleSet} />
+  ) : (
+    <div className={`${className || ''} connect-spinner`}>
+      <div className="content">
+        <div className="icon">
+          <span className="ms-Icon ms-Icon--Robot" />
         </div>
+        <p>Please wait while we are connecting.</p>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default WebChat;
