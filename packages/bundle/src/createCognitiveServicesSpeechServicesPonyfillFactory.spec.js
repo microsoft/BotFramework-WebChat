b4f60bbea78126ea79dc0078a27a31e19a3a26fb
William Wong <compulim@hotmail.com>
2020-01-16 12:18:01 -0800

    Add more tests

--- packages/bundle/src/createCognitiveServicesSpeechServicesPonyfillFactory.spec.js
+++ packages/bundle/src/createCognitiveServicesSpeechServicesPonyfillFactory.spec.js
@@ -1,6 +1,7 @@
+let consoleWarns;
+let createCognitiveServicesSpeechServicesPonyfillFactory;
 let createPonyfill;
 let originalConsole;
-let consoleWarns;
 
 async function resolveFunctionOrValue(fnOrValue) {
   return await (typeof fnOrValue === 'function' ? fnOrValue() : fnOrValue);
@@ -18,6 +19,8 @@ beforeEach(() => {
   };
 
   createPonyfill = require('web-speech-cognitive-services/lib/SpeechServices');
+  createCognitiveServicesSpeechServicesPonyfillFactory = require('./createCognitiveServicesSpeechServicesPonyfillFactory')
+    .default;
 });
 
 afterEach(() => {
@@ -26,10 +29,7 @@ afterEach(() => {
   jest.resetModules();
 });
 
-test('upgrading authorizationToken to credentials', async () => {
-  const createCognitiveServicesSpeechServicesPonyfillFactory = require('./createCognitiveServicesSpeechServicesPonyfillFactory')
-    .default;
-
+test('upgrading string-based authorizationToken to credentials', async () => {
   const ponyfillFactory = createCognitiveServicesSpeechServicesPonyfillFactory({
     authorizationToken: 'a1b2c3d',
     region: 'westus2'
@@ -49,13 +49,130 @@ test('upgrading authorizationToken to credentials', async () => {
   );
 });
 
-test('upgrading subscriptionKey to credentials', async () => {
-  const createCognitiveServicesSpeechServicesPonyfillFactory = require('./createCognitiveServicesSpeechServicesPonyfillFactory')
-    .default;
+test('upgrading Promise-based authorizationToken to credentials', async () => {
+  const ponyfillFactory = createCognitiveServicesSpeechServicesPonyfillFactory({
+    authorizationToken: (async () => 'a1b2c3d')(),
+    region: 'westus2'
+  });
 
+  ponyfillFactory();
+
+  const { credentials } = createPonyfill.mock.calls[0][0];
+
+  await expect(resolveFunctionOrValue(credentials)).resolves.toEqual({
+    authorizationToken: 'a1b2c3d',
+    region: 'westus2'
+  });
+
+  expect(consoleWarns[0]).toMatchInlineSnapshot(
+    `"botframework-webchat: \\"authorizationToken\\", \\"region\\", and \\"subscriptionKey\\" are being deprecated and will be removed on or after 2020-12-17. Please use \\"credentials\\" instead."`
+  );
+});
+
+test('upgrading function-based authorizationToken to credentials', async () => {
+  const ponyfillFactory = createCognitiveServicesSpeechServicesPonyfillFactory({
+    authorizationToken: () => 'a1b2c3d',
+    region: 'westus2'
+  });
+
+  ponyfillFactory();
+
+  const { credentials } = createPonyfill.mock.calls[0][0];
+
+  await expect(resolveFunctionOrValue(credentials)).resolves.toEqual({
+    authorizationToken: 'a1b2c3d',
+    region: 'westus2'
+  });
+
+  expect(consoleWarns[0]).toMatchInlineSnapshot(
+    `"botframework-webchat: \\"authorizationToken\\", \\"region\\", and \\"subscriptionKey\\" are being deprecated and will be removed on or after 2020-12-17. Please use \\"credentials\\" instead."`
+  );
+});
+
+test('upgrading Promise function-based authorizationToken to credentials', async () => {
+  const ponyfillFactory = createCognitiveServicesSpeechServicesPonyfillFactory({
+    authorizationToken: async () => 'a1b2c3d',
+    region: 'westus2'
+  });
+
+  ponyfillFactory();
+
+  const { credentials } = createPonyfill.mock.calls[0][0];
+
+  await expect(resolveFunctionOrValue(credentials)).resolves.toEqual({
+    authorizationToken: 'a1b2c3d',
+    region: 'westus2'
+  });
+
+  expect(consoleWarns[0]).toMatchInlineSnapshot(
+    `"botframework-webchat: \\"authorizationToken\\", \\"region\\", and \\"subscriptionKey\\" are being deprecated and will be removed on or after 2020-12-17. Please use \\"credentials\\" instead."`
+  );
+});
+
+test('upgrading string-based subscriptionKey to credentials', async () => {
   const ponyfillFactory = createCognitiveServicesSpeechServicesPonyfillFactory({
     region: 'westus2',
     subscriptionKey: 'a1b2c3d'
+  });
+
+  ponyfillFactory();
+
+  const { credentials } = createPonyfill.mock.calls[0][0];
+
+  await expect(resolveFunctionOrValue(credentials)).resolves.toEqual({
+    region: 'westus2',
+    subscriptionKey: 'a1b2c3d'
+  });
+
+  expect(consoleWarns[0]).toMatchInlineSnapshot(
+    `"botframework-webchat: \\"authorizationToken\\", \\"region\\", and \\"subscriptionKey\\" are being deprecated and will be removed on or after 2020-12-17. Please use \\"credentials\\" instead."`
+  );
+});
+
+test('upgrading string-based subscriptionKey to credentials', async () => {
+  const ponyfillFactory = createCognitiveServicesSpeechServicesPonyfillFactory({
+    region: 'westus2',
+    subscriptionKey: (async () => 'a1b2c3d')()
+  });
+
+  ponyfillFactory();
+
+  const { credentials } = createPonyfill.mock.calls[0][0];
+
+  await expect(resolveFunctionOrValue(credentials)).resolves.toEqual({
+    region: 'westus2',
+    subscriptionKey: 'a1b2c3d'
+  });
+
+  expect(consoleWarns[0]).toMatchInlineSnapshot(
+    `"botframework-webchat: \\"authorizationToken\\", \\"region\\", and \\"subscriptionKey\\" are being deprecated and will be removed on or after 2020-12-17. Please use \\"credentials\\" instead."`
+  );
+});
+
+test('upgrading function-based subscriptionKey to credentials', async () => {
+  const ponyfillFactory = createCognitiveServicesSpeechServicesPonyfillFactory({
+    region: 'westus2',
+    subscriptionKey: () => 'a1b2c3d'
+  });
+
+  ponyfillFactory();
+
+  const { credentials } = createPonyfill.mock.calls[0][0];
+
+  await expect(resolveFunctionOrValue(credentials)).resolves.toEqual({
+    region: 'westus2',
+    subscriptionKey: 'a1b2c3d'
+  });
+
+  expect(consoleWarns[0]).toMatchInlineSnapshot(
+    `"botframework-webchat: \\"authorizationToken\\", \\"region\\", and \\"subscriptionKey\\" are being deprecated and will be removed on or after 2020-12-17. Please use \\"credentials\\" instead."`
+  );
+});
+
+test('upgrading Promise function-based subscriptionKey to credentials', async () => {
+  const ponyfillFactory = createCognitiveServicesSpeechServicesPonyfillFactory({
+    region: 'westus2',
+    subscriptionKey: async () => 'a1b2c3d'
   });
 
   ponyfillFactory();
@@ -73,9 +190,6 @@ test('upgrading subscriptionKey to credentials', async () => {
 });
 
 test('providing reference grammar ID', () => {
-  const createCognitiveServicesSpeechServicesPonyfillFactory = require('./createCognitiveServicesSpeechServicesPonyfillFactory')
-    .default;
-
   const ponyfillFactory = createCognitiveServicesSpeechServicesPonyfillFactory({
     credentials: {
       authorizationToken: 'a1b2c3d',
@@ -91,9 +205,6 @@ test('providing reference grammar ID', () => {
 });
 
 test('not providing reference grammar ID', () => {
-  const createCognitiveServicesSpeechServicesPonyfillFactory = require('./createCognitiveServicesSpeechServicesPonyfillFactory')
-    .default;
-
   const ponyfillFactory = createCognitiveServicesSpeechServicesPonyfillFactory({
     credentials: {
       authorizationToken: 'a1b2c3d',
