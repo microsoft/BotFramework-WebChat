eda40c19660a778d764b9c78817620ff0ab1633c
William Wong <compulim@hotmail.com>
2020-01-22 17:41:32 -0800

    Simplify test

--- packages/component/src/Middleware/concatMiddleware.spec.js
+++ packages/component/src/Middleware/concatMiddleware.spec.js
@@ -1,6 +1,6 @@
 import concatMiddleware from './concatMiddleware';
 
-test('concat 2 middleware', () => {
+describe('two middleware concatenated and ran', () => {
   const oddMultiplyByTen = () => next => value => {
     if (value % 2) {
       return value * 10;
@@ -17,46 +17,32 @@ test('concat 2 middleware', () => {
     return next(value);
   };
 
-  const middleware = concatMiddleware(oddMultiplyByTen, evenMultiplyByHundred);
-  const work = middleware()(value => value);
+  test('separately', () => {
+    const middleware = concatMiddleware(oddMultiplyByTen, evenMultiplyByHundred);
+    const work = middleware()(value => value);
 
-  expect(work(1)).toEqual(10);
-  expect(work(2)).toEqual(200);
+    expect(work(1)).toEqual(10);
+    expect(work(2)).toEqual(200);
+  });
+
+  test('by a single upstream middleware', () => {
+    const combine = () => next => value => {
+      return next(value) + next(value + 1);
+    };
+
+    const middleware = concatMiddleware(combine, oddMultiplyByTen, evenMultiplyByHundred);
+    const work = middleware()(value => value);
+
+    expect(work(1)).toEqual(210);
+  });
 });
 
-test('run 2 middleware for a single work', () => {
+test('one middleware ran twice by a single upstream middleware', () => {
   const combine = () => next => value => {
     return next(value) + next(value + 1);
   };
 
-  const oddMultiplyByTen = () => next => value => {
-    if (value % 2) {
-      return value * 10;
-    }
-
-    return next(value);
-  };
-
-  const evenMultiplyByHundred = () => next => value => {
-    if (!(value % 2)) {
-      return value * 100;
-    }
-
-    return next(value);
-  };
-
-  const middleware = concatMiddleware(combine, oddMultiplyByTen, evenMultiplyByHundred);
-  const work = middleware()(value => value);
-
-  expect(work(1)).toEqual(210);
-});
-
-test('run a single middleware twice for a single work', () => {
-  const combine = () => next => value => {
-    return next(value) + next(value + 1);
-  };
-
-  const oddMultiplyByTenAndEvenMultiplyByHundred = () => next => value => {
+  const oddMultiplyByTenAndEvenMultiplyByHundred = () => () => value => {
     if (value % 2) {
       return value * 10;
     }
