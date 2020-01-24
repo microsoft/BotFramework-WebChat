705b2ee0cead78cfbbd1284ac4fcaf3a73223a50
Corina <corina.aj@gmail.com>
2020-01-23 20:18:02 -0800

    tranform SendIcon and TypingAnimation based on dir

--- packages/component/src/SendBox/Assets/SendIcon.js
+++ packages/component/src/SendBox/Assets/SendIcon.js
@@ -1,9 +1,20 @@
+import { css } from 'glamor';
 import React from 'react';
 
-const SendIcon = () => (
-  <svg height={28} viewBox="0 0 45.7 33.8" width={28}>
-    <path clipRule="evenodd" d="M8.55 25.25l21.67-7.25H11zm2.41-9.47h19.26l-21.67-7.23zm-6 13l4-11.9L5 5l35.7 11.9z" />
-  </svg>
-);
+import useDirection from '../../hooks/useDirection';
+
+const SendIcon = () => {
+  const [direction] = useDirection();
+  const rtlScale = direction === 'rtl' ? css({ transform: 'scale(-1)' }) : '';
+
+  return (
+    <svg className={rtlScale} height={28} viewBox="0 0 45.7 33.8" width={28}>
+      <path
+        clipRule="evenodd"
+        d="M8.55 25.25l21.67-7.25H11zm2.41-9.47h19.26l-21.67-7.23zm-6 13l4-11.9L5 5l35.7 11.9z"
+      />
+    </svg>
+  );
+};
 
 export default SendIcon;
--- packages/component/src/SendBox/Assets/TypingAnimation.js
+++ packages/component/src/SendBox/Assets/TypingAnimation.js
@@ -1,16 +1,21 @@
+import { css } from 'glamor';
+import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import React from 'react';
 
 import ScreenReaderText from '../../ScreenReaderText';
+import useDirection from '../../hooks/useDirection';
 import useStyleSet from '../../hooks/useStyleSet';
 
 const TypingAnimation = ({ 'aria-label': ariaLabel }) => {
   const [{ typingAnimation: typingAnimationStyleSet }] = useStyleSet();
+  const [direction] = useDirection();
+  const rtlScale = direction === 'rtl' ? css({ transform: 'scale(-1)' }) : '';
 
   return (
     <React.Fragment>
       <ScreenReaderText text={ariaLabel} />
-      <div aria-hidden={true} className={typingAnimationStyleSet} />
+      <div aria-hidden={true} className={classNames(rtlScale + '', typingAnimationStyleSet + '')} />
     </React.Fragment>
   );
 };
