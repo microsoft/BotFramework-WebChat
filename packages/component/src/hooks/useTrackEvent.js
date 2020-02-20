b95604ae30b06a09b626e56ac16a3e14d12684a4
William Wong <compulim@hotmail.com>
2020-02-20 04:40:26 -0800

    Fix type checking

--- packages/component/src/hooks/useTrackEvent.js
+++ packages/component/src/hooks/useTrackEvent.js
@@ -11,7 +11,7 @@ export default function useTrackEvent() {
     (name, data) => {
       if (!name || typeof name !== 'string') {
         return console.warn('botframework-webchat: "name" passed to "useTrackEvent" hook must be a string. Ignoring.');
-      } else if (typeof data !== 'string' && typeof data === 'undefined') {
+      } else if (typeof data !== 'string' && typeof data !== 'undefined') {
         return console.warn(
           'botframework-webchat: "data" passed to "useTrackEvent" hook must be a string or undefined. Ignoring.'
         );
