18903247c4e33285c77ad197a9f3b8ad34c712d2
patniko <patrick@isolated.dev>
2020-03-16 18:36:12 -0700

    Fix backwards compatibility in tests

--- packages/bundle/src/renderMarkdown.js
+++ packages/bundle/src/renderMarkdown.js
@@ -54,25 +54,27 @@ const customMarkdownIt = new MarkdownIt({
   linkify: true,
   typographer: true,
   xhtmlOut: true
-}).use(markdownItAttrs, iterator, 'url_new_win', 'link_open', (tokens, index) => {
-  // TODO: [P4] This is copied from v3 and looks clunky
-  //       We should refactor this code
-  const targetAttrIndex = tokens[index].attrIndex('target');
+})
+  .use(markdownItAttrs)
+  .use(iterator, 'url_new_win', 'link_open', (tokens, index) => {
+    // TODO: [P4] This is copied from v3 and looks clunky
+    //       We should refactor this code
+    const targetAttrIndex = tokens[index].attrIndex('target');
 
-  if (~targetAttrIndex) {
-    tokens[index].attrs[targetAttrIndex][1] = '_blank';
-  } else {
-    tokens[index].attrPush(['target', '_blank']);
-  }
+    if (~targetAttrIndex) {
+      tokens[index].attrs[targetAttrIndex][1] = '_blank';
+    } else {
+      tokens[index].attrPush(['target', '_blank']);
+    }
 
-  const relAttrIndex = tokens[index].attrIndex('rel');
+    const relAttrIndex = tokens[index].attrIndex('rel');
 
-  if (~relAttrIndex) {
-    tokens[index].attrs[relAttrIndex][1] = 'noopener noreferrer';
-  } else {
-    tokens[index].attrPush(['target', 'noopener noreferrer']);
-  }
-});
+    if (~relAttrIndex) {
+      tokens[index].attrs[relAttrIndex][1] = 'noopener noreferrer';
+    } else {
+      tokens[index].attrPush(['target', 'noopener noreferrer']);
+    }
+  });
 
 export default function render(markdown, { markdownRespectCRLF }) {
   if (markdownRespectCRLF) {
