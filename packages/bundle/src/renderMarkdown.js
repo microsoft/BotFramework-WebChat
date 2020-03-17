31291d588e782bdef63d8f37ae01e81ea54cfd87
Patrick Nikoletich <pnikoletich@gmail.com>
2020-03-17 02:05:06 -0500

    Support for aria-labels on link (#3022)
    * Support for aria-labels on link using [link](url){aria-label="Screen reader description"} format.
    
    * Added doc
    
    * Sort
    
    Co-authored-by: William Wong <compulim@hotmail.com>

--- packages/bundle/src/renderMarkdown.js
+++ packages/bundle/src/renderMarkdown.js
@@ -2,11 +2,12 @@
 
 import iterator from 'markdown-it-for-inline';
 import MarkdownIt from 'markdown-it';
+import markdownItAttrs from 'markdown-it-attrs';
 import sanitizeHTML from 'sanitize-html';
 
 const SANITIZE_HTML_OPTIONS = {
   allowedAttributes: {
-    a: ['href', 'name', 'target', 'title'],
+    a: ['aria-label', 'href', 'name', 'target', 'title'],
     img: ['alt', 'src']
   },
   allowedSchemes: ['data', 'http', 'https', 'ftp', 'mailto', 'sip', 'tel'],
@@ -53,25 +54,27 @@ const customMarkdownIt = new MarkdownIt({
   linkify: true,
   typographer: true,
   xhtmlOut: true
-}).use(iterator, 'url_new_win', 'link_open', (tokens, index) => {
-  // TODO: [P4] This is copied from v3 and looks clunky
-  //       We should refactor this code
-  const targetAttrIndex = tokens[index].attrIndex('target');
+})
+  .use(markdownItAttrs)
+  .use(iterator, 'url_new_win', 'link_open', (tokens, index) => {
+    // TODO: [P4] This is copied from v3 and looks clunky
+    //       We should refactor this code
+    const targetAttrIndex = tokens[index].attrIndex('target');
 
-  if (~targetAttrIndex) {
-    tokens[index].attrs[targetAttrIndex][1] = '_blank';
-  } else {
-    tokens[index].attrPush(['target', '_blank']);
-  }
+    if (~targetAttrIndex) {
+      tokens[index].attrs[targetAttrIndex][1] = '_blank';
+    } else {
+      tokens[index].attrPush(['target', '_blank']);
+    }
 
-  const relAttrIndex = tokens[index].attrIndex('rel');
+    const relAttrIndex = tokens[index].attrIndex('rel');
 
-  if (~relAttrIndex) {
-    tokens[index].attrs[relAttrIndex][1] = 'noopener noreferrer';
-  } else {
-    tokens[index].attrPush(['target', 'noopener noreferrer']);
-  }
-});
+    if (~relAttrIndex) {
+      tokens[index].attrs[relAttrIndex][1] = 'noopener noreferrer';
+    } else {
+      tokens[index].attrPush(['target', 'noopener noreferrer']);
+    }
+  });
 
 export default function render(markdown, { markdownRespectCRLF }) {
   if (markdownRespectCRLF) {
