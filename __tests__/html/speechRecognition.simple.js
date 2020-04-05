855b81d3633ece9442b71299db3045ed45c94f2e
William Wong <compulim@hotmail.com>
2020-04-04 22:24:43 -0700

    Add authorization token from webchat-waterbottle

--- __tests__/html/speechRecognition.simple.js
+++ __tests__/html/speechRecognition.simple.js
@@ -2,27 +2,60 @@
  * @jest-environment ./__tests__/html/__jest__/WebChatEnvironment.js
  */
 
+import fetch from 'node-fetch';
+
 import fetchSpeechServicesAuthorizationToken from './__jest__/fetchSpeechServicesAuthorizationToken';
+
+const { COGNITIVE_SERVICES_REGION, COGNITIVE_SERVICES_SUBSCRIPTION_KEY } = process.env;
 
 describe.each([
   ['authorization token', { useAuthorizationToken: true }],
   ['subscription key', {}]
 ])('speech recognition using %s', (_, { useAuthorizationToken }) => {
   test('should recognize "Hello, World!".', async () => {
-    const queryParams = new URLSearchParams({
-      sr: process.env.COGNITIVE_SERVICES_REGION,
-      ...(useAuthorizationToken
-        ? {
-            sa: await fetchSpeechServicesAuthorizationToken({
-              region: process.env.COGNITIVE_SERVICES_REGION,
-              subscriptionKey: process.env.COGNITIVE_SERVICES_SUBSCRIPTION_KEY
-            })
-          }
-        : {
-            ss: process.env.COGNITIVE_SERVICES_SUBSCRIPTION_KEY
-          })
-    });
+    let queryParams;
 
-    return runHTMLTest(`speechRecognition.simple.html#${queryParams}`, { ignoreConsoleError: true });
+    if (useAuthorizationToken) {
+      if (COGNITIVE_SERVICES_SUBSCRIPTION_KEY) {
+        queryParams = {
+          sa: await fetchSpeechServicesAuthorizationToken({
+            region: COGNITIVE_SERVICES_REGION,
+            subscriptionKey: COGNITIVE_SERVICES_SUBSCRIPTION_KEY
+          }),
+          sr: COGNITIVE_SERVICES_REGION
+        };
+      } else {
+        console.warn(
+          'No environment variable "COGNITIVE_SERVICES_SUBSCRIPTION_KEY" is set, using the authorization token from webchat-waterbottle.'
+        );
+
+        const res = await fetch('https://webchat-waterbottle.azurewebsites.net/token/speechservices');
+
+        if (!res.ok) {
+          throw new Error(
+            `Failed to fetch Cognitive Services Speech Services credentials, server returned ${res.status}`
+          );
+        }
+
+        const { region, token: authorizationToken } = await res.json();
+
+        queryParams = { sa: authorizationToken, sr: region };
+      }
+    } else {
+      if (COGNITIVE_SERVICES_SUBSCRIPTION_KEY) {
+        queryParams = {
+          sr: COGNITIVE_SERVICES_REGION,
+          ss: COGNITIVE_SERVICES_SUBSCRIPTION_KEY
+        };
+      } else {
+        return console.warn(
+          'No environment variable "COGNITIVE_SERVICES_SUBSCRIPTION_KEY" is set, skipping this test.'
+        );
+      }
+    }
+
+    return runHTMLTest(`speechRecognition.simple.html#${new URLSearchParams(queryParams)}`, {
+      ignoreConsoleError: true
+    });
   });
 });
