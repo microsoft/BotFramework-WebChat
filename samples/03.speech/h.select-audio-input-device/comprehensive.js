7adabcb32bf9b90c9ae4aa1fbfed8e34f4b507fa
William Wong <compulim@hotmail.com>
2020-04-06 15:57:05 -0700

    Support Direct Line Speech

--- samples/03.speech/h.select-audio-input-device/comprehensive.js
+++ samples/03.speech/h.select-audio-input-device/comprehensive.js
@@ -1,5 +1,20 @@
 'use strict';
 
+// Fetch the Direct Line Speech credentials.
+async function fetchDirectLineSpeechCredentials() {
+  const res = await fetch('https://webchat-mockbot-streaming.azurewebsites.net/speechservices/token', {
+    method: 'POST'
+  });
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch authorization token and region.');
+  }
+
+  const { region, token: authorizationToken } = await res.json();
+
+  return { authorizationToken, region };
+}
+
 // Create a function to fetch the Cognitive Services Speech Services credentials.
 // The async function created will hold expiration information about the token and will return cached token when possible.
 function createFetchSpeechServicesCredentials() {
@@ -45,7 +60,12 @@ function createFetchSpeechServicesCredentials() {
     navigator: { mediaDevices },
     React: { useCallback, useEffect, useMemo, useState },
     ReactDOM,
-    WebChat: { createCognitiveServicesSpeechServicesPonyfillFactory, createDirectLine, ReactWebChat }
+    WebChat: {
+      createCognitiveServicesSpeechServicesPonyfillFactory,
+      createDirectLine,
+      createDirectLineSpeechAdapters,
+      ReactWebChat
+    }
   } = window;
 
   const App = () => {
@@ -53,37 +73,76 @@ function createFetchSpeechServicesCredentials() {
     // If the browser does not support media devices, we will set it to false to show an error message.
     const [audioInputDevices, setAudioInputDevices] = useState(mediaDevices ? [] : false);
 
+    // Channel to use, either "directline" or "directlinespeech".
+    const [channel, setChannel] = useState('directline');
+
     // Time when the device list change. This is used to trigger a re-enumeration of devices.
     const [lastDeviceChangeAt, setLastDeviceChangeAt] = useState(0);
 
     // Device ID of the audio input device selected by the user.
     const [selectedAudioInputDeviceId, setSelectedAudioInputDeviceId] = useState('default');
 
-    // Direct Line adapter instance will be kept across render.
-    const directLine = useMemo(() => createDirectLine({ token }), [token]);
+    // Direct Line adapter instance will be kept across render if the channel does not changed.
+    const directLine = useMemo(() => channel !== 'directlinespeech' && createDirectLine({ token }), [channel, token]);
 
     // Function instance to fetch credentials for Cognitive Services Speech Services. The instance is kept across render to cache the credentials.
     const fetchSpeechServicesCredentials = useMemo(() => createFetchSpeechServicesCredentials(), []);
 
     // Create the ponyfill factory function, which can be called to create a concrete implementation of the ponyfill.
-    const webSpeechPonyfillFactory = useMemo(() =>
-      createCognitiveServicesSpeechServicesPonyfillFactory(
-        {
+    // The ponyfill factory will be discarded when the channel changed.
+    const webSpeechPonyfillFactory = useMemo(
+      () =>
+        channel !== 'directlinespeech' &&
+        createCognitiveServicesSpeechServicesPonyfillFactory({
           // Device ID of the audio input device to use.
           audioInputDeviceId: selectedAudioInputDeviceId,
 
           // We are passing the Promise function to the "credentials" field.
           // This function will be called every time the token is being used.
           credentials: fetchSpeechServicesCredentials
-        },
-        [selectedAudioInputDeviceId, fetchSpeechServicesCredentials]
-      )
+        }),
+      [channel, fetchSpeechServicesCredentials, selectedAudioInputDeviceId]
     );
+
+    // When the user select a different channel, we change the "channel" based on their selection.
+    const handleChannelChange = useCallback(({ target: { value } }) => setChannel(value));
 
     // When the user select a different device, we change the "selectedAudioInputDeviceId" based on their selection.
     const handleSelectedAudioInputDeviceIdChange = useCallback(({ target: { value } }) =>
       setSelectedAudioInputDeviceId(value)
     );
+
+    // This is the set of adapters for Web Chat to use.
+    const [adapters, setAdapters] = useState(false);
+
+    // When channel or selected audio input device changed, we will re-create the set of adapters.
+    useEffect(() => {
+      if (channel === 'directlinespeech') {
+        // Direct Line Speech adapter set creation is asynchronous call,
+        // we will temporarily disable Web Chat until the adapter set is ready to use.
+        setAdapters(false);
+
+        let directLine;
+
+        (async function() {
+          const adapters = await createDirectLineSpeechAdapters({
+            audioInputDeviceId: selectedAudioInputDeviceId,
+            fetchCredentials: fetchDirectLineSpeechCredentials
+          });
+
+          directLine = adapters.directLine;
+          setAdapters(adapters);
+        })();
+
+        return () => directLine && directLine.end();
+      } else {
+        // Direct Line adapter and Web Speech ponyfill are created.
+        // We separate the adapter/ponyfill creation code so we can update either one of them without affecting the other one.
+        setAdapters({ directLine, webSpeechPonyfillFactory });
+
+        return () => directLine.end();
+      }
+    }, [channel, selectedAudioInputDeviceId]);
 
     // When device is plugged/unplugged, we will re-enumerate the device list.
     useEffect(() => {
@@ -112,16 +171,47 @@ function createFetchSpeechServicesCredentials() {
 
     return (
       <React.Fragment>
-        <div className="app__audio-input-devices" role="radiogroup">
-          <div className="app__audio-input-devices__header">Audio input devices</div>
+        <div className="app__settings-panel" role="radiogroup">
+          <div className="app__settings-panel__header">Channel</div>
+          <ul className="app__settings-panel__list">
+            <li className="app__settings-panel__list-item">
+              <label className="app__settings-panel__row">
+                <input
+                  checked={channel !== 'directlinespeech'}
+                  className="app__settings-panel__radio-button"
+                  name="channel-direct-line"
+                  onChange={handleChannelChange}
+                  type="radio"
+                  value="directline"
+                />
+                Direct Line
+              </label>
+            </li>
+            <li className="app__settings-panel__list-item">
+              <label className="app__settings-panel__row">
+                <input
+                  checked={channel === 'directlinespeech'}
+                  className="app__settings-panel__radio-button"
+                  name="channel-direct-line-speech"
+                  onChange={handleChannelChange}
+                  type="radio"
+                  value="directlinespeech"
+                />
+                Direct Line Speech
+              </label>
+            </li>
+          </ul>
+        </div>
+        <div className="app__settings-panel" role="radiogroup">
+          <div className="app__settings-panel__header">Audio input devices</div>
           {audioInputDevices ? (
-            <ul className="app__audio-input-devices__list">
+            <ul className="app__settings-panel__list">
               {audioInputDevices.map(({ deviceId, label }) => (
-                <li className="app__audio-input-devices__list-item" key={deviceId}>
-                  <label className="app__audio-input-devices__device">
+                <li className="app__settings-panel__list-item" key={deviceId}>
+                  <label className="app__settings-panel__row">
                     <input
                       checked={selectedAudioInputDeviceId === deviceId}
-                      className="app__audio-input-devices__radio-button"
+                      className="app__settings-panel__radio-button"
                       name="audio-input-device-id"
                       onChange={handleSelectedAudioInputDeviceIdChange}
                       type="radio"
@@ -136,7 +226,8 @@ function createFetchSpeechServicesCredentials() {
             <div>Your browser does not support Web Audio or this page is not loaded via HTTPS or from localhost.</div>
           )}
         </div>
-        <ReactWebChat className="webchat" directLine={directLine} webSpeechPonyfillFactory={webSpeechPonyfillFactory} />
+        {/* We are recreating <ReactWebChat> on channel change */}
+        {adapters && <ReactWebChat className="webchat" key={channel} {...adapters} />}
       </React.Fragment>
     );
   };
