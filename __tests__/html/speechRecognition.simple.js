160bc574192170506c129bf9a6ea1efe22b36a7b
William Wong <compulim@hotmail.com>
2020-04-07 21:14:05 -0700

    Add Direct Line Speech test

--- __tests__/html/speechRecognition.simple.js
+++ __tests__/html/speechRecognition.simple.js
@@ -6,29 +6,51 @@ import fetch from 'node-fetch';
 
 import fetchSpeechServicesAuthorizationToken from './__jest__/fetchSpeechServicesAuthorizationToken';
 
-const { COGNITIVE_SERVICES_REGION, COGNITIVE_SERVICES_SUBSCRIPTION_KEY } = process.env;
+const {
+  COGNITIVE_SERVICES_REGION,
+  COGNITIVE_SERVICES_SUBSCRIPTION_KEY,
+  DIRECT_LINE_SPEECH_REGION,
+  DIRECT_LINE_SPEECH_SUBSCRIPTION_KEY
+} = process.env;
 
 describe.each([
-  ['authorization token', { useAuthorizationToken: true }],
-  ['subscription key', {}]
-])('speech recognition using %s', (_, { useAuthorizationToken }) => {
+  ['authorization token with Direct Line protocol', { useAuthorizationToken: true }],
+  ['subscription key with Direct Line protocol', {}],
+  ['authorization token with Direct Line Speech protocol', { useAuthorizationToken: true, useDirectLineSpeech: true }],
+  ['subscription key with Direct Line Speech protocol', { useDirectLineSpeech: true }]
+])('speech recognition using %s', (_, { useAuthorizationToken, useDirectLineSpeech }) => {
   test('should recognize "Hello, World!".', async () => {
     let queryParams;
 
     if (useAuthorizationToken) {
-      if (COGNITIVE_SERVICES_SUBSCRIPTION_KEY) {
+      if (useDirectLineSpeech && DIRECT_LINE_SPEECH_SUBSCRIPTION_KEY) {
+        queryParams = {
+          sa: await fetchSpeechServicesAuthorizationToken({
+            region: DIRECT_LINE_SPEECH_REGION,
+            subscriptionKey: DIRECT_LINE_SPEECH_SUBSCRIPTION_KEY
+          }),
+          sr: DIRECT_LINE_SPEECH_REGION,
+          t: 'dlspeech'
+        };
+      } else if (!useDirectLineSpeech && COGNITIVE_SERVICES_SUBSCRIPTION_KEY) {
         queryParams = {
           sa: await fetchSpeechServicesAuthorizationToken({
             region: COGNITIVE_SERVICES_REGION,
             subscriptionKey: COGNITIVE_SERVICES_SUBSCRIPTION_KEY
           }),
           sr: COGNITIVE_SERVICES_REGION,
-          t: 'directline'
+          t: 'dl'
         };
       } else {
-        console.warn(
-          'No environment variable "COGNITIVE_SERVICES_SUBSCRIPTION_KEY" is set, using the authorization token from webchat-waterbottle.'
-        );
+        if (useDirectLineSpeech) {
+          console.warn(
+            'No environment variable "DIRECT_LINE_SPEECH_SUBSCRIPTION_KEY" is set, using the authorization token from webchat-waterbottle.'
+          );
+        } else {
+          console.warn(
+            'No environment variable "COGNITIVE_SERVICES_SUBSCRIPTION_KEY" is set, using the authorization token from webchat-waterbottle.'
+          );
+        }
 
         const res = await fetch('https://webchat-waterbottle.azurewebsites.net/token/speechservices');
 
@@ -40,19 +62,33 @@ describe.each([
 
         const { region, token: authorizationToken } = await res.json();
 
-        queryParams = { sa: authorizationToken, sr: region };
+        queryParams = { sa: authorizationToken, sr: region, t: useDirectLineSpeech ? 'dlspeech' : 'dl' };
       }
     } else {
-      if (COGNITIVE_SERVICES_SUBSCRIPTION_KEY) {
-        queryParams = {
-          sr: COGNITIVE_SERVICES_REGION,
-          ss: COGNITIVE_SERVICES_SUBSCRIPTION_KEY,
-          t: 'directline'
-        };
+      if (useDirectLineSpeech) {
+        if (DIRECT_LINE_SPEECH_SUBSCRIPTION_KEY) {
+          queryParams = {
+            sr: DIRECT_LINE_SPEECH_REGION,
+            ss: DIRECT_LINE_SPEECH_SUBSCRIPTION_KEY,
+            t: 'dlspeech'
+          };
+        } else {
+          return console.warn(
+            'No environment variable "DIRECT_LINE_SPEECH_SUBSCRIPTION_KEY" is set, skipping this test.'
+          );
+        }
       } else {
-        return console.warn(
-          'No environment variable "COGNITIVE_SERVICES_SUBSCRIPTION_KEY" is set, skipping this test.'
-        );
+        if (COGNITIVE_SERVICES_SUBSCRIPTION_KEY) {
+          queryParams = {
+            sr: COGNITIVE_SERVICES_REGION,
+            ss: COGNITIVE_SERVICES_SUBSCRIPTION_KEY,
+            t: 'dl'
+          };
+        } else {
+          return console.warn(
+            'No environment variable "COGNITIVE_SERVICES_SUBSCRIPTION_KEY" is set, skipping this test.'
+          );
+        }
       }
     }
 
--- packages/testharness/src/speech/float32ArraysToPcmWaveArrayBuffer.spec.js
+++ packages/testharness/src/speech/float32ArraysToPcmWaveArrayBuffer.spec.js
@@ -1,8 +1,8 @@
-import float32ArrayToPcmWaveArrayBuffer from './float32ArrayToPcmWaveArrayBuffer';
+import float32ArraysToPcmWaveArrayBuffer from './float32ArraysToPcmWaveArrayBuffer';
 
 test('single channel', () => {
   const float32Array = new Float32Array([0, -1, 1, 0]);
-  const result = float32ArrayToPcmWaveArrayBuffer([float32Array]);
+  const result = float32ArraysToPcmWaveArrayBuffer([float32Array]);
 
   expect(result).toBeInstanceOf(ArrayBuffer);
   expect([...new Int16Array(result)]).toEqual([0, -32768, 32767, 0]);
