f995da4b9234879fb5305b7ab86bb84d6330c0eb
Jorge Cupi <jocupi@microsoft.com>
2020-04-10 14:30:20 -0500

    FIX: Add 'Opens in a new window' title to links that open in a new window

--- packages/bundle/src/renderMarkdown.js
+++ packages/bundle/src/renderMarkdown.js
@@ -65,6 +65,7 @@ const customMarkdownIt = new MarkdownIt({
       tokens[index].attrs[targetAttrIndex][1] = '_blank';
     } else {
       tokens[index].attrPush(['target', '_blank']);
+      tokens[index].attrPush(['title', 'Opens in a new window']);
     }
 
     const relAttrIndex = tokens[index].attrIndex('rel');
