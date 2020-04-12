2ccd2b2ff83dc7c8b45a9756b16d9e97615c5741
William Wong <compulim@hotmail.com>
2020-04-11 20:48:32 -0700

    Remove aria-labelledby if label is hidden

--- packages/testharness/src/elements/index.js
+++ packages/testharness/src/elements/index.js
@@ -1,5 +1,7 @@
 import connectivityStatus from './connectivityStatus';
 import sendBoxTextBox from './sendBoxTextBox';
+import toastDismissButtons from './toastDismissButtons';
+import toasterHeader from './toasterHeader';
 import toasts from './toasts';
 
-export { connectivityStatus, sendBoxTextBox, toasts };
+export { connectivityStatus, sendBoxTextBox, toasterHeader, toastDismissButtons, toasts };
