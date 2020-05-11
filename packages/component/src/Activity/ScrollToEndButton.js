45fe1aa50e5d895deec8e14f46f6c7d32bece051
William Wong <compulim@hotmail.com>
2020-05-10 20:53:27 -0700

    Focus on scroll to end

--- packages/component/src/Activity/ScrollToEndButton.js
+++ packages/component/src/Activity/ScrollToEndButton.js
@@ -2,9 +2,10 @@ import { StateContext as ScrollToBottomStateContext } from 'react-scroll-to-bott
 
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useCallback } from 'react';
 
 import useDirection from '../hooks/useDirection';
+import useFocus from '../hooks/useFocus';
 import useLocalizer from '../hooks/useLocalizer';
 import useScrollToEnd from '../hooks/useScrollToEnd';
 import useStyleSet from '../hooks/useStyleSet';
@@ -12,8 +13,14 @@ import useStyleSet from '../hooks/useStyleSet';
 const ScrollToEndButton = ({ className }) => {
   const [{ scrollToEndButton: scrollToEndButtonStyleSet }] = useStyleSet();
   const [direction] = useDirection();
+  const focus = useFocus();
   const localize = useLocalizer();
   const scrollToEnd = useScrollToEnd();
+
+  const handleClick = useCallback(() => {
+    focus('sendBoxWithoutKeyboard');
+    scrollToEnd();
+  }, [focus, scrollToEnd]);
 
   const newMessageText = localize('TRANSCRIPT_NEW_MESSAGES');
 
@@ -25,7 +32,7 @@ const ScrollToEndButton = ({ className }) => {
         className + '',
         direction === 'rtl' ? 'webchat__overlay--rtl' : ''
       )}
-      onClick={scrollToEnd}
+      onClick={handleClick}
       type="button"
     >
       {newMessageText}
