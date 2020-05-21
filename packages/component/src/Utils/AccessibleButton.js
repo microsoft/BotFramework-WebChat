a2ad0512ede830550787ced6df0d934ef75332a8
William Wong <compulim@hotmail.com>
2020-05-21 13:53:11 -0700

    Clean up

--- packages/component/src/Utils/AccessibleButton.js
+++ packages/component/src/Utils/AccessibleButton.js
@@ -5,15 +5,23 @@ import useDisableOnBlurEffect from '../hooks/internal/useDisableOnBlurEffect';
 
 const PREVENT_DEFAULT_HANDLER = event => event.preventDefault();
 
-// Differences between <textarea> and <AccessibleTextArea>:
-// - When the widget is disabled:
-//   - Set "aria-disabled" attribute to "true"
-//   - If the focus is on, don't set "disabled" attribute, until it is blurred
-//     - Otherwise, set "disabled" attribute
-//   - Remove "onClick" handler
+// Differences between <button> and <AccessibleButton>:
+// - Disable behavior
+//   - When the widget is disabled
+//     - Set "aria-disabled" attribute to "true"
+//     - Set "readonly" attribute
+//     - If the focus is on, don't set "disabled" attribute, until it is blurred
+//       - Otherwise, set "disabled" attribute
+//     - Remove "onClick" handler
+//   - Why we need this
+//     - Browser compatibility: when the widget is disabled, different browser send focus to different places
+//     - When the widget become disabled, it's reasonable to keep the focus on the same widget for an extended period of time
+//       - When the user press TAB after the current widget is disabled, it should jump to the next non-disabled widget
 
 // Developers using this accessible widget will need to:
 // - Style the disabled widget themselves, using CSS query `:disabled, [aria-disabled="true"] {}`
+// - Modify all code that check disabled through "disabled" attribute to use aria-disabled="true" instead
+//   - aria-disabled="true" is the source of truth
 // - If the widget is contained by a <form>, the developer need to filter out some `onSubmit` event caused by this widget
 
 const AccessibleButton = forwardRef(({ disabled, onClick, ...props }, forwardedRef) => {
