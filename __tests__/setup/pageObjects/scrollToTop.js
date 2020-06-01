8f82bba26d9f4aecb038875dc0b67694ba474e5d
William Wong <compulim@hotmail.com>
2020-06-01 02:02:30 -0700

    Improve test reliability

--- __tests__/setup/pageObjects/scrollToTop.js
+++ __tests__/setup/pageObjects/scrollToTop.js
@@ -3,5 +3,6 @@ import getTranscriptScrollable from '../elements/getTranscriptScrollable';
 export default async function scrollToTop(driver) {
   await driver.executeScript(scrollable => {
     scrollable.scrollTop = 0;
+    scrollable.dispatchEvent(new Event('scroll'));
   }, await getTranscriptScrollable(driver));
 }
