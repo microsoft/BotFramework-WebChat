f2613a60fe50158a2e4f20c96014dd488d754414
Corina <14900841+corinagum@users.noreply.github.com>
2020-06-23 16:32:05 -0700

    Fix microphone button styling regression

--- packages/component/src/Styles/StyleSet/MicrophoneButton.js
+++ packages/component/src/Styles/StyleSet/MicrophoneButton.js
@@ -1,10 +1,12 @@
 export default function createMicrophoneButtonStyle({ microphoneButtonColorOnDictate }) {
   return {
     // TODO: [P3] This path should not know anything about the DOM tree of <IconButton>
-    '&.dictating > button': {
-      '&, &:focus, &:hover': {
-        '& svg': {
-          fill: microphoneButtonColorOnDictate
+    '&.dictating > .webchat__icon-button': {
+      '&:not(:disabled):not([aria-disabled="true"])': {
+        '&, &:focus, &:hover': {
+          '& svg': {
+            fill: microphoneButtonColorOnDictate
+          }
         }
       }
     }
