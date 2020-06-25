1bc7f60f9144168e9ebc7528828874a3e1a03596
Rohit Agrawal <ragrawal@microsoft.com>
2020-06-25 11:56:53 -0700

    disable test or useInternalHttpSupport when speak field si empty

--- packages/directlinespeech/__tests__/sendSpeechActivity.js
+++ packages/directlinespeech/__tests__/sendSpeechActivity.js
@@ -58,25 +58,28 @@ describe.each([['without internal HTTP support'], ['with internal HTTP support',
       await expect(activityUtterances).resolves.toEqual(['Bellevue.']);
     });
 
-    // 2020-05-11: Direct Line Speech protocol was updated to synthesize "text" if "speak" property is not set.
-    test('should synthesis if "speak" is empty', async () => {
-      const { directLine, fetchCredentials, sendTextAsSpeech } = await createTestHarness(testHarnessOptions);
-
-      const connectedPromise = waitForConnected(directLine);
-      const activitiesPromise = subscribeAll(take(directLine.activity$, 1));
-
-      await connectedPromise;
-
-      // "Don't speak XXX" command will not send "speak" property on respond.
-      await sendTextAsSpeech("Don't speak anything.");
-
-      const activities = await activitiesPromise;
-      const activityUtterances = await Promise.all(
-        activities.map(activity => recognizeActivityAsText(activity, { fetchCredentials }))
-      );
-
-      // Despite it does not have "speak" property, Direct Line Speech protocol will fallback to "text" property for synthesize.
-      expect(activityUtterances).toEqual([`Don't speak anything.`]);
-    });
+    
   }
 );
+
+// TODO: Re-enable this test for "enableInternalHttpSupport = true" once DLS bug fix is lit up in production.
+// 2020-05-11: Direct Line Speech protocol was updated to synthesize "text" if "speak" property is not set.
+test('should synthesis if "speak" is empty', async () => {
+  const { directLine, fetchCredentials, sendTextAsSpeech } = await createTestHarness();
+
+  const connectedPromise = waitForConnected(directLine);
+  const activitiesPromise = subscribeAll(take(directLine.activity$, 1));
+
+  await connectedPromise;
+
+  // "Don't speak XXX" command will not send "speak" property on respond.
+  await sendTextAsSpeech("Don't speak anything.");
+
+  const activities = await activitiesPromise;
+  const activityUtterances = await Promise.all(
+    activities.map(activity => recognizeActivityAsText(activity, { fetchCredentials }))
+  );
+
+  // Despite it does not have "speak" property, Direct Line Speech protocol will fallback to "text" property for synthesize.
+  expect(activityUtterances).toEqual([`Don't speak anything.`]);
+});
