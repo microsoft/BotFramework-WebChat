1678d3aff063e10436d13e55d43e3289394c3b46
William Wong <compulim@hotmail.com>
2020-06-29 00:15:00 -0700

    Remove screen reader text

--- packages/component/src/Attachment/TextContent.js
+++ packages/component/src/Attachment/TextContent.js
@@ -5,28 +5,21 @@
 
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
-import React, { useMemo } from 'react';
+import React from 'react';
 
-import ScreenReaderText from '../ScreenReaderText';
 import useRenderMarkdownAsHTML from '../hooks/useRenderMarkdownAsHTML';
-import useStripMarkdown from '../hooks/internal/useStripMarkdown';
 import useStyleSet from '../hooks/useStyleSet';
 
 const TextContent = ({ contentType, text }) => {
   const [{ textContent: textContentStyleSet }] = useStyleSet();
   const renderMarkdownAsHTML = useRenderMarkdownAsHTML();
   const contentTypeMarkdown = contentType === 'text/markdown';
-  const textAlt = useStripMarkdown(contentTypeMarkdown && text);
 
   return contentTypeMarkdown && renderMarkdownAsHTML ? (
-    <React.Fragment>
-      <ScreenReaderText text={textAlt} />
-      <div
-        aria-hidden={true}
-        className={classNames('markdown', textContentStyleSet + '')}
-        dangerouslySetInnerHTML={{ __html: renderMarkdownAsHTML(text || '') }}
-      />
-    </React.Fragment>
+    <div
+      className={classNames('markdown', textContentStyleSet + '')}
+      dangerouslySetInnerHTML={{ __html: renderMarkdownAsHTML(text || '') }}
+    />
   ) : (
     (text || '').split('\n').map((line, index) => (
       <p className={classNames('plain', textContentStyleSet + '')} key={index}>
