ffd13b25535b07407816c985580d83af6cdf9b12
Corina <14900841+corinagum@users.noreply.github.com>
2020-06-29 10:09:28 -0700

    Enable emoticon converstion to Emoji

---
+++ packages/component/src/hooks/useRenderEmoji.js
@@ -0,0 +1,23 @@
+import useEmojiFromStyles from './useEmojiFromStyles';
+import { useTextBoxValue } from '../SendBox/TextBox';
+import { useCallback } from 'react';
+
+export default function useRenderEmoji() {
+  const [{ emojiAutocorrect, emojiUnicodeMap, emojiRegexp }] = useEmojiFromStyles();
+
+  let [value] = useTextBoxValue();
+
+  const setEmoji = useCallback(
+    value => {
+      const emoticon = value.match(emojiRegexp);
+
+      if (emojiAutocorrect && !!emoticon && !!emojiUnicodeMap[emoticon]) {
+        value = value.replace(emoticon, emojiUnicodeMap[emoticon]);
+      }
+      return value;
+    },
+    [emojiAutocorrect, emojiUnicodeMap, emojiRegexp]
+  );
+
+  return [value, setEmoji];
+}
