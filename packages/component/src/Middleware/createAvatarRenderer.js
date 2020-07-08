6004037cda6d85e10c240a05b3583de874eac901
William Wong <compulim@users.noreply.github.com>
2020-07-07 17:43:37 -0700

    Fix blank screen on rendering red box (#3295)

--- packages/component/src/Middleware/createAvatarRenderer.js
+++ packages/component/src/Middleware/createAvatarRenderer.js
@@ -1,5 +1,6 @@
 /* eslint no-magic-numbers: ["error", { "ignore": [2] }] */
 
+import React from 'react';
 import concatMiddleware from './concatMiddleware';
 import createCoreAvatarMiddleware from './Avatar/createCoreMiddleware';
 import ErrorBox from '../ErrorBox';
