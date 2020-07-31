a0d1d91dba8e352aa84bd5da89d4bbe4c2ebb23f
William Wong <compulim@hotmail.com>
2020-07-31 12:46:33 -0700

    Fix build break

--- packages/component/src/BasicTranscript.js
+++ packages/component/src/BasicTranscript.js
@@ -53,30 +53,6 @@ const ROOT_CSS = css({
   }
 });
 
-function useMemoize(fn) {
-  return useMemo(() => {
-    let cache = [];
-
-    return run => {
-      const nextCache = [];
-      const result = run((...args) => {
-        const { result } = [...cache, ...nextCache].find(
-          ({ args: cachedArgs }) =>
-            args.length === cachedArgs.length && args.every((arg, index) => Object.is(arg, cachedArgs[index]))
-        ) || { result: fn(...args) };
-
-        nextCache.push({ args, result });
-
-        return result;
-      });
-
-      cache = nextCache;
-
-      return result;
-    };
-  }, [fn]);
-}
-
 function nextSiblingAll(element) {
   const {
     parentNode: { children }
