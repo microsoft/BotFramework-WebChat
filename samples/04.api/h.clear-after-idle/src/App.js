0d8d47f91144fd2f46eb21b6a3df1474b6724a83
William Wong <compulim@users.noreply.github.com>
2020-08-03 16:35:18 -0700

    Fix #2828 Clear idle sample (#3376)
    * Clean up
    
    * Update to 4.9.2
    
    * Fix ESLint
    
    * Add entry
    
    * Update README.md

--- samples/04.api/h.clear-after-idle/src/App.js
+++ samples/04.api/h.clear-after-idle/src/App.js
@@ -2,56 +2,58 @@ import React, { useCallback, useEffect, useState } from 'react';
 import ReactWebChat, { createDirectLine, createStore } from 'botframework-webchat';
 
 import './App.css';
-import Timer from './Timer';
-import useTimer from './utils/useTimer';
+import Countdown from './Countdown';
+import useTimeoutAt from './utils/useTimeoutAt';
 
-const TIME_INTERVAL = 30000;
+const IDLE_TIMEOUT = 30000;
+
+// In this demo, we are using Direct Line token from MockBot.
+// Your client code must provide either a secret or a token to talk to your bot.
+// Tokens are more secure. To learn about the differences between secrets and tokens
+// and to understand the risks associated with using secrets, visit
+// https://docs.microsoft.com/en-us/azure/bot-service/rest-api/bot-framework-rest-direct-line-3-0-authentication?view=azure-bot-service-4.0
+async function fetchToken() {
+  const res = await fetch('https://webchat-mockbot2.azurewebsites.net/api/token/directline', { method: 'POST' });
+  const { token } = await res.json();
+
+  return token;
+}
 
 function App() {
-  const [directLine, setDirectLine] = useState(createDirectLine({}));
-  const [store, setStore] = useState();
+  const [resetAt, setResetAt] = useState(() => Date.now() + IDLE_TIMEOUT);
+  const [session, setSession] = useState();
 
   const initConversation = useCallback(() => {
-    setStore(
-      createStore({}, ({ dispatch }) => next => action => {
-        if (action.type === 'DIRECT_LINE/CONNECT_FULFILLED') {
-          dispatch({
-            type: 'WEB_CHAT/SEND_EVENT',
-            payload: {
-              name: 'webchat/join',
-              value: { language: window.navigator.language }
-            }
-          });
-        } else if (action.type === 'WEB_CHAT/SUBMIT_SEND_BOX') {
-          // Reset the timer when the user sends an activity
-          setTimeRemaining(TIME_INTERVAL);
-        }
-
-        return next(action);
-      })
-    );
+    setSession(false);
 
     (async function() {
-      // In this demo, we are using Direct Line token from MockBot.
-      // Your client code must provide either a secret or a token to talk to your bot.
-      // Tokens are more secure. To learn about the differences between secrets and tokens
-      // and to understand the risks associated with using secrets, visit
-      // https://docs.microsoft.com/en-us/azure/bot-service/rest-api/bot-framework-rest-direct-line-3-0-authentication?view=azure-bot-service-4.0
-      const res = await fetch('https://webchat-mockbot.azurewebsites.net/directline/token', { method: 'POST' });
-      const { token } = await res.json();
+      const token = await fetchToken();
+      const key = Date.now();
 
-      setDirectLine(createDirectLine({ token }));
-    })().catch(error => console.log(error));
-  }, [setStore, setDirectLine]);
+      setSession({
+        directLine: createDirectLine({ token }),
+        key,
+        store: createStore({}, () => next => action => {
+          if (action.type === 'DIRECT_LINE/CONNECT_FULFILLED' || action.type === 'WEB_CHAT/SUBMIT_SEND_BOX') {
+            // Reset the timer when the connection established, or the user sends an activity
+            setResetAt(Date.now() + IDLE_TIMEOUT);
+          }
 
-  useEffect(initConversation, []);
+          return next(action);
+        })
+      });
+    })();
+  }, [setResetAt, setSession]);
 
-  const [timeRemaining, setTimeRemaining] = useTimer(initConversation);
+  useTimeoutAt(initConversation, resetAt);
+  useEffect(initConversation, [initConversation]);
 
   return (
     <div className="App">
-      <Timer timeRemaining={timeRemaining} />
-      <ReactWebChat className="chat" directLine={directLine} store={store} />
+      <Countdown to={resetAt} />
+      {!!session && (
+        <ReactWebChat className="chat" directLine={session.directLine} key={session.key} store={session.store} />
+      )}
     </div>
   );
 }
