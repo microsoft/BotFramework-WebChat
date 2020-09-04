4feea28182fb94e420f068e1e6aef336a8427a8e
Kevin William Jasker <kevinjasker@gmail.com>
2020-09-04 14:13:02 -0700

    add webpack-cli, user built in terser for minimization

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -3,17 +3,15 @@ const webpackDevConfig = require('./webpack.config');
 
 module.exports = webpackDevConfig.map(config => ({
     ...config,
+    mode: "production",
+    optimization: {
+      minimize: true,
+    },
     plugins: [
         new webpack.DefinePlugin({
             'process.env': {
                 'NODE_ENV': JSON.stringify('production')
             }
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-            compressor: {
-                warnings: false
-            },
-            sourceMap: true
         }),
         new webpack.optimize.OccurrenceOrderPlugin(),
     ]
