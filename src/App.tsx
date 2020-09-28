b74ce82a786aa8e2d0f7036dc0448b28a71a51a1
Jan Dvorak <dvorakj@gmail.com>
2020-09-28 15:17:40 +0200

    configurable templates

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Chat, ChatProps } from './Chat';
+import { render, renderExpandableTemplate, renderFullScreenTemplate } from './AppService'
 import { DirectLine } from 'botframework-directlinejs';
 import * as konsole from './Konsole';
 
 export type Theme = {
     mainColor: string
+    template: any,
 }
 
 export type AppProps = ChatProps & {theme?: Theme, header?: {textWhenCollapsed?: string, text: string}, autoExpandTimeout?: number};
@@ -19,23 +21,12 @@ export const App = async (props: AppProps, container?: HTMLElement) => {
         ...props.user,
         id: props.user && props.user.id ? "" + props.user.id : MakeId()
     };
-    props.showUploadButton = props.hasOwnProperty('showUploadButton') ? props.showUploadButton : false;
-    props.resize = props.hasOwnProperty('resize') ? props.resize : 'detect';
-    props.locale = props.hasOwnProperty('locale') ? props.locale : 'cs-cz';
-
-    // FEEDYOU configurable theming
-    if (props.theme) {
-        const themeStyle = document.createElement('style');
-        themeStyle.type = 'text/css';
-        themeStyle.appendChild(document.createTextNode(ThemeTemplate(props.theme)));
-        document.head.appendChild(themeStyle);
-    }
 
     // FEEDYOU fetch DL token from bot when no token or secret found
     if (props.bot && props.bot.id && !props.botConnection && (!props.directLine || (!props.directLine.secret && !props.directLine.token))) {
         // TODO test IE11 https://github.com/matthew-andrews/isomorphic-fetch
         try {
-            const response = await fetch(`https://${props.bot.id}.azurewebsites.net/auth/directline/token`, {
+            const response = await fetch(`https://${props.bot.id}.azurewebsites.net/webchat/config`, {
                 method: 'POST',
                 headers: {
                     'Accept': 'application/json',
@@ -57,9 +48,30 @@ export const App = async (props: AppProps, container?: HTMLElement) => {
             } else {
                 document.getElementsByTagName('body')[0].classList.add('feedbot-enabled')
             }
+
+            // TODO configurable template system based on config
+            const config = body.config
+            if (config && config.template) {
+                props.theme = {...props.theme, template: {...props.theme.template, ...config.template}, mainColor: config.mainColor || props.theme.mainColor}
+            }
+
         } catch (err) {
             console.error('Token response error', err)
         }
+    }
+
+    // FEEDYOU props defaults
+    props.showUploadButton = props.hasOwnProperty('showUploadButton') ? props.showUploadButton : false;
+    props.resize = props.hasOwnProperty('resize') ? props.resize : 'detect';
+    props.locale = props.hasOwnProperty('locale') ? props.locale : 'cs-cz';
+
+    // FEEDYOU configurable theming
+    if (props.theme || !container) {
+        const theme = {mainColor: '#D83838', ...props.theme}
+        const themeStyle = document.createElement('style');
+        themeStyle.type = 'text/css';
+        themeStyle.appendChild(document.createTextNode(getStyleForTheme(theme)));
+        document.head.appendChild(themeStyle);
     }
 
     // FEEDYOU use twemoji to make emoji compatible
@@ -82,53 +94,18 @@ export const App = async (props: AppProps, container?: HTMLElement) => {
 
     // FEEDYOU if no container provided, generate default one
     if (!container) {
-        let rendered = false
-        container = document.createElement( 'div' );
-        container.className = 'feedbot'
-        
-        const wrapper = document.createElement( 'div' );
-        wrapper.className = 'feedbot-wrapper collapsed'
-
-        const header = document.createElement('div')
-        header.className='feedbot-header'
-        header.style.backgroundColor = props.theme && props.theme.mainColor ? props.theme.mainColor : '#e51836'
-        header.innerText = props.header ? (props.header.textWhenCollapsed || props.header.text || 'Chatbot') : 'Chatbot'
-        header.onclick = () => {
-            wrapper.classList.toggle('collapsed');
-
-            if (!rendered) {
-                header.innerHTML = '<span class="feedbot-title">'+((props.header && props.header.text) || 'Chatbot')+'</span><a onclick="return false;" class="feedbot-minimize" href="#">_</a>'
-                render(props, container)
-            }
-
-            // when closed manually, store flag to do not open automatically after reload
-            localStorage && (localStorage.feedbotClosed = wrapper.classList.contains('collapsed'))
-        }
-        wrapper.appendChild(header)
-
-        wrapper.appendChild(container)
-        document.body.appendChild( wrapper );
-        
-        if (props.autoExpandTimeout && (!localStorage || localStorage.feedbotClosed !== 'true') && window.matchMedia && window.matchMedia("(min-width: 1024px)").matches) {
-            setTimeout(() => {
-                if (wrapper.className.indexOf('collapsed') >= 0) {
-                    header.click()
-                }
-            }, props.autoExpandTimeout)
+        switch (props.theme && props.theme.template && props.theme.template.type) {
+            case 'full-screen':
+                renderFullScreenTemplate(props)
+                break;
+            default:
+                renderExpandableTemplate(props)
         }
     } else {
         render(props, container) 
     }
 }
 
-const render = (props: AppProps, container?: HTMLElement) => {
-    ReactDOM.render(React.createElement(AppContainer, props), container);
-}
-
-const AppContainer = (props: AppProps) =>
-    <div className="wc-app">
-        <Chat { ...props } />
-    </div>;
 
 export function MakeId() {
     var text = "";
@@ -140,7 +117,343 @@ export function MakeId() {
     return text;
 }
 
-const ThemeTemplate = (theme: Theme) => `
+function getStyleForTheme(theme: Theme): string {
+    switch (theme && theme.template && theme.template.type) {
+      case 'expandable-bar':
+        return ExpandableTheme(theme)      
+      case 'full-screen':
+        return FullScreenTheme(theme)  
+      case 'expandable-knob':
+      default:
+        return KnobTheme(theme)
+    }
+}
+
+const FullScreenTheme = (theme: Theme) =>`
+  body {
+    font-family: Helvetica, Arial;
+    padding: 30px;
+
+    display: -ms-flexbox;
+    display: -webkit-flex;
+    display: flex;
+    -webkit-flex-direction: row;
+    -ms-flex-direction: row;
+    flex-direction: row;
+    -webkit-flex-wrap: wrap;
+    -ms-flex-wrap: wrap;
+    flex-wrap: wrap;
+    -webkit-justify-content: center;
+    -ms-flex-pack: center;
+    justify-content: center;
+    -webkit-align-items: flex-end;
+    -ms-flex-align: end;
+    align-items: flex-end;
+  }
+
+  .feedbot-logo {
+    height: 10%;
+
+    background-color: transparent;
+    text-align: center;
+    padding-top: 0px;
+    padding-bottom: 5px;
+  }
+
+  .feedbot-logo img {
+    max-height: 95%;
+    max-width: 70%;
+    padding-top: 15px;
+    object-fit: contain;
+  }
+  
+  @media (min-width: 768px) {
+    .wc-adaptive-card {
+      width: 398px;  
+    }
+  }
+
+  .wc-adaptive-card {
+    border-radius: 8px;
+    padding: 2px 6px;
+  }
+
+  .feedbot-wrapper {
+    background-color: transparent;
+    width: 95%;
+    max-width: 900px;
+    min-width: 300px;
+    height: 96%;
+    max-height: 98.5%;
+    min-height: 500px;
+
+    display: -ms-flexbox;
+    display: -webkit-flex;
+    display: flex;
+    -webkit-flex-direction: column;
+    -ms-flex-direction: column;
+    flex-direction: column;
+
+    position: absolute;
+    bottom: 0;
+  }
+
+  .feedbot-wrapper .feedbot {
+    position: relative;
+    height: 90%;
+  }
+
+  .wc-message-from.wc-message-from-bot {
+    visibility: hidden;
+    height: 2px;
+  }
+
+  .wc-message-wrapper:not([data-activity-id='retry']) .wc-message-from {
+    visibility: hidden;
+  }
+
+  .wc-message-wrapper:not([data-activity-id]) .wc-message-from {
+    visibility: visible;
+  }
+
+  .wc-message-content {
+    padding: 10px;
+    border-radius: 13px;
+  }
+
+  .wc-message-from-bot .wc-message-content {
+    color: #424242 !important;
+  }
+
+  .wc-carousel {
+    margin-top: 10px;
+  }
+
+  .wc-suggested-actions .wc-hscroll > ul > li {
+    margin: 6px;
+  }
+
+  .wc-message-pane .wc-suggested-actions {
+    position: absolute;
+    bottom: 0px;
+    z-index: 10000;
+    background-color: white;
+  }
+
+  .wc-message-pane.show-actions .wc-suggested-actions {
+    height: 70px;
+  }
+
+  .feedbot-wrapper .wc-console {
+    /*-webkit-box-shadow: 0px 0px 8px 0px rgba(0,0,0,0.5);
+    -moz-box-shadow: 0px 0px 8px 0px rgba(0,0,0,0.5);
+    box-shadow: 0px 0px 8px 0px rgba(0,0,0,0.5);*/
+
+    border-width: 0px;
+    height: 70px;
+  }
+
+  .wc-message-pane.show-actions .wc-message-groups {
+    top: 0px;
+    transform: translateY(0px);
+  }
+
+  .wc-console.disable-input {
+    background-color: white !important;
+  }
+
+  .feedbot .wc-suggested-actions .wc-hscroll > ul > li button, .wc-app .wc-card button {
+    border-radius: 20px;
+  }
+
+  .feedbot .wc-suggested-actions .wc-hscroll > ul > li button:focus, .wc-console .wc-mic, .wc-console .wc-send, .wc-app .wc-card button {
+    outline:0;
+  }
+
+  .wc-console .wc-mic, .wc-console .wc-send {
+    top: 10px;
+  }
+
+  .wc-console input[type=text], .wc-console textarea {
+    margin: 0px 15px;
+  }
+
+  .wc-textbox {
+    border-radius: 13px;
+    background-color: #eceff1;
+    height: 70%;
+    margin-bottom: 0px;
+    top: 6px !important;
+  }
+
+  .wc-suggested-actions .wc-hscroll > ul {
+    text-align: center;
+    margin-top: 10px;
+  }
+
+  .wc-message-from-bot .wc-message-content {
+   background-color: #f5f5f5;
+  }
+
+  .wc-message-from-bot svg.wc-message-callout path {
+    fill: #f5f5f5;
+  }
+
+  .wc-suggested-actions .wc-hscroll > ul > li {
+    max-width: 60%;
+  }
+
+  .wc-message-content {
+   box-shadow: none;
+  }
+
+  .wc-message .wc-list {
+    text-align: center;
+  }
+
+  .wc-app ::-webkit-scrollbar-thumb {
+    background-color: #ececec;
+  }
+
+  .wc-app h1, .wc-app h2, .wc-app h3, .wc-app h4, .wc-app p, .wc-app ul, .wc-app ol {
+    padding: 4px;
+  }
+
+  .wc-carousel button.scroll {
+    background-color: #1f357a !important;
+    border-width: 0px !important;
+  }
+
+  .feedbot .wc-suggested-actions .wc-hscroll > ul > li button, .wc-app .wc-card button {
+    color: white !important;
+    background-color: #1f357a !important;
+    border-color: #1f357a !important;
+  }
+
+  .feedbot .wc-suggested-actions .wc-hscroll > ul > li button:active, .wc-app .wc-card button:active {
+    color: #1f357a !important;
+    background-color: white !important;
+    border-color: #1f357a !important;
+  }
+
+  ${BaseTheme(theme)}
+`
+
+
+const KnobTheme = (theme: Theme) => `
+  body .feedbot-wrapper {
+    bottom: calc(10px + 1vw);
+    right: calc(10px + 1vw);
+    border-radius: 15px;
+  }
+
+  body .feedbot-header {
+    border-top-left-radius: 15px;
+    border-top-right-radius: 15px;
+  }
+
+  body .feedbot-wrapper.collapsed {
+    border-radius: 40px;
+    width: 75px;
+    min-width: auto;
+    height: 75px;
+  }
+
+  body .feedbot-wrapper.collapsed .feedbot-header {
+    border-radius: 40px;
+    height: 57px;
+
+    background-image: url(https://feedbothospodarskako1866.blob.core.windows.net/public/message-icon.png);
+    background-size: 50px 50px;
+    background-position: 12px 12px;
+    background-repeat: no-repeat;
+
+    text-indent: 999%;
+    white-space: nowrap;
+    overflow: hidden;
+  }
+
+  body .feedbot-wrapper {
+    width: 420px;
+    height: 565px;
+  }
+
+  ${ExpandableTheme(theme)}
+`
+
+const ExpandableTheme = (theme: Theme) => `
+  .feedbot-header {
+      z-index: 10;
+      color: white;
+      line-height: 30px;
+      padding: 9px 8px 8px 16px;
+      cursor: pointer;
+      font-size: 1.1em;
+      letter-spacing: 1px;
+      display: flex;
+  }
+
+  .feedbot-header .feedbot-title {
+      flex-grow: 1;
+  }
+
+  .feedbot-header .feedbot-minimize {
+      width: 30px;
+      text-align: center;
+      color: white;
+      font-weight: bolder;
+      font-family: Verdana;
+      font-size: 1.2em;
+      line-height: 0.9em;
+  }
+
+  .feedbot-wrapper.collapsed .feedbot-minimize {
+      display: none;
+  }
+
+  .feedbot-header .feedbot-minimize:hover {
+      font-size: 1.5em;
+      line-height: 0.8em;
+  }
+
+  .feedbot-header, .feedbot-wrapper {
+      border-top-left-radius: 5px;
+      border-top-right-radius: 5px;
+  }
+
+  .feedbot-wrapper {
+    background-color: #fff;
+    width: 450px;
+    min-width: 275px;
+    max-width: 90%;
+    height: 700px;
+    max-height: 90%;
+    position: fixed;
+    right: 5%;
+    bottom: 0px;
+    z-index: 10000;
+
+    -webkit-box-shadow: 0px 0px 10px 0px rgba(167, 167, 167, 0.25);
+    -moz-box-shadow: 0px 0px 10px 0px rgba(167, 167, 167, 0.25);
+    box-shadow: 0px 0px 10px 0px rgba(167, 167, 167, 0.25);
+  }
+
+  .feedbot-wrapper.collapsed > .feedbot {
+      display: none;
+  }
+
+  .feedbot-wrapper.collapsed {
+      height: auto;
+  }
+
+  .feedbot-wrapper.collapsed .feedbot-header {
+      padding-top: 10px;
+  }
+
+  ${BaseTheme(theme)}
+`
+
+const BaseTheme = (theme: Theme) => `
     body.feedbot-disabled div.feedbot {
         display: none;
     }
@@ -203,35 +516,6 @@ const ThemeTemplate = (theme: Theme) => `
         fill: ${theme.mainColor};
     }
 
-    .feedbot-wrapper {
-        background-color: #fff;
-        width: 450px;
-        min-width: 275px;
-        max-width: 90%;
-        height: 700px;
-        max-height: 90%;
-        position: fixed;
-        right: 5%;
-        bottom: 0px;
-        z-index: 10000;
-
-        -webkit-box-shadow: 0px 0px 10px 0px rgba(167, 167, 167, 0.25);
-        -moz-box-shadow: 0px 0px 10px 0px rgba(167, 167, 167, 0.25);
-        box-shadow: 0px 0px 10px 0px rgba(167, 167, 167, 0.25);
-    }
-
-    .feedbot-wrapper.collapsed > .feedbot {
-        display: none;
-    }
-
-    .feedbot-wrapper.collapsed {
-        height: auto;
-    }
-
-    .feedbot-wrapper.collapsed .feedbot-header {
-        padding-top: 10px;
-    }
-
     /* TODO finish - no blank space below suggested actions when input disabled
     .feedbot-wrapper .wc-message-pane .wc-suggested-actions {
         position: absolute;
@@ -264,45 +548,6 @@ const ThemeTemplate = (theme: Theme) => `
     .feedbot-wrapper .wc-message-pane.show-actions .wc-message-groups {
         top: 0px;
         transform: translateY(-40px);
-    }
-
-    .feedbot-header {
-        z-index: 10;
-        color: white;
-        line-height: 30px;
-        padding: 9px 8px 8px 16px;
-        cursor: pointer;
-        font-size: 1.1em;
-        letter-spacing: 1px;
-        display: flex;
-    }
-
-    .feedbot-header .feedbot-title {
-        flex-grow: 1;
-    }
-
-    .feedbot-header .feedbot-minimize {
-        width: 30px;
-        text-align: center;
-        color: white;
-        font-weight: bolder;
-        font-family: Verdana;
-        font-size: 1.2em;
-        line-height: 0.9em;
-    }
-
-    .feedbot-wrapper.collapsed .feedbot-minimize {
-        display: none;
-    }
-
-    .feedbot-header .feedbot-minimize:hover {
-        font-size: 1.5em;
-        line-height: 0.8em;
-    }
-
-    .feedbot-header, .feedbot-wrapper {
-        border-top-left-radius: 5px;
-        border-top-right-radius: 5px;
     }
 
     .feedbot-wrapper .wc-suggested-actions {
---
+++ src/AppService.tsx
@@ -0,0 +1,87 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { Chat } from "./Chat";
+import { AppProps } from "./App";
+
+export function renderExpandableTemplate(props: AppProps) {
+  let rendered = false;
+  let container = document.createElement("div");
+  container.className = "feedbot";
+
+  const wrapper = document.createElement("div");
+  wrapper.className = "feedbot-wrapper collapsed";
+
+  const header = document.createElement("div");
+  header.className = "feedbot-header";
+  header.style.backgroundColor =
+    props.theme && props.theme.mainColor ? props.theme.mainColor : "#e51836";
+  header.innerText = props.header
+    ? props.header.textWhenCollapsed || props.header.text || "Chatbot"
+    : "Chatbot";
+  header.onclick = () => {
+    wrapper.classList.toggle("collapsed");
+
+    if (!rendered) {
+      header.innerHTML =
+        '<span class="feedbot-title">' +
+        ((props.header && props.header.text) || "Chatbot") +
+        '</span><a onclick="return false;" class="feedbot-minimize" href="#">_</a>';
+      render(props, container);
+    }
+
+    // when closed manually, store flag to do not open automatically after reload
+    localStorage &&
+      (localStorage.feedbotClosed = wrapper.classList.contains("collapsed"));
+  };
+  wrapper.appendChild(header);
+
+  wrapper.appendChild(container);
+  document.body.appendChild(wrapper);
+
+  if (
+    props.autoExpandTimeout &&
+    (!localStorage || localStorage.feedbotClosed !== "true") &&
+    window.matchMedia &&
+    window.matchMedia("(min-width: 1024px)").matches
+  ) {
+    setTimeout(() => {
+      if (wrapper.className.indexOf("collapsed") >= 0) {
+        header.click();
+      }
+    }, props.autoExpandTimeout);
+  }
+}
+
+export function renderFullScreenTemplate(props: AppProps) {
+    let container = document.createElement("div");
+    container.className = "feedbot";
+  
+    const wrapper = document.createElement("div");
+    wrapper.className = "feedbot-wrapper";
+  
+    const logo = document.createElement("div");
+    logo.className = "feedbot-logo";
+    
+    const logoImg = document.createElement('img')
+    console.log('render fullscreen', props.theme)
+    logoImg.src = props.theme && props.theme.template && props.theme.template.logoUrl || "https://feedyou.blob.core.windows.net/webchat/feedyou_logo_red.png"
+    logoImg.alt = "Logo"
+    logo.appendChild(logoImg)
+    
+    wrapper.appendChild(logo);
+  
+    wrapper.appendChild(container);
+    document.body.appendChild(wrapper);
+
+    render(props, container);
+  }
+
+export const render = (props: AppProps, container?: HTMLElement) => {
+  ReactDOM.render(React.createElement(AppContainer, props), container);
+};
+
+const AppContainer = (props: AppProps) => (
+  <div className="wc-app">
+    <Chat {...props} />
+  </div>
+);
