1646e91c6f9cdc099e8570c215c5aad1876004f4
Felix van Leeuwen <felixvanleeuwen@dsw.nl>
2020-10-12 11:07:41 +0200

    added tests

---
+++ __tests__/html/sendBoxRenderer.Default.js
@@ -0,0 +1,7 @@
+/**
+ * @jest-environment ./__tests__/html/__jest__/WebChatEnvironment.js
+ */
+
+describe('sendBoxRenderer.DefaultSet', () => {
+  test('should render UI.', () => runHTMLTest('sendBoxRenderer.DefaultSet.html'));
+});
