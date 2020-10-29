74f332123ad33d510c9fc1eaa25a9700e39ec543
Héctor García Delgado <hectorgd@intelequia.com>
2020-10-29 11:16:15 +0000

    send parameter to react intelequia

--- samples/06.recomposing-ui/e.intelequia/src/index.js
+++ samples/06.recomposing-ui/e.intelequia/src/index.js
@@ -4,7 +4,16 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App  />, document.getElementById('root'));
+
+
+const renderApp = (element) => {
+    ReactDOM.render(<App />, document.getElementById(element));
+};
+
+window['Intelequia'] = {
+    renderApp
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
