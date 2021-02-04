18fca4eac0b15a7a9f1bef84724e0dffbf077f20
William Wong <compulim@hotmail.com>
2021-02-04 00:01:27 -0800

    Add exclusive padding for suggested actions flippers

--- packages/component/src/Styles/StyleSet/SuggestedActions.js
+++ packages/component/src/Styles/StyleSet/SuggestedActions.js
@@ -3,6 +3,7 @@
 
 export default function createSuggestedActionsStyle({
   paddingRegular,
+  suggestedActionsCarouselFlipperSize,
   suggestedActionsFlowMaxHeight,
   suggestedActionsStackedHeight,
   suggestedActionsStackedOverflow,
@@ -60,13 +61,43 @@ export default function createSuggestedActionsStyle({
         },
 
         '&:not(.webchat__suggested-actions--rtl)': {
-          '& .react-film__filmstrip__item:first-child': { paddingLeft: paddingRegular / 2 },
-          '& .react-film__filmstrip__item:last-child': { paddingRight: paddingRegular / 2 }
+          '& .react-film__filmstrip__item:first-child': {
+            paddingLeft: paddingRegular / 2
+          },
+
+          '& .react-film__filmstrip__item:last-child': {
+            paddingRight: paddingRegular / 2
+          },
+
+          '& .react-film__flipper + .react-film__filmstrip': {
+            '& .react-film__filmstrip__item:first-child': {
+              paddingLeft: suggestedActionsCarouselFlipperSize + paddingRegular * 1.5
+            },
+
+            '& .react-film__filmstrip__item:last-child': {
+              paddingRight: suggestedActionsCarouselFlipperSize + paddingRegular * 1.5
+            }
+          }
         },
 
         '&.webchat__suggested-actions--rtl': {
-          '& .react-film__filmstrip__item:first-child': { paddingRight: paddingRegular / 2 },
-          '& .react-film__filmstrip__item:last-child': { paddingLeft: paddingRegular / 2 }
+          '& .react-film__filmstrip__item:first-child': {
+            paddingRight: suggestedActionsCarouselFlipperSize + paddingRegular * 1.5
+          },
+
+          '& .react-film__filmstrip__item:last-child': {
+            paddingLeft: suggestedActionsCarouselFlipperSize + paddingRegular * 1.5
+          },
+
+          '& .react-film__flipper + .react-film__filmstrip': {
+            '& .react-film__filmstrip__item:first-child': {
+              paddingRight: suggestedActionsCarouselFlipperSize + paddingRegular * 1.5
+            },
+
+            '& .react-film__filmstrip__item:last-child': {
+              paddingLeft: suggestedActionsCarouselFlipperSize + paddingRegular * 1.5
+            }
+          }
         }
       },
 
