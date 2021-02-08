1998475becf20d8c5a0e52fb7706f1538988631f
Corina <14900841+corinagum@users.noreply.github.com>
2021-02-08 10:36:43 -0800

    Add test for uploading .txt attachment

--- packages/component/src/Middleware/AttachmentForScreenReader/createCoreMiddleware.js
+++ packages/component/src/Middleware/AttachmentForScreenReader/createCoreMiddleware.js
@@ -17,7 +17,7 @@ export default function createCoreMiddleware() {
         }
       ] = args;
 
-      return role === 'user' && !/^text\//u.test(contentType) && !thumbnailUrl
+      return role === 'user' && !/^audio|image|video\//u.test(contentType) && !attachment.content && !thumbnailUrl
         ? () => <FileAttachment attachment={attachment} />
         : /^audio\//u.test(contentType)
         ? () => <AudioAttachment />
