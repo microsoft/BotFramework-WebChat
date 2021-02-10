11e312a189f42dba6ebeb05b49b1960eddf72458
Corina <14900841+corinagum@users.noreply.github.com>
2021-02-09 17:23:29 -0800

    Fix logic and add tests

--- packages/component/src/Middleware/AttachmentForScreenReader/TextAttachment.js
+++ packages/component/src/Middleware/AttachmentForScreenReader/TextAttachment.js
@@ -3,20 +3,15 @@ import { hooks } from 'botframework-webchat-api';
 import PropTypes from 'prop-types';
 import React from 'react';
 
-import FileAttachment from './FileAttachment';
 import useUniqueId from '../../hooks/internal/useUniqueId';
 
 const { useLocalizer } = hooks;
 
-const TextAttachment = ({ activity, attachment, attachment: { content = '' } = {} }) => {
+const TextAttachment = ({ attachment: { content = '' } = {} }) => {
   const labelId = useUniqueId('webchat__id');
   const localize = useLocalizer();
 
   const label = localize('ATTACHMENT_TEXT', content);
-
-  if (!attachment.content) {
-    return <FileAttachment activity={activity} attachment={attachment} />;
-  }
 
   return (
     <article aria-labelledby={labelId}>
@@ -26,7 +21,6 @@ const TextAttachment = ({ activity, attachment, attachment: { content = '' } = {
 };
 
 TextAttachment.propTypes = {
-  activity: PropTypes.any.isRequired,
   attachment: PropTypes.shape({
     content: PropTypes.string.isRequired
   }).isRequired
