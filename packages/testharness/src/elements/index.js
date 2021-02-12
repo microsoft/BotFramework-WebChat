3fc2ccf1f2f9bd5bb95dd03673b5cb107efe4bc9
William Wong <compulim@hotmail.com>
2021-02-12 00:57:20 -0800

    Move useTextBox to html

--- packages/testharness/src/elements/index.js
+++ packages/testharness/src/elements/index.js
@@ -2,6 +2,7 @@ import activities from './activities';
 import connectivityStatus from './connectivityStatus';
 import newMessageButton from './newMessageButton';
 import sendBoxTextBox from './sendBoxTextBox';
+import sendButton from './sendButton';
 import suggestedActions from './suggestedActions';
 import toastDismissButtons from './toastDismissButtons';
 import toasterHeader from './toasterHeader';
@@ -17,6 +18,7 @@ export {
   connectivityStatus,
   newMessageButton,
   sendBoxTextBox,
+  sendButton,
   suggestedActions,
   toasterHeader,
   toastDismissButtons,
