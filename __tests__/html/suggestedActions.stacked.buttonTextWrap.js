abc9ac42642a5ab21491beccd5be9fe39855db58
Corina <14900841+corinagum@users.noreply.github.com>
2021-02-17 17:06:25 -0800

    Give shorter test names

--- __tests__/html/suggestedActions.stacked.buttonTextWrap.js
+++ __tests__/html/suggestedActions.stacked.buttonTextWrap.js
@@ -3,24 +3,24 @@
  */
 
 // sah - suggested actions height
-// tw - text wrap
-// mh - max height
+// tw - textWrap
+// mh - maxHeight
 
-describe('suggested actions stacked button text wrap,', () => {
-  test('when false, should ignore max height.', () =>
+describe('suggested actions stacked button textWrap,', () => {
+  test('when false, should ignore maxHeight.', () =>
     runHTMLTest('suggestedActions.stacked.buttonTextWrap.html#sah=40&tw=false&mh=80'));
-  test('when true and max height is 20 and suggested action height is default, change max height, max height will override height', () =>
+  test('when true & maxHeight is 20 & height is default, maxHeight will override height', () =>
     runHTMLTest('suggestedActions.stacked.buttonTextWrap.html#tw=true&mh=20'));
-  test('when true and max height is 30, change min height.', () =>
+  test('when true & maxHeight is 30, change min height.', () =>
     runHTMLTest('suggestedActions.stacked.buttonTextWrap.html#sah=40&tw=true&mh=30'));
-  test('when true and max height is 45, change max height.', () =>
+  test('when true & maxHeight is 45, change maxHeight.', () =>
     runHTMLTest('suggestedActions.stacked.buttonTextWrap.html#sah=40&tw=true&mh=45'));
-  test('when true and max height is 80, change max height to fit text.', () =>
+  test('when true & maxHeight is 80, change maxHeight to fit text.', () =>
     runHTMLTest('suggestedActions.stacked.buttonTextWrap.html#sah=40&tw=true&mh=80'));
-  test('when true and max height is 100% and suggested action height is 60, change max height, height will be 60', () =>
+  test('when true & maxHeight is 100% & suggested action height is 60, height will be 60', () =>
     runHTMLTest('suggestedActions.stacked.buttonTextWrap.html#sah=60&tw=true&mh=100%25'));
-  test('when true and max height is 100%, change max height to fit text.', () =>
+  test('when true & maxHeight is 100%, change maxHeight to fit text.', () =>
     runHTMLTest('suggestedActions.stacked.buttonTextWrap.html#sah=20&tw=true&mh=100%25'));
-  test('when true and max height is undefined, should default max height to 100%25', () =>
+  test('when true & maxHeight is undefined, should default maxHeight to 100%25', () =>
     runHTMLTest('suggestedActions.stacked.buttonTextWrap.html#sah=40&tw=true'));
 });
