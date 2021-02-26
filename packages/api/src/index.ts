7dc5890ef2bd16aa0911ec8eaeec0a357e5dc72a
Corina <14900841+corinagum@users.noreply.github.com>
2021-02-26 12:38:32 -0800

    api - Add StyleOptions typing definitions

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -2,5 +2,7 @@ import * as hooks from './hooks';
 import Composer from './hooks/Composer';
 import concatMiddleware from './hooks/middleware/concatMiddleware';
 import Localize, { localize } from './localization/Localize';
+import StyleOptions from './StyleOptions';
 
 export { Composer, concatMiddleware, hooks, Localize, localize };
+export type { StyleOptions };
