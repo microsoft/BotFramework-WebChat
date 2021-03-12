5e003659bc7671303db87ef04ece216184044931
William Wong <compulim@hotmail.com>
2021-03-11 17:07:35 -0800

    Clean typing

--- packages/api/src/index.js
+++ packages/api/src/index.js
@@ -3,7 +3,5 @@ import Composer from './hooks/Composer';
 import concatMiddleware from './hooks/middleware/concatMiddleware';
 import defaultStyleOptions from './defaultStyleOptions';
 import Localize, { localize } from './localization/Localize';
-import StyleOptions from './StyleOptions';
 
 export { Composer, concatMiddleware, defaultStyleOptions, hooks, Localize, localize };
-export type { StyleOptions };
---
+++ packages/api/src/types.ts
@@ -0,0 +1,5 @@
+// Any changes to this file, please include it in tsconfig.json.
+
+import StyleOptions from './StyleOptions';
+
+export type { StyleOptions };
