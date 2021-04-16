0c585ce89bc38bf236e218b68addc3d95feda5e9
Corina <14900841+corinagum@users.noreply.github.com>
2021-04-15 17:23:38 -0700

    Update ACHostConfig textblock colors to meet a11y contrast requirements

--- packages/bundle/src/adaptiveCards/defaultStyleOptions.ts
+++ packages/bundle/src/adaptiveCards/defaultStyleOptions.ts
@@ -3,7 +3,7 @@ import AdaptiveCardsStyleOptions from './AdaptiveCardsStyleOptions';
 const ADAPTIVE_CARDS_DEFAULT_STYLE_OPTIONS: Required<AdaptiveCardsStyleOptions> = {
   cardEmphasisBackgroundColor: '#F0F0F0',
   cardPushButtonBackgroundColor: '#0063B1',
-  cardPushButtonTextColor: 'white',
+  cardPushButtonTextColor: 'White',
   richCardWrapTitle: false
 };
 
