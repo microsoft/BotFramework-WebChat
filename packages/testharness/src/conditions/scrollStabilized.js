b96de4e413380758273810e98e936561050061fa
William Wong <compulim@hotmail.com>
2021-04-22 13:13:50 -0700

    Add debug

--- packages/testharness/src/conditions/scrollStabilized.js
+++ packages/testharness/src/conditions/scrollStabilized.js
@@ -6,13 +6,18 @@ function sleep(durationMS = 17) {
 
 const COUNT = 5;
 
-export default function scrollStabilized() {
+export default function scrollStabilized(message) {
   const scrollTops = [];
+  const start = Date.now();
+
+  console.log(`t=0: START ${message}`);
 
   return {
-    message: `scroll stabilized after ${COUNT} counts`,
+    message: `scroll stabilized after ${COUNT} counts${message ? ': ' + message : ''}`,
     fn: async () => {
       scrollTops.push(getTranscriptScrollableElement().scrollTop);
+
+      console.log(`t=${Date.now() - start}: GOT ${getTranscriptScrollableElement().scrollTop}`);
 
       while (scrollTops.length > COUNT) {
         scrollTops.splice(0, 1);
