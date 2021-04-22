36318d2c5508f72234a0e1961cd7768fc28f1d02
William Wong <compulim@hotmail.com>
2021-04-22 13:06:13 -0700

    Update husky and lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -10,6 +10,13 @@ function prettierMarkdown(filenames) {
 module.exports = {
   '{docs,samples}/**/*.{html,js,jsx,ts,tsx}': prettierCode,
   '**/*.md': prettierMarkdown,
-  'packages/{api,bundle,component,core,directlinespeech,playground}/src/**/*.{js,jsx,ts,tsx}': prettierCode,
-  '*.{js,jsx,ts,tsx}': 'npm run eslint'
+  'packages/acs-chat-adapter/src/**/*.{js,ts,tsx}': ['npm run precommit:eslint:acs-chat-adapter'],
+  'packages/api/src/**/*.{js,ts,tsx}': ['npm run precommit:eslint:api'],
+  'packages/bundle/src/**/*.{js,ts,tsx}': ['npm run precommit:eslint:bundle'],
+  'packages/component/src/**/*.{js,ts,tsx}': ['npm run precommit:eslint:component'],
+  'packages/core/src/**/*.{js,ts,tsx}': ['npm run precommit:eslint:core'],
+  'packages/directlinespeech/src/**/*.{js,ts,tsx}': ['npm run precommit:eslint:directlinespeech'],
+  'packages/isomorphic-react/src/**/*.{js,ts,tsx}': ['npm run precommit:eslint:isomorphic-react'],
+  'packages/isomorphic-react-dom/src/**/*.{js,ts,tsx}': ['npm run precommit:eslint:isomorphic-react-dom'],
+  'packages/*/src/**/*.{ts,tsx}': [() => 'npm run precommit:typecheck']
 };
