d30074ec02b1d7a4b7f3627b52eb37d4cfb75aa3
William Wong <compulim@hotmail.com>
2021-04-30 16:05:43 -0700

    Move all activities.* tests

--- __tests__/html/activity.unknownActivity.js
+++
@@ -1,8 +0,0 @@
-/**
- * @jest-environment ./__tests__/html/__jest__/WebChatEnvironment.js
- */
-
-describe('activity', () => {
-  test('should not show unknown activity', () =>
-    runHTMLTest('activity.unknownActivity.html', { ignoreConsoleError: true }));
-});
