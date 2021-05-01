5573a249c9d1d85609d78a3ae9377d95f3651cc3
William Wong <compulim@hotmail.com>
2021-04-30 20:21:30 -0700

    Move suggestedActions.* tests

--- __tests__/html/suggestedActions.accessKey.js
+++
@@ -1,7 +0,0 @@
-/**
- * @jest-environment ./__tests__/html/__jest__/WebChatEnvironment.js
- */
-
-describe('suggested actions', () => {
-  test('should be focusable using access key.', () => runHTMLTest('suggestedActions.accessKey.html'));
-});
--- __tests__/html/suggestedActions.noAccessKey.js
+++
@@ -1,8 +0,0 @@
-/**
- * @jest-environment ./__tests__/html/__jest__/WebChatEnvironment.js
- */
-
-describe('suggested actions with access key disabled', () => {
-  test('should not have screen reader text related to access key.', () =>
-    runHTMLTest('suggestedActions.noAccessKey.html'));
-});
