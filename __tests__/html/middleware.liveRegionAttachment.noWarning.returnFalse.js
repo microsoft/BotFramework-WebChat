15824389966d2c25a245ba67ea277d31f586f579
William Wong <compulim@hotmail.com>
2021-04-30 18:51:26 -0700

    Move middleware.* tests

--- __tests__/html/middleware.liveRegionAttachment.noWarning.returnFalse.js
+++
@@ -1,8 +0,0 @@
-/**
- * @jest-environment ./__tests__/html/__jest__/WebChatEnvironment.js
- */
-
-describe('attachment for screen reader middleware', () => {
-  test('should not warn if returning false', () =>
-    runHTMLTest('middleware.liveRegionAttachment.noWarning.returnFalse.html'));
-});
