b38a103caa0e8a862adbd0dec40af47994522b02
William Wong <compulim@hotmail.com>
2021-05-01 19:49:31 -0700

    Fix tests and clean up

--- packages/testpageobjects/src/globals/pageConditions/allOutgoingActivitiesSent.js
+++ packages/testpageobjects/src/globals/pageConditions/allOutgoingActivitiesSent.js
@@ -4,11 +4,10 @@ import getActivities from '../pageObjects/getActivities';
 export default function allOutgoingActivitiesSent() {
   return became(
     'all outgoing activities to be sent',
-    () => {
-      return getActivities()
-        .filter(({ from: { role }, name }) => role === 'user' && name !== '__RUN_HOOK')
-        .every(({ channelData: { state } = {} }) => state === 'sent');
-    },
+    () =>
+      getActivities()
+        .filter(({ from: { role }, name, type }) => role === 'user' && name !== '__RUN_HOOK' && type === 'message')
+        .every(({ channelData: { state } = {} }) => state === 'sent'),
     15000
   );
 }
