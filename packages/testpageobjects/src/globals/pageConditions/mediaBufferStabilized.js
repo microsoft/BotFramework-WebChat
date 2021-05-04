46e8a90d5f00a4c8841f7e08c1ad922230de382c
William Wong <compulim@hotmail.com>
2021-05-03 21:49:58 -0700

    Use stabilized

--- packages/testpageobjects/src/globals/pageConditions/mediaBufferStabilized.js
+++ packages/testpageobjects/src/globals/pageConditions/mediaBufferStabilized.js
@@ -1,28 +1,5 @@
-import became from './became';
-import sleep from '../../utils/sleep';
-
-const CHECK_INTERVAL = 200;
-
-function getBufferedEnd(mediaElement) {
-  return mediaElement.readyState === HTMLMediaElement.HAVE_ENOUGH_DATA
-    ? mediaElement.buffered.length && mediaElement.buffered.end(0)
-    : 0;
-}
+import stabilized from './stabilized';
 
 export default function mediaBuffered(mediaElement) {
-  return became(
-    'for media buffer stabilized',
-    async () => {
-      const initialBufferedEnd = getBufferedEnd(mediaElement);
-
-      if (getBufferedEnd(mediaElement) !== initialBufferedEnd) {
-        await sleep(CHECK_INTERVAL);
-
-        return false;
-      }
-
-      return true;
-    },
-    15000
-  );
+  return stabilized('media', () => getBufferedEnd(mediaElement));
 }
