67f345e130b6496266987a03a574661669f04c35
William Wong <compulim@hotmail.com>
2021-05-04 13:45:52 -0700

    Fix test

--- packages/test/harness/src/host/common/host/snapshot.js
+++ packages/test/harness/src/host/common/host/snapshot.js
@@ -2,7 +2,7 @@ const { join } = require('path');
 
 module.exports = webDriver => {
   return function snapshot() {
-    expect(webDriver.takeScreenshot()).resolves.toMatchImageSnapshot({
+    return expect(webDriver.takeScreenshot()).resolves.toMatchImageSnapshot({
       customSnapshotsDir: join(__dirname, '../../../../../../../__tests__/__image_snapshots__/html/')
     });
   };
