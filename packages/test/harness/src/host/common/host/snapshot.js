1e08bb0b872f792e037b1fff719c8af53db5dd6d
William Wong <compulim@hotmail.com>
2021-05-05 07:44:50 -0700

    Wait for image load before snapshot

--- packages/test/harness/src/host/common/host/snapshot.js
+++ packages/test/harness/src/host/common/host/snapshot.js
@@ -1,10 +1,25 @@
 const { join } = require('path');
+const sleep = require('../../../common/utils/sleep');
 
-module.exports = webDriver => {
-  return function snapshot() {
-    return expect(webDriver.takeScreenshot()).resolves.toMatchImageSnapshot({
+const TIME_FOR_IMAGE_COMPLETE = 5000;
+
+module.exports = webDriver =>
+  async function snapshot() {
+    // Wait until all images are loaded/errored.
+    for (const start = Date.now(); Date.now() - start < TIME_FOR_IMAGE_COMPLETE; ) {
+      if (
+        await webDriver.executeScript(() =>
+          [].every.call(document.getElementsByTagName('img'), ({ complete }) => complete)
+        )
+      ) {
+        break;
+      }
+
+      sleep(100);
+    }
+
+    await expect(webDriver.takeScreenshot()).resolves.toMatchImageSnapshot({
       // jest-image-snapshot does not support <rootDir>.
       customSnapshotsDir: join(__dirname, '../../../../../../../__tests__/__image_snapshots__/html/')
     });
   };
-};
