e498903c57e9f1fb680a0d61720bb100a078e061
William Wong <compulim@hotmail.com>
2021-05-05 13:55:14 -0700

    Fix tests

--- packages/test/page-object/src/globals/pageConditions/index.js
+++ packages/test/page-object/src/globals/pageConditions/index.js
@@ -9,7 +9,6 @@ import minNumActivitiesShown from './minNumActivitiesShown';
 import newMessageButtonHidden from './newMessageButtonHidden';
 import newMessageButtonShown from './newMessageButtonShown';
 import numActivitiesShown from './numActivitiesShown';
-import scrolling from './scrolling';
 import scrollStabilized from './scrollStabilized';
 import scrollToBottomCompleted from './scrollToBottomCompleted';
 import stabilized from './stabilized';
@@ -31,7 +30,6 @@ export {
   minNumActivitiesShown,
   newMessageButtonShown,
   numActivitiesShown,
-  scrolling,
   scrollStabilized,
   scrollToBottomCompleted,
   stabilized,
