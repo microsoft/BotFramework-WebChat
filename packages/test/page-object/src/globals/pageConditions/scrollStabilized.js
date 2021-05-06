8360a559c8112a0119edeceb5711a7efac9a7b44
William Wong <compulim@hotmail.com>
2021-05-05 19:54:09 -0700

    Fix test reliability

--- packages/test/page-object/src/globals/pageConditions/scrollStabilized.js
+++ packages/test/page-object/src/globals/pageConditions/scrollStabilized.js
@@ -1,8 +1,17 @@
 import getTranscriptScrollableElement from '../pageElements/transcriptScrollable';
 import stabilized from './stabilized';
 
-export default async function scrollStabilized() {
+export default async function scrollStabilized(scrollTop) {
   const transcriptScrollable = getTranscriptScrollableElement();
 
-  return await stabilized('scroll', () => transcriptScrollable.scrollTop, 5, 5000);
+  if (typeof scrollTop === 'number') {
+    return await stabilized(
+      `scroll is at ${scrollTop}px and`,
+      () => (transcriptScrollable.scrollTop === scrollTop ? scrollTop : {}),
+      5,
+      5000
+    );
+  } else {
+    return await stabilized('scroll', () => transcriptScrollable.scrollTop, 5, 5000);
+  }
 }
