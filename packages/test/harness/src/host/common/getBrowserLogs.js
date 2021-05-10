7310b0f96925ff7b6462e3c259e702cf9b8c7e31
William Wong <compulim@users.noreply.github.com>
2021-05-10 15:24:01 -0700

    Apply suggestions from code review
    Co-authored-by: Corina <14900841+corinagum@users.noreply.github.com>

--- packages/test/harness/src/host/common/getBrowserLogs.js
+++ packages/test/harness/src/host/common/getBrowserLogs.js
@@ -8,7 +8,7 @@ const IGNORE_CONSOLE_MESSAGE_FRAGMENTS = [
 ];
 
 module.exports = async function getBrowserLogs(webDriver, { clear = false } = {}) {
-  // Every calls to webDriver.manage().logs().get() will clean up the log.
+  // Every call to webDriver.manage().logs().get() will clean up the log.
   // This function will persist the logs across function calls, until `clear` is set to `true`.
   const newLogs = (await webDriver.manage().logs().get(logging.Type.BROWSER)).filter(
     // Ignore console entries that contains specified fragments.
