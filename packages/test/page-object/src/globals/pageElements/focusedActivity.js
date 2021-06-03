dccadee2c6105dbdb1910964c15f1d2816886a73
William Wong <compulim@hotmail.com>
2021-06-02 19:18:40 -0700

    Fix test

--- packages/test/page-object/src/globals/pageElements/focusedActivity.js
+++ packages/test/page-object/src/globals/pageElements/focusedActivity.js
@@ -1,8 +1,22 @@
 import transcript from './transcript';
 
+function isSelfOrAncestor(parent, node) {
+  while (node) {
+    if (parent === node) {
+      return true;
+    }
+
+    node = node.parentElement;
+  }
+
+  return false;
+}
+
 export default function focusedActivity() {
-  return document.activeElement === transcript()
-    ? document.getElementById(transcript().getAttribute('aria-activedescendant'))
-    : // If the transcript is not focused, active descendant means nothing, returns undefined (no activity is focused).
-      undefined;
+  const activity = document.getElementById(transcript().getAttribute('aria-activedescendant'));
+
+  // Could be focusing on Adaptive Card or <input> inside the activity, we will consider it is focused.
+  if (activity && (document.activeElement === transcript() || isSelfOrAncestor(activity, document.activeElement))) {
+    return activity;
+  }
 }
