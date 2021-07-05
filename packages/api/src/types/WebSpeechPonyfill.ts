146dab35aef162e17f5eee9aab808fac92b44995
William Wong <compulim@hotmail.com>
2021-07-05 06:47:18 -0700

    Add descriptions

--- packages/api/src/types/WebSpeechPonyfill.ts
+++ packages/api/src/types/WebSpeechPonyfill.ts
@@ -1,10 +1,23 @@
 /* globals SpeechGrammarList, SpeechRecognition, SpeechSynthesis */
 
 type WebSpeechPonyfill = {
+  /**
+   * Function to resume `AudioContext` object when called.
+   *
+   * Web Chat will call this function on user gestures to resume suspended `AudioContext`.
+   */
   resumeAudioContext?: () => Promise<void>;
+
+  /** Polyfill for Web Speech API `SpeechGrammarList` class. */
   SpeechGrammarList?: typeof SpeechGrammarList;
+
+  /** Polyfill for Web Speech API `SpeechRecognition` class. */
   SpeechRecognition?: typeof SpeechRecognition;
+
+  /** Polyfill for Web Speech API `speechSynthesis` instance. */
   speechSynthesis?: SpeechSynthesis;
+
+  /** Polyfill for Web Speech API `SpeechSynthesisUtterance` class. */
   SpeechSynthesisUtterance?: typeof SpeechSynthesisUtterance;
 };
 
