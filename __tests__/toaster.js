3da4df8a5d7893079bc09a86450ff8e673dc293d
William Wong <compulim@hotmail.com>
2021-09-20 00:09:11 -0700

    Improve test reliability

--- __tests__/toaster.js
+++ __tests__/toaster.js
@@ -34,8 +34,7 @@ test('show 2 notifications, expand, close one, and add new', async () => {
     }
   });
 
-  // Pause between actions for reliability.
-  await new Promise(resolve => setTimeout(resolve, 0));
+  await driver.wait(toastShown(1), timeouts.ui);
 
   await pageObjects.dispatchAction({
     type: 'WEB_CHAT/SET_NOTIFICATION',
@@ -64,7 +63,7 @@ test('show 2 notifications, expand, close one, and add new', async () => {
     }
   });
 
-  await driver.wait(toastShown(1), 60000);
+  await driver.wait(toastShown(1), timeouts.ui);
   await driver.wait(negationOf(toasterExpandable()), timeouts.ui);
 
   expect(await driver.takeScreenshot()).toMatchImageSnapshot(imageSnapshotOptions);
@@ -106,6 +105,8 @@ test('show 2 notifications, expand, and collapse', async () => {
       message: 'Notification 1.'
     }
   });
+
+  await driver.wait(toastShown(1), timeouts.ui);
 
   await pageObjects.dispatchAction({
     type: 'WEB_CHAT/SET_NOTIFICATION',
@@ -200,6 +201,8 @@ test('move recently updated toast to the top', async () => {
       message: 'Notification 1.'
     }
   });
+
+  await driver.wait(toastShown(1), timeouts.ui);
 
   await pageObjects.dispatchAction({
     type: 'WEB_CHAT/SET_NOTIFICATION',
