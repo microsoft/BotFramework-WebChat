64acf49cf69a3f4b1c2e9a0ad2ea3f4629ca17d5
William Wong <compulim@users.noreply.github.com>
2021-11-02 19:30:21 +0000

    Mark as CommonJS

--- packages/isomorphic-react-dom/src/ReactDOM.js
+++ packages/isomorphic-react-dom/src/ReactDOM.js
@@ -1,7 +1,3 @@
-/* global module */
-
-// We intentionally use require() here.
-// eslint-disable-next-line no-undef
 const ReactDOM = require('react-dom');
 
 module.exports = typeof window.ReactDOM === 'undefined' ? ReactDOM : window.ReactDOM;
--- packages/isomorphic-react/src/React.js
+++ packages/isomorphic-react/src/React.js
@@ -1,7 +1,3 @@
-/* global module */
-
-// We intentionally use require() here.
-// eslint-disable-next-line no-undef
 const React = require('react');
 
 module.exports = typeof window.React === 'undefined' ? React : window.React;
