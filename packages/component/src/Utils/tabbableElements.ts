761f9145606dd5b22f1506d9001b342ce4f54b1b
William Wong <compulim@hotmail.com>
2022-01-12 18:37:34 -0800

    Update to TypeScript

--- packages/component/src/Utils/tabbableElements.ts
+++ packages/component/src/Utils/tabbableElements.ts
@@ -1,11 +1,12 @@
-export default function tabbableElements(element) {
+export default function tabbableElements(element: HTMLElement): HTMLElement[] {
   // This is an incomplete list, due to the complexity of testing for every scenario.
   // For full-list, please refer to https://allyjs.io/data-tables/focusable.html
-  const candidates = element.querySelectorAll(
-    'a[href], area[href], audio[controls], button:not(:disabled), iframe, input:not(:disabled), keygen, select:not(:disabled), summary, textarea:not(:disabled), video[controls], [contenteditable], [tabindex]'
-  );
+  const candidates: ArrayLike<HTMLElement> =
+    element.querySelectorAll(
+      'a[href], area[href], audio[controls], button:not(:disabled), iframe, input:not(:disabled), keygen, select:not(:disabled), summary, textarea:not(:disabled), video[controls], [contenteditable], [tabindex]'
+    ) || [];
 
-  return [].filter.call(candidates, element => {
+  return ([] as HTMLElement[]).filter.call(candidates, (element: HTMLElement) => {
     const tabIndexAttribute = element.attributes.getNamedItem('tabindex');
 
     if (tabIndexAttribute && tabIndexAttribute.specified) {
