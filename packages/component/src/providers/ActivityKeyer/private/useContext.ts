e1b2aefb682f15aeed0c474ba08e81293ec960c8
William Wong <compulim@hotmail.com>
2022-01-14 04:05:19 -0800

    Assert under Provider

--- packages/component/src/providers/ActivityKeyer/private/useContext.ts
+++ packages/component/src/providers/ActivityKeyer/private/useContext.ts
@@ -2,6 +2,14 @@ import { useContext } from 'react';
 
 import ActivityKeyerContext from './Context';
 
-export default function useActivityKeyerContext() {
-  return useContext(ActivityKeyerContext);
+import type { ActivityKeyerContextType } from './Context';
+
+export default function useActivityKeyerContext(thrownOnUndefined = true): ActivityKeyerContextType {
+  const contextValue = useContext(ActivityKeyerContext);
+
+  if (thrownOnUndefined && !contextValue) {
+    throw new Error('botframework-webchat internal: This hook can only be used under <ActivityKeyerComposer>.');
+  }
+
+  return contextValue;
 }
