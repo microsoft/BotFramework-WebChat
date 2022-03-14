d10333dd0743afe9c06cad3a816a6574eba18e99
William Wong <compulim@hotmail.com>
2022-03-14 03:49:54 -0700

    Fix test

--- packages/core/src/types/external/DirectLineJSBotConnection.ts
+++ packages/core/src/types/external/DirectLineJSBotConnection.ts
@@ -1,11 +1,5 @@
 // TODO: [P1] #3953 We should fully type it out.
 
-import type { AnyAnd } from '../AnyAnd';
-import type { DirectLineActivity } from './DirectLineActivity';
-import type { Observable } from './Observable';
-
-type DirectLineJSBotConnection = AnyAnd<{
-  postActivity: (activity: DirectLineActivity) => Observable<string>;
-}>;
+type DirectLineJSBotConnection = any;
 
 export type { DirectLineJSBotConnection };
