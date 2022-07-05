456aceb7c573b19637bd3e4acb05578b6d7e401a
William Wong <compulim@users.noreply.github.com>
2022-07-05 18:41:48 +0000

    Remove upgraded test

--- __tests__/adaptiveCards.js
+++ __tests__/adaptiveCards.js
@@ -97,69 +97,6 @@ test('breakfast card with custom style options', async () => {
   expect(base64PNG).toMatchImageSnapshot(imageSnapshotOptions);
 });
 
-test('disable card inputs', async () => {
-  const { driver, pageObjects } = await setupWebDriver();
-
-  await driver.wait(uiConnected(), timeouts.directLine);
-  await pageObjects.sendMessageViaSendBox('card inputs', { waitForSend: true });
-
-  await driver.wait(minNumActivitiesShown(2), timeouts.directLine);
-  await driver.wait(allImagesLoaded(), timeouts.fetchImage);
-  await driver.wait(scrollToBottomCompleted(), timeouts.scrollToBottom);
-
-  await driver.executeScript(() => {
-    document.querySelector('.ac-adaptiveCard input[type="checkbox"]').checked = true;
-    document.querySelector('.ac-adaptiveCard input[type="date"]').value = '2019-11-01';
-    document.querySelector('.ac-adaptiveCard input[type="radio"]').checked = true;
-    document.querySelector('.ac-adaptiveCard input[type="text"]').value = 'William';
-    document.querySelector('.ac-adaptiveCard input[type="time"]').value = '12:34';
-    document.querySelector('.ac-adaptiveCard input[type="number"]').value = '1';
-    document.querySelector('.ac-adaptiveCard select').value = '1';
-    document.querySelector('.ac-adaptiveCard textarea').value = 'One Redmond Way, Redmond, WA';
-  });
-
-  expect(await driver.takeScreenshot()).toMatchImageSnapshot(imageSnapshotOptions);
-
-  await pageObjects.updateProps({ disabled: true });
-  await driver.wait(scrollToBottomCompleted(), timeouts.scrollToBottom);
-
-  // Click "Submit" button should have no effect
-  await driver.executeScript(() => {
-    document.querySelector('.ac-actionSet button:nth-of-type(2)').click();
-  });
-
-  //@todo change to use scrollStabilizer after release
-  await new Promise(resolve => setTimeout(resolve, 1000));
-
-  expect(await driver.takeScreenshot()).toMatchImageSnapshot(imageSnapshotOptions);
-
-  await pageObjects.updateProps({ disabled: false });
-
-  // Wait until render after update props
-  await driver.wait(
-    () =>
-      driver.executeScript(() => {
-        const button = document.querySelector('.ac-actionSet button:nth-of-type(2)');
-
-        return button && !button.disabled;
-      }),
-    timeouts.ui
-  );
-
-  // Click "Submit" button should send values to the bot
-  await driver.executeScript(() => {
-    document.querySelector('.ac-actionSet button:nth-of-type(2)').click();
-  });
-
-  //@todo change to use scrollStabilizer after release
-  await new Promise(resolve => setTimeout(resolve, 1000));
-
-  await driver.wait(minNumActivitiesShown(3), timeouts.directLine);
-  await driver.wait(scrollToBottomCompleted(), timeouts.scrollToBottom);
-
-  expect(await driver.takeScreenshot()).toMatchImageSnapshot(imageSnapshotOptions);
-});
-
 test('broken card of invalid version', async () => {
   const { driver, pageObjects } = await setupWebDriver();
 
