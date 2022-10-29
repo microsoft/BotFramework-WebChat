d5065467e739b9d2c017fb5e41e468ac363d4a2d
William Wong <compulim@users.noreply.github.com>
2022-10-29 06:24:49 +0000

    Upgrade to HTML tests

--- packages/test/page-object/src/globals/pageConditions/toasterExpandable.js
+++ packages/test/page-object/src/globals/pageConditions/toasterExpandable.js
@@ -1,6 +1,10 @@
 import became from './became';
 import getToaster from '../pageElements/toaster';
 
-export default function toasterExpandable() {
-  return became('toaster is expandable', () => getToaster().matches('.webchat__toaster--expandable'), 1000);
+export default function toasterExpandable(expandable = true) {
+  return became(
+    'toaster is expandable',
+    () => getToaster().matches('.webchat__toaster--expandable') === expandable,
+    1000
+  );
 }
