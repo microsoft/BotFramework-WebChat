96cd6772c1c4f701786069422416a03d2e8c6632
Benjamin Yackley <113474767+beyackle2@users.noreply.github.com>
2022-12-08 23:42:49 +0000

    fix test typo

--- __tests__/html/accessibility.liveRegion.activityStatus.sendFailed.contrast.js
+++ __tests__/html/accessibility.liveRegion.activityStatus.sendFailed.contrast.js
@@ -2,5 +2,5 @@
 
 describe('live region', () => {
   test('should render "Send Failed. Retry" correctly in high-contrast mode', () =>
-    runHTML('accessibility.liveRegion.activityStatus.sendFailed.html'));
+    runHTML('accessibility.liveRegion.activityStatus.sendFailed.contrast.html'));
 });
