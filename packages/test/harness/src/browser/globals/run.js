da5bb9913fd02552e7fd2b14b54e74ed67d1ddcb
William Wong <compulim@users.noreply.github.com>
2022-12-15 02:24:27 +0000

    Reset forced-colors when test start

--- packages/test/harness/src/browser/globals/run.js
+++ packages/test/harness/src/browser/globals/run.js
@@ -10,7 +10,11 @@ export default function () {
 
       // Run the test, signal start by host.ready().
       // On success or failure, call host.done() or host.error() correspondingly.
-      return Promise.resolve()
+      return host
+        .sendDevToolsCommand('Emulation.setEmulatedMedia', {
+          // Setting "value" to "" (empty string) does not unset "forced-colors".
+          features: [{ name: 'forced-colors', value: 'none' }]
+        })
         .then(host.ready)
         .then(fn)
         .then(() => host.done(doneOptions))
