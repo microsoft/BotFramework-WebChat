ac2a162ba58b0f2b27aaed8ed7362082be375ba5
William Wong <compulim@users.noreply.github.com>
2023-03-07 20:29:19 +0000

    Temporary rollback sleep

--- packages/test/page-object/src/utils/sleep.js
+++ packages/test/page-object/src/utils/sleep.js
@@ -1,5 +1,10 @@
 import signalToReject from './signalToReject';
 
+// TODO: Remove this when we no longer support global installed clock.
+
+// Lolex may get installed and impact the sleep.
+const globalSetTimeout = setTimeout;
+
 export default function sleep(duration = 1000, signal) {
-  return Promise.race([new Promise(resolve => setTimeout(resolve, duration)), signalToReject(signal)]);
+  return Promise.race([new Promise(resolve => globalSetTimeout(resolve, duration)), signalToReject(signal)]);
 }
