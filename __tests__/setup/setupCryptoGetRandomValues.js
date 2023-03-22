562f954a3192fef0738ce496496f7f0ab07bc6de
William Wong <compulim@users.noreply.github.com>
2023-03-22 23:17:36 +0000

    Fix ESBuild

--- __tests__/setup/setupCryptoGetRandomValues.js
+++ __tests__/setup/setupCryptoGetRandomValues.js
@@ -2,4 +2,6 @@ import { randomFillSync } from 'crypto';
 
 // When microsoft-cognitiveservices-speech-sdk is loaded, it call "uuid" package to create a new GUID.
 // "uuid" package requires crypto.getRandomValues().
-global.crypto = { ...global.crypto, getRandomValues: randomFillSync };
+if (!global.crypto.getRandomValues) {
+  global.crypto = { ...global.crypto, getRandomValues: randomFillSync };
+}
