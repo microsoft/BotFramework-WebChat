8931e6c240a0a2ffa1fec9e9fee88f583074d8b2
William Wong <compulim@users.noreply.github.com>
2023-03-23 06:03:32 +0000

    Fix crypto

--- __tests__/setup/setupCryptoGetRandomValues.js
+++ __tests__/setup/setupCryptoGetRandomValues.js
@@ -2,6 +2,6 @@ import { randomFillSync } from 'crypto';
 
 // When microsoft-cognitiveservices-speech-sdk is loaded, it call "uuid" package to create a new GUID.
 // "uuid" package requires crypto.getRandomValues().
-if (!global.crypto.getRandomValues) {
+if (!global.crypto?.getRandomValues) {
   global.crypto = { ...global.crypto, getRandomValues: randomFillSync };
 }
