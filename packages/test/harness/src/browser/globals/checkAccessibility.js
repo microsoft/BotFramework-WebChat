47deee0553272fd5526bd37ecef59fd848b0924c
William Wong <compulim@users.noreply.github.com>
2023-04-08 19:48:24 +0000

    No IIFE

--- packages/test/harness/src/browser/globals/checkAccessibility.js
+++ packages/test/harness/src/browser/globals/checkAccessibility.js
@@ -55,5 +55,5 @@ export default function initializeCheckAccessibility() {
       } finally {
         console.log(`[TESTHARNESS] Accessibility checks took ${Date.now() - startTime} ms.`);
       }
-    })();
+    });
 }
