e4e7e1df99317dd0f9d6025f5d627a93c81498af
William Wong <compulim@users.noreply.github.com>
2023-04-09 06:40:02 +0000

    Fix contrast

--- packages/bundle/src/adaptiveCards/defaultStyleOptions.ts
+++ packages/bundle/src/adaptiveCards/defaultStyleOptions.ts
@@ -2,7 +2,7 @@ import AdaptiveCardsStyleOptions from './AdaptiveCardsStyleOptions';
 
 const ADAPTIVE_CARDS_DEFAULT_STYLE_OPTIONS: Required<AdaptiveCardsStyleOptions> = {
   adaptiveCardsParserMaxVersion: undefined,
-  cardEmphasisBackgroundColor: '#F0F0F0',
+  cardEmphasisBackgroundColor: '#F9F9F9',
   cardPushButtonBackgroundColor: '#0063B1',
   cardPushButtonTextColor: 'White',
   richCardWrapTitle: false
