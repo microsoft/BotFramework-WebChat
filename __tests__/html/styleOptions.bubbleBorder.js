112a43720fa114b7d4c67714a0c5f2acdd4d917c
William Wong <compulim@users.noreply.github.com>
2023-04-12 08:12:32 +0000

    Port to HTML test

---
+++ __tests__/html/styleOptions.bubbleBorder.js
@@ -0,0 +1,3 @@
+/** @jest-environment ./packages/test/harness/src/host/jest/WebDriverEnvironment.js */
+
+test('bubble border with color, radius, style, and width set', () => runHTML('styleOptions.bubbleBorder.html'));
