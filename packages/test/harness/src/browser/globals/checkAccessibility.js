13657dc533180be9fcd827e9320b84172f111706
William Wong <compulim@users.noreply.github.com>
2023-06-22 04:06:42 +0000

    Fix heading-one test

--- packages/test/harness/src/browser/globals/checkAccessibility.js
+++ packages/test/harness/src/browser/globals/checkAccessibility.js
@@ -4,15 +4,32 @@ import 'axe-core';
 
 export default function initializeCheckAccessibility() {
   axe.configure({
+    checks: [
+      {
+        evaluate: (node, { selector }) => !node.querySelector(selector),
+        id: 'webchat-has-no-heading-one',
+        metadata: {
+          impact: 'moderate',
+          messages: {
+            pass: 'Web Chat has no level-one heading',
+            fail: 'Web Chat must have no level-one heading'
+          }
+        },
+        options: {
+          selector:
+            'h1:not([role], [aria-level]), :is(h1, h2, h3, h4, h5, h6):not([role])[aria-level="1"], [role=heading][aria-level="1"]'
+        }
+      }
+    ],
     rules: [
       {
+        all: ['webchat-has-no-heading-one'],
         id: 'webchat-has-no-heading-one',
         metadata: {
           description: 'As a component, ensures Web Chat does not contains any level-one heading.',
           help: 'No level-one heading is allowed in Web Chat',
           helpUrl: 'https://github.com/microsoft/BotFramework-WebChat/issues/4721'
         },
-        none: ['page-has-heading-one'],
         selector: '#webchat',
         tags: ['webchat', 'cat.semantics', 'best-practice']
       }
