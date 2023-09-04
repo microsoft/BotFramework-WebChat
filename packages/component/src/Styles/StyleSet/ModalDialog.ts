b2795be89094b9c37a4ebaaf0665d37474c4ed5f
William Wong <compulim@users.noreply.github.com>
2023-09-04 22:40:31 +0000

    Use constants

--- packages/component/src/Styles/StyleSet/ModalDialog.ts
+++ packages/component/src/Styles/StyleSet/ModalDialog.ts
@@ -1,7 +1,9 @@
-const DARK_THEME_SELECTOR = '@media (forced-colors: none) and (prefers-color-scheme: dark)';
-const FORCED_COLORS_SELECTOR = '@media (forced-colors: active)';
-const LIGHT_THEME_SELECTOR = '@media (forced-colors: none) and (prefers-color-scheme: light)';
-const NOT_FORCED_COLORS_SELECTOR = '@media (forced-colors: none)';
+import {
+  DARK_THEME_SELECTOR,
+  FORCED_COLORS_SELECTOR,
+  LIGHT_THEME_SELECTOR,
+  NOT_FORCED_COLORS_SELECTOR
+} from './Constants';
 
 export default function createCitationPopoverStyleSet() {
   return {
