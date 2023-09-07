fac68a76f0226e4cdd36158d87ee7b74439aa19b
William Wong <compulim@users.noreply.github.com>
2023-09-07 06:40:22 +0000

    Allow different type for default value

--- packages/core/src/utils/onErrorResumeNext.ts
+++ packages/core/src/utils/onErrorResumeNext.ts
@@ -1,5 +1,5 @@
 export default function onErrorResumeNext<T>(fn: () => T): T | undefined;
-export default function onErrorResumeNext<T>(fn: () => T, defaultValue: T): T;
+export default function onErrorResumeNext<T, U = T>(fn: () => T, defaultValue: U): T | U;
 
 /**
  * Calls the function and returns its return value.
