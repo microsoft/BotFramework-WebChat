dc50322693ee28e12b3abdce1cce569194caba79
William Wong <compulim@users.noreply.github.com>
2023-09-14 21:45:06 +0000

    Move typecasting outside

--- packages/component/src/Styles/CSSTokens.ts
+++ packages/component/src/Styles/CSSTokens.ts
@@ -7,12 +7,15 @@ type CSSTokensType<T extends Readonly<Record<string, string>>> = {
 };
 
 // To add/remove/update a token, go to `CustomPropertyName.ts`.
-const CSSTokens = new Proxy<CSSTokensType<CustomPropertyNamesType>>({} as CSSTokensType<CustomPropertyNamesType>, {
-  get(_, key: keyof CustomPropertyNamesType) {
-    // We already checked in the `CustomPropertyName`.
-    // eslint-disable-next-line security/detect-object-injection
-    return `var(${CustomPropertyNames[key]})`;
+const CSSTokens = new Proxy(
+  {},
+  {
+    get(_, key: keyof CustomPropertyNamesType) {
+      // We already checked in the `CustomPropertyName`.
+      // eslint-disable-next-line security/detect-object-injection
+      return `var(${CustomPropertyNames[key]})`;
+    }
   }
-});
+) as CSSTokensType<CustomPropertyNamesType>;
 
 export default CSSTokens;
