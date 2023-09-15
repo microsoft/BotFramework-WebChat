b1aea1a3a7fc3931e6ce0271f6170d81d5914d4d
William Wong <compulim@users.noreply.github.com>
2023-09-15 18:00:52 +0000

    Added a test

--- packages/component/src/Attachment/Text/private/extractHostnameWithSubdomain.spec.ts
+++ packages/component/src/Attachment/Text/private/extractHostnameWithSubdomain.spec.ts
@@ -11,6 +11,7 @@ test.each([
   ['https://www.microsoft/index.html', 'microsoft'],
   ['https://www.例子.com/index.html', 'xn--fsqu00a.com'],
   ['https://www.example.com:8443/index.html', 'www.example.com:8443'],
+  ['https://www.example.com:8080', 'www.example.com:8080'],
   ['https://support.example.com/index.html', 'support.example.com'],
   ['https://www2.example.com/index.html', 'www2.example.com'],
   ['ftp://www.example.com/', 'ftp://www.example.com/'],
