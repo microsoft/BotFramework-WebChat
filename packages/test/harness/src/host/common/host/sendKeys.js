8842aab70381e5806dcb7d910e80c1a7e63c21b5
William Wong <compulim@users.noreply.github.com>
2023-09-16 05:23:18 +0000

    Add key up/down

--- packages/test/harness/src/host/common/host/sendKeys.js
+++ packages/test/harness/src/host/common/host/sendKeys.js
@@ -1,15 +1,41 @@
 const { Key } = require('selenium-webdriver');
 
-const isForbiddenPropertyName = require('../../../common/utils/isForbiddenPropertyName');
+const KeyMap = new Map(Object.entries(Key));
+
+const KEY_DOWN_PATTERN = /^\+(.*)/u;
+const KEY_UP_PATTERN = /^\+(.*)/u;
 
 module.exports = webDriver =>
   function sendKeys(...keys) {
     return keys
-      .reduce(
-        // Mitigated through denylisting.
-        // eslint-disable-next-line security/detect-object-injection
-        (actions, key) => actions.sendKeys((!isForbiddenPropertyName(key) && Key[key]) || key),
-        webDriver.actions()
-      )
+      .reduce((actions, key) => {
+        const keyDownMatch = KEY_DOWN_PATTERN.exec(key);
+
+        if (keyDownMatch) {
+          const keyDown = KeyMap.get(keyDownMatch[1]);
+
+          if (keyDown) {
+            return actions.keyDown(keyDown);
+          }
+        }
+
+        const keyUpMatch = KEY_UP_PATTERN.exec(key);
+
+        if (keyUpMatch) {
+          const keyUp = KeyMap.get(keyUpMatch[1]);
+
+          if (keyUp) {
+            return actions.keyUp(keyUp);
+          }
+        }
+
+        const keyToSend = KeyMap.get(key);
+
+        if (keyToSend) {
+          return actions.sendKeys(keyToSend);
+        }
+
+        return actions.sendKeys(key);
+      }, webDriver.actions())
       .perform();
   };
