e597a41b40d0bafa5d41e407851e6537bc4c2c93
William Wong <compulim@users.noreply.github.com>
2023-09-16 05:23:39 +0000

    Update constant name

--- packages/test/harness/src/host/common/host/sendKeys.js
+++ packages/test/harness/src/host/common/host/sendKeys.js
@@ -1,8 +1,7 @@
 const { Key } = require('selenium-webdriver');
 
-const KeyMap = new Map(Object.entries(Key));
-
 const KEY_DOWN_PATTERN = /^\+(.*)/u;
+const KEY_MAP = new Map(Object.entries(Key));
 const KEY_UP_PATTERN = /^\+(.*)/u;
 
 module.exports = webDriver =>
@@ -12,7 +11,7 @@ module.exports = webDriver =>
         const keyDownMatch = KEY_DOWN_PATTERN.exec(key);
 
         if (keyDownMatch) {
-          const keyDown = KeyMap.get(keyDownMatch[1]);
+          const keyDown = KEY_MAP.get(keyDownMatch[1]);
 
           if (keyDown) {
             return actions.keyDown(keyDown);
@@ -22,14 +21,14 @@ module.exports = webDriver =>
         const keyUpMatch = KEY_UP_PATTERN.exec(key);
 
         if (keyUpMatch) {
-          const keyUp = KeyMap.get(keyUpMatch[1]);
+          const keyUp = KEY_MAP.get(keyUpMatch[1]);
 
           if (keyUp) {
             return actions.keyUp(keyUp);
           }
         }
 
-        const keyToSend = KeyMap.get(key);
+        const keyToSend = KEY_MAP.get(key);
 
         if (keyToSend) {
           return actions.sendKeys(keyToSend);
