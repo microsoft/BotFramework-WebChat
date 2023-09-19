0dbd13d3915541a0a9de9f67c30981b2cd5c3e65
William Wong <compulim@users.noreply.github.com>
2023-09-19 03:11:57 +0000

    Rename tests

---
+++ __tests__/html/withEmoji.7.js
@@ -0,0 +1,5 @@
+/** @jest-environment ./packages/test/harness/src/host/jest/WebDriverEnvironment.js */
+
+describe('withEmoji scenario 7', () => {
+  test('should pass', () => runHTML('withEmoji.7.html'));
+});
---
+++ __tests__/html/withEmoji.9.js
@@ -0,0 +1,5 @@
+/** @jest-environment ./packages/test/harness/src/host/jest/WebDriverEnvironment.js */
+
+describe('withEmoji scenario 9', () => {
+  test('should pass', () => runHTML('withEmoji.9.html'));
+});
