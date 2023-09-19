0dbfa262ad317c3bacd3f484b480df8dc0f5e5fe
William Wong <compulim@users.noreply.github.com>
2023-09-19 02:07:43 +0000

    Disable dark theme

--- packages/component/src/Styles/StyleSet/Constants.ts
+++ packages/component/src/Styles/StyleSet/Constants.ts
@@ -1,6 +1,10 @@
-const DARK_THEME_SELECTOR = '@media (forced-colors: none) and (prefers-color-scheme: dark)';
+// TODO: Temporarily disable dark theme until all of Web Chat support dark theme.
+// const DARK_THEME_SELECTOR = '@media (forced-colors: none) and (prefers-color-scheme: dark)';
+// const LIGHT_THEME_SELECTOR = '@media (forced-colors: none) and (prefers-color-scheme: light)';
+const DARK_THEME_SELECTOR = '@media (forced-colors: none) and not (forced-colors: none)'; // Always return false
+const LIGHT_THEME_SELECTOR = '@media (forced-colors: none)';
+
 const FORCED_COLORS_SELECTOR = '@media (forced-colors: active)';
-const LIGHT_THEME_SELECTOR = '@media (forced-colors: none) and (prefers-color-scheme: light)';
 const NOT_FORCED_COLORS_SELECTOR = '@media (forced-colors: none)';
 
 export { DARK_THEME_SELECTOR, FORCED_COLORS_SELECTOR, LIGHT_THEME_SELECTOR, NOT_FORCED_COLORS_SELECTOR };
