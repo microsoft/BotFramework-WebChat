c29ea1b2f1aaf662142fff981082b050113928a8
William Wong <compulim@users.noreply.github.com>
2023-12-05 04:20:21 +0000

    Add GC to Jest

--- jest.config.js
+++ jest.config.js
@@ -114,7 +114,8 @@ module.exports = {
     '<rootDir>/__tests__/setup/setupCryptoGetRandomValues.js',
     '<rootDir>/__tests__/setup/setupImageSnapshot.js',
     '<rootDir>/__tests__/setup/setupTestNightly.js',
-    '<rootDir>/__tests__/setup/setupTimeout.js'
+    '<rootDir>/__tests__/setup/setupTimeout.js',
+    '<rootDir>/__tests__/setup/setupGlobalGarbageCollect.js'
   ],
   testPathIgnorePatterns: [
     '/lib/',
