7d4491e33a9b7c17f2366b94342e9000abcf04a6
ms-jb <jordanberger@microsoft.com>
2024-03-08 08:24:35 -0700

    Rename and add multi upload prop

--- packages/api/src/defaultStyleOptions.ts
+++ packages/api/src/defaultStyleOptions.ts
@@ -97,7 +97,7 @@ const DEFAULT_OPTIONS: Required<StyleOptions> = {
   sendBoxBackground: 'White',
   uploadAccept: undefined,
   uploadMultiple: true,
-  attachmentAccept: '*',
+  uploadFileTypes: '*',
 
   // Send box buttons
   sendBoxButtonColor: undefined,
