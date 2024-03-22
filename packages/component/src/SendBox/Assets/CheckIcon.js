56b7ad90d2d9b5e9dd3f3d58629c9f9518ad3519
ms-jb <jordanberger@microsoft.com>
2024-03-22 12:55:50 -0600

    Switch to check icon

---
+++ packages/component/src/SendBox/Assets/CheckIcon.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CheckIcon = () => (
+  <svg focusable={false} height={28} role="presentation" viewBox="0 0 2048 2048" width={28}>
+    <path clipRule="evenodd" d="M1837 557L768 1627l-557-558 90-90 467 466 979-978 90 90z" />
+  </svg>
+);
+
+export default CheckIcon;
