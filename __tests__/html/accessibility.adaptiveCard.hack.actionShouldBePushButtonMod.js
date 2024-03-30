04b84644885711ebd662548af66baff26dcf6671
William Wong <compulim@users.noreply.github.com>
2024-03-30 06:37:56 +0000

    Add test entrypoints

---
+++ __tests__/html/accessibility.adaptiveCard.hack.actionShouldBePushButtonMod.js
@@ -0,0 +1,5 @@
+/** @jest-environment ./packages/test/harness/src/host/jest/WebDriverEnvironment.js */
+
+describe('accessibility hacks for Adaptive Cards', () => {
+  test('action should be push button', () => runHTML('accessibility.adaptiveCard.hack.actionShouldBePushButton.html'));
+});
