67319e54dc08b3a607671b63d6e996161e51b51f
William Wong <compulim@users.noreply.github.com>
2024-04-04 10:01:19 +0000

    Add deprecation warning

--- packages/api/src/hooks/useSendFiles.ts
+++ packages/api/src/hooks/useSendFiles.ts
@@ -3,8 +3,13 @@
 import { type WebChatPostActivityAttachment } from 'botframework-webchat-core';
 import { useCallback } from 'react';
 
+import { warnOnce } from 'botframework-webchat-core';
 import useWebChatAPIContext from './internal/useWebChatAPIContext';
 import useTrackEvent from './useTrackEvent';
+
+const warnDeprecation = warnOnce(
+  'This hook will be removed on or after 2026-04-03. Please use `useSendMessage` instead.'
+);
 
 /**
  * @deprecated This hook will be removed on or after 2026-04-03. Please use `useSendMessage` instead.
@@ -16,6 +21,7 @@ export default function useSendFiles(): (files: readonly WebChatPostActivityAtta
   return useCallback(
     files => {
       if (files && files.length) {
+        warnDeprecation();
         sendFiles(files);
 
         trackEvent('sendFiles', {
