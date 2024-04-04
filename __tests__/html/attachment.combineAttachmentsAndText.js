26d78fc90db4d7c46c87cd66e45abb47fcba3684
William Wong <compulim@users.noreply.github.com>
2024-04-04 10:21:53 +0000

    Delete test

--- __tests__/html/attachment.combineAttachmentsAndText.js
+++
@@ -1,6 +0,0 @@
-/** @jest-environment ./packages/test/harness/src/host/jest/WebDriverEnvironment.js */
-
-describe('Attachment', () => {
-  test('with combined attachments and text', () =>
-    runHTML('attachment.combineAttachmentsAndText.html'));
-});
