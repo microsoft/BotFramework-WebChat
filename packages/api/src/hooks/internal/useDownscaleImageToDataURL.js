1af831e3b3e10733b5b8c053ffae91413547681f
William Wong <compulim@users.noreply.github.com>
2024-04-06 22:46:08 +0000

    Remove unused file

--- packages/api/src/hooks/internal/useDownscaleImageToDataURL.js
+++
@@ -1,7 +0,0 @@
-import useWebChatAPIContext from './useWebChatAPIContext';
-
-export default function useDownscaleImageToDataURL() {
-  const { downscaleImageToDataURL } = useWebChatAPIContext();
-
-  return downscaleImageToDataURL;
-}
