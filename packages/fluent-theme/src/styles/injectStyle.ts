0187567533cdc054ca194a8a4604a63a5fd4c93e
Eugene Olonov <eolonov@gmail.com>
2024-04-15 14:33:42 -0700

    Fix nits

--- packages/fluent-theme/src/styles/injectStyle.ts
+++ packages/fluent-theme/src/styles/injectStyle.ts
@@ -3,7 +3,7 @@ export const injectedStyles = '@--INJECTED-STYLES-CONTENT--@';
 export default function injectStyles() {
   if (globalThis.document) {
     const style = document.createElement('style');
-    style.innerText = injectedStyles;
+    style.append(document.createTextNode(injectedStyles));
     document.head.appendChild(style);
   }
 }
