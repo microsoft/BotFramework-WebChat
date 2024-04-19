025b213844414113936c1233991c09da8c77f96e
Eugene <EOlonov@gmail.com>
2024-04-19 01:35:02 -0400

    Do not use non-recommended styleSet in samples (#5023)
    Co-authored-by: William Wong <compulim@users.noreply.github.com>

--- samples/06.recomposing-ui/a.minimizable-web-chat/src/WebChat.js
+++ samples/06.recomposing-ui/a.minimizable-web-chat/src/WebChat.js
@@ -1,25 +1,22 @@
 import React, { useEffect, useMemo } from 'react';
-import ReactWebChat, { createDirectLine, createStyleSet } from 'botframework-webchat';
+import ReactWebChat, { createDirectLine } from 'botframework-webchat';
 
 import './WebChat.css';
 
-const WebChat = ({ className, onFetchToken, store, token }) => {
+const WebChat = ({ className, onFetchToken, store, styleOptions, token }) => {
   const directLine = useMemo(() => createDirectLine({ token }), [token]);
-
-  const styleSet = useMemo(
-    () =>
-      createStyleSet({
-        backgroundColor: 'Transparent'
-      }),
-    []
-  );
 
   useEffect(() => {
     onFetchToken();
   }, [onFetchToken]);
 
   return token ? (
-    <ReactWebChat className={`${className || ''} web-chat`} directLine={directLine} store={store} styleSet={styleSet} />
+    <ReactWebChat
+      className={`${className || ''} web-chat`}
+      directLine={directLine}
+      store={store}
+      styleOptions={styleOptions}
+    />
   ) : (
     <div className={`${className || ''} connect-spinner`}>
       <div className="content">
