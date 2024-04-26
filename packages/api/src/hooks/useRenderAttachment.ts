42ce5c105c26ddbd93206965d60e711a08433450
William Wong <compulim@users.noreply.github.com>
2024-04-26 01:56:51 +0000

    Rename to .ts

--- packages/api/src/hooks/useRenderAttachment.ts
+++ packages/api/src/hooks/useRenderAttachment.ts
@@ -1,6 +1,7 @@
+import { type RenderAttachment } from '../types/AttachmentMiddleware';
 import useWebChatAPIContext from './internal/useWebChatAPIContext';
 
-export default function useRenderAttachment() {
+export default function useRenderAttachment(): RenderAttachment | undefined {
   const { attachmentRenderer } = useWebChatAPIContext();
 
   return attachmentRenderer;
