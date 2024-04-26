f205335ab29302d46064816fe34b092ed192e1fe
William Wong <compulim@users.noreply.github.com>
2024-04-26 23:26:06 +0000

    Add instrumentation to tsup

--- tsup.base.config.ts
+++ tsup.base.config.ts
@@ -1,9 +1,11 @@
+import { esbuildPluginIstanbul } from 'esbuild-plugin-istanbul';
 import { defineConfig, type Options } from 'tsup';
 
 const env = process.env.NODE_ENV || 'development';
 const { npm_package_version } = process.env;
 
 export default defineConfig({
+  dts: true,
   env: {
     node_env: env,
     NODE_ENV: env,
@@ -12,8 +14,15 @@ export default defineConfig({
   esbuildOptions: options => {
     options.legalComments = 'linked';
   },
-  clean: true,
-  dts: true,
+  esbuildPlugins:
+    env === 'test'
+      ? [
+          esbuildPluginIstanbul({ filter: /\.[cm]?js$/u, loader: 'jsx', name: 'istanbul-loader-js' }),
+          esbuildPluginIstanbul({ filter: /\.jsx$/u, loader: 'jsx', name: 'istanbul-loader-jsx' }),
+          esbuildPluginIstanbul({ filter: /\.[cm]?ts$/u, loader: 'ts', name: 'istanbul-loader-ts' }),
+          esbuildPluginIstanbul({ filter: /\.tsx$/u, loader: 'tsx', name: 'istanbul-loader-tsx' })
+        ]
+      : [],
   format: 'esm',
   loader: { '.js': 'jsx' },
   metafile: true,
