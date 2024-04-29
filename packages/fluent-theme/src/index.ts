bb3c12ad153fdc4f0d69f2d6ec1c3491f133d10c
William Wong <compulim@users.noreply.github.com>
2024-04-29 21:56:43 +0000

    Remove older tag

--- packages/fluent-theme/src/index.ts
+++ packages/fluent-theme/src/index.ts
@@ -4,9 +4,9 @@ import FluentThemeProvider from './private/FluentThemeProvider';
 import { injectStyle } from './styles';
 import testIds from './testIds';
 
-injectMetaTag('botframework-webchat-fluent-theme:version', process.env['npm_package_version']);
+injectMetaTag('botframework-webchat:fluent-theme:version', process.env['npm_package_version']);
 injectMetaTag(
-  'botframework-webchat-fluent-theme',
+  'botframework-webchat:fluent-theme',
   `version=${process.env['npm_package_version']}; format=${process.env['module_format']}; transpiler=${process.env['transpiler']}`
 );
 
