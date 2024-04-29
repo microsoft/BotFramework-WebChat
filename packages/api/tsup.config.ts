9fd9535ff857d19031d8347cf45f587c1c4f42c8
William Wong <compulim@users.noreply.github.com>
2024-04-29 23:12:11 +0000

    Clean up

--- packages/api/tsup.config.ts
+++ packages/api/tsup.config.ts
@@ -3,7 +3,6 @@ import baseConfig from '../../tsup.base.config';
 
 export default defineConfig({
   ...baseConfig,
-  dts: { resolve: true },
   entry: {
     'botframework-webchat-api': './src/index.ts',
     'botframework-webchat-api.internal': './src/internal.ts'
