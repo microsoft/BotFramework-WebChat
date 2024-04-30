18c8a8e1287e0173d0095a7f1ae5f4cf61ce9c50
Eugene Olonov <eolonov@gmail.com>
2024-04-30 08:52:07 -0700

    Update use-propagate

--- packages/component/src/hooks/sendBoxFocus.ts
+++ packages/component/src/hooks/sendBoxFocus.ts
@@ -1,49 +1,5 @@
 import { WaitUntilable } from './internal/createWaitUntilable';
-
-// TODO [P0]: remove and use `use-propagate` when it
-// is able to work with React v16+
-// import { createPropagation } from 'use-propagate';
-import { useEffect } from 'react';
-import { useRefFrom } from 'use-ref-from';
-
-function removeInline<T>(array: T[], ...items: T[]): void {
-  for (const item of items) {
-    let index;
-
-    while (~(index = array.indexOf(item))) {
-      array.splice(index, 1);
-    }
-  }
-}
-
-type Listener<T> = (value: T) => void;
-
-export default function createPropagation<T>() {
-  type Fn = Listener<T>;
-
-  const listeners: Fn[] = [];
-
-  // eslint-disable-next-line no-void
-  const addListener = (listener: Fn): void => void listeners.push(listener);
-  const removeListener = (listener: Fn): void => removeInline(listeners, listener);
-  const usePropagate = () => (value: T) => listeners.forEach(listener => listener(value));
-
-  return {
-    useListen: (listener: Fn) => {
-      const listenerRef = useRefFrom(listener);
-
-      useEffect(() => {
-        const wrappingListener = (value: T) => listenerRef.current(value);
-
-        addListener(wrappingListener);
-
-        return () => removeListener(wrappingListener);
-      }, [listenerRef]);
-    },
-    usePropagate
-  };
-}
-// end of the TODO
+import { createPropagation } from 'use-propagate';
 
 export type SendBoxFocusOptions = WaitUntilable<{ noKeyboard: boolean }>;
 
