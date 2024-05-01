82be6dbd38064a46216407e781c27095a87df363
William Wong <compulim@users.noreply.github.com>
2024-05-01 00:54:20 +0000

    Add performance test

---
+++ __tests__/html/performance/manyMessages.allAtOnce.js
@@ -0,0 +1,6 @@
+/** @jest-environment ./packages/test/harness/src/host/jest/WebDriverEnvironment.js */
+
+describe('time taken to render 200 activities at batch of 20', () => {
+  test('should have similar performance for first half and second half', () =>
+    runHTML('performnace/manyMessages.allAtOnce.html'));
+});
