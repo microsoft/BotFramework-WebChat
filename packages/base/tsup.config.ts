8204e4d60be37ef7672e04f4ee7404f5d1ae11a4
Eugene Olonov <eolonov@gmail.com>
2024-05-08 11:16:03 -0700

    Add base package

---
+++ packages/base/tsup.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'tsup';
+import baseConfig from '../../tsup.base.config';
+
+export default defineConfig({
+  ...baseConfig,
+  entry: {
+    'botframework-webchat-base': './src/index.ts'
+  }
+});
