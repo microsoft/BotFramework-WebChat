423677c000dccaf149f95f79f3182797a85a492d
William Wong <compulim@users.noreply.github.com>
2024-05-08 19:32:50 +0000

    Replace headless with addArguments

--- __tests__/setup/setupTestEnvironment.js
+++ __tests__/setup/setupTestEnvironment.js
@@ -6,34 +6,37 @@ export default function setupTestEnvironment(browserName, builder, { height = 64
 
   preferences.setLevel(logging.Type.BROWSER, logging.Level.WARNING);
 
+  let chromeOptions;
+
   switch (browserName) {
     case 'chrome-local':
-      const localOptions = (builder.getChromeOptions() || new Options()).windowSize({
+      chromeOptions = (builder.getChromeOptions() || new Options()).windowSize({
         height: height * zoom,
         width: width * zoom
       });
 
-      localOptions.setLoggingPrefs(preferences);
+      chromeOptions.setLoggingPrefs(preferences);
 
       return {
         baseURL: 'http://localhost:$PORT/index.html',
-        builder: builder.forBrowser('chrome').setChromeOptions(localOptions)
+        builder: builder.forBrowser('chrome').setChromeOptions(chromeOptions)
       };
 
     case 'chrome-docker':
-    default:
-      const dockerOptions = (builder.getChromeOptions() || new Options())
-        .headless()
+    default: {
+      chromeOptions = (builder.getChromeOptions() || new Options())
+        .addArguments('--headless=chrome') // Should change to "--headless=new" for Chrome >= 109
         .windowSize({ height: height * zoom, width: width * zoom });
 
-      dockerOptions.setLoggingPrefs(preferences);
+      chromeOptions.setLoggingPrefs(preferences);
 
       return {
         baseURL: 'http://webchat/',
         builder: builder
           .forBrowser('chrome')
           .usingServer('http://localhost:4444/wd/hub')
-          .setChromeOptions(dockerOptions)
+          .setChromeOptions(chromeOptions)
       };
+    }
   }
 }
--- packages/test/harness/src/host/jest/allocateWebDriver.js
+++ packages/test/harness/src/host/jest/allocateWebDriver.js
@@ -8,9 +8,12 @@ module.exports = async function allocateWebDriver({ webDriverURL }) {
 
   preferences.setLevel(logging.Type.BROWSER, logging.Level.ALL);
 
-  const builder = new Builder()
-    .forBrowser('chrome')
-    .setChromeOptions(new ChromeOptions().addArguments('--single-process').headless().setLoggingPrefs(preferences));
+  const builder = new Builder().forBrowser('chrome').setChromeOptions(
+    new ChromeOptions()
+      .addArguments('--headless=chrome') // Should change to "--headless=new" for Chrome >= 109
+      .addArguments('--single-process')
+      .setLoggingPrefs(preferences)
+  );
 
   const webDriver = (global.webDriver = await builder.usingServer(webDriverURL).build());
 
