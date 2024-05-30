44d88d648efd585eba80e8e0391b2d86b90dd27d
Eugene Olonov <eolonov@gmail.com>
2024-05-30 16:05:00 -0700

    Fix telemetry init and small improvements

--- packages/component/src/hooks/internal/useInjectStyles.ts
+++ packages/component/src/hooks/internal/useInjectStyles.ts
@@ -4,16 +4,15 @@ import { useStylesRoot } from './useStylesRoot';
 export default function useInjectStyles(styles: readonly HTMLStyleElement[]) {
   const root = useStylesRoot();
   useEffect(() => {
-    if (!root || !styles?.length) {
-      return;
-    }
-    for (const style of styles) {
-      root.appendChild(style);
-    }
-    return () => {
+    if (root && styles?.length) {
       for (const style of styles) {
-        style.parentElement?.removeChild(style);
+        root.appendChild(style);
       }
-    };
+      return () => {
+        for (const style of styles) {
+          style.remove();
+        }
+      };
+    }
   }, [root, styles]);
 }
