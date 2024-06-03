a80a4a1d2f29c6de02a9ee7e0557d96dd5707363
Eugene <EOlonov@gmail.com>
2024-06-03 19:26:41 +0000

    Fix nonce removal

--- packages/component/src/hooks/internal/useInjectStyles.ts
+++ packages/component/src/hooks/internal/useInjectStyles.ts
@@ -18,7 +18,7 @@ export default function useInjectStyles(styles: readonly HTMLStyleElement[], non
         // if it's already added to the different root. we clone the style node and add it to the new root
         const injectedStyle = isAddedToTheDOM && !isAddedToTheRoot ? (style.cloneNode() as HTMLStyleElement) : style;
         // Update nonce in case it got changed
-        nonce && injectedStyle.setAttribute('nonce', nonce);
+        nonce ? injectedStyle.setAttribute('nonce', nonce) : injectedStyle.removeAttribute('nonce');
         if (!isAddedToTheRoot) {
           stylesRoot.appendChild(injectedStyle);
         }
