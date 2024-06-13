825e4f0d32ffebdeffd31791c7c94f8aef32cacf
William Wong <compulim@users.noreply.github.com>
2024-06-13 02:23:41 +0000

    Adding tests and clean up useInjectStyles

---
+++ packages/bundle/src/index-withInternal.ts
@@ -0,0 +1,12 @@
+/* eslint dot-notation: ["error", { "allowPattern": "^WebChat$" }] */
+// window['WebChat'] is required for TypeScript
+
+import { useInjectStyles } from 'botframework-webchat-component/internal';
+import './index';
+
+export { useInjectStyles };
+
+window['WebChat'] = {
+  ...window['WebChat'],
+  internal: { useInjectStyles }
+};
