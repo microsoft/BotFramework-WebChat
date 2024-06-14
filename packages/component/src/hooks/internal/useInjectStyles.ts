f931a16c3dabcd3da24923f212214b5120062eb8
William Wong <compulim@users.noreply.github.com>
2024-06-14 01:07:51 +0000

    Add tests

--- packages/component/src/hooks/internal/useInjectStyles.ts
+++ packages/component/src/hooks/internal/useInjectStyles.ts
@@ -1,5 +1,5 @@
 import { hooks } from 'botframework-webchat-api';
-import { useEffect, useMemo } from 'react';
+import { useEffect } from 'react';
 
 const { useStyleOptions } = hooks;
 
@@ -13,28 +13,42 @@ type InjectedStylesInstance = Readonly<{
 
 const sharedInstances: InjectedStylesInstance[] = [];
 
+function arrayEqual<T>(x: readonly T[], y: readonly T[]): boolean {
+  if (x.length !== y.length) {
+    return false;
+  }
+
+  const set = new Set<T>(x);
+
+  for (const value of y) {
+    if (set.has(value)) {
+      set.delete(value);
+    } else {
+      return false;
+    }
+  }
+
+  return true;
+}
+
 export default function useInjectStyles(styles: readonly InjectedStylesElement[], nonce?: string) {
+  for (const style of styles) {
+    if (style.localName !== 'style' && !(style.localName === 'link' && style.getAttribute('rel') === 'stylesheet')) {
+      throw new Error(
+        `botframework-webchat: useInjectStyles() hook supports injecting <link rel="stylesheet"> or <style> only, got <${style.localName}>.`
+      );
+    }
+  }
+
   const [{ stylesRoot: root }] = useStyleOptions();
 
-  const instance = useMemo(() => {
+  useEffect(() => {
     if (!root || !styles.length) {
       return;
     }
 
-    for (const style of styles) {
-      if (style.localName !== 'style' && !(style.localName === 'link' && style.getAttribute('rel') === 'stylesheet')) {
-        throw new Error(
-          `botframework-webchat: useInjectStyles() hook supports injecting <link rel="stylesheet"> or <style> only, got <${style.localName}>.`
-        );
-      }
-    }
-
     let instance = sharedInstances.find(
-      instance =>
-        instance.styles.length === styles.length &&
-        instance.styles.every(style => styles.includes(style)) &&
-        instance.root === root &&
-        instance.nonce === nonce
+      instance => arrayEqual(instance.styles, styles) && instance.root === root && instance.nonce === nonce
     );
 
     if (!instance) {
@@ -49,6 +63,7 @@ export default function useInjectStyles(styles: readonly InjectedStylesElement[]
 
       for (const style of instance.styles) {
         nonce ? style.setAttribute('nonce', nonce) : style.removeAttribute('nonce');
+
         root.appendChild(style);
       }
     }
@@ -56,23 +71,16 @@ export default function useInjectStyles(styles: readonly InjectedStylesElement[]
     // Extraneous push is intentional. It is for ref counting.
     sharedInstances.push(instance);
 
-    return instance;
-  }, [root, nonce, styles]);
+    return () => {
+      const index = sharedInstances.lastIndexOf(instance);
 
-  useEffect(
-    () =>
-      instance &&
-      (() => {
-        const index = sharedInstances.lastIndexOf(instance);
+      ~index && sharedInstances.splice(index, 1);
 
-        ~index && sharedInstances.splice(index, 1);
-
-        if (!sharedInstances.includes(instance)) {
-          for (const style of instance.styles) {
-            style.remove();
-          }
+      if (!sharedInstances.includes(instance)) {
+        for (const style of instance.styles) {
+          style.remove();
         }
-      }),
-    [instance]
-  );
+      }
+    };
+  }, [nonce, root, styles]);
 }
