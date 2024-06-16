9c6cf51cc4269f19b30aefaa0184ff4caa311079
Amir Mursal <amirmursal@microsoft.com>
2024-06-16 15:04:49 +0530

    removed unwanted indentations

--- packages/component/src/BasicTranscript.tsx
+++ packages/component/src/BasicTranscript.tsx
@@ -345,16 +345,16 @@ const InternalTranscript = forwardRef<HTMLDivElement, InternalTranscriptProps>(
         const activityKeyJustAboveScrollBottom: string | undefined = (
           scrollableElement.scrollTop
             ? activityElements
-              .reverse()
-              // Add subpixel tolerance
-              .find(([, element]) => {
-                // "getClientRects()" is not returning an array, thus, it is not destructurable.
-                // eslint-disable-next-line prefer-destructuring
-                const elementClientRect = element.getClientRects()[0];
+                .reverse()
+                // Add subpixel tolerance
+                .find(([, element]) => {
+                  // "getClientRects()" is not returning an array, thus, it is not destructurable.
+                  // eslint-disable-next-line prefer-destructuring
+                  const elementClientRect = element.getClientRects()[0];
 
-                // If the activity is not attached to DOM tree, we should not count it as "bottommost visible activity", as it is not visible.
-                return elementClientRect && elementClientRect.bottom < scrollableClientBottom + 1;
-              })
+                   // If the activity is not attached to DOM tree, we should not count it as "bottommost visible activity", as it is not visible.
+                 return elementClientRect && elementClientRect.bottom < scrollableClientBottom + 1;
+               })
             : activityElements[0]
         )?.[0];
 
