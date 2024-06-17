e86398870746de125eb039dde8454f17adb9ab77
William Wong <compulim@users.noreply.github.com>
2024-06-17 16:04:28 -0700

    Sort

--- packages/api/src/decorator/private/ActivityBorderDecoratorMiddleware.ts
+++ packages/api/src/decorator/private/ActivityBorderDecoratorMiddleware.ts
@@ -2,18 +2,18 @@ import ActivityDecoratorRequest from './activityDecoratorRequest';
 import templateMiddleware from './templateMiddleware';
 
 const {
+  initMiddleware: initActivityBorderDecoratorMiddleware,
   Provider: ActivityBorderDecoratorMiddlewareProvider,
   Proxy: ActivityBorderDecoratorMiddlewareProxy,
-  initMiddleware: initActivityBorderDecoratorMiddleware,
   types
 } = templateMiddleware<{}, ActivityDecoratorRequest, typeof activityBorderDecoratorTypeName>(
   'ActivityBorderDecoratorMiddleware'
 );
 
 type ActivityBorderDecoratorMiddleware = typeof types.middleware;
+type ActivityBorderDecoratorMiddlewareInit = typeof types.init;
 type ActivityBorderDecoratorMiddlewareProps = typeof types.props;
 type ActivityBorderDecoratorMiddlewareRequest = typeof types.request;
-type ActivityBorderDecoratorMiddlewareInit = typeof types.init;
 
 const activityBorderDecoratorTypeName = 'activity border' as const;
 
@@ -23,7 +23,7 @@ export {
   activityBorderDecoratorTypeName,
   initActivityBorderDecoratorMiddleware,
   type ActivityBorderDecoratorMiddleware,
+  type ActivityBorderDecoratorMiddlewareInit,
   type ActivityBorderDecoratorMiddlewareProps,
-  type ActivityBorderDecoratorMiddlewareRequest,
-  type ActivityBorderDecoratorMiddlewareInit
+  type ActivityBorderDecoratorMiddlewareRequest
 };
