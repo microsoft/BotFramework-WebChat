770f38def88a8a20e98be6b912008b56b2a884f8
Amir Mursal <amirmursal@microsoft.com>
2024-06-20 15:05:39 +0530

    removed keyboard helper screen from basic transcript

--- __tests__/html/accessibility.keyboardHelp.reducedHeight.js
+++
@@ -1,6 +0,0 @@
-/** @jest-environment ./packages/test/harness/src/host/jest/WebDriverEnvironment.js */
-
-describe('keyboard help screen with reduced height', () => {
-  test('should show a scrollbar', () =>
-    runHTML('accessibility.keyboardHelp.reducedHeight'));
-});
--- packages/test/page-object/src/globals/pageElements/keyboardHelpScreenCloseButton.js
+++
@@ -1,3 +0,0 @@
-export default function keyboardHelpScreenCloseButton() {
-  return document.querySelector('button.webchat__keyboard-help__close-button[type="button"]');
-}
