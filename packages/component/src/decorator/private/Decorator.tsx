aa70aef043af2dcf66d6c517837659eb5ade8469
Eugene Olonov <eolonov@gmail.com>
2024-06-27 11:29:12 -0700

    Firefox implementation

--- packages/component/src/decorator/private/Decorator.tsx
+++ packages/component/src/decorator/private/Decorator.tsx
@@ -1,11 +1,10 @@
 import { DecoratorComposer, type DecoratorMiddleware } from 'botframework-webchat-api/decorator';
-import React, { memo, useEffect, type ReactNode } from 'react';
+import React, { memo, type ReactNode } from 'react';
 
 import BorderFlair from './BorderFlair';
 import BorderLoader from './BorderLoader';
 import createStyles from './createStyles';
 import ThemeProvider from '../../providers/Theme/ThemeProvider';
-import initializeCustomProperties from './initializeCustomProperties';
 
 const middleware: DecoratorMiddleware[] = [
   init =>
@@ -17,9 +16,6 @@ const middleware: DecoratorMiddleware[] = [
 const styles = createStyles();
 
 function WebChatDecorator({ children }: Readonly<{ readonly children?: ReactNode | undefined }>) {
-  useEffect(() => {
-    initializeCustomProperties();
-  });
   return (
     <ThemeProvider styles={styles}>
       <DecoratorComposer middleware={middleware}>{children}</DecoratorComposer>
