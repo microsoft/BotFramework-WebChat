8dd138ec3bfbe7c2dde904a1896c3e07908b5846
Amir Mursal <amirmursal@microsoft.com>
2024-07-02 12:02:42 +0530

    remove keyboard helper screen from web chat

--- __tests__/html/accessibility.keyboardHelp.ariaLabel.js
+++
@@ -1,5 +0,0 @@
-/** @jest-environment ./packages/test/harness/src/host/jest/WebDriverEnvironment.js */
-
-describe('keyboard help screen', () => {
-  test('should have a label', () => runHTML('accessibility.keyboardHelp.ariaLabel'));
-});
--- __tests__/html/accessibility.keyboardHelp.darkTheme.js
+++
@@ -1,6 +0,0 @@
-/** @jest-environment ./packages/test/harness/src/host/jest/WebDriverEnvironment.js */
-
-describe('keyboard help screen', () => {
-  test('should show with dark theme', () =>
-    runHTML('accessibility.keyboardHelp.darkTheme'));
-});
