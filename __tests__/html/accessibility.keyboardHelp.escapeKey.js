4e0bb4f819b6a50af410f5c9608f971edc4a1769
Amir Mursal <amirmursal@microsoft.com>
2024-07-02 12:02:42 +0530

    remove keyboard helper screen from web chat

--- __tests__/html/accessibility.keyboardHelp.escapeKey.js
+++
@@ -1,6 +0,0 @@
-/** @jest-environment ./packages/test/harness/src/host/jest/WebDriverEnvironment.js */
-
-describe('keyboard help screen', () => {
-  test('should close when ESCAPE key is pressed', () =>
-    runHTML('accessibility.keyboardHelp.escapeKey'));
-});
