62dae2cb781bcb7c03e6b8b649e06482dd753e83
Eugene Olonov <eolonov@gmail.com>
2024-07-08 15:46:21 -0700

    Add injeced styles check

--- packages/styles/src/build/private/injectCSSPlugin.ts
+++ packages/styles/src/build/private/injectCSSPlugin.ts
@@ -21,14 +21,18 @@ export default function injectCSSPlugin({ stylesPlaceholder }: InjectCSSPluginOp
             const js = file;
             const entryName = js.path.replace(/(\.js|\.mjs)$/u, '');
             const css = outputFiles.find(f => f.path.replace(/(\.css)$/u, '') === entryName);
-            if (css && js?.text.includes(stylesPlaceholderQuoted)) {
-              const jsText = js.text;
+            const jsText = js?.text;
+            if (css && jsText?.includes(stylesPlaceholderQuoted)) {
               const cssText = JSON.stringify(css.text);
               const index = jsText.indexOf(stylesPlaceholderQuoted);
               const map = outputFiles.find(f => f.path.replace(/(\.map)$/u, '') === js.path);
 
               const updatedJsText = [jsText.slice(0, index), cssText, jsText.slice(index + stylesPlaceholderQuoted.length)].join('');
               js.contents = Buffer.from(updatedJsText);
+
+              if (updatedJsText.indexOf(stylesPlaceholder) !== -1) {
+                throw new Error(`Duplicate placeholders are not supported.\nFound ${stylesPlaceholder} in ${js.path}.`)
+              }
 
               if (map) {
                 const parsed = JSON.parse(map.text);
