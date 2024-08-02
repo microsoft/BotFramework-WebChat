c36ddfcf7c8a09453475c4087f78d2a4b29379b7
William Wong <compulim@users.noreply.github.com>
2024-08-02 23:40:45 +0000

    Fix ASE token

--- packages/test/page-object/src/globals/testHelpers/token/fetchDirectLineAppServiceExtensionToken.js
+++ packages/test/page-object/src/globals/testHelpers/token/fetchDirectLineAppServiceExtensionToken.js
@@ -1,7 +1,8 @@
-export default async function fetchDirectLineAppServiceExtensionToken(
-  url = 'https://hawo-mockbot4-token-app.blueriver-ce85e8f0.westus.azurecontainerapps.io/api/token/directlinease'
-) {
-  const res = await fetch(url, { method: 'POST' });
+export default async function fetchDirectLineAppServiceExtensionToken() {
+  const res = await fetch(
+    'https://hawo-mockbot4-token-app.blueriver-ce85e8f0.westus.azurecontainerapps.io/api/token/directlinease',
+    { method: 'POST' }
+  );
 
   if (!res.ok) {
     throw new Error('Failed to fetch Direct Line App Service Extension token.');
@@ -9,5 +10,5 @@ export default async function fetchDirectLineAppServiceExtensionToken(
 
   const { token } = await res.json();
 
-  return token;
+  return { domain: new URL('https://hawo-mockbot4-bot-app.azurewebsites.net/.bot/v3/directline').toString(), token };
 }
