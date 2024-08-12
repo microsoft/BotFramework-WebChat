8c650327b4ca97cf0b875a1451797f32705f5425
Eugene Olonov <eolonov@gmail.com>
2024-08-12 15:00:29 -0700

    Dark theme support and modal styling

---
+++ __tests__/html/fluentTheme/side-by-side.wide.dark.js
@@ -0,0 +1,8 @@
+/** @jest-environment ./packages/test/harness/src/host/jest/WebDriverEnvironment.js */
+
+describe('Fluent theme applied', () => {
+  describe('dark theme applied', () => {
+    test('side by side left - transcript, right - pre chat message', () => runHTML('fluentTheme/side-by-side.wide.dark?focus=1'));
+    test('side by side left - transcript, right - transcript', () => runHTML('fluentTheme/side-by-side.wide.dark?transcript=0&transcript=2&focus=0&focus=1'));
+  });
+});
