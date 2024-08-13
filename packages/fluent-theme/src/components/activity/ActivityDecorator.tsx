d51c57e0fdf3e4f9f92428df1018ee0a122f3492
William Wong <compulim@users.noreply.github.com>
2024-08-13 18:38:14 +0000

    Sort/newline

--- packages/fluent-theme/src/components/activity/ActivityDecorator.tsx
+++ packages/fluent-theme/src/components/activity/ActivityDecorator.tsx
@@ -1,16 +1,18 @@
-import React, { ReactNode, memo } from 'react';
-import styles from './ActivityDecorator.module.css';
-import { useStyles, useVariantClassName } from '../../styles';
-import cx from 'classnames';
-import useVariants from '../../private/useVariants';
 import { WebChatActivity } from 'botframework-webchat-component';
+import cx from 'classnames';
+import React, { ReactNode, memo } from 'react';
+import useVariants from '../../private/useVariants';
+import { useStyles, useVariantClassName } from '../../styles';
+import styles from './ActivityDecorator.module.css';
 import CopilotMessageHeader from './CopilotMessageHeader';
 
 function ActivityDecorator({ activity, children }: Readonly<{ activity: WebChatActivity; children: ReactNode }>) {
   const classNames = useStyles(styles);
   const variants = useVariants();
   const variantClassName = useVariantClassName(styles);
+
   const shouldRenderHeader = variants.includes('copilot') && activity?.from?.role !== 'user' && !!children;
+
   return (
     <div className={cx(classNames['activity-decorator'], variantClassName)}>
       {shouldRenderHeader && <CopilotMessageHeader activity={activity} />}
