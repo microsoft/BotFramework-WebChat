287eb6784d9996cbfe6e7c62730642b074fdb988
William Wong <compulim@users.noreply.github.com>
2024-08-22 08:22:40 +0000

    Sort

--- packages/api/src/decorator/private/activityDecoratorRequest.ts
+++ packages/api/src/decorator/private/activityDecoratorRequest.ts
@@ -7,7 +7,7 @@ type ActivityDecoratorRequestType = {
    * - `"preparing"` - decorate as the livestreaming is being prepared
    * - `undefined` - not participated in a livestreaming session
    */
-  decorateForLivestreaming: 'completing' | 'preparing' | 'ongoing' | undefined;
+  decorateForLivestreaming: 'completing' | 'ongoing' | 'preparing' | undefined;
 
   /**
    * Gets the role of the sender for the activity.
