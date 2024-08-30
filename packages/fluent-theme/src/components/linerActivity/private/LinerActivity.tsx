aa455077a20d3666c2805c111ade0e1ba21f632c
Eugene <EOlonov@gmail.com>
2024-08-30 21:42:51 +0000

    Fluent: liner message activity (#5272)
    * Fluent: liner message activity
    
    * Changelog
    
    * update snapshots
    
    * Tweak from Figma and add snapshots
    
    * Fix lint
    
    * Update other snaps
    
    * Sort
    
    ---------
    
    Co-authored-by: William Wong <compulim@users.noreply.github.com>

---
+++ packages/fluent-theme/src/components/linerActivity/private/LinerActivity.tsx
@@ -0,0 +1,20 @@
+import { type WebChatActivity } from 'botframework-webchat-core';
+import React, { memo } from 'react';
+import { useStyles } from '../../../styles/index.js';
+import styles from './LinerMessageActivity.module.css';
+
+type Props = Readonly<{ activity: WebChatActivity & { type: 'message' } }>;
+
+const LinerMessageActivity = ({ activity }: Props) => {
+  const classNames = useStyles(styles);
+
+  return (
+    <div className={classNames['liner-message-activity']} role="separator">
+      {activity.text}
+    </div>
+  );
+};
+
+LinerMessageActivity.displayName = 'LinerMessageActivity';
+
+export default memo(LinerMessageActivity);
