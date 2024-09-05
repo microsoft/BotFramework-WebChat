1fe223b15a843a545b6dfb78d8d57f228badc470
William Wong <compulim@users.noreply.github.com>
2024-09-05 02:46:38 +0000

    Use useUIState instead of useDisabled

--- packages/fluent-theme/src/components/suggestedActions/SuggestedAction.tsx
+++ packages/fluent-theme/src/components/suggestedActions/SuggestedAction.tsx
@@ -2,12 +2,13 @@ import { hooks } from 'botframework-webchat-component';
 import { type DirectLineCardAction } from 'botframework-webchat-core';
 import cx from 'classnames';
 import React, { MouseEventHandler, memo, useCallback } from 'react';
-import styles from './SuggestedAction.module.css';
+
 import { useStyles } from '../../styles';
 import AccessibleButton from './AccessibleButton';
 import { useRovingFocusItemRef } from './private/rovingFocus';
+import styles from './SuggestedAction.module.css';
 
-const { useDisabled, useFocus, usePerformCardAction, useScrollToEnd, useStyleSet, useSuggestedActions } = hooks;
+const { useFocus, usePerformCardAction, useScrollToEnd, useStyleSet, useSuggestedActions, useUIState } = hooks;
 
 type SuggestedActionProps = Readonly<{
   buttonText: string | undefined;
@@ -44,7 +45,7 @@ function SuggestedAction({
 }: SuggestedActionProps) {
   const [_, setSuggestedActions] = useSuggestedActions();
   const [{ suggestedAction: suggestedActionStyleSet }] = useStyleSet();
-  const [disabled] = useDisabled();
+  const [uiState] = useUIState();
   const focus = useFocus();
   const focusRef = useRovingFocusItemRef<HTMLButtonElement>(itemIndex);
   const performCardAction = usePerformCardAction();
@@ -75,7 +76,7 @@ function SuggestedAction({
   return (
     <AccessibleButton
       className={cx(classNames['suggested-action'], suggestedActionStyleSet + '', (className || '') + '')}
-      disabled={disabled}
+      disabled={uiState === 'disabled'}
       onClick={handleClick}
       ref={focusRef}
       type="button"
