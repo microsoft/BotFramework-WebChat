daac793c233e779f232ce78962e5318dba79bcb7
William Wong <compulim@users.noreply.github.com>
2024-09-06 11:56:16 -0700

    Polish blueprint mode of pre-chat starter prompts (#5279)
    * Adds RTL
    
    * Fix gap in blueprint
    
    * Add entry
    
    * Update packages/fluent-theme/src/components/preChatActivity/StarterPromptsCardAction.module.css
    
    Co-authored-by: Eugene <EOlonov@gmail.com>
    
    * Update packages/fluent-theme/src/components/preChatActivity/StarterPromptsCardAction.module.css
    
    Co-authored-by: Eugene <EOlonov@gmail.com>
    
    * Update packages/fluent-theme/src/components/preChatActivity/StarterPromptsCardAction.module.css
    
    Co-authored-by: Eugene <EOlonov@gmail.com>
    
    * Use gap instead
    
    * Move CSS rule order
    
    * Fix flakiness
    
    * Update snapshots
    
    * Revert "Update snapshots"
    
    This reverts commit 350bafff541bad0df5c499ebb6bb2711f86b1e82.
    
    * Use row gap
    
    * Fix layout
    
    ---------
    
    Co-authored-by: Eugene <EOlonov@gmail.com>

--- __tests__/html/fluentTheme/uiState.js
+++ __tests__/html/fluentTheme/uiState.js
@@ -2,5 +2,6 @@
 
 describe('Fluent theme applied', () => {
   test('UI state should display properly in dark theme', () => runHTML('fluentTheme/uiState?theme=dark'));
+  test('UI state should display properly in dark theme and RTL', () => runHTML('fluentTheme/uiState?dir=rtl&theme=dark'));
   test('UI state should display properly in light theme', () => runHTML('fluentTheme/uiState?theme=light'));
 });
