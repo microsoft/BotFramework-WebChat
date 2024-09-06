e8d670d0731b4bfd46a146e5a5907b84094107b1
William Wong <compulim@users.noreply.github.com>
2024-09-06 02:02:15 +0000

    Adds RTL

--- __tests__/html/fluentTheme/uiState.js
+++ __tests__/html/fluentTheme/uiState.js
@@ -3,4 +3,5 @@
 describe('Fluent theme applied', () => {
   test('UI state should display properly in dark theme', () => runHTML('fluentTheme/uiState?theme=dark'));
   test('UI state should display properly in light theme', () => runHTML('fluentTheme/uiState?theme=light'));
+  test('UI state should display properly in light theme and RTL', () => runHTML('fluentTheme/uiState?dir=rtl&theme=light'));
 });
