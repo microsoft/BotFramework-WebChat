2b50a2e28023469a1a27a00432d9392a0af6d880
Eugene Olonov <eolonov@gmail.com>
2024-09-16 14:14:48 -0700

    feat: add generated text to the messages with corresponding keywords

--- __tests__/html/fluentTheme/side-by-side.wide.js
+++ __tests__/html/fluentTheme/side-by-side.wide.js
@@ -9,4 +9,6 @@ describe('Fluent theme applied', () => {
     runHTML('fluentTheme/side-by-side.wide?transcript=0&transcript=3'));
   test('side by side left - transcript, right - streaming', () =>
     runHTML('fluentTheme/side-by-side.wide?transcript=0&transcript=4'));
+  test('side by side left - fluent, right - fluent', () =>
+    runHTML('fluentTheme/side-by-side.wide?transcript=0&transcript=2&focus=1&variant=fluent&variant=fluent'));
 });
