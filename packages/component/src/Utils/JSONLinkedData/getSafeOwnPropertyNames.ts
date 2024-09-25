7982ac0c3a4508d77098adddc65a9dfcbaccc6e1
William Wong <compulim@users.noreply.github.com>
2024-09-25 12:08:43 -0700

    Fix for Policheck (#5308)

--- packages/component/src/Utils/JSONLinkedData/getSafeOwnPropertyNames.ts
+++ packages/component/src/Utils/JSONLinkedData/getSafeOwnPropertyNames.ts
@@ -1,12 +1,12 @@
 const DANGEROUS_PROPERTY_NAMES = ['__proto__', 'constructor', 'prototype'];
 
 /**
- * Returns a whitelisted own-property names.
+ * Returns an allowlisted own-property names.
  *
  * If `null` or `undefined` is passed, an empty list will be returned.
  *
  * @param object An object (including primitives).
- * @returns A whitelisted own-property names.
+ * @returns An allowlisted own-property names.
  */
 export default function getSafeOwnPropertyNames(object: unknown): string[] {
   return Object.getOwnPropertyNames(object).filter(name => !DANGEROUS_PROPERTY_NAMES.includes(name));
