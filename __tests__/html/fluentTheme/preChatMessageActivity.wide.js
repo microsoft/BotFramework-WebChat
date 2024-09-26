be287d77e18833e3db72877ff46e92660a97efec
William Wong <compulim@users.noreply.github.com>
2024-09-26 10:09:24 -0700

    Restructure HTML tests (#5311)
    * Restructure tests
    
    * Clean up
    
    * Use rootDir
    
    * Fix path
    
    * Port to html2
    
    * Add rootDir and reporters
    
    * Fix snapshots
    
    * Fix snapshots
    
    * Move coverage and reporter to root Jest config
    
    * Add roots
    
    * Pack jest.*.config.js
    
    * Include other Jest files
    
    * Specify other Jest config
    
    * Include HTML2 transformer
    
    * Clean up
    
    * Move maxWorkers to root config
    
    * Add comments

--- __tests__/html/fluentTheme/preChatMessageActivity.wide.js
+++
@@ -1,5 +0,0 @@
-/** @jest-environment ./packages/test/harness/src/host/jest/WebDriverEnvironment.js */
-
-describe('Fluent theme applied', () => {
-  test('should display pre-chat message in wide format', () => runHTML('fluentTheme/preChatMessageActivity.wide'));
-});
