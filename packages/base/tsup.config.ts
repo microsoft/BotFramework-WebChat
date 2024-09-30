ab58597e09f4b68c5a6d67c176fc209d2c81e86e
Eugene <EOlonov@gmail.com>
2024-09-30 22:53:25 +0000

    Make base a bit useful ⚡️

--- packages/base/tsup.config.ts
+++ packages/base/tsup.config.ts
@@ -4,7 +4,7 @@ import baseConfig from '../../tsup.base.config';
 export default defineConfig({
   ...baseConfig,
   entry: {
-    'botframework-webchat-base': './src/index.ts'
-  },
-  format: ['esm', 'cjs']
+    'botframework-webchat-base': './src/index.ts',
+    'botframework-webchat-base.utils': './src/utils/index.ts'
+  }
 });
