afc0b9b3752636297341ef78dad3f97cf86b96e7
William Wong <compulim@users.noreply.github.com>
2024-10-07 22:25:49 +0000

    Fix tests

--- packages/component/src/Utils/serializeDocumentFragmentIntoString.ts
+++ packages/component/src/Utils/serializeDocumentFragmentIntoString.ts
@@ -1,10 +1,3 @@
 export default function serializeDocumentFragmentIntoString(documentFragment: DocumentFragment): string {
-  const serializer = new XMLSerializer();
-  const elementHTML: string[] = [];
-
-  for (const element of documentFragment.childNodes) {
-    elementHTML.push(serializer.serializeToString(element));
-  }
-
-  return `${elementHTML.join('\n')}\n`.replace(/\n{2,}/gu, '\n');
+  return new XMLSerializer().serializeToString(documentFragment).trim();
 }
