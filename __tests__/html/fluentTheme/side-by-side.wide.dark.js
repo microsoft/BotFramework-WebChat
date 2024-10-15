4744d6989a16b0fbf8114c0c4c5173289219a02d
Eugene <EOlonov@gmail.com>
2024-10-15 14:13:27 -0700

    Fluent: fix classes clash between component and fluent packages (#5326)
    * Fluent: fix classes clash between component and fluent packages
    
    * Changelog
    
    * Tests
    
    * Fix snapshots
    
    * Skip failing

--- __tests__/html/fluentTheme/side-by-side.wide.dark.js
+++ __tests__/html/fluentTheme/side-by-side.wide.dark.js
@@ -4,6 +4,8 @@ describe('Fluent theme applied', () => {
   describe('dark theme applied', () => {
     test('side by side left - transcript, right - pre chat message', () =>
       runHTML('fluentTheme/side-by-side.wide.dark?focus=1'));
+    test('side by side left - transcript, right - pre chat message fluent', () =>
+      runHTML('fluentTheme/side-by-side.wide.dark?variant=fluent&variant=fluent'));
     test('side by side left - transcript, right - transcript', () =>
       runHTML('fluentTheme/side-by-side.wide.dark?transcript=0&transcript=2&focus=0&focus=1'));
     test('side by side left - transcript, right - pre liner', () =>
