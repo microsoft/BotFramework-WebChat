cd6b6382716d91fe358e663f83317592006c90ee
Eugene <EOlonov@gmail.com>
2024-10-22 20:21:25 +0000

    Fix es5 directline

--- packages/bundle/src/module/exports-es5.ts
+++ packages/bundle/src/module/exports-es5.ts
@@ -1,7 +1,25 @@
-import { buildInfo as minimalBuildInfo } from './exports-minimal';
+import { buildInfo as minimalBuildInfo, version } from './exports-minimal';
 
 export * from './exports';
 
-const buildInfo = Object.freeze({ ...minimalBuildInfo, variant: 'full-es5' });
+export const buildInfo = Object.freeze({ ...minimalBuildInfo, variant: 'full-es5' });
 
-export { buildInfo };
+export const createDirectLine = (options: Omit<Parameters<typeof createDirectLine>[0], 'botAgent'>) => {
+  (options as any).botAgent &&
+    console.warn(
+      'Web Chat: Developers are not currently allowed to set botAgent. See https://github.com/microsoft/BotFramework-WebChat/issues/2119 for more details.'
+    );
+
+  return createDirectLine({ ...options, botAgent: `WebChat/${version} (ES5)` });
+};
+
+export const createDirectLineAppServiceExtension = (
+  options: Omit<Parameters<typeof createDirectLineAppServiceExtension>[0], 'botAgent'>
+) => {
+  (options as any).botAgent &&
+    console.warn(
+      'Web Chat: Developers are not currently allowed to set botAgent. See https://github.com/microsoft/BotFramework-WebChat/issues/2119 for more details.'
+    );
+
+  return createDirectLineAppServiceExtension({ ...options, botAgent: `WebChat/${version} (ES5)` });
+};
