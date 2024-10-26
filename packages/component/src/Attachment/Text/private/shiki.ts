6e93a21b100204c5b7751f8b63a3d3fa6d44076f
William Wong <compulim@users.noreply.github.com>
2024-10-26 00:55:52 +0000

    Sort

--- packages/component/src/Attachment/Text/private/shiki.ts
+++ packages/component/src/Attachment/Text/private/shiki.ts
@@ -3,21 +3,21 @@ import { createHighlighterCore } from 'shiki/core';
 import { createJavaScriptRegexEngine } from 'shiki/engine-javascript.mjs';
 
 // directly import the theme and language modules, only the ones you imported will be bundled.
-import ghLight from 'shiki/themes/github-light-default.mjs';
-import ghDark from 'shiki/themes/github-dark-default.mjs';
+import themeGitHubDark from 'shiki/themes/github-dark-default.mjs';
+import themeGitHubLight from 'shiki/themes/github-light-default.mjs';
 
-import langJS from 'shiki/langs/js.mjs';
-import langTS from 'shiki/langs/ts.mjs';
-import langPy from 'shiki/langs/py.mjs';
+import languageJavaScript from 'shiki/langs/js.mjs';
+import languagePython from 'shiki/langs/py.mjs';
+import languageTypeScript from 'shiki/langs/ts.mjs';
 
 function createHighlighter() {
   return createHighlighterCore({
     themes: [
       // instead of strings, you need to pass the imported module
-      ghLight,
-      ghDark
+      themeGitHubDark,
+      themeGitHubLight
     ],
-    langs: [langJS, langTS, langPy],
+    langs: [languageJavaScript, languagePython, languageTypeScript],
     engine: createJavaScriptRegexEngine()
   });
 }
