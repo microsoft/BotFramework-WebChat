1a1bcb51a33c408745ace2903ad6b379790a638c
Eugene <EOlonov@gmail.com>
2024-12-05 20:31:46 +0000

    Fix missing super call

--- packages/component/src/providers/CustomElements/customElements/CodeBlock.ts
+++ packages/component/src/providers/CustomElements/customElements/CodeBlock.ts
@@ -147,6 +147,8 @@ const createReactCodeBlockClass = ({
     }
 
     disconnectedCallback() {
+      super.disconnectedCallback();
+
       this.#controller?.abort();
       this.#controller = undefined;
     }
