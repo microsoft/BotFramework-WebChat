de60e353685b69b533b89b3f12b588a126f06f99
Eugene <EOlonov@gmail.com>
2025-01-02 20:12:03 +0000

    Migrate to happydom

--- packages/bundle/src/markdown/private/betterLinkDocumentMod.rel.spec.ts
+++ packages/bundle/src/markdown/private/betterLinkDocumentMod.rel.spec.ts
@@ -1,4 +1,4 @@
-/** @jest-environment jsdom */
+/** @jest-environment @happy-dom/jest-environment */
 
 import {
   parseDocumentFragmentFromString,
@@ -27,7 +27,7 @@ describe('When passing "rel" option with "noopener noreferer"', () => {
 
   test('should match snapshot', () =>
     expect(serializeDocumentFragmentIntoString(actual)).toBe(
-      '<p xmlns="http://www.w3.org/1999/xhtml"><a href="https://example.com" rel="noopener noreferer">Example</a></p>'
+      '<p><a href="https://example.com" rel="noopener noreferer">Example</a></p>'
     ));
 });
 
@@ -45,7 +45,5 @@ describe('When passing "rel" option with false', () => {
   test('should have "rel" attribute removed', () => expect(actual.querySelector('a').hasAttribute('rel')).toBe(false));
 
   test('should match snapshot', () =>
-    expect(serializeDocumentFragmentIntoString(actual)).toBe(
-      '<a xmlns="http://www.w3.org/1999/xhtml" href="https://example.com">Example</a>'
-    ));
+    expect(serializeDocumentFragmentIntoString(actual)).toBe('<a href="https://example.com">Example</a>'));
 });
