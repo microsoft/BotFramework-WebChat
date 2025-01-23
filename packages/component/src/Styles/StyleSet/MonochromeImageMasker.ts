469bb1efe1e66744cae59e7d8ec97f75ee601a72
Eugene <EOlonov@gmail.com>
2025-01-23 18:52:59 +0000

    Fluent: make icons customizable

--- packages/component/src/Styles/StyleSet/MonochromeImageMasker.ts
+++ packages/component/src/Styles/StyleSet/MonochromeImageMasker.ts
@@ -2,10 +2,15 @@ export default function createMonochromeImageMaskerStyleSet() {
   return {
     '&.webchat__monochrome-image-masker': {
       backgroundColor: '#707070',
-      maskImage: 'var(--webchat__monochrome-image-masker__mask-image)',
+      maskImage: 'var(--monochrome-image-masker__mask-image, var(--webchat__monochrome-image-masker__mask-image))',
       maskRepeat: 'no-repeat',
-      WebkitMaskImage: 'var(--webchat__monochrome-image-masker__mask-image)',
-      WebkitMaskRepeat: 'no-repeat'
+      maskSize: 'contain',
+      WebkitMaskImage:
+        'var(--monochrome-image-masker__mask-image, var(--webchat__monochrome-image-masker__mask-image))',
+      WebkitMaskRepeat: 'no-repeat',
+      maskPosition: 'center',
+      WebkitMaskPosition: 'center',
+      WebkitMaskSize: 'contain'
     }
   };
 }
