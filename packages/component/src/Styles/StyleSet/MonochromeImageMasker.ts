f9952a0e083b1cc4abb7e61ee100840f679b1f33
Eugene <EOlonov@gmail.com>
2025-01-23 22:55:22 +0000

    Tests

--- packages/component/src/Styles/StyleSet/MonochromeImageMasker.ts
+++ packages/component/src/Styles/StyleSet/MonochromeImageMasker.ts
@@ -2,15 +2,9 @@ export default function createMonochromeImageMaskerStyleSet() {
   return {
     '&.webchat__monochrome-image-masker': {
       backgroundColor: '#707070',
-      maskImage: 'var(--monochrome-image-masker__mask-image, var(--webchat__monochrome-image-masker__mask-image))',
-      maskRepeat: 'no-repeat',
-      maskSize: 'contain',
-      WebkitMaskImage:
-        'var(--monochrome-image-masker__mask-image, var(--webchat__monochrome-image-masker__mask-image))',
-      WebkitMaskRepeat: 'no-repeat',
-      maskPosition: 'center',
-      WebkitMaskPosition: 'center',
-      WebkitMaskSize: 'contain'
+      mask: 'var(--monochrome-image-masker__mask-image, var(--webchat__monochrome-image-masker__mask-image)) no-repeat 100% / 100%',
+      WebkitMask:
+        'var(--monochrome-image-masker__mask-image, var(--webchat__monochrome-image-masker__mask-image)) no-repeat 100% / 100%'
     }
   };
 }
