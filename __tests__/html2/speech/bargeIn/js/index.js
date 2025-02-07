8019538e71fe4482ef52b8ab9dbda06d86ccf420
William Wong <compulim@users.noreply.github.com>
2025-02-07 22:26:07 +0000

    Use jest-mock

--- __tests__/html2/speech/bargeIn/js/index.js
+++ __tests__/html2/speech/bargeIn/js/index.js
@@ -1,4 +1,3 @@
-export { default as fn } from './fn.js';
 export { default as SpeechSynthesis } from './MockedSpeechSynthesis.js';
 export { default as SpeechSynthesisEvent } from './MockedSpeechSynthesisEvent.js';
 export { default as SpeechSynthesisUtterance } from './MockedSpeechSynthesisUtterance.js';
