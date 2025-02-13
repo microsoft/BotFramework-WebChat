d8608d1d046274ebe8dfc34e3045d884207aa4d7
William Wong <compulim@users.noreply.github.com>
2025-02-13 10:33:10 +0000

    Add test

--- packages/core/src/sagas/startDictateOnSpeakCompleteSaga.js
+++ packages/core/src/sagas/startDictateOnSpeakCompleteSaga.js
@@ -1,12 +1,12 @@
 import { put, select, takeEvery } from 'redux-saga/effects';
 
 import { MARK_ACTIVITY } from '../actions/markActivity';
-import { of as activitiesOf } from '../selectors/activities';
 import { SET_DICTATE_STATE } from '../actions/setDictateState';
-import { WILL_START } from '../constants/DictateState';
-import dictateStateSelector from '../selectors/dictateState';
-import speakingActivity from '../definitions/speakingActivity';
 import startDictate from '../actions/startDictate';
+import { WILL_START } from '../constants/DictateState';
+import speakingActivity from '../definitions/speakingActivity';
+import { of as activitiesOf } from '../selectors/activities';
+import dictateStateSelector from '../selectors/dictateState';
 
 function* startDictateOnSpeakComplete() {
   const speakingActivities = yield select(activitiesOf(speakingActivity));
