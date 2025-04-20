707be00d9acf634964d1f83e42b27b629240e25c
William Wong <compulim@users.noreply.github.com>
2025-04-20 05:27:05 +0000

    Allow container/media query and custom variables

--- packages/component/src/Styles/StyleSet/types/StyleSet.ts
+++ packages/component/src/Styles/StyleSet/types/StyleSet.ts
@@ -1,3 +1,6 @@
-type StyleSet = Partial<CSSStyleDeclaration> & { [key: `&${string}`]: StyleSet };
+type StyleSet = Partial<CSSStyleDeclaration> & {
+  [key: `${'@container ' | '@media ' | '&'}${string}`]: StyleSet;
+  [key: `--${string}`]: string;
+};
 
 export { type StyleSet };
