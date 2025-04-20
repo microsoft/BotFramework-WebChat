5ba5d974cc38e492eb237de1841e7f3ba98a2133
William Wong <compulim@users.noreply.github.com>
2025-04-20 23:25:54 +0000

    Add test for ESCAPE key

--- packages/component/src/testIds.ts
+++ packages/component/src/testIds.ts
@@ -3,6 +3,7 @@ const testIds = {
   copyButton: 'copy button',
   sendBoxAttachmentBar: 'send box attachment bar',
   sendBoxAttachmentBarItem: 'send box attachment bar item',
+  sendBoxAttachmentBarItemDeleteButton: 'send box attachment bar item delete button',
   sendBoxSpeechBox: 'send box speech box',
   sendBoxTextBox: 'send box text area',
   typingIndicator: 'typing indicator',
