75eb7446e02614937a2b7dd270a534f654e5e238
William Wong <compulim@users.noreply.github.com>
2025-04-21 08:49:27 +0000

    Use test ID

--- __tests__/setup/elements/getSendBoxTextBox.js
+++ __tests__/setup/elements/getSendBoxTextBox.js
@@ -1,6 +1,7 @@
 import { By } from 'selenium-webdriver';
+import { testIds } from 'botframework-webchat';
 
-const CSS_SELECTOR = '[role="form"] > * > form > input[type="text"]';
+const CSS_SELECTOR = `[data-testid="send box text area"]`;
 
 export default async function getSendBoxTextBox(driver) {
   return await driver.findElement(By.css(CSS_SELECTOR));
