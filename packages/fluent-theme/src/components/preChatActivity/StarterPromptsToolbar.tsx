d7da17a8405c7a816351ef03006e99589147f28d
James Cheung (from Dev Box) <jamescheung@microsoft.com>
2025-05-12 16:22:32 -0700

    Add pre-chat message activity HTML test  and removed starterPromptCard from StarterPromptsToolbar component

--- packages/fluent-theme/src/components/preChatActivity/StarterPromptsToolbar.tsx
+++ packages/fluent-theme/src/components/preChatActivity/StarterPromptsToolbar.tsx
@@ -1,7 +1,7 @@
 import { hooks } from 'botframework-webchat-api';
 import { type DirectLineCardAction } from 'botframework-webchat-core';
 import cx from 'classnames';
-import React, { Fragment, memo } from 'react';
+import React, { memo } from 'react';
 import { useStyles } from '../../styles/index.js';
 import StarterPromptsCardAction from './StarterPromptsCardAction.js';
 import styles from './StarterPromptsToolbar.module.css';
@@ -21,13 +21,7 @@ const StarterPromptsToolbar = ({ cardActions, className }: Props) => {
     // TODO: Accessibility-wise, this should be role="toolbar" with keyboard navigation.
     <div className={cx(className, classNames['pre-chat-message-activity__card-action-toolbar'])}>
       <div className={classNames['pre-chat-message-activity__card-action-toolbar-grid']}>
-        {uiState === 'blueprint' ? (
-          <Fragment>
-            <StarterPromptsCardAction />
-            <StarterPromptsCardAction />
-            <StarterPromptsCardAction />
-          </Fragment>
-        ) : (
+        {uiState !== 'blueprint' &&
           cardActions
             .filter<DirectLineCardAction & { type: 'messageBack' }>(
               (card: DirectLineCardAction): card is DirectLineCardAction & { type: 'messageBack' } =>
@@ -35,8 +29,7 @@ const StarterPromptsToolbar = ({ cardActions, className }: Props) => {
             )
             // There is no other usable keys in card actions.
             // eslint-disable-next-line react/no-array-index-key
-            .map((cardAction, index) => <StarterPromptsCardAction key={index} messageBackAction={cardAction} />)
-        )}
+            .map((cardAction, index) => <StarterPromptsCardAction key={index} messageBackAction={cardAction} />)}
       </div>
     </div>
   );
