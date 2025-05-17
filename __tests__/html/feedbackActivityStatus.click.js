bcff76b2763e20c317fe7c898605845d5a49e3ad
William Wong <compulim@users.noreply.github.com>
2025-05-17 22:53:54 +0000

    Fix tests

--- __tests__/html/feedbackActivityStatus.click.js
+++
@@ -1,5 +0,0 @@
-/** @jest-environment ./packages/test/harness/src/host/jest/WebDriverEnvironment.js */
-
-describe('vote button', () => {
-  test('should send event on click', () => runHTML('feedbackActivityStatus.click.html'));
-});
