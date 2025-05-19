2c3d844f0b8bf7879835a44693eb9bdcd46cd188
William Wong <compulim@users.noreply.github.com>
2025-05-19 00:31:26 +0000

    Sort

--- packages/component/src/ActivityFeedback/private/getDisclaimer.ts
+++ packages/component/src/ActivityFeedback/private/getDisclaimer.ts
@@ -1,4 +1,5 @@
 import { type WebChatActivity } from 'botframework-webchat-core';
+
 import { hasDisclaimer } from './hasFeedbackLoop';
 
 export default function getDisclaimer(activity: WebChatActivity): string | undefined {
