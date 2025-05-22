1674a04bf76b4cfa64c39f90defe82ef79496e73
Eugene <EOlonov@gmail.com>
2025-05-22 00:28:54 +0000

    Fiddle with config

--- packages/bundle/tsdown.config.ts
+++ packages/bundle/tsdown.config.ts
@@ -1,25 +1,39 @@
 import path from 'path';
 import { defineConfig } from 'tsdown';
 import baseConfig from '../../tsdown.base.config';
+import type { Plugin as RollupPlugin } from 'rollup';
+
+import pkg from './package.json'
+
+const iifeDeps = Object.keys(pkg.dependencies);
 
 // Redirect import paths for "microsoft-cognitiveservices-speech-sdk(...)"
 // to point to es2015 distribution for all importing modules
-const resolveCognitiveServicesToES2015 = {
+const resolveCognitiveServicesToES2015: RollupPlugin = {
   name: 'microsoft-cognitiveservices-speech-sdk',
-  setup(build) {
-    build.onResolve({ filter: /microsoft-cognitiveservices-speech-sdk.+/u }, args => ({
-      path: path.join(process.cwd(), '../../node_modules', args.path.replace('distrib/lib', 'distrib/es2015') + '.js')
-    }));
+  resolveId(source) {
+    if (/^microsoft-cognitiveservices-speech-sdk.*$/u.test(source)) {
+      let p = path.join(process.cwd(), '../../node_modules', source.replace('distrib/lib', 'distrib/es2015') + '.js');
+      p.endsWith('node_modules/microsoft-cognitiveservices-speech-sdk.js') &&
+        (p = path.join(
+          p,
+          '../microsoft-cognitiveservices-speech-sdk/distrib/es2015/microsoft.cognitiveservices.speech.sdk.js'
+        ));
+      return p;
+    }
+    return null;
   }
 };
 
 // Redirect import paths for "react" and "react-dom"
-const resolveReact = {
+const resolveReact: RollupPlugin = {
   name: 'isomorphic-react',
-  setup(build) {
-    build.onResolve({ filter: /^(react|react-dom)$/u }, ({ path: pkgNamne }) => ({
-      path: path.join(process.cwd(), '../../node_modules', `isomorphic-${pkgNamne}/dist/${pkgNamne}.js`)
-    }));
+  resolveId(source) {
+    if (source === 'react' || source === 'react-dom') {
+      const p = path.join(process.cwd(), '../../node_modules', `isomorphic-${source}/dist/${source}.js`);
+      return p;
+    }
+    return null;
   }
 };
 
@@ -38,7 +52,7 @@ const config: typeof baseConfig = {
     SPEECH_CONDUCT_OCSP_CHECK: '',
     SPEECH_OCSP_CACHE_ROOT: ''
   },
-  esbuildPlugins: [...(baseConfig.esbuildPlugins || []), resolveCognitiveServicesToES2015],
+  plugins: [resolveCognitiveServicesToES2015],
   noExternal: [
     '@babel/runtime',
     'memoize-one',
@@ -54,35 +68,54 @@ const config: typeof baseConfig = {
   ]
 };
 
+const iifeConfig = {
+  ...config,
+  dts: false,
+  env: {
+    ...config.env,
+    module_format: 'global'
+  },
+  plugins: [...config.plugins, resolveReact],
+  format: 'iife',
+  platform: 'browser',
+  target: [...config.target, 'es2019'],
+  noExternal: [
+    // ...iifeDeps,
+    ...config.noExternal,
+  ],
+  outputOptions(outputOptions) {
+    outputOptions.entryFileNames = '[name].js';
+    return outputOptions;
+  },
+};
+
 export default defineConfig([
   // Build IIFE before CJS/ESM to make npm start faster.
   {
-    ...config,
-    dts: false,
+    ...iifeConfig,
     entry: {
-      webchat: './src/bundle/index.ts',
-      'webchat-es5': './src/bundle/index-es5.ts',
       'webchat-minimal': './src/bundle/index-minimal.ts'
-    },
-    env: {
-      ...config.env,
-      module_format: 'global'
-    },
-    esbuildPlugins: [...config.esbuildPlugins, resolveReact],
-    format: 'iife',
-    outExtension() {
-      return { js: '.js' };
-    },
-    platform: 'browser',
-    target: [...config.target, 'es2019']
+    }
+  },
+  {
+    ...iifeConfig,
+    entry: {
+      'webchat': './src/bundle/index.ts'
+    }
+  },
+  {
+    ...iifeConfig,
+    entry: {
+      'webchat-es5': './src/bundle/index-es5.ts'
+    }
   },
   {
     ...config,
     format: 'esm'
   },
-  {
-    ...config,
-    format: 'cjs',
-    target: [...config.target, 'es2019']
-  }
+  // {
+  //   ...config,
+  //   format: 'cjs',
+  //   target: [...config.target, 'es2019']
+  // }
 ]);
