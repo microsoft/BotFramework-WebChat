1923964b38c6a0f40d8a09d15d96abb0b3dc1df1
William Wong <compulim@users.noreply.github.com>
2025-05-29 04:31:11 +0000

    Clean up

--- packages/test/page-object/src/globals/testHelpers/createDirectLineEmulator.js
+++ packages/test/page-object/src/globals/testHelpers/createDirectLineEmulator.js
@@ -3,10 +3,10 @@ import random from 'math-random';
 import updateIn from 'simple-update-in';
 
 import createDeferredObservable from '../../utils/createDeferredObservable';
+import withResolvers from '../../utils/withResolvers';
 import became from '../pageConditions/became';
 import { createStoreWithOptions } from './createStore';
 import shareObservable from './shareObservable';
-import withResolvers from '../../utils/withResolvers';
 
 function isNativeClock() {
   return ('' + setTimeout).endsWith('() { [native code] }');
