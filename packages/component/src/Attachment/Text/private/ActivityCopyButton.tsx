8e93bb20568ad088889ff320472181410056afaa
William Wong <compulim@users.noreply.github.com>
2025-05-30 23:53:31 +0000

    Use state context version

--- packages/component/src/Attachment/Text/private/ActivityCopyButton.tsx
+++ packages/component/src/Attachment/Text/private/ActivityCopyButton.tsx
@@ -8,7 +8,7 @@ import { instance, nullable, object, optional, pipe, readonly, string, type Infe
 import useStyleSet from '../../../hooks/useStyleSet';
 import ClipboardWritePermissionComposer, {
   useClipboardWritePermissionHooks
-} from '../../../providers/ClipboardWritePermissionWithStable/ClipboardWritePermissionComposer';
+} from '../../../providers/ClipboardWritePermissionWithStateContext/ClipboardWritePermissionComposer';
 import { useQueueStaticElement } from '../../../providers/LiveRegionTwin';
 import refObject from '../../../types/internal/refObject';
 import ActivityButton from './ActivityButton';
