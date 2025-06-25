e9375b74269c98f59be8c972027cf0a395b77931
William Wong <compulim@users.noreply.github.com>
2025-06-25 07:31:58 +0000

    Better middleware array check

--- packages/api/src/middleware/private/templateMiddleware.check.test.tsx
+++ packages/api/src/middleware/private/templateMiddleware.check.test.tsx
@@ -1,5 +1,17 @@
 import templateMiddleware from './templateMiddleware';
 
+test('should warn if middleware is not an array of function', () => {
+  const warn = jest.fn();
+  const template = templateMiddleware('Check');
+
+  jest.spyOn(console, 'warn').mockImplementation(warn);
+
+  template.extractMiddleware(1 as any);
+
+  expect(warn).toHaveBeenCalledTimes(1);
+  expect(warn).toHaveBeenNthCalledWith(1, expect.stringContaining('must be an array of function'));
+});
+
 test('should warn if middleware did not return function', () => {
   const warn = jest.fn();
   const template = templateMiddleware('Check');
