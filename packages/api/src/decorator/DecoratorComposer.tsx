352b7f8b4ac53e1485971eba709b86aa706f08ea
William Wong <compulim@users.noreply.github.com>
2025-06-25 12:05:24 +0000

    Mark middleware as readonly

--- packages/api/src/decorator/DecoratorComposer.tsx
+++ packages/api/src/decorator/DecoratorComposer.tsx
@@ -1,6 +1,6 @@
 import { reactNode, validateProps } from 'botframework-webchat-react-valibot';
-import React, { Fragment, memo, useMemo, type ReactNode } from 'react';
-import { array, custom, object, optional, pipe, readonly, safeParse } from 'valibot';
+import React, { Fragment, memo, useMemo } from 'react';
+import { array, custom, object, optional, pipe, readonly, safeParse, type InferInput } from 'valibot';
 
 import { middlewareFactoryMarker } from '../middleware/private/templateMiddleware';
 import InternalDecoratorComposer from './internal/InternalDecoratorComposer';
@@ -18,12 +18,12 @@ const warnInvalidMiddlewarePropsSchema = optional(
   array(custom(value => value[middlewareFactoryMarker satisfies symbol] === middlewareFactoryMarker))
 );
 
-type DecoratorComposerProps = Readonly<{
-  children?: ReactNode | undefined;
-  // TODO: How could we mark `middleware` as readonly if we use InferInput<typeof decoratorComposerPropsSchema>?
-  //       Intersection doesn't work.
-  middleware?: readonly DecoratorMiddleware[] | undefined;
-}>;
+type DecoratorComposerProps = Omit<InferInput<typeof decoratorComposerPropsSchema>, 'middleware'> & {
+  // Mark "middleware" as read-only.
+  // Otherwise, passing a read-only middleware would fail because we prefer writable.
+  // eslint-disable-next-line react/require-default-props, react/no-unused-prop-types
+  readonly middleware?: readonly DecoratorMiddleware[] | undefined;
+};
 
 function DecoratorComposer(props: DecoratorComposerProps) {
   const { children, middleware } = validateProps(decoratorComposerPropsSchema, props);
