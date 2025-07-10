c1bd6b1fc8f7b461d942f0826c00ee98b13072c1
Eugene <EOlonov@gmail.com>
2025-07-10 10:51:22 -0700

    feat: template decorator (#5510)
    * feat: template decorator
    
    * Fix tests
    
    * Changelog

--- packages/api/src/decorator/ActivityBorder/private/ActivityBorderDecoratorMiddleware.ts
+++ packages/api/src/decorator/ActivityBorder/private/ActivityBorderDecoratorMiddleware.ts
@@ -1,52 +1,33 @@
 import { type ReactNode } from 'react';
-import templateMiddleware, {
+import templateDecorator, {
+  InferRequest,
   type InferMiddleware,
-  type InferProps,
-  type InferRequest
-} from '../../../middleware/private/templateMiddleware';
+  type InferProps
+} from '../../private/templateDecorator';
+import ActivityBorderDecoratorRequestContext, {
+  type ActivityBorderDecoratorRequest
+} from './ActivityBorderDecoratorRequestContext';
 
-type Request = Readonly<{
-  /**
-   * Decorate the activity as it participate in a livestreaming session.
-   *
-   * - `"completing"` - decorate as the livestreaming is completing
-   * - `"ongoing"` - decorate as the livestreaming is ongoing
-   * - `"preparing"` - decorate as the livestreaming is being prepared
-   * - `undefined` - not participated in a livestreaming session
-   */
-  livestreamingState: 'completing' | 'ongoing' | 'preparing' | undefined;
-
-  /**
-   * Gets the role of the sender for the activity.
-   *
-   * - `"bot"` - the sender is a bot or other users
-   * - `"channel"` - the sender is the channel service
-   * - `"user"` - the sender is the current user
-   * - `undefined` - the sender is unknown
-   */
-  from: 'bot' | 'channel' | `user` | undefined;
+type Props = Readonly<{
+  children?: ReactNode | undefined;
+  showFlair?: boolean | undefined;
+  showLoader?: boolean | undefined;
 }>;
 
-type Props = Readonly<{ children?: ReactNode | undefined }>;
+const template = templateDecorator<Props, ActivityBorderDecoratorRequest>(
+  'activity border',
+  ActivityBorderDecoratorRequestContext
+);
 
-const template = templateMiddleware<Request, Props>('activity border');
-
-const {
-  createMiddleware: createActivityBorderMiddleware,
-  extractMiddleware: extractActivityBorderDecoratorMiddleware,
-  Provider: ActivityBorderDecoratorMiddlewareProvider,
-  Proxy: ActivityBorderDecoratorMiddlewareProxy
-} = template;
+const { createMiddleware: createActivityBorderMiddleware, Proxy: ActivityBorderDecoratorMiddlewareProxy } = template;
 
 type ActivityBorderDecoratorMiddleware = InferMiddleware<typeof template>;
 type ActivityBorderDecoratorMiddlewareProps = InferProps<typeof template>;
 type ActivityBorderDecoratorMiddlewareRequest = InferRequest<typeof template>;
 
 export {
-  ActivityBorderDecoratorMiddlewareProvider,
   ActivityBorderDecoratorMiddlewareProxy,
   createActivityBorderMiddleware,
-  extractActivityBorderDecoratorMiddleware,
   type ActivityBorderDecoratorMiddleware,
   type ActivityBorderDecoratorMiddlewareProps,
   type ActivityBorderDecoratorMiddlewareRequest
