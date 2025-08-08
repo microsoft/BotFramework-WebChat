89e99df560f1be2185898752d3965fb61f682ef2
William Wong <compulim@users.noreply.github.com>
2025-08-08 18:57:58 +0000

    Upgraded `activityGrouping/*`

--- __tests__/html/activityGrouping.disableTimestamp.js
+++
@@ -1,6 +0,0 @@
-/** @jest-environment ./packages/test/harness/src/host/jest/WebDriverEnvironment.js */
-
-describe('activity grouping', () => {
-  test('should show timestamp even if timestamp is disabled', () =>
-    runHTML('activityGrouping.disableTimestamp.html'));
-});
