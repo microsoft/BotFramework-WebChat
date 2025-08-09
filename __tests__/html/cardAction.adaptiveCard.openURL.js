1993d13563edfa20020f073ae6b77855eeea4403
William Wong <compulim@users.noreply.github.com>
2025-08-09 23:48:00 +0000

    Upgraded `cardAction*`

--- __tests__/html/cardAction.adaptiveCard.openURL.js
+++
@@ -1,6 +0,0 @@
-/** @jest-environment ./packages/test/harness/src/host/jest/WebDriverEnvironment.js */
-
-describe('"openUrl" action on Adaptive Card', () => {
-  test('should open URL in a new tab with "noopener" and "noreferrer"', () =>
-    runHTML('cardAction.adaptiveCard.openURL.html'));
-});
