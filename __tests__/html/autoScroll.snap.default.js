4f353ea83e97704539d8e541a28db6a340f4a72d
William Wong <compulim@users.noreply.github.com>
2025-08-09 23:13:24 +0000

    Move files and remove entry files

--- __tests__/html/autoScroll.snap.default.js
+++
@@ -1,5 +0,0 @@
-/** @jest-environment ./packages/test/harness/src/host/jest/WebDriverEnvironment.js */
-
-describe('Auto-scroll with "default" snap behavior', () => {
-  test('should scroll correctly', () => runHTML('autoScroll.snap.default.html'));
-});
