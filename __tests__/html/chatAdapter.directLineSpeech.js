52f7a37952b58ab75070d55a7e4309ff16090cf8
William Wong <compulim@users.noreply.github.com>
2025-08-11 12:46:11 -0700

    Upgrade tests to `html2` (part 5) (#5546)
    * Upgraded `carousel.*`
    
    * Upgraded `chatAdapter.*`

--- __tests__/html/chatAdapter.directLineSpeech.js
+++
@@ -1,5 +0,0 @@
-/** @jest-environment ./packages/test/harness/src/host/jest/WebDriverEnvironment.js */
-
-describe('Direct Line Speech chat adapter', () => {
-  test.skip('should connect to the MockBot.', () => runHTML('chatAdapter.directLineSpeech.html'));
-});
--- __tests__/html/chatAdapter.sequenceId.noSequenceId.js
+++
@@ -1,3 +0,0 @@
-/** @jest-environment ./packages/test/harness/src/host/jest/WebDriverEnvironment.js */
-
-test('should sort activities by fallback to timestamp', () => runHTML('chatAdapter.sequenceId.noSequenceId.html'));
