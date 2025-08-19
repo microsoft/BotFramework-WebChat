2fb1a2a45028fc4f38c98bca4f275ea89afac868
Eugene <EOlonov@gmail.com>
2025-08-19 14:18:34 +0000

    component: break down StackedLayout and add message status

---
+++ packages/component/src/Activity/StackedLayoutMain.tsx
@@ -0,0 +1,26 @@
+import { useStyles } from 'botframework-webchat-styles/react';
+import cx from 'classnames';
+import React, { memo, type ReactNode } from 'react';
+
+import styles from './StackedLayout.module.css';
+
+type StackedLayoutMainProps = Readonly<{
+  avatar?: ReactNode | undefined;
+  children?: ReactNode | undefined;
+}>;
+
+const StackedLayoutMain = memo(({ avatar, children }: StackedLayoutMainProps) => {
+  const classNames = useStyles(styles);
+
+  return (
+    <div className={classNames['stacked-layout__main']}>
+      <div className={cx(classNames['stacked-layout__avatar-gutter'])}>{avatar}</div>
+      <div className={cx(classNames['stacked-layout__content'])}>{children}</div>
+      <div className={classNames['stacked-layout__alignment-pad']} />
+    </div>
+  );
+});
+
+StackedLayoutMain.displayName = 'StackedLayoutMain';
+
+export default StackedLayoutMain;
---
+++ packages/component/src/Activity/StackedLayoutRoot.tsx
@@ -0,0 +1,60 @@
+import { useStyles } from 'botframework-webchat-styles/react';
+import cx from 'classnames';
+import React, { memo, type ReactNode } from 'react';
+
+import styles from './StackedLayout.module.css';
+
+type StackedLayoutRootProps = Readonly<{
+  ariaLabelId?: string | undefined;
+  children?: ReactNode | undefined;
+  extraTrailing?: boolean | undefined;
+  fromUser?: boolean | undefined;
+  hideAvatar?: boolean | undefined;
+  hideNub?: boolean | undefined;
+  isGroup?: boolean | undefined;
+  noMessage?: boolean | undefined;
+  showAvatar?: boolean | undefined;
+  showNub?: boolean | undefined;
+  topCallout?: boolean | undefined;
+}>;
+
+const StackedLayoutRoot = memo(
+  ({
+    ariaLabelId,
+    children,
+    extraTrailing,
+    fromUser,
+    hideAvatar,
+    hideNub,
+    isGroup,
+    noMessage,
+    showAvatar,
+    showNub,
+    topCallout
+  }: StackedLayoutRootProps) => {
+    const classNames = useStyles(styles);
+
+    return (
+      <div
+        aria-labelledby={ariaLabelId}
+        className={cx('webchat__stacked-layout', classNames['stacked-layout'], {
+          [classNames['stacked-layout--from-user']]: fromUser,
+          [classNames['stacked-layout--extra-trailing']]: extraTrailing,
+          [classNames['stacked-layout--hide-avatar']]: hideAvatar && !showAvatar,
+          [classNames['stacked-layout--hide-nub']]: hideNub,
+          [classNames['stacked-layout--group']]: isGroup,
+          [classNames['stacked-layout--no-message']]: !noMessage,
+          [classNames['stacked-layout--show-avatar']]: showAvatar,
+          [classNames['stacked-layout--show-nub']]: showNub,
+          [classNames['stacked-layout--top-callout']]: topCallout
+        })}
+      >
+        {children}
+      </div>
+    );
+  }
+);
+
+StackedLayoutRoot.displayName = 'StackedLayoutRoot';
+
+export default StackedLayoutRoot;
