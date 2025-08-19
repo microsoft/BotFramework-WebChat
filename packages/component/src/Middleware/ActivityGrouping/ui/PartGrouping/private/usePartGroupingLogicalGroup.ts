a8880ca8e97927decdddc0f86d824ee2b195e25b
Eugene <EOlonov@gmail.com>
2025-08-19 14:28:47 +0000

    component: estabilish part grouping

---
+++ packages/component/src/Middleware/ActivityGrouping/ui/PartGrouping/private/usePartGroupingLogicalGroup.ts
@@ -0,0 +1,54 @@
+import { useMemo, useRef } from 'react';
+import { useAddLogicalGrouping } from '../../../../../providers/ActivityLogicalGrouping';
+
+const getPartGropKey = (key: string) => `part-grouping-${key}`;
+
+type UsePartGroupingLogicalGroupOptions = {
+  activityKeys: readonly string[];
+  isGroupOpen: boolean;
+};
+
+type UsePartGroupingLogicalGroupReturn = {
+  shouldSkipRender: boolean;
+};
+
+/**
+ * Custom hook for managing part grouping logical groups.
+ *
+ * @param options - Configuration options for the grouping
+ * @returns Object containing shouldSkipRender flag
+ */
+function usePartGroupingLogicalGroup({
+  activityKeys,
+  isGroupOpen
+}: UsePartGroupingLogicalGroupOptions): UsePartGroupingLogicalGroupReturn {
+  const addLogicalGrouping = useAddLogicalGrouping();
+  const isInitialGroupingRef = useRef(true);
+
+  const shouldSkipRender = useMemo(() => {
+    const isInitialGrouping = isInitialGroupingRef.current;
+
+    addLogicalGrouping({
+      // use activity key for group identifier as there should be only one logical group per activity
+      id: getPartGropKey(activityKeys.at(0)),
+      name: 'part',
+      activityKeys: [...activityKeys],
+      getGroupState: () => ({
+        isCollapsed: !isGroupOpen
+      })
+    });
+
+    // If this is the initial grouping, mark it and skip render
+    if (isInitialGrouping) {
+      isInitialGroupingRef.current = false;
+      return true;
+    }
+
+    return false;
+  }, [activityKeys, addLogicalGrouping, isGroupOpen]);
+
+  return { shouldSkipRender };
+}
+
+export default usePartGroupingLogicalGroup;
+export { type UsePartGroupingLogicalGroupOptions, type UsePartGroupingLogicalGroupReturn, getPartGropKey };
