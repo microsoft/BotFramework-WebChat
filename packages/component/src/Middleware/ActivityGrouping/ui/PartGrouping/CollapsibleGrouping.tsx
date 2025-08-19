793b32c062fcd640a3f9f2655da54e71f399c1f0
Eugene <EOlonov@gmail.com>
2025-08-19 14:28:47 +0000

    component: estabilish part grouping

---
+++ packages/component/src/Middleware/ActivityGrouping/ui/PartGrouping/CollapsibleGrouping.tsx
@@ -0,0 +1,90 @@
+import { useStyles } from 'botframework-webchat-styles/react';
+import { reactNode, validateProps } from 'botframework-webchat-react-valibot';
+import cx from 'classnames';
+import React, { memo, useCallback, useRef, useState } from 'react';
+import { boolean, custom, object, optional, pipe, readonly, string, type InferInput } from 'valibot';
+import random from 'math-random';
+
+import styles from './CollapsibleGrouping.module.css';
+import { ComponentIcon } from '../../../../Icon';
+import ActivityButton from '../../../../Attachment/Text/private/ActivityButton';
+import { useRefFrom } from 'use-ref-from';
+
+const collapsibleGroupingPropsSchema = pipe(
+  object({
+    children: reactNode(),
+    title: string(),
+    titleClassName: optional(string()),
+    isOpen: optional(boolean()),
+    onToggle: optional(pipe(custom<(isOpen: boolean) => void>(toggle => typeof toggle === 'function')), readonly())
+  }),
+  readonly()
+);
+
+type CollapsibleGroupingProps = InferInput<typeof collapsibleGroupingPropsSchema>;
+
+function uniqueId(count = Infinity) {
+  return (
+    random()
+      // eslint-disable-next-line no-magic-numbers
+      .toString(36)
+      // eslint-disable-next-line no-magic-numbers
+      .substring(2, 2 + count)
+  );
+}
+
+function CollapsibleGrouping(props: CollapsibleGroupingProps) {
+  const {
+    children,
+    title,
+    titleClassName,
+    isOpen = true,
+    onToggle
+  } = validateProps(collapsibleGroupingPropsSchema, props);
+  const [id] = useState(() => `webchat-collapsible-grouping-${uniqueId()}`);
+  const classNames = useStyles(styles);
+  const buttonRef = useRef<HTMLButtonElement>(null);
+  const isOpenRef = useRefFrom(isOpen);
+  const onToggleRef = useRefFrom(onToggle);
+
+  const handleToggle = useCallback(() => {
+    const newIsOpen = !isOpenRef.current;
+    const button = buttonRef.current;
+    if (button) {
+      button.setAttribute('aria-expanded', newIsOpen.toString());
+      button.setAttribute('aria-pressed', newIsOpen.toString());
+    }
+    onToggleRef.current?.(newIsOpen);
+  }, [isOpenRef, onToggleRef]);
+
+  return (
+    <div className={cx(classNames['collapsible-grouping'], { [classNames['collapsible-grouping--open']]: isOpen })}>
+      <div className={cx(classNames['collapsible-grouping__header'])}>
+        <div className={cx(classNames['collapsible-grouping__title'], titleClassName)}>{title}</div>
+        <ActivityButton
+          aria-controls={id}
+          aria-expanded={isOpen}
+          aria-pressed={isOpen}
+          className={cx(classNames['collapsible-grouping__toggle'])}
+          onClick={handleToggle}
+          ref={buttonRef}
+        >
+          <ComponentIcon appearance="text" className={classNames['collapsible-grouping__chevron']} icon="chevron" />
+          {isOpen ? 'Show less' : 'Show more'}
+        </ActivityButton>
+      </div>
+      <div
+        className={cx(classNames['collapsible-grouping__content'], {
+          [classNames['collapsible-grouping__content--open']]: isOpen
+        })}
+        // eslint-disable-next-line react/forbid-dom-props
+        id={id}
+      >
+        {children}
+      </div>
+    </div>
+  );
+}
+
+export default memo(CollapsibleGrouping);
+export { collapsibleGroupingPropsSchema, type CollapsibleGroupingProps };
---
+++ packages/component/src/Middleware/ActivityGrouping/ui/PartGrouping/private/PartGroupingActivity.tsx
@@ -0,0 +1,185 @@
+import { getOrgSchemaMessage, type WebChatActivity } from 'botframework-webchat-core';
+import { reactNode } from 'botframework-webchat-react-valibot';
+import React, { memo, useCallback, useMemo, useState } from 'react';
+import { array, minLength, object, optional, pipe, readonly, transform, any, parse, type InferOutput } from 'valibot';
+import { hooks } from 'botframework-webchat-api';
+
+import usePartGroupingLogicalGroup, { getPartGropKey } from './usePartGroupingLogicalGroup';
+import CollapsibleGrouping from '../CollapsibleGrouping';
+import useActivityElementMapRef from '../../../../../providers/ChatHistoryDOM/useActivityElementRef';
+import StackedLayoutMain from '../../../../../Activity/StackedLayoutMain';
+import StackedLayoutRoot from '../../../../../Activity/StackedLayoutRoot';
+import StackedLayoutStatus from '../../../../../Activity/StackedLayoutStatus';
+import useActiveDescendantId from '../../../../../providers/TranscriptFocus/useActiveDescendantId';
+import useGetGroupDescendantIdByActivityKey from '../../../../../providers/TranscriptFocus/useGetGroupDescendantIdByActivityKey';
+import {
+  TranscriptFocusContent,
+  TranscriptFocusContentActiveDescendant,
+  TranscriptFocusContentBody,
+} from '../../../../../Transcript/TranscriptFocus';
+import useFocusByGroupKey from '../../../../../providers/TranscriptFocus/useFocusByGroupKey';
+import FocusTrap from '../../../../../Transcript/FocusTrap';
+import useRenderActivityProps from '../../../../../Transcript/hooks/useRenderActivityProps';
+import isZeroOrPositive from '../../../../../Utils/isZeroOrPositive';
+
+const { useAvatarForBot, useStyleOptions, useGetKeyByActivity } = hooks;
+
+const partGroupingActivityPropsSchema = pipe(
+  object({
+    activities: pipe(
+      array(
+        pipe(
+          any(),
+          transform(value => value as WebChatActivity)
+        )
+      ),
+      minLength(1, 'botframework-webchat: "activities" must have at least 1 activity'),
+      readonly()
+    ),
+    children: optional(reactNode())
+  }),
+  readonly()
+);
+
+type PartGroupingActivityProps = InferOutput<typeof partGroupingActivityPropsSchema>;
+
+const partGroupingFocusableActivityPropsSchema = pipe(
+  object({
+    activity: pipe(
+      any(),
+      transform(value => value as WebChatActivity),
+      readonly()
+    ),
+    children: optional(reactNode())
+  }),
+  readonly()
+);
+
+type FocusablePartGroupingActivityProps = InferOutput<typeof partGroupingFocusableActivityPropsSchema>;
+
+function FocusablePartGroupingActivity(props: FocusablePartGroupingActivityProps) {
+  const [activeDescendantId] = useActiveDescendantId();
+  const { activity, children } = parse(partGroupingFocusableActivityPropsSchema, props);
+
+  const getGroupDescendantIdByActivityKey = useGetGroupDescendantIdByActivityKey();
+  const focusByGroupKey = useFocusByGroupKey();
+
+  const getKeyByActivity = useGetKeyByActivity();
+  const groupingActivityDescendantId = getGroupDescendantIdByActivityKey(getKeyByActivity(activity));
+
+  const groupKey = useMemo(() => getPartGropKey(getKeyByActivity(activity)), [getKeyByActivity, activity]);
+
+  const focusSelf = useCallback<(withFocus?: boolean) => void>(
+    (withFocus?: boolean) => focusByGroupKey(groupKey, withFocus),
+    [groupKey, focusByGroupKey]
+  );
+
+  // When a child of the activity receives focus, notify the transcript to set the `aria-activedescendant` to this activity.
+  const handleDescendantFocus: () => void = useCallback(() => focusSelf(false), [focusSelf]);
+
+  // When receive Escape key from descendant, focus back to the activity.
+  const handleLeaveFocusTrap = useCallback(() => focusSelf(), [focusSelf]);
+
+  const isActiveDescendant = groupingActivityDescendantId === activeDescendantId;
+
+  const activityElementMapRef = useActivityElementMapRef();
+
+  const groupCallbackRef = useCallback(
+    (activityElement: HTMLElement) => {
+      activityElement
+        ? activityElementMapRef.current.set(groupKey, activityElement)
+        : activityElementMapRef.current.delete(groupKey);
+    },
+    [activityElementMapRef, groupKey]
+  );
+
+  return (
+    <TranscriptFocusContent
+      focused={isActiveDescendant}
+      ref={groupCallbackRef}
+    >
+      <TranscriptFocusContentBody>
+        <FocusTrap
+          onFocus={handleDescendantFocus}
+          onLeave={handleLeaveFocusTrap}
+          targetClassName="webchat__basic-transcript__group-focus-target"
+        >
+          {children}
+        </FocusTrap>
+        <TranscriptFocusContentActiveDescendant id={groupingActivityDescendantId} />
+      </TranscriptFocusContentBody>
+    </TranscriptFocusContent>
+  );
+}
+
+function PartGroupingActivity(props: PartGroupingActivityProps) {
+  const { activities, children } = parse(partGroupingActivityPropsSchema, props);
+  const getKeyByActivity = useGetKeyByActivity();
+  const [isGroupOpen, setIsGroupOpen] = useState(true);
+
+  const activityKeys = useMemo(
+    () => activities.map(activity => getKeyByActivity(activity)),
+    [activities, getKeyByActivity]
+  );
+
+  const { shouldSkipRender } = usePartGroupingLogicalGroup({
+    activityKeys,
+    isGroupOpen
+  });
+
+  const firstActivity = activities.at(0);
+  // eslint-disable-next-line no-magic-numbers
+  const lastActivity = activities.at(-1);
+
+  const lastMessage = useMemo(
+    () =>
+      getOrgSchemaMessage(
+        (
+          activities
+            .toReversed()
+            .find(
+              activity => 'streamType' in activity.channelData && activity.channelData.streamType === 'informative'
+            ) || lastActivity
+        )?.entities
+      ),
+    [activities, lastActivity]
+  );
+
+  const { renderAvatar, showCallout } = useRenderActivityProps(firstActivity);
+  const { renderActivityStatus, hideTimestamp } = useRenderActivityProps(lastActivity);
+  const [{ initials: botInitials }] = useAvatarForBot();
+
+  const hasAvatar = botInitials || typeof botInitials === 'string';
+  const showAvatar = showCallout && hasAvatar && !!renderAvatar;
+  const [{ bubbleNubOffset }] = useStyleOptions();
+
+  const topAlignedCallout = isZeroOrPositive(bubbleNubOffset);
+
+  // Skip render if this is the initial grouping setup
+  if (shouldSkipRender) {
+    return null;
+  }
+
+  return (
+    <FocusablePartGroupingActivity activity={firstActivity}>
+      <StackedLayoutRoot
+        hideAvatar={hasAvatar && !showAvatar}
+        isGroup={true}
+        showAvatar={showAvatar}
+        topCallout={topAlignedCallout}
+      >
+        <StackedLayoutMain avatar={showAvatar && renderAvatar && renderAvatar()}>
+          <CollapsibleGrouping isOpen={isGroupOpen} onToggle={setIsGroupOpen} title={lastMessage?.abstract || ''}>
+            {children}
+          </CollapsibleGrouping>
+        </StackedLayoutMain>
+        {renderActivityStatus && <StackedLayoutStatus>{renderActivityStatus({ hideTimestamp })}</StackedLayoutStatus>}
+      </StackedLayoutRoot>
+    </FocusablePartGroupingActivity>
+  );
+}
+
+PartGroupingActivity.displayName = 'PartGroupingActivity';
+
+export default memo(PartGroupingActivity);
+export { type PartGroupingActivityProps };
