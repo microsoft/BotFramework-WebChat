d0a48bdb1e1b7fead5fbcee397bfe99520c99746
Eugene <EOlonov@gmail.com>
2025-08-19 15:10:10 +0000

    fluent: part grouping decorator and related adjustments

---
+++ packages/fluent-theme/src/components/activity/PartGroupingDecorator.tsx
@@ -0,0 +1,36 @@
+import { type WebChatActivity } from 'botframework-webchat-core';
+import { PartGrouping } from 'botframework-webchat-component/internal';
+import cx from 'classnames';
+import React, { memo } from 'react';
+
+import CopilotMessageHeader from './CopilotMessageHeader';
+import useVariants from '../../private/useVariants';
+import { useStyles, useVariantClassName } from '../../styles';
+
+import styles from './PartGroupingingDecorator.module.css';
+
+type PartGroupingDecoratorProps = Readonly<{
+  activities: readonly WebChatActivity[];
+  children?: React.ReactNode;
+}>;
+
+function PartGroupingDecorator(props: PartGroupingDecoratorProps) {
+  const {
+    activities: [activity]
+  } = props;
+  const variants = useVariants();
+  const classNames = useStyles(styles);
+  const variantClassName = useVariantClassName(styles);
+
+  const shouldRenderHeader = variants.includes('copilot') && activity?.from?.role === 'bot';
+
+  return (
+    <div className={cx(classNames['part-grouping-decorator'], variantClassName)}>
+      {shouldRenderHeader && <CopilotMessageHeader activity={activity} />}
+      <PartGrouping {...props} />
+    </div>
+  );
+}
+
+export default memo(PartGroupingDecorator);
+export { type PartGroupingDecoratorProps };
