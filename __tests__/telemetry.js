6fab0e751d0dae1dbde803d588fd97fe537aea02
William Wong <compulim@users.noreply.github.com>
2025-08-25 08:19:17 +0000

    Upgrade a test

--- __tests__/telemetry.js
+++ __tests__/telemetry.js
@@ -9,56 +9,6 @@ jest.setTimeout(timeouts.test);
 jest.setTimeout(20000);
 
 describe('telemetry', () => {
-  test('should collect "init" event', async () => {
-
-    const { driver } = await setupWebDriver({
-      props: {
-        onTelemetry: event => {
-          const { data, dimensions, duration, error, fatal, name, type, value } = event;
-
-          window.WebChatTest.telemetryMeasurements.push({
-            data,
-            dimensions,
-            duration,
-            error,
-            fatal,
-            name,
-            type,
-            value
-          });
-        }
-      },
-      setup: () => {
-        window.WebChatTest.telemetryMeasurements = [];
-      }
-    });
-
-    await driver.wait(uiConnected(), timeouts.directLine);
-
-    await expect(
-      driver.executeScript(() => window.WebChatTest.telemetryMeasurements.filter(({ name }) => name === 'init'))
-    ).resolves.toMatchInlineSnapshot(`
-      [
-        {
-          "data": null,
-          "dimensions": {
-            "capability:downscaleImage:workerType": "web worker",
-            "capability:renderer": "html",
-            "prop:locale": "en-US",
-            "prop:speechRecognition": "false",
-            "prop:speechSynthesis": "false",
-          },
-          "duration": null,
-          "error": null,
-          "fatal": null,
-          "name": "init",
-          "type": "event",
-          "value": null,
-        },
-      ]
-    `);
-  });
-
   test('should collect fatal error', async () => {
     const { driver, pageObjects } = await setupWebDriver({
       props: {
