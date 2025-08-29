4305766f2542d0061deb43261ecf38b502f1124e
Eugene <EOlonov@gmail.com>
2025-08-29 00:04:29 +0000

    cleanup: remove unused context

--- packages/component/src/Middleware/ActivityGrouping/ui/PartGrouping/private/usePartGroupingContext.ts
+++
@@ -1,7 +0,0 @@
-import { useContext } from 'react';
-
-import PartGroupingContext, { type PartGroupingContextType } from './PartGroupingContext';
-
-export default function usePartGroupingContext(): PartGroupingContextType {
-  return useContext(PartGroupingContext);
-}
