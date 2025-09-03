d915569780401372f9e0cdc3fafa1fe00aa9a9cc
William Wong <compulim@users.noreply.github.com>
2025-09-03 06:51:01 +0000

    Port to TypeScript

--- packages/component/src/Utils/readDataURIToBlob.ts
+++ packages/component/src/Utils/readDataURIToBlob.ts
@@ -4,7 +4,13 @@ const PATTERN = /^data:([^,]*?)(;(base64)){0,1},([A-Za-z0-9+/=]+)/u;
 
 const DEFAULT_CONTENT_TYPE = 'text/plain;charset=US-ASCII';
 
-export function parse(dataURI) {
+export function parse(dataURI: string):
+  | {
+      base64: string;
+      contentType: string;
+      encoding: string;
+    }
+  | undefined {
   const match = PATTERN.exec(dataURI);
 
   if (!match) {
@@ -20,12 +26,12 @@ export function parse(dataURI) {
   return { base64, contentType: contentType || DEFAULT_CONTENT_TYPE, encoding };
 }
 
-export default function readDataURIToBlob(dataURI) {
+export default function readDataURIToBlob(dataURI: string): Blob | undefined {
   const parsed = parse(dataURI);
 
   if (!parsed) {
     return;
   }
 
-  return new Blob([toByteArray(parsed.base64)], { type: parsed.contentType });
+  return new Blob([toByteArray(parsed.base64).buffer as ArrayBuffer], { type: parsed.contentType });
 }
