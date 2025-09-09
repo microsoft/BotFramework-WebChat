621ff8cd460dab3d53206970a487039b43f6c8ff
William Wong <compulim@users.noreply.github.com>
2025-09-09 00:19:24 +0000

    Sort

--- packages/component/src/Middleware/ActivityGrouping/createDefaultActivityGroupingDecoratorMiddleware.tsx
+++ packages/component/src/Middleware/ActivityGrouping/createDefaultActivityGroupingDecoratorMiddleware.tsx
@@ -1,8 +1,8 @@
 import { createActivityGroupingMiddleware, type DecoratorMiddleware } from 'botframework-webchat-api/decorator';
+import PartGrouping from './ui/PartGrouping/PartGrouping';
 import RenderActivityGrouping from './ui/RenderActivityGrouping';
 import SenderGrouping from './ui/SenderGrouping/SenderGrouping';
 import StatusGrouping from './ui/StatusGrouping/StatusGrouping';
-import PartGrouping from './ui/PartGrouping/PartGrouping';
 
 export default function createDefaultActivityGroupingDecoratorMiddleware(): readonly DecoratorMiddleware[] {
   return Object.freeze([
