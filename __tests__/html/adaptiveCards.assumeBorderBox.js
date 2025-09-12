d7576cc9003a45982077e9861155570096ffb11f
William Wong <compulim@users.noreply.github.com>
2025-09-12 11:15:08 -0700

    Port tests to `html2` (a) (#5576)
    * Port tests to html2
    
    * Port tests to html2
    
    ---------
    
    Co-authored-by: Eugene <EOlonov@gmail.com>

--- __tests__/html/adaptiveCards.assumeBorderBox.js
+++
@@ -1,6 +0,0 @@
-/** @jest-environment ./packages/test/harness/src/host/jest/WebDriverEnvironment.js */
-
-describe('Adaptive Cards', () => {
-  test('assumes the container is using "forced border-box" mode', () =>
-    runHTML('adaptiveCards.assumeBorderBox.html'));
-});
--- __tests__/html/adaptiveCards.tapAction.js
+++
@@ -1,6 +0,0 @@
-/** @jest-environment ./packages/test/harness/src/host/jest/WebDriverEnvironment.js */
-
-describe('Adaptive Cards', () => {
-  test('with "tapAction" prop should react to click, ENTER, and SPACEBAR', () =>
-    runHTML('adaptiveCards.tapAction.html'));
-});
