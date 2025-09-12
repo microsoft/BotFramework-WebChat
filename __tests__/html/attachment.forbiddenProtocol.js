f1c15a4bce233ca1bc67518b1d610424c70b179a
William Wong <compulim@users.noreply.github.com>
2025-09-12 04:54:16 +0000

    Port tests to html2

--- __tests__/html/attachment.forbiddenProtocol.js
+++
@@ -1,6 +0,0 @@
-/** @jest-environment ./packages/test/harness/src/host/jest/WebDriverEnvironment.js */
-
-describe('Attachment', () => {
-  test('with "contentUrl" of forbidden protocols', () =>
-    runHTML('attachment.forbiddenProtocol.html'));
-});
