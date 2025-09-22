e1f0e1ba751e82ed27fc91311731606b41f7c779
William Wong <compulim@users.noreply.github.com>
2025-09-22 11:32:00 +0000

    Clean up

--- packages/bundle/esbuild.static.mjs
+++ packages/bundle/esbuild.static.mjs
@@ -6,7 +6,6 @@
 import * as esbuild from 'esbuild';
 import { resolve as importMetaResolve } from 'import-meta-resolve';
 import { dirname, join, resolve } from 'path';
-import { getPackageEntryPoints } from 'pkg-entry-points';
 import { readPackageUp } from 'read-pkg-up';
 import { fileURLToPath, pathToFileURL } from 'url';
 
@@ -29,31 +28,6 @@ const umdResolvePlugin = {
   }
 };
 
-// /** @type { (exports: any) => Generator<[string, string]> } */
-// function* walkExports(exports) {
-//   let name;
-
-//   for (const [key, value1] of Object.entries(exports)) {
-//     if (key.startsWith('.')) {
-//       name = key.slice(2);
-
-//       for (const [key, value2] of Object.entries(value1)) {
-//         if (key === 'import') {
-//           if (typeof value2 === 'string') {
-//             yield [name, value2];
-//           } else {
-//             for (const [key, value3] of Object.entries(value2)) {
-//               if (key === 'default') {
-//                 yield [name, value3];
-//               }
-//             }
-//           }
-//         }
-//       }
-//     }
-//   }
-// }
-
 function extractName(entry) {
   const tokens = entry.split('/');
   const names = tokens.splice(0, entry.startsWith('@') ? 2 : 1);
@@ -63,8 +37,6 @@ function extractName(entry) {
 
 /** @type { Map<string, import('esbuild').BuildOptions> } */
 const configs = new Map();
-/** @type { Set<string> } */
-const doneConfigs = new Set();
 
 function flatName(name) {
   return name.replace(/\//gu, '.').replace(/^@/u, '');
@@ -87,120 +59,11 @@ async function readPackageUpForReal(cwd) {
   return result;
 }
 
-// async function addConfig(cwd, entry) {
-//   const result = fileURLToPath(importMetaResolve(entry, pathToFileURL(cwd)));
-//   // const { packageJson, path: packagePath } = await readPackageUp({ cwd: result });
-//   const { packageJson, path: packagePath } = await readPackageUpForReal(result);
-//   const { name } = packageJson;
-
-//   const packageExports = await getPackageEntryPoints(dirname(packagePath));
-
-//   // console.log('addConfig', packagePath);
-
-//   if (configs.has(name) || doneConfigs.has(name)) {
-//     return;
-//   }
-
-//   /** @type { import('esbuild').BuildOptions } */
-//   const baseConfig = {
-//     chunkNames: `${flatName(name)}.[name].chunk-[hash]`,
-//     entryNames: `${flatName(name)}.[name]`
-//   };
-
-//   const exports = [];
-
-//   console.log(name, packageExports);
-
-//   for (const [key, value] of Object.entries(packageExports)) {
-//     exports.push(key.slice(2));
-//   }
-
-//   const config = {
-//     ...baseConfig,
-//     absWorkingDir: dirname(packagePath),
-//     entryPoints: exports.map(key => {
-//       const keyPrefixSlash = key && `/${key}`;
-
-//       return {
-//         // in: `npm:${name}${keyPrefixSlash}`,
-//         in: `${name}${keyPrefixSlash}`,
-//         out: `${name}${keyPrefixSlash}`
-//       };
-//     })
-//   };
-
-//   configs.set(name, config);
-
-//   // if (!packageJson.exports) {
-//   //   configs.set(name, {
-//   //     ...baseConfig,
-//   //     entryPoints: [
-//   //       {
-//   //         // in: `npm:${name}`,
-//   //         in: name,
-//   //         out: `${flatName(name)}`
-//   //       }
-//   //     ]
-//   //   });
-//   // } else {
-//   //   if (name.includes('classname')) {
-//   //     console.log(packageJson.exports);
-//   //   }
-
-//   //   let exports = Array.from(walkExports(packageJson.exports));
-//   //   let exports2 = [];
-
-//   //   if (name === 'html-react-parser') {
-//   //     console.log(packagePath);
-//   //     console.log(exports);
-
-//   //     for (const [key, value] of exports) {
-//   //       if (key.endsWith('/*')) {
-//   //         const matches = await promisify(glob)(value, { cwd: dirname(packagePath) });
-
-//   //         // Hacking for now.
-//   //         for (const match of matches) {
-//   //           exports2.push([match.replace('esm/', 'lib/').replace('.mjs', ''), `./${match}`]);
-//   //         }
-//   //       } else {
-//   //         exports2.push([key, value]);
-//   //       }
-//   //     }
-
-//   //     exports = exports2;
-
-//   //     console.log(exports);
-//   //     console.log(exports2);
-//   //   }
-
-//   //   const config = {
-//   //     ...baseConfig,
-//   //     absWorkingDir: dirname(packagePath),
-//   //     entryPoints: exports.map(([key, value]) => {
-//   //       const keyPrefixSlash = key && `/${key}`;
-
-//   //       return {
-//   //         // in: `npm:${name}${keyPrefixSlash}`,
-//   //         in: `${name}${keyPrefixSlash}`,
-//   //         out: `${name}${keyPrefixSlash}`
-//   //       };
-//   //     })
-//   //   };
-
-//   //   if (name.includes('classname')) {
-//   //     console.log(config);
-//   //   }
-
-//   //   configs.set(name, config);
-//   // }
-// }
-
 async function addConfig2(
   /** @type { import('esbuild').OnResolveArgs } */
   args
 ) {
   const result = fileURLToPath(importMetaResolve(args.path, pathToFileURL(args.resolveDir)));
-  // const { packageJson, path: packagePath } = await readPackageUp({ cwd: result });
   const { packageJson, path: packagePath } = await readPackageUpForReal(result);
   const { name } = packageJson;
 
@@ -227,14 +90,6 @@ async function addConfig2(
   const [moduleName, namedExports] = extractName(args.path);
 
   if (!entries.has(args.path)) {
-    //       const keyPrefixSlash = key && `/${key}`;
-
-    //       return {
-    //         // in: `npm:${name}${keyPrefixSlash}`,
-    //         in: `${name}${keyPrefixSlash}`,
-    //         out: `${name}${keyPrefixSlash}`
-    //       };
-
     // console.log('---', args.path, namedExports);
     entries.set(flatName(namedExports), args.path);
     currentConfig.entryPoints = Object.fromEntries(Array.from(entries.entries()));
@@ -326,7 +181,6 @@ const CJS = [
     entryPoints: [
       {
         in: process.argv[2],
-        // in: 'a.mjs',
         out: name
       }
     ]
