fedc95838529bd0e704c6dc269c39a4624051d5e
William Wong <compulim@users.noreply.github.com>
2025-09-24 02:22:51 +0000

    Clean up

--- packages/bundle/esbuild.static.mjs
+++ packages/bundle/esbuild.static.mjs
@@ -1,15 +1,12 @@
-/* eslint-disable no-magic-numbers */
-/* eslint-disable arrow-body-style */
-/* eslint-disable require-unicode-regexp */
 /// <reference types="node" />
+
+/* eslint-disable no-magic-numbers */
 
 import * as esbuild from 'esbuild';
 import { resolve as importMetaResolve } from 'import-meta-resolve';
 import { dirname, resolve } from 'path';
 import { readPackageUp } from 'read-pkg-up';
 import { fileURLToPath, pathToFileURL } from 'url';
-
-const CJS = [];
 
 function extractName(entry) {
   const tokens = entry.split('/');
@@ -134,21 +131,12 @@ async function crawl() {
       write: true,
       /** @type { import('esbuild').Plugin[] } */
       plugins: [
-        // umdResolvePlugin,
         {
           name: 'static-builder',
           setup(build) {
+            // eslint-disable-next-line require-unicode-regexp
             build.onResolve({ filter: /^[^.]/ }, async args => {
-              if (args.path === 'mime') {
-                return undefined;
-              }
-
               if (args.kind === 'import-statement') {
-                // Prevent external.umd from looping to self.
-                if (args.importer.includes('external.umd')) {
-                  return undefined;
-                }
-
                 const path = await addConfig(args);
 
                 if (!path) {
@@ -158,19 +146,10 @@ async function crawl() {
                 importMap.set(args.path, path);
 
                 return { external: true, path };
-                // } else if (args.kind === 'require-call') {
-                //   if (args.path === 'react') {
-                //     return { path: 'stub:react', namespace: 'stub' };
-                //   }
               }
 
               return undefined;
             });
-
-            // build.onLoad({ filter: /^stub:react$/, namespace: 'stub' }, () => ({
-            //   contents: "export * from 'react'; export { default } from 'react';",
-            //   resolveDir: resolve(fileURLToPath(import.meta.url), '../static')
-            // }));
           }
         }
       ]
@@ -189,17 +168,6 @@ async function crawl() {
     entryNames: `[name]`,
     entryPoints: [{ in: process.argv[2], out: name }]
   });
-
-  for (const moduleId of CJS) {
-    configs.set(moduleId, {
-      banner: { js: `/* botframework-webchat@0.0.0-0 - ${moduleId} */` },
-      chunkNames: `${flatName(moduleId.replace('@', '__'))}__[name]-[hash]`,
-      entryNames: `${flatName(moduleId.replace('@', '__'))}__[name]`,
-      entryPoints: {
-        [`${moduleId.split('@')[0]}`]: `./external.umd/${moduleId}.ts`
-      }
-    });
-  }
 
   for (let i = 0; i < 10000; i++) {
     // eslint-disable-next-line no-await-in-loop
