7bc8e335d0e2227ceec84776717e4014e52d541c
William Wong <compulim@users.noreply.github.com>
2025-09-24 12:07:04 -0700

    Add support for <script type="module"> (a.k.a. fat module) (#5592)
    * First attempt
    
    * Clean up
    
    * Ignore static
    
    * Better separator
    
    * Add importmap
    
    * Add static
    
    * Add version number
    
    * Clean up
    
    * Short chunk name
    
    * Unify entryNames
    
    * Use __ (double underscore)
    
    * Add sourcemap
    
    * Better React
    
    * Clean up
    
    * Add version number
    
    * Save ReactDOM
    
    * Use React CJS without global
    
    * Add Speech SDK
    
    * Repack
    
    * Add base64-js
    
    * Fix eslint
    
    * Repack botframework-directlinejs
    
    * Better esbuild src
    
    * Repack react and react-dom
    
    * Add sourcemap
    
    * Repack react-is
    
    * Clean up
    
    * Add @types/*
    
    * Clean up
    
    * Delete object-assign
    
    * Delete object-assign
    
    * Cheaper object-assign
    
    * Remove unnecessary devDependencies
    
    * Clean up
    
    * Add comment for 1.45.0
    
    * Fix eslint
    
    * Remove isConcurrentMode
    
    * Bump to react-is@16
    
    * Revert
    
    * Add precommit
    
    * Debump to 1.17.0
    
    * Add repack
    
    * Fix npm start for repacks
    
    * Fix npm run build
    
    * Add start:static
    
    * Better message
    
    * Better message
    
    * Clean up
    
    * Add first fat module test
    
    * Add more tests
    
    * Add more exports and tests
    
    * Use isomorphic React
    
    * Add snapshot
    
    * Fix watch mode
    
    * Isomorphic React as esbuild plugin
    
    * Fix npm run build
    
    * Remove isomorphic React
    
    * Rename to skip
    
    * Add to lint-staged
    
    * Fix dev server
    
    * Add npm start html-react-parser
    
    * Remove comment
    
    * Clean up
    
    * Don't print token
    
    * Add test for createRoot
    
    * Fix import react-dom
    
    * Add entry
    
    * Add static
    
    * Upload static artifact

---
+++ packages/repack/microsoft-cognitiveservices-speech-sdk/src/common.speech/Exports.ts
@@ -0,0 +1,7 @@
+// Commented lines are for microsoft-cognitiveservices-speech-sdk@1.45.0.
+
+export {
+  connectivity,
+  // ISpeechConfigAudioDevice,
+  type
+} from 'microsoft-cognitiveservices-speech-sdk/distrib/lib/src/common.speech/Exports.js';
---
+++ packages/repack/microsoft-cognitiveservices-speech-sdk/src/common/AudioSourceEvents.ts
@@ -0,0 +1,12 @@
+export {
+  AudioSourceErrorEvent,
+  AudioSourceEvent,
+  AudioSourceInitializingEvent,
+  AudioSourceOffEvent,
+  AudioSourceReadyEvent,
+  AudioStreamNodeAttachedEvent,
+  AudioStreamNodeAttachingEvent,
+  AudioStreamNodeDetachedEvent,
+  AudioStreamNodeErrorEvent,
+  AudioStreamNodeEvent
+} from 'microsoft-cognitiveservices-speech-sdk/distrib/lib/src/common/AudioSourceEvents.js';
