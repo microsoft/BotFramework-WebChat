f70605b47f1b628427f86de5a71e4139d866acc1
William Wong <compulim@users.noreply.github.com>
2025-09-24 01:37:55 +0000

    Fix eslint

--- packages/repack/microsoft-cognitiveservices-speech-sdk/src/common.speech/Exports.ts
+++ packages/repack/microsoft-cognitiveservices-speech-sdk/src/common.speech/Exports.ts
@@ -1,5 +1,5 @@
 export {
   connectivity,
-  ISpeechConfigAudioDevice,
+  // ISpeechConfigAudioDevice,
   type
 } from 'microsoft-cognitiveservices-speech-sdk/distrib/lib/src/common.speech/Exports.js';
