4e82abedf935a4594c695fe2804c258dafeb58d3
Eugene <EOlonov@gmail.com>
2025-10-07 21:00:12 +0000

    fix tsd

---
+++ packages/base/utils.js
@@ -0,0 +1,4 @@
+// oxlint-disable no-empty-file
+// This file is needed for tsd primarily
+// as it seems to not respect moduleResolution setting in tsconfig.json
+// nor it respects exports field in package.json properly.
