909aee0a01a6e5a726e663f6d771f285b75a1d8d
William Wong <compulim@users.noreply.github.com>
2025-10-15 00:50:47 +0000

    Add stylesRoot

--- packages/experience-chat-launcher/src/private/ChatLauncher.tsx
+++ packages/experience-chat-launcher/src/private/ChatLauncher.tsx
@@ -1,8 +1,9 @@
 import { validateProps } from '@msinternal/botframework-webchat-react-valibot';
 import { useStyles } from '@msinternal/botframework-webchat-styles/react';
+import { ThemeProvider } from 'botframework-webchat/component';
 import cx from 'classnames';
-import React, { memo } from 'react';
-import { object, optional, pipe, readonly, string, type InferInput } from 'valibot';
+import React, { memo, useMemo } from 'react';
+import { custom, object, optional, pipe, readonly, string, union, type InferInput } from 'valibot';
 
 import ChatLauncherStylesheet from '../stylesheet/ChatLauncherStylesheet';
 import styles from './ChatLauncher.module.css';
@@ -10,7 +11,16 @@ import ChatLauncherButton from './private/ChatLauncherButton';
 
 const chatLauncherPropsSchema = pipe(
   object({
-    nonce: optional(string())
+    nonce: optional(string()),
+    stylesRoot: optional(
+      union(
+        [
+          custom<HTMLLinkElement>(value => value instanceof HTMLLinkElement && value.rel === 'stylesheet'),
+          custom<HTMLStyleElement>(value => value instanceof HTMLStyleElement)
+        ],
+        '"stylesRoot" must be either <link rel="stylesheet"> or <style>'
+      )
+    )
   }),
   readonly()
 );
@@ -18,13 +28,17 @@ const chatLauncherPropsSchema = pipe(
 type ChatLauncherProps = InferInput<typeof chatLauncherPropsSchema>;
 
 function ChatLauncher(props: ChatLauncherProps) {
-  const { nonce } = validateProps(chatLauncherPropsSchema, props);
+  const { nonce, stylesRoot } = validateProps(chatLauncherPropsSchema, props);
+
   const classNames = useStyles(styles);
+  const styleOptions = useMemo(() => ({ stylesRoot }), [stylesRoot]);
 
   return (
     <div className={cx('webchat', classNames['webchat-experience-chat-launcher'])}>
-      <ChatLauncherStylesheet nonce={nonce} />
-      <ChatLauncherButton />
+      <ThemeProvider styleOptions={styleOptions}>
+        <ChatLauncherStylesheet nonce={nonce} />
+        <ChatLauncherButton />
+      </ThemeProvider>
     </div>
   );
 }
