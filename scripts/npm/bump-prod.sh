6b9a652aa6e3f81a9c9a0bb3cdf032dc993573b2
William Wong <compulim@users.noreply.github.com>
2025-10-15 20:58:30 +0000

    Move bump and pre/postversion to /scripts

---
+++ scripts/npm/bump-prod.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+PACKAGES_TO_BUMP=$(cat package.json | jq -r '
+  (.pinDependencies // {}) as $P
+  | (.localDependencies // {} | keys) as $L
+  | (.dependencies // {})
+  | to_entries
+  | map(select(.key as $K | $L | contains([$K]) | not))
+  | map(.key + "@" + ($P[.key] // ["latest"])[0])
+  | join(" ")
+')
+
+[ ! -z "$PACKAGES_TO_BUMP" ] && npm install --save-exact $PACKAGES_TO_BUMP || true
