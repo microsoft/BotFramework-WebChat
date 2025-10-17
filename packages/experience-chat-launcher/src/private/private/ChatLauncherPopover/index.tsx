2ec1e761db3e0689c7314eb63faf6113018afa73
William Wong <compulim@users.noreply.github.com>
2025-10-17 07:01:08 +0000

    Add Web Chat inside

--- packages/experience-chat-launcher/src/private/private/ChatLauncherPopover/index.tsx
+++ packages/experience-chat-launcher/src/private/private/ChatLauncherPopover/index.tsx
@@ -1,9 +1,12 @@
+import { useStyles } from '@msinternal/botframework-webchat-styles/react';
 import { PopoverPolymiddlewareProxy } from 'botframework-webchat-api/middleware';
+import { BasicWebChat } from 'botframework-webchat-component/component';
 import mergeRefs from 'merge-refs';
 import React, { forwardRef, memo, useRef, type ForwardedRef } from 'react';
 import { object, pipe, readonly, type InferInput } from 'valibot';
 
 import TitleBar from './TitleBar';
+import styles from './index.module.css';
 
 const chatLauncherPopoverPropsSchema = pipe(object({}), readonly());
 
@@ -12,10 +15,20 @@ type ChatLauncherPopoverProps = InferInput<typeof chatLauncherPopoverPropsSchema
 function ChatLauncherPopover(_: ChatLauncherPopoverProps, ref: ForwardedRef<Element>) {
   const popoverRef = useRef<Element>(null);
 
+  const classNames = useStyles(styles);
+
   return (
     // TODO: [P2] Is it correct to force-cast ref to HTMLDivElement?
-    <PopoverPolymiddlewareProxy popover="manual" ref={mergeRefs(popoverRef, ref)} type="nonmodal">
-      <TitleBar popoverRef={popoverRef} />
+    <PopoverPolymiddlewareProxy
+      className={classNames['chat-launcher-popover']}
+      popover="manual"
+      ref={mergeRefs(popoverRef, ref)}
+      type="nonmodal"
+    >
+      <div className={classNames['chat-launcher-popover__box']}>
+        <TitleBar popoverRef={popoverRef} />
+        <BasicWebChat className={classNames['chat-launcher-popover__webchat']} />
+      </div>
     </PopoverPolymiddlewareProxy>
   );
 }
