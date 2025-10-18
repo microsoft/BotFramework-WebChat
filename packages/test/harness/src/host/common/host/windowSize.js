6f0e414ebf5ca0b148b10b582c48f97f75a9f328
William Wong <compulim@users.noreply.github.com>
2025-10-18 08:18:56 +0000

    Fix window size

--- packages/test/harness/src/host/common/host/windowSize.js
+++ packages/test/harness/src/host/common/host/windowSize.js
@@ -4,10 +4,13 @@ module.exports = (/** @type import('selenium-webdriver').WebDriver */ webDriver)
   async function windowSize(width, height, element) {
     const rect = await webDriver.manage().window().getRect();
 
-    height = +height + ADJUST_WINDOW_SIZE_HEIGHT || rect.height;
+    height = +height || rect.height;
     width = +width || rect.width;
 
-    await webDriver.manage().window().setRect({ height, width });
+    await webDriver
+      .manage()
+      .window()
+      .setRect({ height: +height + ADJUST_WINDOW_SIZE_HEIGHT || rect.height, width });
 
     /* istanbul ignore next */
     element &&
