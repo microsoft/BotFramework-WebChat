5627c35ba2b0f6983a87368889eb24ef46fa6756
William Wong <compulim@users.noreply.github.com>
2025-10-27 23:39:31 +0000

    Add expandArray

---
+++ packages/api-graph/src/private/schemas/expandArray.ts
@@ -0,0 +1,57 @@
+import { array, objectWithRest, optional, parse, pipe, readonly, string, union, type InferOutput } from 'valibot';
+
+import type { FlattenedNodeObject } from './FlattenedNodeObject';
+import identifier from './Identifier';
+import { literal, type Literal } from './Literal';
+import { nodeReference, type NodeReference } from './NodeReference';
+
+const expandedFlatNodeObjectSchema = pipe(
+  objectWithRest(
+    {
+      '@context': optional(string()),
+      '@id': optional(identifier()),
+      '@type': optional(array(string()))
+    },
+    array(union([literal(), nodeReference()]))
+  ),
+  readonly()
+);
+
+type ExpandedFlatNodeObject = InferOutput<typeof expandedFlatNodeObjectSchema>;
+
+function expandArray(node: FlattenedNodeObject): ExpandedFlatNodeObject {
+  const propertyMap = new Map<string, readonly (Literal | NodeReference)[]>();
+  let context: string | undefined;
+  let id: string | undefined;
+
+  for (const [key, value] of Object.entries(node)) {
+    const parsedValue = parse(union([array(nodeReference()), array(literal()), nodeReference(), literal()]), value);
+
+    switch (key) {
+      case '@context':
+        context = parse(string(), value);
+        break;
+
+      case '@id':
+        id = parse(string(), value);
+        break;
+
+      default:
+        // TODO: [P*] Test mixed array with literal and node reference.
+        propertyMap.set(key, Array.isArray(parsedValue) ? parsedValue : Object.freeze([parsedValue]));
+        break;
+    }
+  }
+
+  return parse(
+    expandedFlatNodeObjectSchema,
+    Object.fromEntries([
+      ...(context ? [['@context', context]] : []),
+      ...(id ? [['@id', id]] : []),
+      ...Array.from(propertyMap)
+    ])
+  );
+}
+
+export default expandArray;
+export { expandedFlatNodeObjectSchema, type ExpandedFlatNodeObject };
