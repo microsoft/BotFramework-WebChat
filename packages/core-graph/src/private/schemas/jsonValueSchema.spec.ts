197b9813bd1a00d0557caf69d9848d4d4d748971
William Wong <compulim@users.noreply.github.com>
2025-11-05 12:50:35 +0000

    Test assumptions

--- packages/core-graph/src/private/schemas/jsonValueSchema.spec.ts
+++ packages/core-graph/src/private/schemas/jsonValueSchema.spec.ts
@@ -7,15 +7,22 @@ scenario('jsonValueSchema', bdd => {
   bdd
     .given('a JSON object with value of undefined', () => ({ one: 1, two: undefined, three: 3 }))
     .when('parsed', target => parse(jsonValueSchema, target))
-    .then('should remove undefined values', (_, result) =>
-      expect(result).toEqual({
-        one: 1,
-        three: 3
-      })
-    );
+    .then('should remove undefined values', (_, result) => expect(result).toEqual({ one: 1, three: 3 }));
 
   bdd
     .given('a JSON array with value of undefined', () => [1, undefined, 3])
     .when('parsed', target => parse(jsonValueSchema, target))
+    .then('should transform undefined into null', (_, result) => expect(result).toEqual([1, null, 3]));
+});
+
+scenario('Assumptions', bdd => {
+  bdd
+    .given('a JSON object with value of undefined', () => ({ one: 1, two: undefined, three: 3 }))
+    .when('stringified then parsed', target => JSON.parse(JSON.stringify(target)))
+    .then('should remove undefined values', (_, result) => expect(result).toEqual({ one: 1, three: 3 }));
+
+  bdd
+    .given('a JSON array with value of undefined', () => [1, undefined, 3])
+    .when('stringified then parsed', target => JSON.parse(JSON.stringify(target)))
     .then('should transform undefined into null', (_, result) => expect(result).toEqual([1, null, 3]));
 });
