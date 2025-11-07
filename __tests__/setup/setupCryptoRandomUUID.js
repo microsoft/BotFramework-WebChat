0d22179451fdfef35529102ebfba8968bc03ff38
William Wong <compulim@users.noreply.github.com>
2025-11-07 19:36:43 +0000

    Fix randomUUID

--- __tests__/setup/setupCryptoRandomUUID.js
+++ __tests__/setup/setupCryptoRandomUUID.js
@@ -1,5 +1,5 @@
 import { v4 } from 'uuid';
 
 if (!global.crypto?.randomUUID) {
-  global.crypto = { ...global.crypto, randomUUID: v4() };
+  global.crypto = { ...global.crypto, randomUUID: v4 };
 }
