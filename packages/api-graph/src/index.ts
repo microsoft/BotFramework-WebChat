92b4648135d27467f8c15dd48b33c95fd36c982e
William Wong <compulim@users.noreply.github.com>
2025-11-07 21:48:53 +0000

    Clean up

--- packages/api-graph/src/index.ts
+++ packages/api-graph/src/index.ts
@@ -1,3 +1,2 @@
-// export { default as useNodeObject } from './private/useNodeObject';
 export { default as GraphProvider } from './private/GraphProvider';
 export { default as useOrderedActivities } from './private/useOrderedActivities';
--- packages/api-graph/src/private/GraphProvider.tsx
+++ packages/api-graph/src/private/GraphProvider.tsx
@@ -31,33 +31,6 @@ function GraphProvider(props: GraphProviderProps) {
 
   const graph = useMemo(() => createGraphFromStore(store), [store]);
 
-  // const getSnapshot = useCallback(() => {
-  //   console.log('%%%%%%%%%%%%%%%%%% getSnapshot');
-
-  //   return graph.snapshot();
-  // }, [graph]);
-
-  // const subscribe = useCallback<(onStoreChange: () => void) => () => void>(
-  //   onStoreChange => {
-  //     console.log('***************2 start subscribe');
-
-  //     (async () => {
-  //       for await (const _ of graph.observe()) {
-  //         console.log('XXXXXXXXXXX subscribing onStoreChange', _);
-  //         onStoreChange();
-  //       }
-
-  //       console.log('!!!!!!!!!! DONE');
-  //     })();
-
-  //     return () => {
-  //       // TODO: Need to find a way to unsubscribe, it seems iteration is not a good idea as we cannot unsubscribe immediately.
-  //     };
-  //   },
-  //   [graph]
-  // );
-
-  // const nodeMap: ReadonlyMap<Identifier, SlantNode> = useSyncExternalStore(subscribe, getSnapshot);
   const [nodeMap, setNodeMap] = useState<ReadonlyMap<Identifier, SlantNode>>(() => Object.freeze(new Map()));
   const [orderedMessages, setOrderedMessages] = useState<readonly MessageNode[]>(Object.freeze([]));
 
@@ -65,9 +38,6 @@ function GraphProvider(props: GraphProviderProps) {
 
   useEffect(() => {
     const handleChange: GraphSubscriber = record => {
-      // eslint-disable-next-line no-console
-      // console.log('🔔🔔🔔🔔🔔 GRAPH2 updated via CHANGE', record);
-
       let nextOrderedMessages: MessageNode[] | undefined;
       const state = graph.getState();
 
@@ -104,127 +74,6 @@ function GraphProvider(props: GraphProviderProps) {
     [orderedMessages]
   );
 
-  // useEffect(() => {
-  //   (async () => {
-  //     for await (const change of graph.observe()) {
-  //       // eslint-disable-next-line no-console
-  //       console.log('🔔🔔🔔🔔🔔 graph updated', change);
-
-  //       let nextOrderedActivities: WebChatActivity[] | undefined;
-
-  //       for (const id of change.ids) {
-  //         const node = graph.get(id);
-
-  //         if (node && isOfType(node, 'Message')) {
-  //           const message = node as MessageNode;
-
-  //           nextOrderedActivities ||= Array.from(orderedActivitiesRef.current);
-
-  //           nextOrderedActivities.push(
-  //             JSON.parse(message['urn:microsoft:webchat:direct-line-activity:raw-json'][0]) as WebChatActivity
-  //           );
-
-  //           nextOrderedActivities.sort(
-  //             (x, y) => x.channelData['webchat:sequence-id'] - y.channelData['webchat:sequence-id']
-  //           );
-  //         }
-  //       }
-
-  //       nextOrderedActivities && setOrderedActivities(Object.freeze(nextOrderedActivities));
-  //     }
-  //   })();
-  // }, [graph, orderedActivitiesRef, setNodeMap, setOrderedActivities]);
-
-  // const activityJSONMap = useMemoWithPrevious<ReadonlyMap<string, WebChatActivity>>(
-  //   prevActivityJSONMap => {
-  //     const nextMap = new Map();
-
-  //     for (const activityNode of nodeMap.values().filter((node): node is MessageNode => isOfType(node, 'Message'))) {
-  //       const id = activityNode['@id'];
-
-  //       nextMap.set(
-  //         id,
-  //         prevActivityJSONMap?.get(id) ||
-  //           (JSON.parse(activityNode['urn:microsoft:webchat:direct-line-activity:raw-json'][0]) as WebChatActivity)
-  //       );
-  //     }
-
-  //     return nextMap;
-  //   },
-  //   [nodeMap]
-  // );
-
-  // const orderedActivitiesState = useMemo<readonly [readonly WebChatActivity[]]>(() => {
-  //   // eslint-disable-next-line no-console
-  //   console.log('🔍🔍🔍🔍🔍 orderedActivities updated');
-
-  //   performance.mark('orderedActivities:start');
-
-  //   // TODO: [P*] Try insertion sort and mutation.
-  //   const result: readonly [readonly WebChatActivity[]] = Object.freeze([
-  //     Object.freeze(
-  //       Array.from(nodeMap.values().filter((node): node is MessageNode => isOfType(node, 'Message')))
-  //         // TODO: [P0] Insertion sort should be cheaper.
-  //         .sort((x, y) => x.position[0] - y.position[0])
-  //         .map(node => {
-  //           const activityJSON = activityJSONMap.get(node['@id']);
-
-  //           if (typeof activityJSON === 'undefined') {
-  //             throw new Error(`ASSERTION: activityJSON not found for @id of "${node['@id']}"`);
-  //           }
-
-  //           return activityJSON;
-  //         })
-  //     )
-  //   ]);
-
-  //   performance.mark('orderedActivities:end');
-  //   performance.measure('orderedActivities:duration', 'orderedActivities:start', 'orderedActivities:end');
-
-  //   return result;
-  // }, [activityJSONMap, nodeMap]);
-
-  // const orderedActivities = useMemoWithPrevious<readonly WebChatActivity[]>(
-  //   prevOrderedActivities => {
-  //     // eslint-disable-next-line no-console
-  //     console.log('🔍🔍🔍🔍🔍 orderedActivities updated');
-
-  //     performance.mark('orderedActivities:start');
-
-  //     // TODO: [P*] Try insertion sort and mutation.
-  //     const result: readonly [readonly WebChatActivity[]] = Object.freeze([
-  //       Object.freeze(
-  //         Array.from(nodeMap.values().filter((node): node is MessageNode => isOfType(node, 'Message')))
-  //           // TODO: [P0] Insertion sort should be cheaper.
-  //           .sort((x, y) => x.position[0] - y.position[0])
-  //           .map(node => {
-  //             const activityJSON = activityJSONMap.get(node['@id']);
-
-  //             if (typeof activityJSON === 'undefined') {
-  //               throw new Error(`ASSERTION: activityJSON not found for @id of "${node['@id']}"`);
-  //             }
-
-  //             return activityJSON;
-  //           })
-  //       )
-  //     ]);
-
-  //     performance.mark('orderedActivities:end');
-  //     performance.measure('orderedActivities:duration', 'orderedActivities:start', 'orderedActivities:end');
-
-  //     return result;
-  //   },
-  //   [activityJSONMap, nodeMap]
-  // );
-
-  // useEffect(() => {
-  //   // eslint-disable-next-line no-console
-  //   console.log('🔍🔍🔍🔍🔍 nodeMap', nodeMap);
-  //   // eslint-disable-next-line no-console
-  //   console.log('🔍🔍🔍🔍🔍 orderedActivities', orderedActivitiesState[0]);
-  // }, [nodeMap, orderedActivitiesState]);
-
-  // Build graph using `useSyncExternalStore()` so we can run graph everywhere.
   const context = useMemo<GraphContextType>(
     () =>
       Object.freeze({
