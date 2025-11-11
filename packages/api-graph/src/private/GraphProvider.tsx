c1b4eee46c2d417bc19854f3ca0550d5ced9aea5
William Wong <compulim@users.noreply.github.com>
2025-11-11 08:53:43 +0000

    Fix replace activity for activity without ID

--- packages/api-graph/src/private/GraphProvider.tsx
+++ packages/api-graph/src/private/GraphProvider.tsx
@@ -59,16 +59,17 @@ function GraphProvider(props: GraphProviderProps) {
 
           if (node && isOfType(node, 'urn:microsoft:webchat:direct-line-activity')) {
             const activityNode = node as DirectLineActivityNode;
+            const activityId = activityNode['urn:microsoft:webchat:direct-line-activity:id']?.[0];
 
             nextOrderedMessages ||= Array.from(prevOrderedMessages);
 
             // Replace activity node with same activity ID.
             // TODO: [P*] When working on activity keyer, the new activity should reuse same @id.
-            const existingActivityNodeWithSameActivityId = nextOrderedMessages.find(
-              node =>
-                node['urn:microsoft:webchat:direct-line-activity:id'][0] ===
-                activityNode['urn:microsoft:webchat:direct-line-activity:id'][0]
-            );
+            const existingActivityNodeWithSameActivityId =
+              activityId &&
+              nextOrderedMessages.find(
+                node => node['urn:microsoft:webchat:direct-line-activity:id']?.[0] === activityId
+              );
 
             existingActivityNodeWithSameActivityId &&
               removeInline(nextOrderedMessages, existingActivityNodeWithSameActivityId);
