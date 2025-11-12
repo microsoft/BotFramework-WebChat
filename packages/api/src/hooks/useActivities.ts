9c81085fae49463b675c898665ac50ced8bfd9ea
William Wong <compulim@users.noreply.github.com>
2025-11-12 03:06:24 +0000

    Assert with Redux

--- packages/api/src/hooks/useActivities.ts
+++ packages/api/src/hooks/useActivities.ts
@@ -3,12 +3,50 @@ import type { WebChatActivity } from 'botframework-webchat-core';
 import { useOrderedActivities } from '@msinternal/botframework-webchat-api-graph';
 import { useSelector } from './internal/WebChatReduxContext';
 
-export default function useActivities(mode: 'graph' | 'redux' = 'graph'): readonly [readonly WebChatActivity[]] {
-  const activitiesFromGraph = useOrderedActivities();
-  const activitiesFromRedux = useSelector(({ activities }) => activities);
+declare const process: {
+  env: {
+    NODE_ENV?: string | undefined;
+  };
+};
 
-  return mode === 'redux' ? [activitiesFromRedux] : activitiesFromGraph;
+export default function useActivities(): readonly [readonly WebChatActivity[]] {
+  const activitiesFromGraphState = useOrderedActivities();
 
-  // return useOrderedActivities();
-  // return Object.freeze([useSelector(({ activities }) => activities)]);
+  // ASSERTION: Before we fully migrate to graph, make sure graph and Redux are the same.
+  if (process.env.NODE_ENV !== 'production') {
+    const [activitiesFromGraph] = activitiesFromGraphState;
+
+    // Assert based on NODE_ENV.
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const activitiesFromRedux = useSelector(({ activities }) => activities);
+
+    if (activitiesFromGraph.length !== activitiesFromRedux.length) {
+      throw new Error(
+        `botframework-webchat-internal: Activities from graph and Redux are of different size (graph has ${activitiesFromGraph.length} activities, Redux has ${activitiesFromRedux.length} activities)`,
+        {
+          cause: {
+            activitiesFromGraph,
+            activitiesFromRedux
+          }
+        }
+      );
+    }
+
+    for (let index = 0; index < activitiesFromGraph.length; index++) {
+      if (!Object.is(activitiesFromGraph.at(index), activitiesFromRedux.at(index))) {
+        throw new Error(
+          `botframework-webchat-internal: Activities from graph and Redux are of different at index ${index}`,
+          {
+            cause: {
+              activitiesFromGraph,
+              activitiesFromRedux,
+              index
+            }
+          }
+        );
+      }
+    }
+  }
+
+  return activitiesFromGraphState;
 }
