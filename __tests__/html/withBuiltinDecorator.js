6be90c8a75e9c2c1b697c39e89168c7ba1680682
William Wong <compulim@users.noreply.github.com>
2025-12-09 10:16:17 +0000

    Port decorator and video

--- __tests__/html/withBuiltinDecorator.js
+++
@@ -1,5 +0,0 @@
-/** @jest-environment ./packages/test/harness/src/host/jest/WebDriverEnvironment.js */
-
-describe('built-in decorator', () => {
-  test('with decorators', () => runHTML('withBuiltinDecorator'));
-});
