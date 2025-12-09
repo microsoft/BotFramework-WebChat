f5516aed7fe2538068612d9a3b4aa8089de854e5
William Wong <compulim@users.noreply.github.com>
2025-12-09 09:10:06 +0000

    Port fluentTheme

--- __tests__/html/fluentTheme/suggestedActions.layout.flow.focus.js
+++
@@ -1,5 +0,0 @@
-/** @jest-environment ./packages/test/harness/src/host/jest/WebDriverEnvironment.js */
-
-describe('Fluent theme applied', () => {
-  test('suggested actions roving focus in flow layout', () => runHTML('fluentTheme/suggestedActions.layout.flow.focus'));
-});
--- __tests__/html/fluentTheme/telephoneKeypad.tap.js
+++
@@ -1,5 +0,0 @@
-/** @jest-environment ./packages/test/harness/src/host/jest/WebDriverEnvironment.js */
-
-describe('Fluent theme applied', () => {
-  test('telephone keypad when tapped should send message', () => runHTML('fluentTheme/telephoneKeypad.tap'));
-});
