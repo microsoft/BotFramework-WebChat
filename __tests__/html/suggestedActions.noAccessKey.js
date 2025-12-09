cdb9a2c00f9754d43a36aaddd288bd0a8874f6db
William Wong <compulim@users.noreply.github.com>
2025-12-09 09:46:47 +0000

    Port some suggestedActions

--- __tests__/html/suggestedActions.noAccessKey.js
+++
@@ -1,5 +0,0 @@
-/** @jest-environment ./packages/test/harness/src/host/jest/WebDriverEnvironment.js */
-
-describe('suggested actions with access key disabled', () => {
-  test('should not have screen reader text related to access key.', () => runHTML('suggestedActions.noAccessKey.html'));
-});
