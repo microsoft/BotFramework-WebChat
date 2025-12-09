e62bf04d4cd6ff1ad8e6950955a2ad92a64fa29f
William Wong <compulim@users.noreply.github.com>
2025-12-09 08:33:03 +0000

    Port send box related tests

--- __tests__/html/sendBoxMiddleware/changeModality.js
+++
@@ -1,5 +0,0 @@
-/** @jest-environment ./packages/test/harness/src/host/jest/WebDriverEnvironment.js */
-
-describe('sendBoxMiddleware', () => {
-  test('should change modality based on last activity received', () => runHTML('sendBoxMiddleware/changeModality'));
-});
--- __tests__/html/sendBoxToolbarMiddleware/replace.js
+++
@@ -1,5 +0,0 @@
-/** @jest-environment ./packages/test/harness/src/host/jest/WebDriverEnvironment.js */
-
-describe('sendBoxToolbarMiddleware', () => {
-  test('should replace default send box toolbar', () => runHTML('sendBoxToolbarMiddleware/replace'));
-});
