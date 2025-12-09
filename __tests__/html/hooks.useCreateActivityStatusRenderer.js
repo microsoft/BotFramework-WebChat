b57915fa027b4860d1c41d95a8e3f5cd93df5663
William Wong <compulim@users.noreply.github.com>
2025-12-09 10:21:05 +0000

    Port hooks

--- __tests__/html/hooks.useCreateActivityStatusRenderer.js
+++
@@ -1,5 +0,0 @@
-/** @jest-environment ./packages/test/harness/src/host/jest/WebDriverEnvironment.js */
-
-describe('useCreateActivityStatusRenderer', () => {
-  test('should render activity status', () => runHTML('hooks.useCreateActivityStatusRenderer.html'));
-});
--- __tests__/html/hooks.useRenderAttachment.js
+++
@@ -1,5 +0,0 @@
-/** @jest-environment ./packages/test/harness/src/host/jest/WebDriverEnvironment.js */
-
-describe('useRenderAttachment', () => {
-  test('should render attachment', () => runHTML('hooks.useRenderAttachment.html'));
-});
