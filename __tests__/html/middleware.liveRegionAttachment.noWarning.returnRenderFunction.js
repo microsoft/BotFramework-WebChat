db5765ad7a0dd537713822733654320deb15b40b
William Wong <compulim@users.noreply.github.com>
2025-12-09 09:25:11 +0000

    Port middleware*

--- __tests__/html/middleware.liveRegionAttachment.noWarning.returnRenderFunction.js
+++
@@ -1,6 +0,0 @@
-/** @jest-environment ./packages/test/harness/src/host/jest/WebDriverEnvironment.js */
-
-describe('attachment for screen reader middleware', () => {
-  test('should not warn if returning a render function which return a React element', () =>
-    runHTML('middleware.liveRegionAttachment.noWarning.returnRenderFunction.html'));
-});
