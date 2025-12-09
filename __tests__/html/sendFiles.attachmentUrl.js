f8153a61782e2616d3ce477a55706cd0f17cf5a9
William Wong <compulim@users.noreply.github.com>
2025-12-09 09:37:09 +0000

    Port sendFiles

--- __tests__/html/sendFiles.attachmentUrl.js
+++
@@ -1,3 +0,0 @@
-/** @jest-environment ./packages/test/harness/src/host/jest/WebDriverEnvironment.js */
-
-test('text url attachment sent from bot should be rendered', () => runHTML('sendFiles.attachmentUrl.html'));
