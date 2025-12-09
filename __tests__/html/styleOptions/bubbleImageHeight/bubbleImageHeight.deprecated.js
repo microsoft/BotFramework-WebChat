1c7162719c7cae483abfc084574037f0a82940b0
William Wong <compulim@users.noreply.github.com>
2025-12-09 08:41:16 +0000

    Port tests

--- __tests__/html/styleOptions/bubbleImageHeight/bubbleImageHeight.deprecated.js
+++
@@ -1,6 +0,0 @@
-/** @jest-environment ./packages/test/harness/src/host/jest/WebDriverEnvironment.js */
-
-describe('styleOptions.bubbleImageHeight', () => {
-  test('should resize images with deprecation', () =>
-    runHTML('styleOptions/bubbleImageHeight/bubbleImageHeight.deprecated'));
-});
