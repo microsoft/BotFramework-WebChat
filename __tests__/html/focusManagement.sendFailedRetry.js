ca13ff43efce4dcf36b3173c37fcab67662408c0
William Wong <compulim@users.noreply.github.com>
2025-12-09 09:17:31 +0000

    Port focusManagement

--- __tests__/html/focusManagement.sendFailedRetry.js
+++
@@ -1,6 +0,0 @@
-/** @jest-environment ./packages/test/harness/src/host/jest/WebDriverEnvironment.js */
-
-describe('focus management', () => {
-  test('click on retry button should focus on main', () =>
-    runHTML('focusManagement.sendFailedRetry.html'));
-});
