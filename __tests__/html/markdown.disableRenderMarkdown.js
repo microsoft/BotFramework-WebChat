c3fa7d9fb676fc90d76853c7597c4a0476acfdec
William Wong <compulim@users.noreply.github.com>
2025-12-09 09:23:29 +0000

    Port Markdown and meta tag

--- __tests__/html/markdown.disableRenderMarkdown.js
+++
@@ -1,5 +0,0 @@
-/** @jest-environment ./packages/test/harness/src/host/jest/WebDriverEnvironment.js */
-
-describe('Markdown', () => {
-  test('should be disabled when setting "renderMarkdown" to false', () => runHTML('markdown.disableRenderMarkdown.html'));
-});
--- __tests__/html/metaTag.directLine.es5.js
+++
@@ -1,5 +0,0 @@
-/** @jest-environment ./packages/test/harness/src/host/jest/WebDriverEnvironment.js */
-
-describe('Meta tag for Direct Line', () => {
-  test('should contains build variant for ES5', () => runHTML('metaTag.directLine.es5.html'));
-});
