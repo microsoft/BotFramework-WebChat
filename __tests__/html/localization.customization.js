e8f02bb9507aa4a6c61253e9957b19cd28c54db5
William Wong <compulim@users.noreply.github.com>
2025-12-09 09:20:21 +0000

    Port rich card and localization

--- __tests__/html/localization.customization.js
+++
@@ -1,6 +0,0 @@
-/** @jest-environment ./packages/test/harness/src/host/jest/WebDriverEnvironment.js */
-
-describe('localization strings', () => {
-  test('should be customizable.', () =>
-    runHTML('localization.customization.html'));
-});
