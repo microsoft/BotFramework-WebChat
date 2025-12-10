168fe524a0404ca15f78638c1516d137263c1691
William Wong <compulim@users.noreply.github.com>
2025-12-10 15:01:26 -0800

    Upgrade last set of tests from `html` to `html2` (#5651)
    * Port heroCard and sendBox
    
    * Port suggestedActions layout
    
    * Port suggested actions stacked button text wrap
    
    * Port suggested actions style options
    
    * Move tests to skipped
    
    * Clean up
    
    * Default to html2
    
    * Move assets to html2
    
    * Fix html2 snapshot to use local
    
    * Remove leftovers
    
    * Move assets
    
    * Remove html and fix assets

--- __tests__/html/suggestedActions.styleOptions.js
+++
@@ -1,7 +0,0 @@
-/** @jest-environment ./packages/test/harness/src/host/jest/WebDriverEnvironment.js */
-
-describe('suggested actions', () => {
-  test('when using default style options', () => runHTML('suggestedActions.styleOptions?preset=default'));
-  test('when overriding with deprecated style options', () => runHTML('suggestedActions.styleOptions?preset=deprecated'));
-  test('when overriding style options', () => runHTML('suggestedActions.styleOptions'));
-});
---
+++ __tests__/html2/skipped/README.md
@@ -0,0 +1,1 @@
+This folder keeps all tests that are no longer used, or requires significant investment and not cost effective to get them back online.
