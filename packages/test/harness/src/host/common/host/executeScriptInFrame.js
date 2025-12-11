0bf952d86f1e9c88687c32d158d544e92f4fd75a
William Wong <compulim@users.noreply.github.com>
2025-12-11 07:08:15 +0000

    Migrate video

---
+++ packages/test/harness/src/host/common/host/executeScriptInFrame.js
@@ -0,0 +1,37 @@
+/* eslint-env browser */
+/* eslint-disable no-magic-numbers */
+const { Condition } = require('selenium-webdriver');
+
+module.exports = webDriver =>
+  async function executeScriptInFrame(element, fnString) {
+    const parentLocationHref = await webDriver.executeScript(() => document.location.href);
+
+    await webDriver.switchTo().frame(element);
+
+    // TODO: [P2] Workaround the bug or bump selenium-webdriver
+    //            selenium-webdriver has a bug that frame switching is not complete until 2 seconds later.
+    //            There is currently no workaround other than sleeping.
+
+    // await new Promise(resolve => setTimeout(resolve, 2500));
+
+    await webDriver.wait(
+      new Condition('until switched to IFRAME', async webDriver => {
+        // We only have #document and cannot send WebElement from other frames (it would crash silently.)
+        // The only way to detect when we switch frame is check if `document.location.href` changed to something new.
+
+        // Few things we tried:
+        // - Use HTMLIFrameElement.src, however, the #document could have different URL due to 304
+        // - Send WebElement, it crash silently
+        // - Set #hash, the page would reload
+
+        const locationHref = await webDriver.executeScript(() => document.location.href);
+
+        return locationHref !== parentLocationHref;
+      }),
+      5000
+    );
+
+    await webDriver.executeScript(`(${fnString})()`);
+
+    await webDriver.switchTo().defaultContent();
+  };
