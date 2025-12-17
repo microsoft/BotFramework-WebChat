e56eac10754b6ab25669c917fab4679bd769f041
William Wong <compulim@users.noreply.github.com>
2025-12-17 20:13:19 +0000

    Remove NODE_ENV branching

--- packages/api/src/hooks/useActivities.ts
+++ packages/api/src/hooks/useActivities.ts
@@ -10,53 +10,57 @@ declare const process: {
   };
 };
 
-export default function useActivities(): readonly [readonly WebChatActivity[]] {
-  const activitiesFromGraphState = useOrderedActivities();
+function useActivitiesForProduction(): readonly [readonly WebChatActivity[]] {
+  return useOrderedActivities();
+}
+
+function useActivitiesForDevelopment(): readonly [readonly WebChatActivity[]] {
+  const activitiesFromGraphState = useActivitiesForProduction();
 
   // Checks if store changed.
   const store = useStore();
   const prevStore = usePrevious(store);
 
-  // ASSERTION: Before we fully migrate to graph, make sure graph and Redux are the same.
-  if (process.env.NODE_ENV !== 'production') {
-    const [activitiesFromGraph] = activitiesFromGraphState;
+  const [activitiesFromGraph] = activitiesFromGraphState;
 
-    // Assert based on NODE_ENV.
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const activitiesFromRedux = useSelector(({ activities }) => activities);
+  const activitiesFromRedux = useSelector(({ activities }) => activities);
 
-    // If store changed, skip one assertion turn.
-    // This is because <GraphProvider> is using `useState()` for propagating changes.
-    // It is always one render behind if `store` changed.
-    if (prevStore === store) {
-      if (activitiesFromGraph.length !== activitiesFromRedux.length) {
+  // If store changed, skip one assertion turn.
+  // This is because <GraphProvider> is using `useState()` for propagating changes.
+  // It is always one render behind if `store` changed.
+  if (prevStore === store) {
+    if (activitiesFromGraph.length !== activitiesFromRedux.length) {
+      throw new Error(
+        `botframework-webchat-internal: Activities from graph and Redux are of different size (graph has ${activitiesFromGraph.length} activities, Redux has ${activitiesFromRedux.length} activities)`,
+        {
+          cause: {
+            activitiesFromGraph,
+            activitiesFromRedux
+          }
+        }
+      );
+    }
+
+    for (let index = 0; index < activitiesFromGraph.length; index++) {
+      if (!Object.is(activitiesFromGraph.at(index), activitiesFromRedux.at(index))) {
         throw new Error(
-          `botframework-webchat-internal: Activities from graph and Redux are of different size (graph has ${activitiesFromGraph.length} activities, Redux has ${activitiesFromRedux.length} activities)`,
+          `botframework-webchat-internal: Activities from graph and Redux are of different at index ${index}`,
           {
             cause: {
               activitiesFromGraph,
-              activitiesFromRedux
+              activitiesFromRedux,
+              index
             }
           }
         );
-      }
-
-      for (let index = 0; index < activitiesFromGraph.length; index++) {
-        if (!Object.is(activitiesFromGraph.at(index), activitiesFromRedux.at(index))) {
-          throw new Error(
-            `botframework-webchat-internal: Activities from graph and Redux are of different at index ${index}`,
-            {
-              cause: {
-                activitiesFromGraph,
-                activitiesFromRedux,
-                index
-              }
-            }
-          );
-        }
       }
     }
   }
 
   return activitiesFromGraphState;
 }
+
+const useActivities: () => readonly [readonly WebChatActivity[]] =
+  process.env.NODE_ENV === 'production' ? useActivitiesForProduction : useActivitiesForDevelopment;
+
+export default useActivities;
