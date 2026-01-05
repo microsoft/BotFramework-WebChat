ee5c621a5351d0a3d41318def73d4b1aacf2d555
Eugene <EOlonov@gmail.com>
2026-01-05 21:57:52 +0000

    Update related

--- packages/test/page-object/src/globals/pageElements/activityStatuses.js
+++ packages/test/page-object/src/globals/pageElements/activityStatuses.js
@@ -2,6 +2,6 @@ import activities from './activities';
 
 export default function getActivityStatuses() {
   return [].map.call(activities(), element =>
-    element.querySelector('.webchat__carousel-filmstrip__status, .stacked-layout__status')
+    element.querySelector('.carousel-filmstrip__status, .stacked-layout__status')
   );
 }
