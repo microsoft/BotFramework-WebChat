515d77029108ff352df95d3a6de600578528c5ea
William Wong <compulim@users.noreply.github.com>
2026-02-03 09:35:21 -0800

    Fix InferOutput

--- packages/core/src/utils/voiceActivity/isVoiceTranscriptActivity.ts
+++ packages/core/src/utils/voiceActivity/isVoiceTranscriptActivity.ts
@@ -18,6 +18,6 @@ const VoiceTranscriptActivitySchema = object({
 
 const isVoiceTranscriptActivity = (
   activity: WebChatActivity
-): activity is WebChatActivity & InferOutput<VoiceTranscriptActivitySchema> => safeParse(VoiceTranscriptActivitySchema, activity).success;
+): activity is WebChatActivity & InferOutput<typeof VoiceTranscriptActivitySchema> => safeParse(VoiceTranscriptActivitySchema, activity).success;
 
 export default isVoiceTranscriptActivity;
