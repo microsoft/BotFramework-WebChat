91c470ba762567bb59af32b11eb928bc354b2954
William Wong <compulim@users.noreply.github.com>
2026-02-14 09:58:48 +0000

    Add commented debugging code

--- __tests__/html2/modelContextProtocol/packages/app/src/createPolymiddleware.ts
+++ __tests__/html2/modelContextProtocol/packages/app/src/createPolymiddleware.ts
@@ -1,5 +1,5 @@
 import { activityComponent, createActivityPolymiddleware } from 'botframework-webchat/middleware';
-import { is } from 'valibot';
+import { is, safeParse } from 'valibot';
 import MCPAppActivity from './MCPAppActivity.js';
 import { MessageEntityForMCPAppSchema, type MessageEntityForMCPApp } from './types/MessageEntityForMCPApp.js';
 
@@ -13,6 +13,15 @@ export default function createPolymiddleware() {
       const messageEntity: MessageEntityForMCPApp | undefined = (
         entities as any[] | undefined
       )?.find<MessageEntityForMCPApp>(entity => is(MessageEntityForMCPAppSchema, entity));
+
+      // if (!messageEntity && entities) {
+      //   for (const entity of entities) {
+      //     // eslint-disable-next-line no-console
+      //     console.log(safeParse(MessageEntityForMCPAppSchema, entity));
+      //   }
+
+      //   return;
+      // }
 
       if (messageEntity?.additionalType?.includes('WebPage')) {
         return activityComponent(MCPAppActivity, { entity: messageEntity });
