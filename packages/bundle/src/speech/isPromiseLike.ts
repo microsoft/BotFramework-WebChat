13527da3354282b8451f01576b2a8daa02be8386
William Wong <compulim@users.noreply.github.com>
2026-03-05 11:00:54 +0000

    Use PromiseLike

--- packages/bundle/src/speech/isPromiseLike.ts
+++ packages/bundle/src/speech/isPromiseLike.ts
@@ -1,4 +1,4 @@
-export default function isPromiseLike<T>(promiseLike: unknown): promiseLike is Promise<T> {
+export default function isPromiseLike<T>(promiseLike: unknown): promiseLike is PromiseLike<T> {
   return (
     !!promiseLike && typeof promiseLike === 'object' && 'then' in promiseLike && typeof promiseLike.then === 'function'
   );
