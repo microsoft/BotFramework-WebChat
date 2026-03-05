118af145210709e5f387209c41d35033bd0cf508
Eugene <EOlonov@gmail.com>
2026-03-05 17:32:59 +0000

    fix: sliding dots have white background

--- packages/fluent-theme/src/components/assets/SlidingDots.tsx
+++ packages/fluent-theme/src/components/assets/SlidingDots.tsx
@@ -1,6 +1,5 @@
 import { hooks } from 'botframework-webchat';
-import React, { memo, useCallback, useEffect, useRef } from 'react';
-import { useRefFrom } from 'use-ref-from';
+import React, { memo } from 'react';
 
 import useAssetURL from './private/useAssetURL';
 
@@ -10,50 +9,12 @@ type SlidingDotsProps = Readonly<{ className: string }>;
 
 const SlidingDots = ({ className }: SlidingDotsProps) => {
   const [shouldReduceMotion] = useShouldReduceMotion();
-  const [url] = useAssetURL('sliding dots');
+  const [url] = useAssetURL(shouldReduceMotion ? 'sliding dots reduced-motion' : 'sliding dots');
   const localize = useLocalizer();
-  const objectElementRef = useRef<HTMLObjectElement>(null);
 
   const altText = localize('TYPING_INDICATOR_ALT');
-  const shouldReduceMotionRef = useRefFrom(shouldReduceMotion);
 
-  const pauseAnimations = useCallback(() => {
-    const contentDocument = objectElementRef.current?.contentDocument;
-    const svgElement = contentDocument?.documentElement;
-    const { SVGSVGElement } = contentDocument?.defaultView || {};
-
-    SVGSVGElement && svgElement instanceof SVGSVGElement && svgElement.pauseAnimations();
-  }, [objectElementRef]);
-
-  const unpauseAnimations = useCallback(() => {
-    const contentDocument = objectElementRef.current?.contentDocument;
-    const svgElement = contentDocument?.documentElement;
-    const { SVGSVGElement } = contentDocument?.defaultView || {};
-
-    SVGSVGElement && svgElement instanceof SVGSVGElement && svgElement.unpauseAnimations();
-  }, [objectElementRef]);
-
-  const pauseOrUnpauseAnimations = useCallback(
-    () => (shouldReduceMotionRef.current ? pauseAnimations() : unpauseAnimations()),
-    [pauseAnimations, shouldReduceMotionRef, unpauseAnimations]
-  );
-
-  useEffect(pauseOrUnpauseAnimations, [
-    pauseOrUnpauseAnimations,
-    // Call "pauseOrUnpauseAnimations()" when "shouldReduceMotion" change.
-    shouldReduceMotion
-  ]);
-
-  return (
-    <object
-      aria-label={altText}
-      className={className}
-      data={url.href}
-      onLoad={pauseOrUnpauseAnimations}
-      ref={objectElementRef}
-      type="image/svg+xml"
-    />
-  );
+  return <img aria-label={altText} className={className} src={url.href} />;
 };
 
 SlidingDots.displayName = 'SlidingDots';
