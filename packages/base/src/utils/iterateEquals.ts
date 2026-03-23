356004f480827da573ab4245bbcb229d3e0548f2
William Wong <compulim@users.noreply.github.com>
2026-03-23 07:51:10 +0000

    Fix ESLint

--- packages/base/src/utils/iterateEquals.ts
+++ packages/base/src/utils/iterateEquals.ts
@@ -1,10 +1,10 @@
+// eslint-disable-next-line no-magic-numbers
 const MAX_ITERATION = 1_000_000;
 
 export default function iterateEquals<T>(x: Iterable<T>, y: Iterable<T>): boolean {
   const xIterator = x[Symbol.iterator]();
   const yIterator = y[Symbol.iterator]();
 
-  // eslint-disable-next-line no-magic-numbers
   for (let count = 0; count < MAX_ITERATION; count++) {
     const resultX = xIterator.next();
     const resultY = yIterator.next();
