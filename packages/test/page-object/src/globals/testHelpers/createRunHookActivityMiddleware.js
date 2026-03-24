4add0902b5262ef2ffb0bdbe6217aa9363e5e06e
William Wong <compulim@users.noreply.github.com>
2026-03-24 02:21:52 +0000

    Fix window.React

--- packages/test/page-object/src/globals/testHelpers/createRunHookActivityMiddleware.js
+++ packages/test/page-object/src/globals/testHelpers/createRunHookActivityMiddleware.js
@@ -1,19 +1,17 @@
-const { createElement, memo, useRef } = React;
-
-const RunHook = memo(({ fn, resolve }) => {
-  const numCalledRef = useRef(0);
+const RunHook = ({ fn, resolve }) => {
+  const numCalledRef = window.React.useRef(0);
 
   resolve(fn(window.WebChat.hooks, numCalledRef.current++));
 
   return false;
-});
+};
 
 function createRunHookActivityMiddleware() {
   return () =>
     next =>
     ({ activity, ...others }) => {
       if (activity.type === 'event' && activity.name === '__RUN_HOOK') {
-        return () => !activity.ref.count++ && createElement(RunHook, activity.value);
+        return () => !activity.ref.count++ && window.React.createElement(RunHook, activity.value);
       }
 
       return next({ activity, ...others });
