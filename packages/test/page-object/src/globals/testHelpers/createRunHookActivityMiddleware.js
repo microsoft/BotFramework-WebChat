10b6ffc6e2f63ff7a2528ca7ad9029b75f8b7489
William Wong <compulim@users.noreply.github.com>
2026-03-24 01:56:12 +0000

    Fix runHook twice

--- packages/test/page-object/src/globals/testHelpers/createRunHookActivityMiddleware.js
+++ packages/test/page-object/src/globals/testHelpers/createRunHookActivityMiddleware.js
@@ -1,15 +1,19 @@
-const RunHook = ({ fn, resolve }) => {
-  resolve(fn(window.WebChat.hooks));
+const { createElement, memo, useRef } = React;
+
+const RunHook = memo(({ fn, resolve }) => {
+  const numCalledRef = useRef(0);
+
+  resolve(fn(window.WebChat.hooks, numCalledRef.current++));
 
   return false;
-};
+});
 
 function createRunHookActivityMiddleware() {
   return () =>
     next =>
     ({ activity, ...others }) => {
       if (activity.type === 'event' && activity.name === '__RUN_HOOK') {
-        return () => !activity.ref.count++ && window.React.createElement(RunHook, activity.value);
+        return () => !activity.ref.count++ && createElement(RunHook, activity.value);
       }
 
       return next({ activity, ...others });
