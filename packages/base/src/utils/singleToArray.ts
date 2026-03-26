192ff27b5da7bd5d94a89ba072bacb9e8617be9c
William Wong <compulim@users.noreply.github.com>
2026-03-26 06:42:46 +0000

    singleToArray return new frozen array

--- packages/base/src/utils/singleToArray.ts
+++ packages/base/src/utils/singleToArray.ts
@@ -1,11 +1,8 @@
 const EMPTY_ARRAY: readonly any[] = Object.freeze([]);
 
-export default function singleToArray(singleOrArray: undefined): readonly never[];
-export default function singleToArray<T>(singleOrArray: T | T[]): readonly T[];
-
 /** @deprecated Will be removed on or after 2028-03-16. */
-export default function singleToArray<T>(singleOrArray: T | T[]): readonly T[] {
+export default function singleToArray<T>(singleOrArray: T | readonly T[]): readonly T[] {
   return typeof singleOrArray === 'undefined'
-    ? EMPTY_ARRAY
+    ? (EMPTY_ARRAY as readonly T[])
     : Object.freeze(Array.isArray(singleOrArray) ? [...singleOrArray] : [singleOrArray]);
 }
