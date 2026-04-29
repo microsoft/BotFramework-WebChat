28fc5309ac549cb13edd758ec0ea93bb37c92d2b
William Wong <compulim@users.noreply.github.com>
2026-04-29 21:36:37 +0000

    Fix action require review check

--- packages/component/src/ActivityFeedback/private/isActionRequireReview.ts
+++ packages/component/src/ActivityFeedback/private/isActionRequireReview.ts
@@ -3,5 +3,5 @@ import { type OrgSchemaAction } from 'botframework-webchat-core';
 export default function isActionRequireReview(
   action: OrgSchemaAction | undefined
 ): action is OrgSchemaAction & { result: { '@type': 'UserReview' } } {
-  return action?.result?.['@type'] === 'UserReview';
+  return action?.result[0]?.['@type'] === 'UserReview';
 }
