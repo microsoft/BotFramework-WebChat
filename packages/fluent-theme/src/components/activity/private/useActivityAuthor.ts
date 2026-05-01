797198ad63ea854894837d4774f38a2f467f9301
William Wong <compulim@users.noreply.github.com>
2026-05-01 20:47:34 +0000

    Add comment

--- packages/fluent-theme/src/components/activity/private/useActivityAuthor.ts
+++ packages/fluent-theme/src/components/activity/private/useActivityAuthor.ts
@@ -3,6 +3,11 @@ import { orgSchemaPersonSchema, type OrgSchemaPerson } from 'botframework-webcha
 import { useMemo } from 'react';
 import { parse } from 'valibot';
 
+/**
+ * Returns the first `Message.author` if it is of type `Person` or `string`.
+ *
+ * String-based author will be convertewd to `Person` thing.
+ */
 export default function useActivityAuthor(activity?: WebChatActivity | undefined): OrgSchemaPerson | undefined {
   return useMemo(() => {
     const firstAuthor = getOrgSchemaMessage(activity?.entities || [])?.author[0];
