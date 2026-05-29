588a76e33618e0a1bb6aaba293a4eea2b6e3e836
William Wong <compulim@users.noreply.github.com>
2026-05-29 06:43:50 +0000

    Remove trailing </p>

--- packages/component-better-link/src/stripParagraphContainer.ts
+++ packages/component-better-link/src/stripParagraphContainer.ts
@@ -1,3 +1,3 @@
 export default function stripParagraphContainer(html: string): string {
-  return html.replace(/^<p\sxmlns="http:\/\/www.w3.org\/1999\/xhtml">/u, '').replace(/<\/p>$/u, '');
+  return html.trim().replace(/^<p\sxmlns="http:\/\/www.w3.org\/1999\/xhtml">/u, '').replace(/<\/p>$/u, '');
 }
