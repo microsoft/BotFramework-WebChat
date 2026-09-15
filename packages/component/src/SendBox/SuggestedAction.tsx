2d3f7863abb513e42eca37cbc7db540883c34146
William Wong <compulim@users.noreply.github.com>
2026-09-15 08:38:22 +0000

    Add test for suggested actions

--- packages/component/src/SendBox/SuggestedAction.tsx
+++ packages/component/src/SendBox/SuggestedAction.tsx
@@ -99,7 +99,8 @@ function SuggestedAction(props: SuggestedActionProps) {
         //       Instead, we should pass a "cardAction" props.
         performCardAction({ displayText, text, type, value } as DirectLineCardAction, { target });
 
-        // Since "openUrl" and "webchat:callURL" actions do not submit, the suggested action buttons do not hide after click.
+        // Since "openUrl" and "webchat:callURL" actions do not submit, the suggested action buttons do not automatically hide after click.
+        // We need to hide it manually.
         (type === 'openUrl' || type === 'webchat:callURL') && setSuggestedActions([]);
 
         scrollToEnd();
