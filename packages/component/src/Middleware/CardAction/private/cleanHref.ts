a1b4f40d631f800d3d3c473dca9e83951e299a51
William Wong <compulim@users.noreply.github.com>
2026-09-15 07:38:15 +0000

    Add license

--- packages/component/src/Middleware/CardAction/private/cleanHref.ts
+++ packages/component/src/Middleware/CardAction/private/cleanHref.ts
@@ -1,6 +1,18 @@
 // TODO: [P1] Should move to [launder](https://www.npmjs.com/package/launder) package.
 
-// This code is adopted from sanitize-html/naughtyScheme.
+/*!
+ * (launder@1.7.1 is MIT but does not have license file, we are using the license file from sanitize-html instead.)
+ *
+ * Copyright (c) 2013, 2014, 2015 P'unk Avenue LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+// This code is adopted from sanitize-html/launder.
 // sanitize-html is a dependency of Web Chat but the naughtScheme function is neither exposed nor reusable.
 // https://github.com/apostrophecms/sanitize-html/
 
